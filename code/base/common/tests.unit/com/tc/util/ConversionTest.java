ae7dde5df74aa62960293be9ad5163b6b6d92a89
Juris Galang <jgalang@terracottatech.com>
2006-11-28 20:45:51 +0000

    Scrubbed comments; Added copyright information.
    git-svn-id: https://svn.terracotta.org/repo/tc/trunk/community/open@28 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/common/tests.unit/com/tc/util/ConversionTest.java
+++ code/base/common/tests.unit/com/tc/util/ConversionTest.java
@@ -1,6 +1,5 @@
-/*
- * Created on Dec 20, 2003 To change the template for this generated file go to Window&gt;Preferences&gt;Java&gt;Code
- * Generation&gt;Code and Comments
+/**
+ * Copyright (c) 2003-2006 Terracotta, Inc. All rights reserved.
  */
 package com.tc.util;
 
