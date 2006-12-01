3c4dd57b8d6b295a487ec153c9e5d40e0c8065a0
Jason Voegele <jason@jvoegele.com>
2006-12-01 18:19:28 +0000

    Merged r167 from lawton into trunk.
    	------------------------------------------------------------------------
    	r167 | jvoegele | 2006-12-01 12:57:16 -0500 (Fri, 01 Dec 2006) | 2 lines
    
    	SHA-303: Use TC_CONTAINER_HOME to specify which container to use for container tests.
    
    	------------------------------------------------------------------------
    
    
    git-svn-id: https://svn.terracotta.org/repo/tc/trunk/community/open@170 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/common/tests.base/com/tc/test/TestConfigObject.java
+++ code/base/common/tests.base/com/tc/test/TestConfigObject.java
@@ -74,7 +74,7 @@ public class TestConfigObject {
 
   public static final String      APP_SERVER_REPOSITORY_URL_BASE   = STATIC_PROPERTIES_PREFIX + "appserver.repository";
 
-  public static final String      APP_SERVER_HOME                  = STATIC_PROPERTIES_PREFIX + "appserver.home";
+  public static final String      APP_SERVER_HOME                  = DYNAMIC_PROPERTIES_PREFIX + "appserver.home";
 
   private static final String     APP_SERVER_FACTORY_NAME          = STATIC_PROPERTIES_PREFIX
                                                                      + "appserver.factory.name";
