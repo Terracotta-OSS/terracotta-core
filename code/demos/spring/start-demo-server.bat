5a4e2f4c9fa251631ad836ae6bed7fd6d767c5b8
Juris Galang <jgalang@terracottatech.com>
2006-12-07 19:08:06 +0000

    Merged rev 276 from Lawton
    git-svn-id: https://svn.terracotta.org/repo/tc/trunk/community/open@277 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/demos/spring/start-demo-server.bat
+++ code/demos/spring/start-demo-server.bat
@@ -1,12 +1,16 @@
 @echo off
 rem @COPYRIGHT@
 
-IF NOT "x%1"=="x" GOTO invalid_args
+if not "x%1"=="x" goto show_usage
 
 cd "%~d0%~p0"
-start "Terracotta for Spring Sample Server" "..\bin\start-tc-server.bat"
+start "Terracotta Demo Server" "..\bin\start-tc-server.bat"
+goto :done
 
-:invalid_args
-	SET tc_tmp=%~p0start-demo-server.bat
-	echo Usage:
-	echo %tc_tmp%
+:show_usage
+  set tc_tmp=%~p0start-demo-server.bat
+  echo Usage:
+  echo %tc_tmp%
+
+:done
+
