13731dd63458fdcd5f67efbe8ad9f843e6593e16
Juris Galang <jgalang@terracottatech.com>
2006-12-15 22:30:52 +0000

    Merged rev 460 from 2.2.0
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@461 7fc7bbf3-cf45-46d4-be06-341739edd864

--- kits/skeletons/shared-base/unix/bin/dso-env.sh
+++ kits/skeletons/shared-base/unix/bin/dso-env.sh
@@ -7,7 +7,7 @@
 #
 
 if test "$1" = "-q"; then
-  if test -z ${TOPDIR}; then
+  if test -z "${TOPDIR}"; then
     echo "Error: When the -q option is specified, I expect that"
     echo "the environment variable TOPDIR is set so that I"
     echo "can locate the libexec directory of your Terracotta"
@@ -26,8 +26,8 @@ __DSO_ENV_CONFIG=$1
 tc_install_dir "${TOPDIR}"/.. true
 tc_config "${__DSO_ENV_CONFIG}"
 tc_set_dso_boot_jar
- 
+
 test -z "${__DSO_ENV_CONFIG}" && unset D_TC_CONFIG
 TC_JAVA_OPTS="-Xbootclasspath/p:${DSO_BOOT_JAR} -Dtc.install-root=${TC_INSTALL_DIR} ${D_TC_CONFIG}"
 
-test -z ${__DSO_ENV_QUIET} && echo ${TC_JAVA_OPTS}
+test -z "${__DSO_ENV_QUIET}" && echo "${TC_JAVA_OPTS}"
