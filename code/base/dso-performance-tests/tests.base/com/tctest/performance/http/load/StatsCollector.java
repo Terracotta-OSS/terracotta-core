78f0a157f9ee778594910bea1001d1759a34f302
Tim Eck <timeck@gmail.com>
2007-01-12 23:59:25 +0000

    merge r706 from 2.2.1 (overflow problem in load test program)
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@707 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-performance-tests/tests.base/com/tctest/performance/http/load/StatsCollector.java
+++ code/base/dso-performance-tests/tests.base/com/tctest/performance/http/load/StatsCollector.java
@@ -25,19 +25,21 @@ final class StatsCollector implements Serializable {
   private final LinkedQueue      stats         = new LinkedQueue();
   private final Object           END           = new Object();
   private long                   lastStartTime = -1;
+  private long                   firstStat     = -1;
   private final NumberFormat     format        = NumberFormat.getInstance();
   private final SimpleDateFormat dateFormat    = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss,SSS");
-
-  // 2007-01-12 13:26:47,287
 
   public StatsCollector() {
     format.setMaximumFractionDigits(2);
   }
 
   void addStat(ResponseStatistic stat) {
-    final long first = FirstStat.startTime;
 
     synchronized (this) {
+      if (firstStat == -1) {
+        firstStat = System.currentTimeMillis();
+      }
+
       count++;
 
       boolean isSuccess = stat.statusCode() == HttpStatus.SC_OK;
@@ -48,11 +50,16 @@ final class StatsCollector implements Serializable {
       }
 
       if ((count % REPORT) == 0) {
-        final long start = lastStartTime == -1 ? first : lastStartTime;
+        final long start = lastStartTime == -1 ? firstStat : lastStartTime;
         final long end = System.currentTimeMillis();
 
-        final double rate = ((double) (REPORT * 1000)) / ((double) (end - start));
-        final double overallRate = ((double) (count * 1000)) / ((double) (end - first));
+        if (end < start) {
+          System.out.println("\n**********************\nWARN: backward clock movement detected (" + end + " < " + start
+                             + ")\n**********************\n");
+        }
+
+        final double rate = ((double) (REPORT * 1000L)) / ((double) (end - start));
+        final double overallRate = ((double) (count * 1000L)) / ((double) (end - firstStat));
 
         System.out.println(dateFormat.format(new Date()) + ": Completed " + count + " requests (" + success + " OK, "
                            + errors + " errors), " + format.format(rate) + " tps, overall "
@@ -92,10 +99,6 @@ final class StatsCollector implements Serializable {
       e.printStackTrace();
       throw new RuntimeException(e);
     }
-  }
-
-  private static class FirstStat {
-    static final long startTime = System.currentTimeMillis();
   }
 
 }
