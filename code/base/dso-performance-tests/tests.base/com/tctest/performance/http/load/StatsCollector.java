7a85816c0f66e53690535d55084a3cf6295c42f1
Tim Eck <timeck@gmail.com>
2007-01-12 21:40:32 +0000

    merge r701 from 2.2.1 (more output on load test client)
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@702 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-performance-tests/tests.base/com/tctest/performance/http/load/StatsCollector.java
+++ code/base/dso-performance-tests/tests.base/com/tctest/performance/http/load/StatsCollector.java
@@ -1,5 +1,6 @@
 /*
- * All content copyright (c) 2003-2006 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ * All content copyright (c) 2003-2006 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
  */
 package com.tctest.performance.http.load;
 
@@ -10,20 +11,32 @@ import EDU.oswego.cs.dl.util.concurrent.LinkedQueue;
 
 import java.io.InputStream;
 import java.io.Serializable;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 final class StatsCollector implements Serializable {
 
-  private int               count;
-  private int               success = 0;
-  private int               errors  = 0;
-  private final LinkedQueue stats   = new LinkedQueue();
-  private final Object      END     = new Object();
+  private static final long      REPORT        = 500;
+
+  private int                    count;
+  private int                    success       = 0;
+  private int                    errors        = 0;
+  private final LinkedQueue      stats         = new LinkedQueue();
+  private final Object           END           = new Object();
+  private long                   lastStartTime = -1;
+  private final NumberFormat     format        = NumberFormat.getInstance();
+  private final SimpleDateFormat dateFormat    = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss,SSS");
+
+  // 2007-01-12 13:26:47,287
 
   public StatsCollector() {
-    // ;
+    format.setMaximumFractionDigits(2);
   }
 
   void addStat(ResponseStatistic stat) {
+    final long first = FirstStat.startTime;
+
     synchronized (this) {
       count++;
 
@@ -34,8 +47,18 @@ final class StatsCollector implements Serializable {
         errors++;
       }
 
-      if ((count % 500) == 0) {
-        System.out.println("Completed " + count + " requests (" + success + " OK, " + errors + " errors)");
+      if ((count % REPORT) == 0) {
+        final long start = lastStartTime == -1 ? first : lastStartTime;
+        final long end = System.currentTimeMillis();
+
+        final double rate = ((double) (REPORT * 1000)) / ((double) (end - start));
+        final double overallRate = ((double) (count * 1000)) / ((double) (end - first));
+
+        System.out.println(dateFormat.format(new Date()) + ": Completed " + count + " requests (" + success + " OK, "
+                           + errors + " errors), " + format.format(rate) + " tps, overall "
+                           + format.format(overallRate) + " tps");
+
+        lastStartTime = end;
       }
     }
 
@@ -70,4 +93,9 @@ final class StatsCollector implements Serializable {
       throw new RuntimeException(e);
     }
   }
+
+  private static class FirstStat {
+    static final long startTime = System.currentTimeMillis();
+  }
+
 }
