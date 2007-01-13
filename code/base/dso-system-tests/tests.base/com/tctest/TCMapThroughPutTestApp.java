b6f6dfd9e6beed0b00de0df2069a29adb61e52bc
Antonio Si <asi@terracottatech.com>
2007-01-13 00:26:33 +0000

    Merge from revision 704 - rewrite the test case.
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@713 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-system-tests/tests.base/com/tctest/TCMapThroughPutTestApp.java
+++ code/base/dso-system-tests/tests.base/com/tctest/TCMapThroughPutTestApp.java
@@ -6,6 +6,7 @@ package com.tctest;
 
 import EDU.oswego.cs.dl.util.concurrent.CyclicBarrier;
 
+import com.tc.exception.TCRuntimeException;
 import com.tc.object.config.ConfigVisitor;
 import com.tc.object.config.DSOClientConfigHelper;
 import com.tc.object.config.TransparencyClassSpec;
@@ -13,13 +14,20 @@ import com.tc.simulator.app.ApplicationConfig;
 import com.tc.simulator.listener.ListenerProvider;
 import com.tctest.runner.AbstractTransparentApp;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Hashtable;
+import java.util.List;
 import java.util.Map;
 
 public class TCMapThroughPutTestApp extends AbstractTransparentApp {
-  private int                 NUM_OF_ITERATIONS = 100000;
-  private final static int    NUM_OF_LEVELS     = 20;
+  private int                 NUM_OF_ITERATIONS = 5000;
+  private final static int    NUM_OF_KEYS       = 20;
+  private final static int    NUM_OF_LEVELS     = 10;
+  private final static int    NUM_OF_THREADS    = 1;
+
+  private final List          hashMapList       = new ArrayList();
+  private final List          hashtableList     = new ArrayList();
 
   private final HashMap       tcHashMap         = new HashMap();
   private final Hashtable     tcHashtable       = new Hashtable();
@@ -34,71 +42,41 @@ public class TCMapThroughPutTestApp extends AbstractTransparentApp {
   public void run() {
     try {
       int nodeId = barrier.barrier();
-      if (nodeId == 0) {
+      /*if (nodeId == 0) {
         long freeMemory = Runtime.getRuntime().freeMemory();
         System.err.println("******free mem: " + freeMemory);
         long requireMemory = 1024000000;
-        NUM_OF_ITERATIONS = (int)(NUM_OF_ITERATIONS * (freeMemory*1.0 / requireMemory));
+        NUM_OF_ITERATIONS = (int) (NUM_OF_ITERATIONS * (freeMemory * 1.0 / requireMemory));
         System.err.println("***********NUM_OF_ITERATIONS: " + NUM_OF_ITERATIONS);
       }
-      
+
+      barrier.barrier();*/
+
+      synchronized (hashMapList) {
+        hashMapList.add(tcHashMap);
+      }
+      synchronized (hashtableList) {
+        hashtableList.add(tcHashtable);
+      }
+
       barrier.barrier();
 
-      putAndGetTest(nodeId);
-
       putTest(nodeId);
-
-      removeTest(nodeId);
 
     } catch (Throwable t) {
       notifyError(t);
     }
   }
-  
-  private void clear() throws Exception {
-    synchronized(tcHashMap) {
-      tcHashMap.clear();
-    }
-    synchronized(tcHashtable) {
-      tcHashtable.clear();
-    }
-  }
-
-  private void putAndGetTest(int nodeId) throws Exception {
-    if (nodeId == 0) {
-      System.err.println("Put and Get Test start ...");
-      clear();
-    }
-    barrier.barrier();
-
-    runPutAndGetTest(nodeId, tcHashMap);
-
-    barrier.barrier();
-
-    runPutAndGetTest(nodeId, tcHashtable);
-
-    barrier.barrier();
-
-    if (nodeId == 0) {
-      System.err.println("Put and Get Test end ...");
-    }
-    barrier.barrier();
-  }
 
   private void putTest(int nodeId) throws Exception {
     if (nodeId == 0) {
       System.err.println("Put Test start ...");
-      clear();
     }
     barrier.barrier();
 
     runPutTest(nodeId, tcHashMap);
 
-    barrier.barrier();
-
     runPutTest(nodeId, tcHashtable);
-
-    barrier.barrier();
 
     if (nodeId == 0) {
       System.err.println("Put Test end ...");
@@ -106,61 +84,18 @@ public class TCMapThroughPutTestApp extends AbstractTransparentApp {
     barrier.barrier();
   }
 
-  private void removeTest(int nodeId) throws Exception {
-    if (nodeId == 0) {
-      System.err.println("Remove Test start ...");
-      clear();
-    }
-    barrier.barrier();
-
-    runRemoveTest(nodeId, tcHashMap);
-
-    barrier.barrier();
-
-    runRemoveTest(nodeId, tcHashtable);
-
-    barrier.barrier();
-
-    if (nodeId == 0) {
-      System.err.println("Remove Test end ...");
-    }
-    barrier.barrier();
-  }
-
-  private void runPutAndGetTest(int nodeId, Map sharedMap) throws Exception {
-    if (nodeId == 0) {
-      new TCMapPutter(NUM_OF_ITERATIONS, NUM_OF_LEVELS, sharedMap);
-    }
-
-    barrier.barrier();
-
-    if (nodeId != 0) {
-      new TCMapReader(NUM_OF_ITERATIONS, sharedMap);
-    }
-  }
-
   private void runPutTest(int nodeId, Map sharedMap) throws Exception {
-    if (nodeId == 0) {
-      new TCMapPutter(NUM_OF_ITERATIONS, NUM_OF_LEVELS, sharedMap);
+    Thread[] numOfPutter = new Thread[NUM_OF_THREADS];
+    CyclicBarrier localBarrier = new CyclicBarrier(NUM_OF_THREADS + 1);
+    for (int i = 0; i < NUM_OF_THREADS; i++) {
+      numOfPutter[i] = new Thread(new TCMapPutter(NUM_OF_ITERATIONS, NUM_OF_LEVELS, localBarrier, sharedMap));
     }
+    for (int i = 0; i < NUM_OF_THREADS; i++) {
+      numOfPutter[i].start();
+    }
+    localBarrier.barrier();
 
     barrier.barrier();
-
-    if (nodeId != 0) {
-      new TCMapPutter(NUM_OF_ITERATIONS, NUM_OF_LEVELS, sharedMap);
-    }
-  }
-
-  private void runRemoveTest(int nodeId, Map sharedMap) throws Exception {
-    if (nodeId == 0) {
-      new TCMapPutter(NUM_OF_ITERATIONS, NUM_OF_LEVELS, sharedMap);
-    }
-
-    barrier.barrier();
-
-    if (nodeId == 1) {
-      new TCMapRemover(NUM_OF_ITERATIONS, sharedMap);
-    }
   }
 
   public static void visitL1DSOConfig(ConfigVisitor visitor, DSOClientConfigHelper config) {
@@ -171,8 +106,6 @@ public class TCMapThroughPutTestApp extends AbstractTransparentApp {
     spec = config.getOrCreateSpec(testClass);
 
     config.addIncludePattern(TCMapPutter.class.getName());
-    config.addIncludePattern(TCMapReader.class.getName());
-    config.addIncludePattern(TCMapRemover.class.getName());
     config.addIncludePattern(DeepLargeObject.class.getName());
 
     String writeAllowdMethodExpression = "* " + testClass + "*.*(..)";
@@ -180,83 +113,48 @@ public class TCMapThroughPutTestApp extends AbstractTransparentApp {
 
     writeAllowdMethodExpression = "* " + TCMapPutter.class.getName() + "*.*(..)";
     config.addWriteAutolock(writeAllowdMethodExpression);
-    writeAllowdMethodExpression = "* " + TCMapReader.class.getName() + "*.*(..)";
-    config.addWriteAutolock(writeAllowdMethodExpression);
-    writeAllowdMethodExpression = "* " + TCMapRemover.class.getName() + "*.*(..)";
-    config.addWriteAutolock(writeAllowdMethodExpression);
     writeAllowdMethodExpression = "* " + DeepLargeObject.class.getName() + "*.*(..)";
     config.addWriteAutolock(writeAllowdMethodExpression);
 
-    spec.addRoot("tcHashMap", "tcHashMap");
-    spec.addRoot("tcHashtable", "tcHashtable");
+    spec.addRoot("hashMapList", "hashMapList");
+    spec.addRoot("hashtableList", "hashtableList");
     spec.addRoot("barrier", "barrier");
     spec.addRoot("NUM_OF_ITERATIONS", "NUM_OF_ITERATIONS");
   }
 
-  public static class TCMapPutter {
-    public TCMapPutter(int numOfObjects, int numOfLevels, Map sharedMap) {
+  public static class TCMapPutter implements Runnable {
+    private final int           numOfObjects;
+    private final int           numOfLevels;
+    private final Map           sharedMap;
+    private final CyclicBarrier barrier;
+
+    public TCMapPutter(int numOfObjects, int numOfLevels, CyclicBarrier barrier, Map sharedMap) {
+      this.numOfObjects = numOfObjects;
+      this.numOfLevels = numOfLevels;
+      this.barrier = barrier;
+      this.sharedMap = sharedMap;
+    }
+
+    public void run() {
       long startTime = System.currentTimeMillis();
 
       for (int i = 0; i < numOfObjects; i++) {
         synchronized (sharedMap) {
-          sharedMap.put("key" + i, new DeepLargeObject(numOfLevels));
+          int key = i % NUM_OF_KEYS;
+          sharedMap.put("key" + key, new DeepLargeObject(numOfLevels));
         }
       }
 
       long endTime = System.currentTimeMillis();
+      System.err.println(endTime-startTime);
       System.err.println("TPS for type " + sharedMap.getClass().getName() + ": " + (numOfObjects * 1000.0)
                          / (endTime - startTime));
-    }
 
-    public TCMapPutter(int numOfObjects, int numOfLevels, int nodeId, Map sharedMap) {
-      long startTime = System.currentTimeMillis();
-
-      for (int i = 0; i < numOfObjects; i++) {
-        synchronized (sharedMap) {
-          sharedMap.put("key" + i, new DeepLargeObject(numOfLevels));
-          System.err.println("Node: " + nodeId + " putting key " + i);
-        }
+      try {
+        barrier.barrier();
+      } catch (Exception e) {
+        throw new TCRuntimeException(e);
       }
-
-      long endTime = System.currentTimeMillis();
-      System.err.println("TPS for type " + sharedMap.getClass().getName() + ": " + (numOfObjects * 1000.0)
-                         / (endTime - startTime));
-    }
-  }
-
-  public static class TCMapReader {
-    public TCMapReader(int numOfObjects, Map sharedMap) {
-      long startTime = System.currentTimeMillis();
-
-      for (int i = 0; i < numOfObjects; i++) {
-        Object o = null;
-        synchronized (sharedMap) {
-          o = sharedMap.get("key" + i);
-        }
-      }
-
-      long endTime = System.currentTimeMillis();
-      System.err.println("TPS for type " + sharedMap.getClass().getName() + ": " + (numOfObjects * 1000.0)
-                         / (endTime - startTime));
-    }
-  }
-
-  public static class TCMapRemover {
-    public TCMapRemover(int numOfObjects, Map sharedMap) {
-      long startTime = System.currentTimeMillis();
-
-      for (int i = 0; i < numOfObjects; i++) {
-        Object o = null;
-        while (o == null) {
-          synchronized (sharedMap) {
-            o = sharedMap.remove("key" + i);
-          }
-        }
-      }
-
-      long endTime = System.currentTimeMillis();
-      System.err.println("TPS for type " + sharedMap.getClass().getName() + ": " + (numOfObjects * 1000.0)
-                         / (endTime - startTime));
     }
   }
 
--- code/base/dso-system-tests/tests.system/com/tctest/TCMapThroughPutTest.java
+++ code/base/dso-system-tests/tests.system/com/tctest/TCMapThroughPutTest.java
@@ -5,7 +5,7 @@ package com.tctest;
 
 public class TCMapThroughPutTest extends TransparentTestBase {
 
-  private static final int NODE_COUNT = 4;
+  private static final int NODE_COUNT = 2;
 
   public void doSetUp(TransparentTestIface t) throws Exception {
     t.getTransparentAppConfig().setClientCount(NODE_COUNT).setIntensity(1);
