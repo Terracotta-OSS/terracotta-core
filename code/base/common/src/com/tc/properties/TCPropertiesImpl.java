0c7f6bf832edf5c56e0693241bd102d54fbdeca8
Saravanan Subbiah <ssubbiah@terracottatech.com>
2007-01-17 05:52:59 +0000

    Added a way to override properties thru a new system propertu -Dcom.tc.properties this is to be used in tests.
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@748 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/common/src/com/tc/properties/TCPropertiesImpl.java
+++ code/base/common/src/com/tc/properties/TCPropertiesImpl.java
@@ -37,6 +37,9 @@ public class TCPropertiesImpl implements TCProperties {
 
   // This file,if present, overrides the default properties and resides in the same directory as tc.jar
   private static final String           TC_PROPERTIES_FILE         = "tc.properties";
+  
+  // This is the system property that can be set to point to a tc.properties file
+  private static final String           TC_PROPERTIES_SYSTEM_PROP  = "com.tc.properties";
 
   private static final TCPropertiesImpl INSTANCE;
 
@@ -53,6 +56,10 @@ public class TCPropertiesImpl implements TCProperties {
     String tcJarDir = getTCJarRootDirectory();
     if (tcJarDir != null) {
       loadOverrides(tcJarDir, TC_PROPERTIES_FILE);
+    }
+    String tcPropFile = System.getProperty(TC_PROPERTIES_SYSTEM_PROP);
+    if(tcPropFile != null) {
+      loadOverrides(tcPropFile);
     }
 
     applySystemPropertyOverrides();
@@ -90,15 +97,24 @@ public class TCPropertiesImpl implements TCProperties {
 
   private void loadOverrides(String propDir, String propFile) {
     File file = new File(propDir, propFile);
+    loadOverrides(file);
+  }
+  
+  private void loadOverrides(String propFile) {
+    File file = new File(propFile);
+    loadOverrides(file);
+  }
+  
+  private void loadOverrides(File file) {
     if (file.canRead()) {
       try {
         FileInputStream fin = new FileInputStream(file);
-        LOG_BUFFER.addLog("Loading override properties from : " + propDir + File.separator + propFile);
+        LOG_BUFFER.addLog("Loading override properties from : " + file);
         props.load(fin);
       } catch (FileNotFoundException e) {
-        LOG_BUFFER.addLog("Couldnt find " + propFile + ". Ignoring it", e);
+        LOG_BUFFER.addLog("Couldnt find " + file + ". Ignoring it", e);
       } catch (IOException e) {
-        LOG_BUFFER.addLog("Couldnt read " + propFile + ". Ignoring it", e);
+        LOG_BUFFER.addLog("Couldnt read " + file + ". Ignoring it", e);
       }
     }
   }
