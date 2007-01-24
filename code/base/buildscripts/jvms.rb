566b4c1e50ffde84bc3402df2f68163440af6369
Jason Voegele <jason@jvoegele.com>
2007-01-24 19:45:31 +0000

    Some minor refactorings to remove duplication.
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@848 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/buildscripts/jvms.rb
+++ code/base/buildscripts/jvms.rb
@@ -12,7 +12,7 @@
 # some JVMs (like IBM's) can be just '1.4.2', too.
 class JavaVersion
     attr_reader :major, :minor, :patch, :release
-    
+
     # Creates a new representation of a version, as specified by a string. This can
     # currently be either something like '1.4.2' or something like '1.4.2_07'.
     def initialize(version_string)
@@ -20,41 +20,41 @@ class JavaVersion
             @major, @minor, @patch = $1.to_i, $2.to_i, $3.to_i
             @release = $4.blank? ? 0 : $4.to_i
         else
-            raise RuntimeError, "Version '#{version_string}' isn't a valid Java version"
+            raise "Version '#{version_string}' isn't a valid Java version"
         end
     end
-    
+
     # Is this version less than another version?
     def <(other)
         [ :major, :minor, :patch, :release ].each do |part|
             this_part = method(part).call
             that_part = other.method(part).call
-            
+
             return true if this_part < that_part
             return false if this_part > that_part
         end
-        
+
         return false
     end
-    
+
     # Is this version greater than another version?
     def >(other)
         [ :major, :minor, :patch, :release ].each do |part|
             this_part = method(part).call
             that_part = other.method(part).call
-            
+
             return true if this_part > that_part
             return false if this_part < that_part
         end
-        
+
         return false
     end
-    
+
     # Is this version exactly equal to another version?
     def ==(other)
         major == other.major && minor == other.minor && patch == other.patch && release == other.release
     end
-    
+
     # Returns a number less than zero, zero, or a number greater than zero as this
     # version is less than, exactly equal to, or greater than the other supplied version.
     def <=>(other)
@@ -64,7 +64,7 @@ class JavaVersion
         out = release <=> other.release if out == 0
         out
     end
-    
+
     # Returns a string representation of this version.
     def to_s
         "%d.%d.%d_%02d" % [ @major, @minor, @patch, @release ]
@@ -78,34 +78,34 @@ class JVMSet
     def initialize
         @jvms = { }
     end
-    
+
     # Returns the JVM that's bound to the given key.
     def [](key)
-        raise RuntimeError, "No JVM for '%s'." % key unless @jvms.has_key?(key)
+        raise "No JVM for '%s'." % key unless @jvms.has_key?(key)
         @jvms[key]
     end
-    
+
     # Sets a JVM for a the given key.
     def set(key, jvm)
         @jvms[key] = jvm
     end
-    
+
     # Aliases JVMs; looking up new_key will return the same JVM as old_key after this
     # method returns.
     def alias(new_key, old_key)
         @jvms[new_key] = @jvms[old_key]
     end
-    
+
     # Is there a JVM bound to the given key?
     def has?(key)
         @jvms.has_key?(key)
     end
-    
+
     # What are all the available keys?
     def keys
         @jvms.keys
     end
-    
+
     # A human-readable string representation of this JVMSet.
     def to_s
         sorted_keys = @jvms.keys.sort
@@ -143,20 +143,20 @@ class JVM
         @max_version = data[:maximum_version]
 
         @actual_version = nil
-        
+
         @platform = platform
     end
-    
+
     # What's the path to the 'java' executable in this JVM? Returns a string.
     def java
         @java_path.to_s
     end
-    
+
     # What's the path to the 'javac' executable in this JVM? Returns a string.
     def javac
         @javac_path.to_s
     end
-    
+
     # What's the Java home for this JVM? Returns a FilePath object.
     def home
         @java_home
@@ -174,24 +174,27 @@ class JVM
                 break
             end
         end
-        
+
         out = nil
         unless root.nil?
             out = JVM.new(platform, root, { :minimum_version => min_version, :maximum_version => max_version })
             out.validate(descrip)
             out.check_version(descrip)
         end
-        
+
         out
     end
 
     # Checks that this JVM is a valid JVM -- that is, has valid 'java' and 'javac' executables.
     def validate(descrip)
+        message = "The Java home for #{descrip}, at '#{@java_home}', has no " +
+                  "'%s' executable at '#{@java_path}'. Please correct this " +
+                  "problem and try again."
         unless FileTest.file?(java)
-            raise RuntimeError, "The Java home for #{descrip}, at '#{@java_home}', has no 'java' executable at '#{@java_path}'. Please correct this problem and try again."
+            raise message % 'java'
         end
         unless FileTest.file?(javac)
-            raise RuntimeError, "The Java home for #{descrip}, at '#{@java_home}', has no 'javac' executable at '#{@javac_path}'. Please correct this problem and try again."
+            raise message % 'javac'
         end
     end
 
@@ -201,47 +204,50 @@ class JVM
         unless @min_version.nil? && @max_version.nil?
             this_version = JavaVersion.new(actual_version)
 
+            # Template for error messages if the JVM does not meet the version requirements
+            message = "The version of #{descrip}, '#{this_version}', is %s than the %s required of '%s'."
+
             unless @min_version.nil?
                 required_version = JavaVersion.new(@min_version)
 
                 if this_version < required_version
-                    raise RuntimeError, "The version of #{descrip}, '#{this_version}', is less than the minimum required of '#{required_version}'."
+                    raise message % ['less', 'minimum', required_version]
                 end
             end
-        
+
             unless @max_version.nil?
                 required_version = JavaVersion.new(@max_version)
 
                 if this_version > required_version
-                    raise RuntimeError, "The version of #{descrip}, '#{this_version}', is greater than the minimum required of '#{required_version}'."
+                    raise message % ['greater', 'maximum', required_version]
                 end
             end
         end
     end
-    
+
     # A short description of this JVM -- the type followed by the version. Therefore, something
     # like 'hotspot-1.4.2_07'.
     def short_description
         "%s-%s" % [ actual_type, actual_version ]
     end
-    
+
     # The type of this JVM -- something like 'hotspot' or 'jrockit'.
     def actual_type
         ensure_have_version
         @actual_type
     end
-    
+
     # The actual version of this JVM, as a string. something like '1.4.2_07'.
     def actual_version
         ensure_have_version
         @actual_version
     end
-    
+
     # A human-readable string representation of this JVM.
     def to_s
         "<JVM at '%s', version '%s'>" % [ @java_home, @actual_version ]
     end
-    
+
     private
     # Makes sure the @actual_version and @actual_type member variables are set.
     def ensure_have_version
