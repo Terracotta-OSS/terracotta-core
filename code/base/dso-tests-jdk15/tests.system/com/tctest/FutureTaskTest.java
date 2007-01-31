aa336700873f74a727e21ba92a37bdf0c1f9e404
Antonio Si <asi@terracottatech.com>
2007-01-31 18:08:47 +0000

    Disable the test for 2 weeks until the DistributedMethodCall is fixed.
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@966 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-tests-jdk15/tests.system/com/tctest/FutureTaskTest.java
+++ code/base/dso-tests-jdk15/tests.system/com/tctest/FutureTaskTest.java
@@ -6,6 +6,10 @@ package com.tctest;
 public class FutureTaskTest extends TransparentTestBase {
 
   private static final int NODE_COUNT = 3;
+  
+  public FutureTaskTest() {
+    disableAllUntil("2007-02-18");
+  }
 
   public void doSetUp(TransparentTestIface t) throws Exception {
     t.getTransparentAppConfig().setClientCount(NODE_COUNT);
