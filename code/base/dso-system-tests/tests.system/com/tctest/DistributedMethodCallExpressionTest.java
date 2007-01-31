846b07f856eaad87165ad7372ed7bb64cf78c685
Cindy Fisher <cfisher@terracottatech.com>
2007-01-31 02:44:34 +0000

    disabling DistributedMethodCallExpressionTest until Feb 9, 2007
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@962 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-system-tests/tests.system/com/tctest/DistributedMethodCallExpressionTest.java
+++ code/base/dso-system-tests/tests.system/com/tctest/DistributedMethodCallExpressionTest.java
@@ -3,9 +3,14 @@
  */
 package com.tctest;
 
+
 public class DistributedMethodCallExpressionTest extends TransparentTestBase implements TestConfigurator {
   private static final int NODE_COUNT           = 3;
   private static final int LOOP_ITERATION_COUNT = 1;
+  
+  public DistributedMethodCallExpressionTest() {
+    disableAllUntil("2007-02-09");
+  }
 
   public void doSetUp(TransparentTestIface t) throws Exception {
     t.getTransparentAppConfig().setClientCount(NODE_COUNT).setIntensity(LOOP_ITERATION_COUNT);
