d69320b48fea175af258c4f5cdaf56bf2ed35a0e
Eric Ellis <eellis@terracottatech.com>
2007-02-10 02:07:50 +0000

    scripts to run the new archive tool
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@1128 7fc7bbf3-cf45-46d4-be06-341739edd864

---
+++ kits/skeletons/shared-base/common/bin/archive-tool.bat
@@ -0,0 +1,16 @@
+@echo off
+
+rem
+rem  All content copyright (c) 2003-2006 Terracotta, Inc.,
+rem  except as may otherwise be noted in a separate copyright notice.
+rem  All rights reserved.
+rem
+
+SETLOCAL
+SET TOPDIR=%~d0%~p0..
+IF "x%TC_INSTALL_DIR%"=="x" SET TC_INSTALL_DIR=%TOPDIR%
+CALL "%TOPDIR%\bin\tc-functions.bat" tc_install_dir "%TC_INSTALL_DIR%" FALSE
+CALL "%TOPDIR%\bin\tc-functions.bat" tc_classpath "" TRUE
+CALL "%TOPDIR%\bin\tc-functions.bat" tc_java_opts "-Dtc.install-root=%TC_INSTALL_DIR%"
+CALL "%TOPDIR%\bin\tc-functions.bat" tc_java -classpath "%TC_CLASSPATH%" %TC_ALL_JAVA_OPTS% com.tc.reporter.ArchiveUtil %*
+ENDLOCAL
