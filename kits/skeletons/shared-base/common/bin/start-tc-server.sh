a1f3ad9e82eec26c1e3f0c4ec5503a3281b398d8
Eric Ellis <eellis@terracottatech.com>
2007-02-13 19:02:13 +0000

    ssl=false for L2 JMX Authentication
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@1180 7fc7bbf3-cf45-46d4-be06-341739edd864

--- kits/skeletons/shared-base/common/bin/start-tc-server.sh
+++ kits/skeletons/shared-base/common/bin/start-tc-server.sh
@@ -11,7 +11,7 @@ TOPDIR=`dirname "$0"`/..
 
 tc_install_dir "${TOPDIR}"
 tc_classpath "" true
-tc_java_opts "-server -Xms256m -Xmx256m -Dcom.sun.management.jmxremote"
+tc_java_opts "-server -Xms256m -Xmx256m -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.ssl=false"
 
 tc_java -classpath "${TC_CLASSPATH}" -Dtc.install-root="${TC_INSTALL_DIR}" ${TC_ALL_JAVA_OPTS} com.tc.server.TCServerMain "$@"
 ~
