3439a4812519f12cc2d2204674a865519a61f555
Hung Huynh <hhuynh@gmail.com>
2007-02-27 00:25:12 +0000

    only svn update up to the good revision published by the general-monkey
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@1401 7fc7bbf3-cf45-46d4-be06-341739edd864

--- buildsystems/cruisecontrol-task/svnupdate.rb
+++ buildsystems/cruisecontrol-task/svnupdate.rb
@@ -8,10 +8,10 @@ def get_current_rev(topdir)
   YAML::load(`svn info #{topdir}`)["Last Changed Rev"].to_i
 end
 
-def svn_update_with_error_tolerant(topdir)
+def svn_update_with_error_tolerant(topdir, revision)
   error_msg=''  
   3.downto(1) do 
-    error_msg=`svn update #{topdir} -q --non-interactive 2>&1`
+    error_msg=`svn update #{topdir} -r #{revision} -q --non-interactive 2>&1`
     return get_current_rev(topdir) if $? == 0
     sleep(5*60)
   end  
@@ -19,13 +19,13 @@ def svn_update_with_error_tolerant(topdir)
 end
 
 def get_current_good_rev(file)
-  currently_good_rev = 0
+  currently_good_rev = 'HEAD'
   begin
     File.open(file, "r") do | f |
       currently_good_rev = f.gets.to_i    
     end
   rescue
-    currently_good_rev = 0
+    currently_good_rev = 'HEAD'
   end
   currently_good_rev
 end
@@ -54,9 +54,9 @@ end
 good_rev_file = File.join(build_archive_dir, "currently_good_rev.txt")
 monkey_name = ARGV[0]
 
-while true  
-  current_good_rev = (monkey_name == "general-monkey") ? 0 : get_current_good_rev(good_rev_file)
-  current_rev = svn_update_with_error_tolerant(topdir)
-  exit(0) if current_good_rev == 0 || current_rev <= current_good_rev
-  sleep(5*60)
+current_rev = get_current_rev(topdir)
+current_good_rev = (monkey_name == "general-monkey") ? 'HEAD' : get_current_good_rev(good_rev_file)
+
+if current_good_rev == 'HEAD' || current_rev <= current_good_rev 
+  svn_update_with_error_tolerant(topdir, current_good_rev)
 end
