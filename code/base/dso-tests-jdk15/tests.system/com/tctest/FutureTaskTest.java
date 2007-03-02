d346b783b02beb7e7eebd2261fdbb42cd466adeb
Tim Eck <timeck@gmail.com>
2007-03-02 03:38:45 +0000

    timebomb FutureTaskTest until 3/3
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@1538 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-tests-jdk15/tests.system/com/tctest/FutureTaskTest.java
+++ code/base/dso-tests-jdk15/tests.system/com/tctest/FutureTaskTest.java
@@ -7,6 +7,10 @@ public class FutureTaskTest extends TransparentTestBase {
 
   private static final int NODE_COUNT = 3;
 
+  public FutureTaskTest() {
+    disableAllUntil("2007-03-03");
+  }
+
   public void doSetUp(TransparentTestIface t) throws Exception {
     t.getTransparentAppConfig().setClientCount(NODE_COUNT);
     t.initializeTestRunner();
