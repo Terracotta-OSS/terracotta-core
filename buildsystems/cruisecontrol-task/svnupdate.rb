bf0120bf4e96d773eae142ee2d66dc9f1c710758
Hung Huynh <hhuynh@gmail.com>
2007-03-02 21:00:21 +0000

    refractored
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@1553 7fc7bbf3-cf45-46d4-be06-341739edd864

--- buildsystems/cruisecontrol-task/svnupdate.rb
+++ buildsystems/cruisecontrol-task/svnupdate.rb
@@ -4,69 +4,84 @@
 require 'tmpdir'
 require 'yaml'
 
-def get_current_rev(topdir)
-  YAML::load(`svn info #{topdir}`)["Last Changed Rev"].to_i
-end
+class SvnUpdate
 
-def svn_update_with_error_tolerant(topdir, revision)
-  error_msg=''  
-  3.downto(1) do 
-    error_msg=`svn update #{topdir} -r #{revision} -q --non-interactive 2>&1`
-    return get_current_rev(topdir) if $? == 0
-    sleep(5*60)
-    `svn cleanup #{topdir}`
-  end  
-  fail(error_msg)
-end
+  def initialize(monkey_name)
+    @monkey_name = monkey_name
+    # get path to top folder of the repo
+    @topdir = File.join(File.expand_path(File.dirname(__FILE__)), "..", "..")
+    @topdir = File.join(@topdir, "..") if @topdir =~ /community/
 
-def get_current_good_rev(file)
-  currently_good_rev = 0
-  begin
-    File.open(file, "r") do | f |
-      currently_good_rev = f.gets.to_i    
+    # default build-archive-dir in monkeys
+    build_archive_dir = "/shares/monkeyoutput"
+
+    if ENV['OS'] =~ /win/i
+      @topdir=`cygpath -u #{@topdir}`
+      build_archive_dir = "o:"    
     end
-  rescue
+
+    @good_rev_file = File.join(build_archive_dir, "currently_good_rev.txt")
+    
+    clean_up_temp_dir
+  end
+
+  def get_current_rev
+    YAML::load(`svn info #{@topdir}`)["Last Changed Rev"].to_i
+  end
+
+  def svn_update_with_error_tolerant(revision)
+    error_msg=''  
+    3.downto(1) do 
+      error_msg=`svn update #{@topdir} -r #{revision} -q --non-interactive 2>&1`
+      return if $? == 0
+      sleep(5*60)
+      `svn cleanup #{@topdir}`
+    end  
+    fail(error_msg)
+  end
+
+  def get_current_good_rev(file)
     currently_good_rev = 0
+    begin
+      File.open(file, "r") do | f |
+        currently_good_rev = f.gets.to_i    
+      end
+    rescue
+      currently_good_rev = 0
+    end
+    currently_good_rev
   end
-  currently_good_rev
-end
 
-
-
-# clean out temp dir
-`rm -rf #{Dir.tmpdir}/terracotta*`
-`rm -rf /var/tmp/terracotta*`
-`rm -rf #{Dir.tmpdir}/open*`
-`rm -rf #{Dir.tmpdir}/*.dat`
-`rm -rf #{Dir.tmpdir}/sprint*`
-
-# get path to top folder of the repo
-topdir = File.join(File.expand_path(File.dirname(__FILE__)), "..", "..")
-topdir = File.join(topdir, "..") if topdir =~ /community/
-
-# default build-archive-dir in monkeys
-build_archive_dir = "/shares/monkeyoutput"
-
-if ENV['OS'] =~ /win/i
-  topdir=`cygpath -u #{topdir}`
-  build_archive_dir = "o:"    
-end
-
-good_rev_file = File.join(build_archive_dir, "currently_good_rev.txt")
-monkey_name = ARGV[0]
-
-while true
-  current_rev = get_current_rev(topdir)
-  current_good_rev = get_current_good_rev(good_rev_file)
-
-  if monkey_name == "general-monkey" || monkey_name == "test-monkey"
-    svn_update_with_error_tolerant(topdir, "HEAD")
-    exit(0)
-  elsif current_rev <= current_good_rev
-    svn_update_with_error_tolerant(topdir, current_good_rev)
-    exit(0)
-  else # I have a revision that is greater than a good known reivision, so I sleep and wait
-    sleep(5*60)
+  def clean_up_temp_dir
+    # clean out temp dir
+    `rm -rf #{Dir.tmpdir}/terracotta*`
+    `rm -rf /var/tmp/terracotta*`
+    `rm -rf #{Dir.tmpdir}/open*`
+    `rm -rf #{Dir.tmpdir}/*.dat`
+    `rm -rf #{Dir.tmpdir}/sprint*`
   end
-end
+  
+  def update
+    while true
+      current_rev = get_current_rev()
+      current_good_rev = get_current_good_rev(@good_rev_file)
+      
+      puts "curr: #{current_rev}"
+      puts "good: #{current_good_rev}"
 
+      if @monkey_name == "general-monkey" || @monkey_name == "test-monkey"
+        svn_update_with_error_tolerant("HEAD")
+        exit(0)
+      elsif current_rev <= current_good_rev
+        svn_update_with_error_tolerant(current_good_rev)
+        exit(0)
+      else # I have a revision that is greater than a good known reivision, so I sleep and wait
+        sleep(5*60)
+      end
+    end
+  end
+  
+end # class SvnUpdate
+
+svn = SvnUpdate.new(ARGV[0])
+svn.update
