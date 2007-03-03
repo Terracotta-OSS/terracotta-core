0fdf94f76063ef06f6c07c74ee6d0b15289a9807
Hung Huynh <hhuynh@gmail.com>
2007-03-03 15:58:02 +0000

    timebomb till Tuesday 03/06
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@1578 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-spring-tests/tests.system/com/tctest/spring/integrationtests/load/DistributedEventsLoadTest.java
+++ code/base/dso-spring-tests/tests.system/com/tctest/spring/integrationtests/load/DistributedEventsLoadTest.java
@@ -26,9 +26,9 @@ public class DistributedEventsLoadTest extends AbstractDeploymentTest {
   
   private Deployment           deployment;
 
-//  public DistributedEventsLoadTest() {
-//    disableAllUntil("2007-03-20");  // XXX timebombed
-//  }
+  public DistributedEventsLoadTest() {
+    disableAllUntil("2007-03-06");  // XXX timebombed
+  }
   
   protected void setUp() throws Exception {
     super.setUp();
