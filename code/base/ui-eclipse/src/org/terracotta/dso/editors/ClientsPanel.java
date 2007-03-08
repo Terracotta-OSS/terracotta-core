761820e33f4ad933754a0ec3cb97bb1dfdfa7787
Eric Ellis <eellis@terracottatech.com>
2007-03-08 20:10:42 +0000

    Eclipse Plugin - added modules edit panel
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@1677 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/ui-eclipse/src/org/terracotta/dso/editors/ClientsPanel.java
+++ code/base/ui-eclipse/src/org/terracotta/dso/editors/ClientsPanel.java
@@ -1,21 +1,21 @@
 /*
- * All content copyright (c) 2003-2006 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ * All content copyright (c) 2003-2006 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
  */
 package org.terracotta.dso.editors;
-
-import org.eclipse.core.resources.IFolder;
-import org.eclipse.core.resources.IProject;
 
 import org.dijon.Button;
 import org.dijon.ContainerResource;
 import org.dijon.DictionaryResource;
 import org.dijon.Label;
-
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
 import org.terracotta.dso.TcPlugin;
 import org.terracotta.dso.editors.chooser.ProjectFolderNavigator;
 import org.terracotta.dso.editors.xmlbeans.XmlObjectStructureChangeEvent;
 import org.terracotta.dso.editors.xmlbeans.XmlObjectStructureListener;
 import org.terracotta.dso.editors.xmlbeans.XmlStringField;
+
 import com.terracottatech.config.Client;
 import com.terracottatech.config.TcConfigDocument.TcConfig;
 
@@ -25,11 +25,8 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
-public class ClientsPanel extends ConfigurationEditorPanel
-  implements ActionListener,
-             ConfigurationEditorRoot,
-             XmlObjectStructureListener
-{
+public class ClientsPanel extends ConfigurationEditorPanel implements ActionListener, ConfigurationEditorRoot,
+    XmlObjectStructureListener {
   private static ContainerResource m_res;
   private IProject                 m_project;
   private TcConfig                 m_config;
@@ -39,17 +36,18 @@ public class ClientsPanel extends ConfigurationEditorPanel
   private Button                   m_logsButton;
   private ProjectFolderNavigator   m_folderNavigator;
   private DsoClientDataPanel       m_dsoClientData;
+  private ModulesPanel             m_modulesPanel;
 
   static {
-    TcPlugin           plugin = TcPlugin.getDefault();
+    TcPlugin plugin = TcPlugin.getDefault();
     DictionaryResource topRes = plugin.getResources();
 
-    m_res = (ContainerResource)topRes.find("ClientsPanel");
+    m_res = (ContainerResource) topRes.find("ClientsPanel");
   }
-  
+
   public ClientsPanel() {
     super();
-    if(m_res != null) {
+    if (m_res != null) {
       load(m_res);
     }
   }
@@ -57,24 +55,26 @@ public class ClientsPanel extends ConfigurationEditorPanel
   public void load(ContainerResource containerRes) {
     super.load(containerRes);
 
-    m_logs = (XmlStringField)findComponent("Logs");
+    m_modulesPanel = (ModulesPanel) findComponent("ModulesPanel");
+
+    m_logs = (XmlStringField) findComponent("Logs");
     m_logs.init(Client.class, "logs");
-    
-    m_logsLabel = (Label)findComponent("LogsLabel");
+
+    m_logsLabel = (Label) findComponent("LogsLabel");
     m_logsLabel.addMouseListener(new MouseAdapter() {
       public void mouseClicked(MouseEvent me) {
-        if(me.getClickCount() == 1) {
+        if (me.getClickCount() == 1) {
           m_logs.unset();
         }
       }
     });
-    
-    m_logsButton = (Button)findComponent("LogsButton");
+
+    m_logsButton = (Button) findComponent("LogsButton");
     m_logsButton.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent ae) {
-        Frame frame = (Frame)getAncestorOfClass(Frame.class);
+        Frame frame = (Frame) getAncestorOfClass(Frame.class);
 
-        if(m_folderNavigator == null) {
+        if (m_folderNavigator == null) {
           m_folderNavigator = new ProjectFolderNavigator(frame);
         }
         m_folderNavigator.init(m_project);
@@ -83,21 +83,21 @@ public class ClientsPanel extends ConfigurationEditorPanel
         m_folderNavigator.setVisible(true);
       }
     });
-    
-    m_dsoClientData = (DsoClientDataPanel)findComponent("DsoClientData");
+
+    m_dsoClientData = (DsoClientDataPanel) findComponent("DsoClientData");
   }
 
   class LogsNavigatorListener implements ActionListener {
     public void actionPerformed(ActionEvent ae) {
       IFolder folder = m_folderNavigator.getSelectedFolder();
 
-      if(folder != null) {
+      if (folder != null) {
         m_logs.setText(folder.getProjectRelativePath().toString());
         m_logs.set();
       }
     }
   }
-  
+
   public boolean hasAnySet() {
     return m_logs.isSet() || m_dsoClientData.hasAnySet();
   }
@@ -105,81 +105,91 @@ public class ClientsPanel extends ConfigurationEditorPanel
   public void ensureXmlObject() {
     super.ensureXmlObject();
 
-    if(m_client == null) {
+    if (m_client == null) {
       removeListeners();
       m_client = m_config.addNewClients();
       updateChildren();
       addListeners();
     }
   }
-  
+
   private void syncModel() {
-    if(!hasAnySet() && m_config.getClients() != null){
+    if (!hasAnySet() && m_config.getClients() != null) {
       m_config.unsetClients();
       m_client = null;
       fireXmlObjectStructureChanged();
       updateChildren();
-    }
-    else {
+    } else {
       setDirty();
     }
   }
-  
+
   private void fireXmlObjectStructureChanged() {
     fireXmlObjectStructureChanged(m_config);
   }
-  
+
   public void actionPerformed(ActionEvent ae) {
     setDirty();
   }
-  
+
   public void structureChanged(XmlObjectStructureChangeEvent e) {
     syncModel();
   }
-  
+
   private void addListeners() {
     m_logs.addActionListener(this);
     m_logs.addXmlObjectStructureListener(this);
 
     m_dsoClientData.addXmlObjectStructureListener(this);
   }
-  
+
   private void removeListeners() {
     m_logs.removeActionListener(this);
     m_logs.removeXmlObjectStructureListener(this);
 
     m_dsoClientData.removeXmlObjectStructureListener(this);
   }
-  
+
   private void updateChildren() {
     m_logs.setup(m_client);
     m_dsoClientData.setup(m_client);
   }
-  
+
   public void setup(IProject project) {
     TcPlugin plugin = TcPlugin.getDefault();
-    
+
     removeListeners();
     setEnabled(true);
 
     m_project = project;
-    m_config  = plugin.getConfiguration(project);
-    m_client  = m_config != null ? m_config.getClients() : null;
+    m_config = plugin.getConfiguration(project);
+    m_client = m_config != null ? m_config.getClients() : null;
+    m_modulesPanel.setup(m_client);
+    m_modulesPanel.addChangeListener(new ModulesPanel.ModulesEventListener() {
+      public void handleEvent() {
+        fireXmlObjectStructureChanged();
+      }
+    });
+    m_modulesPanel.addSetDirtyListener(new ModulesPanel.ModulesEventListener() {
+      public void handleEvent() {
+        setDirty();
+      }
+    });
 
     updateChildren();
     addListeners();
   }
-  
+
   public IProject getProject() {
     return m_project;
   }
-  
+
   public void tearDown() {
     removeListeners();
 
     m_logs.tearDown();
     m_dsoClientData.tearDown();
-    
+
     setEnabled(false);
   }
 }
---
+++ code/base/ui-eclipse/src/org/terracotta/dso/editors/ModulesPanel.java
@@ -0,0 +1,362 @@
+/*
+ * All content copyright (c) 2003-2006 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
+ */
+package org.terracotta.dso.editors;
+
+import org.dijon.Button;
+import org.dijon.ContainerResource;
+import org.eclipse.swt.widgets.Shell;
+import org.terracotta.dso.TcPlugin;
+
+import com.tc.admin.common.XContainer;
+import com.tc.admin.common.XTable;
+import com.terracottatech.config.Client;
+import com.terracottatech.config.Module;
+import com.terracottatech.config.Modules;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
+import javax.swing.table.DefaultTableModel;
+
+// TODO: validate that the repo and modules actually exist (this may require a background thread due to request timeouts
+public class ModulesPanel extends XContainer implements TableModelListener {
+
+  private Modules                m_modules;
+  private Client                 m_dsoClient;
+  private XTable                 m_repositoriesTable;
+  private XTable                 m_modulesTable;
+  private RepositoriesTableModel m_repositoriesTableModel;
+  private ModulesTableModel      m_modulesTableModel;
+  private Button                 m_repoAddButton;
+  private Button                 m_repoRemoveButton;
+  private Button                 m_moduleAddButton;
+  private Button                 m_moduleRemoveButton;
+  private ActionListener         m_repoAddListener;
+  private ActionListener         m_repoRemoveListener;
+  private ActionListener         m_moduleAddListener;
+  private ActionListener         m_moduleRemoveListener;
+  private ListSelectionListener  m_repoSelectionListener;
+  private ListSelectionListener  m_moduleSelectionListener;
+  private ModulesEventListener   m_changeListener;
+  private ModulesEventListener   m_setDirtyListener;
+
+  public ModulesPanel() {
+    super();
+  }
+
+  public void load(ContainerResource containerRes) {
+    super.load(containerRes);
+
+    m_repositoriesTable = (XTable) findComponent("RepositoriesTable");
+    m_repositoriesTable.setModel(m_repositoriesTableModel = new RepositoriesTableModel());
+
+    m_modulesTable = (XTable) findComponent("ModulesTable");
+    m_modulesTable.setModel(m_modulesTableModel = new ModulesTableModel());
+
+    m_moduleAddListener = new ActionListener() {
+      public void actionPerformed(ActionEvent ae) {
+        TcPlugin.getStandardDisplay().asyncExec(new Runnable() {
+          public void run() {
+            Shell shell = TcPlugin.getStandardDisplay().getActiveShell();
+            AddModuleDialog dialog = new AddModuleDialog(shell, "Module Declaration", "Module Declaration");
+            dialog.addValueListener(new AddModuleDialog.ValueListener() {
+              public void setValues(String name, String version) {
+                if (!name.trim().equals("") || !version.trim().equals("")) {
+                  internalAddModule(name, version);
+                }
+              }
+            });
+            dialog.open();
+          }
+        });
+      }
+    };
+
+    m_moduleRemoveButton = (Button) findComponent("RemoveModuleButton");
+    m_moduleRemoveListener = new ActionListener() {
+      public void actionPerformed(ActionEvent ae) {
+        Modules modules = ensureModulesElement();
+        int[] selection = m_modulesTable.getSelectedRows();
+        for (int i = selection.length - 1; i >= 0; i--) {
+          modules.removeModule(selection[i]);
+        }
+        m_modulesTableModel.removeRows(selection);
+      }
+    };
+
+    m_repoAddButton = (Button) findComponent("AddRepositoryButton");
+    m_repoAddListener = new ActionListener() {
+      public void actionPerformed(ActionEvent ae) {
+        TcPlugin.getStandardDisplay().asyncExec(new Runnable() {
+          public void run() {
+            Shell shell = TcPlugin.getStandardDisplay().getActiveShell();
+            RepoLocationDialog dialog = new RepoLocationDialog(shell, "Repository Declaration",
+                "Repository Location (URL)");
+            dialog.addValueListener(new RepoLocationDialog.ValueListener() {
+              public void setValues(String repoLocation) {
+                if (repoLocation != null && !(repoLocation = repoLocation.trim()).equals("")) {
+                  internalAddRepositoryLocation(repoLocation);
+                }
+              }
+            });
+            dialog.open();
+          }
+        });
+      }
+    };
+
+    m_repoRemoveButton = (Button) findComponent("RemoveRepositoryButton");
+    m_repoRemoveListener = new ActionListener() {
+      public void actionPerformed(ActionEvent ae) {
+        Modules modules = ensureModulesElement();
+        int[] selection = m_repositoriesTable.getSelectedRows();
+        for (int i = selection.length - 1; i >= 0; i--) {
+          modules.removeRepository(selection[i]);
+        }
+        m_repositoriesTableModel.removeRows(selection);
+      }
+    };
+
+    m_repoSelectionListener = new ListSelectionListener() {
+      public void valueChanged(ListSelectionEvent lse) {
+        if (!lse.getValueIsAdjusting()) {
+          int[] sel = m_repositoriesTable.getSelectedRows();
+          m_repoRemoveButton.setEnabled(sel != null && sel.length > 0);
+        }
+      }
+    };
+    m_moduleSelectionListener = new ListSelectionListener() {
+      public void valueChanged(ListSelectionEvent lse) {
+        if (!lse.getValueIsAdjusting()) {
+          int[] sel = m_modulesTable.getSelectedRows();
+          m_moduleRemoveButton.setEnabled(sel != null && sel.length > 0);
+        }
+      }
+    };
+
+    m_moduleAddButton = (Button) findComponent("AddModuleButton");
+  }
+
+  private Modules ensureModulesElement() {
+    if (m_modules == null) {
+      ensureXmlObject();
+    }
+    return m_modules;
+  }
+
+  // make sure parent exists
+  public void ensureXmlObject() {
+    if (m_modules == null) {
+      removeListeners();
+      m_modules = m_dsoClient.addNewModules();
+      updateChildren();
+      addListeners();
+    }
+  }
+
+  // match table to xmlbeans
+  private void updateChildren() {
+    m_modulesTableModel.clear();
+    m_repositoriesTableModel.clear();
+    if (m_modules != null) {
+      String[] repos = m_modules.getRepositoryArray();
+      for (int i = 0; i < repos.length; i++) {
+        m_repositoriesTableModel.addRepoLocation(repos[i]);
+      }
+      Module[] modules = m_modules.getModuleArray();
+      for (int i = 0; i < modules.length; i++) {
+        m_modulesTableModel.addModule(modules[i].getName(), modules[i].getVersion());
+      }
+    }
+  }
+
+  public void tableChanged(TableModelEvent e) {
+    syncModel();
+  }
+
+  public void setup(Client dsoClient) {
+    setEnabled(true);
+    removeListeners();
+    m_dsoClient = dsoClient;
+    m_modules = (m_dsoClient != null) ? m_dsoClient.getModules() : null;
+    updateChildren();
+    addListeners();
+  }
+
+  public void tearDown() {
+    removeListeners();
+    m_dsoClient = null;
+    m_repositoriesTableModel.clear();
+    setEnabled(false);
+  }
+
+  private void removeListeners() {
+    m_modulesTableModel.removeTableModelListener(this);
+    m_repositoriesTableModel.removeTableModelListener(this);
+    m_repositoriesTable.getSelectionModel().removeListSelectionListener(m_repoSelectionListener);
+    m_repoAddButton.removeActionListener(m_repoAddListener);
+    m_repoRemoveButton.removeActionListener(m_repoRemoveListener);
+    m_modulesTable.getSelectionModel().removeListSelectionListener(m_moduleSelectionListener);
+    m_moduleAddButton.removeActionListener(m_moduleAddListener);
+    m_moduleRemoveButton.removeActionListener(m_moduleRemoveListener);
+  }
+
+  private void addListeners() {
+    m_modulesTableModel.addTableModelListener(this);
+    m_repositoriesTableModel.addTableModelListener(this);
+    m_repositoriesTable.getSelectionModel().addListSelectionListener(m_repoSelectionListener);
+    m_repoAddButton.addActionListener(m_repoAddListener);
+    m_repoRemoveButton.addActionListener(m_repoRemoveListener);
+    m_modulesTable.getSelectionModel().addListSelectionListener(m_moduleSelectionListener);
+    m_moduleAddButton.addActionListener(m_moduleAddListener);
+    m_moduleRemoveButton.addActionListener(m_moduleRemoveListener);
+  }
+
+  private void internalAddRepositoryLocation(String repoLocation) {
+    ensureModulesElement().addRepository(repoLocation);
+    m_repositoriesTableModel.addRepoLocation(repoLocation);
+    int row = m_repositoriesTableModel.getRowCount() - 1;
+    m_repositoriesTable.setRowSelectionInterval(row, row);
+  }
+
+  private void internalAddModule(String name, String version) {
+    Module module = ensureModulesElement().addNewModule();
+    module.setName(name);
+    module.setVersion(version);
+    m_modulesTableModel.addModule(name, version);
+    int row = m_modulesTableModel.getRowCount() - 1;
+    m_modulesTable.setRowSelectionInterval(row, row);
+  }
+
+  private void syncModel() {
+    if (!hasAnySet() && m_dsoClient.getModules() != null) {
+      m_dsoClient.unsetModules();
+      m_modules = null;
+      fireChangeEvent();
+      updateChildren();
+    } else {
+      fireSetDirtyEvent();
+    }
+  }
+
+  synchronized void addChangeListener(ModulesEventListener listener) {
+    m_changeListener = listener;
+  }
+
+  synchronized void addSetDirtyListener(ModulesEventListener listener) {
+    m_setDirtyListener = listener;
+  }
+
+  private synchronized void fireChangeEvent() {
+    if (m_changeListener != null) m_changeListener.handleEvent();
+  }
+
+  private synchronized void fireSetDirtyEvent() {
+    if (m_setDirtyListener != null) m_setDirtyListener.handleEvent();
+  }
+
+  public boolean hasAnySet() {
+    return m_modules != null && (m_modules.sizeOfRepositoryArray() > 0 || m_modules.sizeOfModuleArray() > 0);
+  }
+
+  // --------------------------------------------------------------------------------
+
+  class RepositoriesTableModel extends DefaultTableModel {
+    RepositoriesTableModel() {
+      super();
+      setColumnIdentifiers(new String[] { "Location" });
+    }
+
+    void clear() {
+      setRowCount(0);
+    }
+
+    void addRepoLocation(String typeName) {
+      addRow(new Object[] { typeName });
+    }
+
+    int indexOf(String typeName) {
+      int count = getRowCount();
+      for (int i = 0; i < count; i++) {
+        if (((String) getValueAt(i, 0)).equals(typeName)) { return i; }
+      }
+      return -1;
+    }
+
+    public void setValueAt(Object value, int row, int col) {
+      m_modules.addRepository((String) value);
+      super.setValueAt(value, row, col);
+    }
+
+    void removeRows(int[] rows) {
+      removeTableModelListener(ModulesPanel.this);
+      for (int i = rows.length - 1; i >= 0; i--) {
+        removeRow(rows[i]);
+      }
+      addTableModelListener(ModulesPanel.this);
+      syncModel();
+    }
+  }
+
+  // --------------------------------------------------------------------------------
+
+  class ModulesTableModel extends DefaultTableModel {
+    ModulesTableModel() {
+      super();
+      setColumnIdentifiers(new String[] { "Name", "Version" });
+    }
+
+    void clear() {
+      setRowCount(0);
+    }
+
+    void addModule(String name, String version) {
+      addRow(new Object[] { name, version });
+    }
+
+    int indexOf(String typeName) {
+      int count = getRowCount();
+      for (int i = 0; i < count; i++) {
+        if (((String) getValueAt(i, 0)).equals(typeName)) { return i; }
+      }
+      return -1;
+    }
+
+    public void setValueAt(Object value, int row, int col) {
+      Module module = m_modules.addNewModule();
+      if (col == 0) switch (col) {
+        case 0:
+          module.setName((String) value);
+          break;
+        case 1:
+          module.setVersion((String) value);
+          break;
+
+        default:
+          break;
+      }
+      super.setValueAt(value, row, col);
+    }
+
+    void removeRows(int[] rows) {
+      removeTableModelListener(ModulesPanel.this);
+      for (int i = rows.length - 1; i >= 0; i--) {
+        removeRow(rows[i]);
+      }
+      addTableModelListener(ModulesPanel.this);
+      syncModel();
+    }
+  }
+
+  // --------------------------------------------------------------------------------
+
+  public static interface ModulesEventListener {
+    void handleEvent();
+  }
+}
