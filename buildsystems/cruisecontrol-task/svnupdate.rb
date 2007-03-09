b63a37f0c16c66ecfe02cd3767aa63bfab384225
Jason Voegele <jason@jvoegele.com>
2007-03-09 18:44:20 +0000

    Do svn update if good rev file could not be found...really this time.
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@1705 7fc7bbf3-cf45-46d4-be06-341739edd864

--- buildsystems/cruisecontrol-task/svnupdate.rb
+++ buildsystems/cruisecontrol-task/svnupdate.rb
@@ -85,11 +85,11 @@ class SvnUpdate
         log("general-monkey - updating to HEAD")
         svn_update_with_error_tolerant("HEAD")
         exit(0)
-      elsif current_rev < current_good_rev
+      elsif current_good_rev == -1 || current_rev < current_good_rev
         log("not general-monkey - updating to #{current_good_rev}")
         svn_update_with_error_tolerant(current_good_rev)
         exit(0)
-      elsif current_good_rev = -1 || current_rev == current_good_rev
+      elsif current_rev == current_good_rev
         log("current_rev == currently_good_rev - no svn update needed")
         exit(0)
       else # I have a revision that is greater than a good known reivision, so I sleep and wait
