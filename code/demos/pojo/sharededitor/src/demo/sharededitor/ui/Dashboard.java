912e7ec6ba94ea9f387306353acbac7626527c85
Juris Galang <jgalang@terracottatech.com>
2007-03-12 20:25:53 +0000

    Got rid of extra files. Proper name for sample image file. Some refactoring.
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@1750 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/demos/pojo/sharededitor/src/demo/sharededitor/ui/Dashboard.java
+++ code/demos/pojo/sharededitor/src/demo/sharededitor/ui/Dashboard.java
@@ -37,6 +37,16 @@ public final class Dashboard
 
 	public Dashboard(Dispatcher dispatcher)
 	{
+	   final String IMAGE_PLACEHOLDER  = "/images/placeholder.gif";
+	   final String IMAGE_SELECTOR     = "/images/selector.gif";
+	   final String IMAGE_LINE         = "/images/line.gif";
+	   final String IMAGE_SQUARE       = "/images/square.gif";
+	   final String IMAGE_FILLEDSQUARE = "/images/filledsquare.gif";
+	   final String IMAGE_CIRCLE       = "/images/circle.gif";
+	   final String IMAGE_FILLEDCIRCLE = "/images/filledcircle.gif";
+	   final String IMAGE_TEXT         = "/images/text.gif";
+	   final String IMAGE_BEDROOM      = "/images/bedroom.gif";
+	   
 		this.dispatcher = dispatcher;
 		
 		setOrientation(VERTICAL);
@@ -45,56 +55,56 @@ public final class Dashboard
 
 		ButtonGroup bg   = new ButtonGroup();
 		JToggleButton b1 = new JToggleButton(new SetDrawToolAction("Selector"));
-		b1.setIcon(new ImageIcon(getResource("/images/selector.gif")));
+		b1.setIcon(new ImageIcon(getResource(IMAGE_SELECTOR)));
 		b1.setToolTipText("Select shapes");
 		bg.add(b1);
 		add(b1);
 
 		JButton b0 = new JButton();
 		b0.setEnabled(false);
-		b0.setIcon(new ImageIcon(getResource("/images/placeholder.gif")));
+		b0.setIcon(new ImageIcon(getResource(IMAGE_PLACEHOLDER)));
 		setWithSolidColorIcon(b0, Color.LIGHT_GRAY);
 
 		b1 = new JToggleButton(new SetDrawToolAction("Line"));
-		b1.setIcon(new ImageIcon(getResource("/images/line.gif")));
+		b1.setIcon(new ImageIcon(getResource(IMAGE_LINE)));
 		b1.setToolTipText("Draw lines");
 		bg.add(b1);
 		add(b1);
 
 		b1 = new JToggleButton(new SetDrawToolAction("Square", IFillStyleConsts.FILLSTYLE_TRANSPARENT));
-		b1.setIcon(new ImageIcon(getResource("/images/square.gif")));
+		b1.setIcon(new ImageIcon(getResource(IMAGE_SQUARE)));
 		b1.setToolTipText("Draw transparent squares and rectangular shapes");
 		bg.add(b1);
 		add(b1);
 
 		b1 = new JToggleButton(new SetDrawToolAction("Square", IFillStyleConsts.FILLSTYLE_SOLID));
-		b1.setIcon(new ImageIcon(getResource("/images/filledsquare.gif")));
+		b1.setIcon(new ImageIcon(getResource(IMAGE_FILLEDSQUARE)));
 		b1.setToolTipText("Draw solid squares and rectangular shapes");
 		bg.add(b1);
 		add(b1);
 
 		b1 = new JToggleButton(new SetDrawToolAction("Circle", IFillStyleConsts.FILLSTYLE_TRANSPARENT));
-		b1.setIcon(new ImageIcon(getResource("/images/circle.gif")));
+		b1.setIcon(new ImageIcon(getResource(IMAGE_CIRCLE)));
 		b1.setToolTipText("Draw transparent circles and oval shapes");
 		bg.add(b1);
 		add(b1);
 
 		b1 = new JToggleButton(new SetDrawToolAction("Circle", IFillStyleConsts.FILLSTYLE_SOLID));
-		b1.setIcon(new ImageIcon(getResource("/images/filledcircle.gif")));
+		b1.setIcon(new ImageIcon(getResource(IMAGE_FILLEDCIRCLE)));
 		b1.setToolTipText("Draw solid circles and oval shapes");
 		bg.add(b1);
 		add(b1);
 
 		b1 = new JToggleButton(new SetDrawToolAction("Text"));
-		b1.setIcon(new ImageIcon(getResource("/images/text.gif")));
+		b1.setIcon(new ImageIcon(getResource(IMAGE_TEXT)));
 		b1.setToolTipText("Render text as images");
 		add(b1);
 		bg.add(b1);
 
 		b1 = new JToggleButton(new SetDrawToolAction("Image", IFillStyleConsts.FILLSTYLE_TEXTURED));
 		textureToolButton = b1;
-		b1.setIcon(new ImageIcon(getResource("/images/placeholder.gif")));
-		setWithImageIcon(b1, new ImageIcon(getResource("/images/warrior.jpg")));
+		b1.setIcon(new ImageIcon(getResource(IMAGE_PLACEHOLDER)));
+		setWithImageIcon(b1, new ImageIcon(getResource(IMAGE_BEDROOM)));
 		b1.setToolTipText("Paint with images");
 		add(b1);
 		bg.add(b1);
@@ -103,21 +113,21 @@ public final class Dashboard
 		add(new JToolBar.Separator());
 
 		JButton b2 = new JButton(new SetColorAction("Foreground", Color.DARK_GRAY));
-		b2.setIcon(new ImageIcon(getResource("/images/placeholder.gif")));
+		b2.setIcon(new ImageIcon(getResource(IMAGE_PLACEHOLDER)));
 		b2.setToolTipText("Select line color");
 		setWithSolidColorIcon(b2, Color.DARK_GRAY);
 		add(b2);
 
 		b2 = new JButton(new SetColorAction("Background", Color.LIGHT_GRAY));
-		b2.setIcon(new ImageIcon(getResource("/images/placeholder.gif")));
+		b2.setIcon(new ImageIcon(getResource(IMAGE_PLACEHOLDER)));
 		b2.setToolTipText("Select fill color");
 		setWithSolidColorIcon(b2, Color.LIGHT_GRAY);
 		add(b2);
 
 		b2 = new JButton(new SetTextureAction());
 		this.textureSelectButton = b2;
-		b2.setIcon(new ImageIcon(getResource("/images/placeholder.gif")));
-		setWithImageIcon(b2, new ImageIcon(getResource("/images/warrior.jpg")));
+		b2.setIcon(new ImageIcon(getResource(IMAGE_PLACEHOLDER)));
+		setWithImageIcon(b2, new ImageIcon(getResource(IMAGE_BEDROOM)));
 		b2.setToolTipText("Select the image to use when painting with images");
 		b2.setEnabled(false);
 		add(b2);
@@ -128,7 +138,7 @@ public final class Dashboard
 		dispatcher.setFillStyle(IFillStyleConsts.FILLSTYLE_SOLID);
 		dispatcher.setForeground(Color.DARK_GRAY);
 		dispatcher.setBackground(Color.LIGHT_GRAY);
-		dispatcher.setTexture(new ImageIcon(getResource("/images/warrior.jpg")).getImage());
+		dispatcher.setTexture(new ImageIcon(getResource(IMAGE_BEDROOM)).getImage());
 		dispatcher.setFontName("Courier New");
 		dispatcher.setFontSize(24);
 		dispatcher.setDrawTool("Line");
@@ -260,12 +270,12 @@ public final class Dashboard
 		{
 		   public boolean accept(java.io.File f) 
 		   { 
-		      return f.getName().endsWith(".jpg") || f.getName().endsWith(".gif") || f.getName().endsWith(".png");
+		      return f.getName().endsWith(".jpeg") || f.getName().endsWith(".jpg") || f.getName().endsWith(".gif") || f.getName().endsWith(".png");
 		   }
 		   
 		   public String getDescription()
 		   {
-		      return "JPG, GIF, & PNG Images";
+		      return "JPEG, JPG, GIF, & PNG Images";
 		   }
 		}
 	}
