cfe0b98bbb8fbc48dd0174aaff746d6e410907ee
Jason Voegele <jason@jvoegele.com>
2007-03-14 21:17:02 +0000

    Fixed POM template.
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@1794 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/eclipse2maven.rb
+++ code/base/eclipse2maven.rb
@@ -8,7 +8,11 @@ ART_2_GRP = {
 }
 
 POM_DEF = <<POM
-<project>
+<?xml version="1.0" encoding="UTF-8"?>
+<project xmlns="http://maven.apache.org/POM/4.0.0"
+         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">
+  <modelVersion>4.0.0</modelVersion>
   <parent>
     <groupId>org.terracotta</groupId>
     <artifactId>parent</artifactId>
