3af18cd991cd15693fd7ed1a7a1c6d830def7701
Gary Keim <gkeim@terracottatech.com>
2007-03-16 01:56:01 +0000

    DEV-512: renamed schema file to match schema version and updated demo/sample config files to refer to schema on website. DEV-516: re-enabled schema packaging in the web kit; the HTML docs will be create if the build monkeys have DOCFLEX_HOME set.
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@1853 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/buildscripts/postscripts/include-schemas.rb
+++ code/base/buildscripts/postscripts/include-schemas.rb
@@ -9,8 +9,6 @@ class BaseCodeTerracottaBuilder <  TerracottaBuilder
   # - generate a javadoc style documentation for the schemas
   protected
   def postscript(ant, build_environment, product_directory, *args)
-    return if @static_resources.docflex_home.nil? 
-    
     srcdir = @static_resources.config_schema_source_directory(@module_set)
     tmpdir = FilePath.new(product_directory, 'tmp').ensure_directory
 
@@ -18,39 +16,41 @@ class BaseCodeTerracottaBuilder <  TerracottaBuilder
       ant.fileset(:dir => srcdir.to_s, :includes => '*.xsd')
     end
 
-    docflex           = FilePath.new(@static_resources.docflex_home)
-    docflex_lib       = FilePath.new(docflex, "lib")
-    docflex_template  = FilePath.new(docflex, "templates", "XSDDocFrames.tpl")
-    docflex_classpath = "#{docflex_lib.to_s}/xercesImpl.jar:#{docflex_lib.to_s}/xml-apis.jar:#{docflex_lib.to_s}/docflex-xml-kit.jar"
-    srcfiles          = FilePath.new(srcdir, "terracotta-2.3.xsd")
+    unless @static_resources.docflex_home.nil?
+      docflex           = FilePath.new(@static_resources.docflex_home)
+      docflex_lib       = FilePath.new(docflex, "lib")
+      docflex_template  = FilePath.new(docflex, "templates", "XSDDocFrames.tpl")
+      docflex_classpath = "#{docflex_lib.to_s}/xercesImpl.jar:#{docflex_lib.to_s}/xml-apis.jar:#{docflex_lib.to_s}/docflex-xml-kit.jar"
+      srcfiles          = FilePath.new(srcdir, "terracotta-3.xsd")
 
-    ant.java(
-      :classname   => 'com.docflex.xml.Generator',
-      :classpath   => docflex_classpath,
-      :fork        => true,
-      :failonerror => true,
-      :dir         => tmpdir.to_s,
-      :maxmemory   => '512m') do
-      ant.jvmarg(:value => "-Djava.awt.headless=true")
-      ant.arg(:line => "-xmlconfig xmltypes.config")
-      ant.arg(:line => "-template #{docflex_template.canonicalize.to_s}")
-      ant.arg(:line => "-format HTML")
-      ant.arg(:line => "-p:docTitle=\"Terracotta Schemas\"")
-      ant.arg(:line => "-d #{tmpdir.to_s}")
-      ant.arg(:line => "-f tc-config")
-      ant.arg(:line => "-nodialog=true")
-      ant.arg(:line => "-quiet=true")
-      ant.arg(:line => "-launchviewer=false")
-      ant.arg(:line => srcfiles.to_s)
-    end
+      ant.java(
+        :classname   => 'com.docflex.xml.Generator',
+        :classpath   => docflex_classpath,
+        :fork        => true,
+        :failonerror => true,
+        :dir         => tmpdir.to_s,
+        :maxmemory   => '512m') do
+        ant.jvmarg(:value => "-Djava.awt.headless=true")
+        ant.arg(:line => "-xmlconfig xmltypes.config")
+        ant.arg(:line => "-template #{docflex_template.canonicalize.to_s}")
+        ant.arg(:line => "-format HTML")
+        ant.arg(:line => "-p:docTitle=\"Terracotta Configuration Schema Version 3\"")
+        ant.arg(:line => "-d #{tmpdir.to_s}")
+        ant.arg(:line => "-f tc-config-v3")
+        ant.arg(:line => "-nodialog=true")
+        ant.arg(:line => "-quiet=true")
+        ant.arg(:line => "-launchviewer=false")
+        ant.arg(:line => srcfiles.to_s)
+      end
 
-    index_file = FilePath.new(tmpdir.to_s, 'index.html')
-    File.open(index_file.to_s, 'w') do |out|
-      out.puts "<html>"
-      out.puts " <head>"
-      out.puts "   <META HTTP-EQUIV=\"REFRESH\" CONTENT=\"0; URL=tc-config.html\">"
-      out.puts " </head>"
-      out.puts "</html>"
+      index_file = FilePath.new(tmpdir.to_s, 'index.html')
+      File.open(index_file.to_s, 'w') do |out|
+        out.puts "<html>"
+        out.puts " <head>"
+        out.puts "   <META HTTP-EQUIV=\"REFRESH\" CONTENT=\"0; URL=tc-config-v3.html\">"
+        out.puts " </head>"
+        out.puts "</html>"
+      end
     end
 
     args.each do |arg|
