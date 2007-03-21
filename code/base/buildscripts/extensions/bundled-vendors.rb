a28aabe5e2281b54dd1604df2e876c535faf09cb
Hung Huynh <hhuynh@gmail.com>
2007-03-21 18:10:01 +0000

    merge from 2.3 rev1959
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@1960 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/buildscripts/extensions/bundled-vendors.rb
+++ code/base/buildscripts/extensions/bundled-vendors.rb
@@ -5,28 +5,29 @@
 #
 
 require 'tmpdir'
+require 'open-uri'
 
 module BundledVendors
     def bundled_vendors(name, directory, spec)
-      begin
-        srcdir = getCachedVendorDir(name)
-        fail "The source for the named vendor set: `#{name}' does not exists in #{srcdir}" unless File.directory?(srcdir)
-
-        # copy vendor sources and binaries but exclude non-native scripts
-        non_native = @build_environment.is_unix_like? ? ['*.bat', '*.cmd', '*.exe'] : '*.sh'
-        destdir    = FilePath.new(product_directory, directory).ensure_directory
-        ant.copy(:todir => destdir.to_s) do
-          #ant.fileset(:dir => srcdir.to_s, :excludes => "**/.svn/**, **/.*, **/**/#{non_native.to_a.join(', **/**/')}")
-          ant.fileset(:dir => srcdir.to_s, :excludes => "**/.svn/**, **/.*")
-        end
-      rescue
-        puts :warn, "Unable to retrieve #{name} bundle... skipping"
+      url       = "#{@static_resources.vendors_url}/#{name}.zip"
+      unless reachable?(url)
+        puts "*" * 30 + " WARNING " + "*" * 30
+        puts "Unable to retrieve #{name} bundle... skipping"
+        puts "*" * 30 + " WARNING " + "*" * 30
+        return
+      end
+      
+      srcdir = getCachedVendorDir(url, name)
+      fail "The source for the named vendor set: `#{name}' does not exists in #{srcdir}" unless File.directory?(srcdir)
+      # copy vendor sources and binaries but exclude non-native scripts
+      non_native = @build_environment.is_unix_like? ? ['*.bat', '*.cmd', '*.exe'] : '*.sh'
+      destdir    = FilePath.new(product_directory, directory).ensure_directory
+      ant.copy(:todir => destdir.to_s) do
+        ant.fileset(:dir => srcdir.to_s, :excludes => "**/.svn/**, **/.*")
       end
     end
 
-    def getCachedVendorDir(vendor)
-        url       = "#{@static_resources.vendors_url}/#{vendor}.zip"
-        raise("Unable to retrieve #{name} bundle") unless DistributionUtils.reachable?(url)
+    def getCachedVendorDir(url, vendor)
         cache_dir = FilePath.new('.tc-build-cache').ensure_directory.canonicalize.to_s
         zipfile   = FilePath.new(cache_dir, vendor + ".zip").to_s
         ant.get(:src => url, :dest => zipfile, :usetimestamp => true)
@@ -34,5 +35,15 @@ module BundledVendors
         FilePath.new(cache_dir, vendor).to_s
     end
       
-    
+    def reachable?(url)
+      begin
+        open(url) do |f|
+          # do nothing
+        end
+        return true
+      rescue
+        return false
+      end    
+    end
+  
 end
