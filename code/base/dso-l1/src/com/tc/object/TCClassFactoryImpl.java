a5d23a6e39e4a94e377a9ba421bbcfe95e6ef03c
Eugene Kuleshov <ekulesho@terracottatech.com>
2007-03-23 03:19:38 +0000

    CDV-144: Support for matching on subtypes for class includes   additional tests and fixes for honoring transients on subtype matching   added adapter for replacing ISessionStore implementation in wicket (to be moved into its own module)
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@2026 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-l1/src/com/tc/object/TCClassFactoryImpl.java
+++ code/base/dso-l1/src/com/tc/object/TCClassFactoryImpl.java
@@ -4,6 +4,8 @@
  */
 package com.tc.object;
 
+import com.tc.aspectwerkz.reflect.ClassInfo;
+import com.tc.aspectwerkz.reflect.impl.java.JavaClassInfo;
 import com.tc.exception.TCRuntimeException;
 import com.tc.object.applicator.AccessibleObjectApplicator;
 import com.tc.object.applicator.ArrayApplicator;
@@ -47,14 +49,16 @@ public class TCClassFactoryImpl implements TCClassFactory {
     synchronized (classes) {
       TCClass rv = (TCClass) classes.get(clazz);
       if (rv == null) {
-        Class tcPeerClass = config.getTCPeerClass(clazz);
-        String loaderDesc = classProvider.getLoaderDescriptionFor(tcPeerClass);
+        String loaderDesc = classProvider.getLoaderDescriptionFor(clazz);
         String className = clazz.getName();
-        rv = new TCClassImpl(fieldFactory, this, objectManager, tcPeerClass,
-                             getLogicalSuperClassWithDefaultConstructor(clazz), loaderDesc, config
-                                 .getLogicalExtendingClassName(className), config.isLogical(className), config
-                                 .isCallConstructorOnLoad(className), config.getOnLoadScriptIfDefined(className),
-                             config.getOnLoadMethodIfDefined(className), config.isUseNonDefaultConstructor(clazz));
+        ClassInfo classInfo = JavaClassInfo.getClassInfo(clazz);
+        rv = new TCClassImpl(fieldFactory, this,
+                             objectManager,
+                             config.getTCPeerClass(clazz),
+                             getLogicalSuperClassWithDefaultConstructor(clazz), //
+                             loaderDesc, config.getLogicalExtendingClassName(className), config.isLogical(className),
+                             config.isCallConstructorOnLoad(classInfo), config.getOnLoadScriptIfDefined(classInfo),
+                             config.getOnLoadMethodIfDefined(classInfo), config.isUseNonDefaultConstructor(clazz));
         classes.put(clazz, rv);
       }
       return rv;
