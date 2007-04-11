e6c7d588cf57646bd5a9bc6bd633103b8a0d95fd
Hung Huynh <hhuynh@gmail.com>
2007-04-11 18:06:23 +0000

    jar file updated, reenable
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@2398 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/clustered-ehcache-1.3.0/tests.system/com/tctest/CacheManagerTest.java
+++ code/base/clustered-ehcache-1.3.0/tests.system/com/tctest/CacheManagerTest.java
@@ -3,7 +3,7 @@ package com.tctest;
 public class CacheManagerTest extends TransparentTestBase {
 
   public CacheManagerTest() {
-    disableAllUntil("2007-04-13");
+    //disableAllUntil("2007-04-13");
   }
   
   public void doSetUp(final TransparentTestIface tt) throws Exception {
