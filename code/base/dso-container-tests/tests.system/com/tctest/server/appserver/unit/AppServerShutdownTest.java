d8c077872789c68b4f19e2efe5847251c56faaa1
Hung Huynh <hhuynh@gmail.com>
2007-04-11 19:18:58 +0000

    ignore ps grep in linux
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@2401 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-container-tests/tests.system/com/tctest/server/appserver/unit/AppServerShutdownTest.java
+++ code/base/dso-container-tests/tests.system/com/tctest/server/appserver/unit/AppServerShutdownTest.java
@@ -48,8 +48,6 @@ public class AppServerShutdownTest extends AbstractAppServerTestCase {
     URL url2 = createUrl(port2, ShutdownNormallyServlet.class, "cmd=query");
     assertEquals("cmd=query", "OK", HttpUtil.getResponseBody(url2, client));
 
-    int count_before = ProcessInfo.ps_grep_java().split("\\n").length;
-
     System.out.println("Shut down app server normally...");
     for (Iterator iter = appservers.iterator(); iter.hasNext();) {
       Server server = (Server) iter.next();
@@ -60,53 +58,48 @@ public class AppServerShutdownTest extends AbstractAppServerTestCase {
     // There could be 2 kinds of failures:
     // 1. Cargo didn't shutdown the appserver normally
     // 2. DSO didn't allow the appserver to shutdown -- We want to catch this
-    System.out.println("Grepping for java processes...");
-    assertFalse("Cargo processes still linger", checkProcesses(count_before));
+
+    if (!Os.isLinux()) { // can't get full command line args in linux
+      System.out.println("Grepping for java processes...");
+      assertFalse("Cargo processes still linger", checkProcesses());
+    }
 
     System.out.println("Polling heartbeat threads...");
     assertFalse("Linked child processes are still alive", checkAlive());
 
   }
-  
+
   /**
    * return true if app server processes are found
    */
-  private boolean checkProcesses(int count_before) throws Exception {
+  private boolean checkProcesses() throws Exception {
     boolean found = false;
     String processes_after;
     long start = System.currentTimeMillis();
     do {
-      Thread.sleep(1000);      
+      Thread.sleep(1000);
       processes_after = ProcessInfo.ps_grep_java();
-      int count_after = processes_after.split("\\n").length;
-      
-      if (Os.isLinux()) {
-        found = count_after + 2 != count_before;
-      } else {
-        found = processes_after.indexOf("CargoLinkedChildProcess") > 0;
-      }
-      
-    }while (found && System.currentTimeMillis() - start < TIME_WAIT_FOR_SHUTDOWN);
-    
+      found = processes_after.indexOf("CargoLinkedChildProcess") > 0;
+    } while (found && System.currentTimeMillis() - start < TIME_WAIT_FOR_SHUTDOWN);
+
     if (found) {
       System.out.println(processes_after);
     }
-    
+
     return found;
   }
-  
+
   /**
-   * check server status by pinging its linked-child-process
-   * return true if any app server is still alive
+   * check server status by pinging its linked-child-process return true if any app server is still alive
    */
-  private boolean checkAlive() throws Exception {    
+  private boolean checkAlive() throws Exception {
     long start = System.currentTimeMillis();
     boolean foundAlive = false;
     do {
       Thread.sleep(1000);
       foundAlive = LinkedJavaProcessPollingAgent.isAnyAppServerAlive();
     } while (foundAlive && System.currentTimeMillis() - start < TIME_WAIT_FOR_SHUTDOWN);
-    
+
     return foundAlive;
   }
 
