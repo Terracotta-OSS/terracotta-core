c9ac1587657b4343f7087f3a2264e651b55bb18c
Hung Huynh <hhuynh@gmail.com>
2007-04-11 17:19:11 +0000

    disable till April 13
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@2396 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/clustered-ehcache-1.3.0/tests.system/com/tctest/CacheManagerTest.java
+++ code/base/clustered-ehcache-1.3.0/tests.system/com/tctest/CacheManagerTest.java
@@ -2,14 +2,19 @@ package com.tctest;
 
 public class CacheManagerTest extends TransparentTestBase {
 
-	public void doSetUp(final TransparentTestIface tt) throws Exception {
-		tt.getTransparentAppConfig().setClientCount(
-				CacheManagerTestApp.EXPECTED_THREAD_COUNT);
-		tt.initializeTestRunner();
-	}
+  public CacheManagerTest() {
+    disableAllUntil("2007-04-13");
+  }
+  
+  public void doSetUp(final TransparentTestIface tt) throws Exception {
+    tt.getTransparentAppConfig().setClientCount(
+        CacheManagerTestApp.EXPECTED_THREAD_COUNT);
+    tt.initializeTestRunner();
+  }
 
-	protected Class getApplicationClass() {
-		return CacheManagerTestApp.class;
-	}
-	
+  protected Class getApplicationClass() {
+    return CacheManagerTestApp.class;
+  }
+  
 }
+
