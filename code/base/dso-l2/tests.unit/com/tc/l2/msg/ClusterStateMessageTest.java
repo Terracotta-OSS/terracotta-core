eca9a4ef41c580efcd62a20ec8fe1d78fc00874e
Cindy Fisher <cfisher@terracottatech.com>
2007-04-19 04:35:04 +0000

    adding another test to ClusterStateMessageTest
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@2556 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-l2/tests.unit/com/tc/l2/msg/ClusterStateMessageTest.java
+++ code/base/dso-l2/tests.unit/com/tc/l2/msg/ClusterStateMessageTest.java
@@ -5,6 +5,7 @@
 package com.tc.l2.msg;
 
 import com.tc.l2.ha.ClusterState;
+import com.tc.net.protocol.transport.ConnectionID;
 import com.tc.net.protocol.transport.ConnectionIDFactory;
 import com.tc.objectserver.gtx.GlobalTransactionIDSequenceProvider;
 import com.tc.objectserver.handler.GlobalTransactionIDBatchRequestHandler;
@@ -15,7 +16,7 @@ import com.tc.objectserver.persistence.api.Persistor;
 import com.tc.objectserver.persistence.impl.InMemoryPersistor;
 import com.tc.objectserver.persistence.impl.TestMutableSequence;
 import com.tc.objectserver.persistence.sleepycat.ConnectionIDFactoryImpl;
-import com.tc.util.UUID;
+import com.tc.util.State;
 import com.tc.util.sequence.ObjectIDSequence;
 
 import java.io.ByteArrayInputStream;
@@ -29,18 +30,38 @@ import java.util.HashMap;
 import junit.framework.TestCase;
 
 public class ClusterStateMessageTest extends TestCase {
-  public void testSerialization() throws Exception {
 
+  private ClusterState clusterState;
+
+  public void setUp() {
+    resetClusterState();
+  }
+
+  public void tearDown() {
+    clusterState = null;
+  }
+
+  private void resetClusterState() {
     Persistor persistor = new InMemoryPersistor();
     PersistentMapStore clusterStateStore = persistor.getClusterStateStore();
     ObjectIDSequence oidSequence = new PersistentManagedObjectStore(new TestManagedObjectPersistor(new HashMap()));
     ConnectionIDFactory connectionIdFactory = new ConnectionIDFactoryImpl(persistor.getClientStatePersistor());
     GlobalTransactionIDSequenceProvider gidSequenceProvider = new GlobalTransactionIDBatchRequestHandler(
                                                                                                          new TestMutableSequence());
-    ClusterState clusterState = new ClusterState(clusterStateStore, oidSequence, connectionIdFactory,
-                                                 gidSequenceProvider);
-    clusterState.setClusterID(UUID.getUUID().toString());
-    ClusterStateMessage csm = (ClusterStateMessage) ClusterStateMessageFactory.createClusterStateMessage(clusterState);
+    clusterState = new ClusterState(clusterStateStore, oidSequence, connectionIdFactory, gidSequenceProvider);
+    clusterState.setClusterID("foobar");
+  }
+
+  private void validate(ClusterStateMessage csm, ClusterStateMessage csm1) {
+    assertEquals(csm.getNextAvailableObjectID(), csm1.getNextAvailableObjectID());
+    assertEquals(csm.getNextAvailableGlobalTxnID(), csm1.getNextAvailableGlobalTxnID());
+    assertEquals(csm.getClusterID(), csm1.getClusterID());
+    assertEquals(csm.getConnectionID(), csm1.getConnectionID());
+    assertEquals(csm.getMessageID(), csm1.getMessageID());
+    assertEquals(csm.getType(), csm1.getType());
+  }
+
+  private ClusterStateMessage writeAndRead(ClusterStateMessage csm) throws Exception {
     ByteArrayOutputStream bo = new ByteArrayOutputStream();
     ObjectOutput oo = new ObjectOutputStream(bo);
     oo.writeObject(csm);
@@ -49,12 +70,97 @@ public class ClusterStateMessageTest extends TestCase {
     ObjectInput oi = new ObjectInputStream(bi);
     ClusterStateMessage csm1 = (ClusterStateMessage) oi.readObject();
     System.err.println("Read : " + csm1);
-
-    assertEquals(csm.getNextAvailableObjectID(), csm1.getNextAvailableGlobalTxnID());
-    assertEquals(csm.getNextAvailableGlobalTxnID(), csm1.getNextAvailableGlobalTxnID());
-    assertEquals(csm.getClusterID(), csm1.getClusterID());
-    assertEquals(csm.getConnectionID(), csm1.getConnectionID());
-    assertEquals(csm.getMessageID(), csm1.getMessageID());
-    assertEquals(csm.getType(), csm1.getType());
+    return csm1;
   }
+
+  private void addConnectionIDs() {
+    ConnectionID connectionID_1 = new ConnectionID(10, "foo");
+    ConnectionID connectionID_2 = new ConnectionID(11, "foo");
+    ConnectionID connectionID_3 = new ConnectionID(12, "goo");
+
+    clusterState.addNewConnection(connectionID_1);
+    clusterState.addNewConnection(connectionID_2);
+    clusterState.addNewConnection(connectionID_3);
+  }
+
+  private void modifyClusterState() {
+    addConnectionIDs();
+    ConnectionID connectionID = new ConnectionID(11, "foo");
+    clusterState.removeConnection(connectionID);
+
+    clusterState.setNextAvailableChannelID(13);
+    clusterState.setNextAvailableObjectID(222);
+    clusterState.setNextAvailableGlobalTransactionID(333);
+    clusterState.setCurrentState(new State("testing"));
+  }
+
+  public void testBasicSerialization() throws Exception {
+    modifyClusterState();
+
+    ClusterStateMessage csm = (ClusterStateMessage) ClusterStateMessageFactory.createClusterStateMessage(clusterState);
+    ClusterStateMessage csm1 = writeAndRead(csm);
+    validate(csm, csm1);
+
+    csm = (ClusterStateMessage) ClusterStateMessageFactory.createOKResponse(new ClusterStateMessage());
+    csm1 = writeAndRead(csm);
+    validate(csm, csm1);
+  }
+
+  public void testInitState() throws Exception {
+    ConnectionID connectionID = new ConnectionID(10, "foo");
+
+    // COMPLETE_STATE
+    modifyClusterState();
+    ClusterStateMessage csm = (ClusterStateMessage) ClusterStateMessageFactory.createClusterStateMessage(clusterState);
+    ClusterStateMessage csm1 = writeAndRead(csm);
+    validate(csm, csm1);
+    resetClusterState();
+    csm1.initState(clusterState);
+    csm = (ClusterStateMessage) ClusterStateMessageFactory.createClusterStateMessage(clusterState);
+    csm1 = writeAndRead(csm);
+    validate(csm, csm1);
+
+    // GLOBAL_TRANSACTION_ID
+    resetClusterState();
+    modifyClusterState();
+    csm = (ClusterStateMessage) ClusterStateMessageFactory.createNextAvailableGlobalTransactionIDMessage(clusterState);
+    csm1 = writeAndRead(csm);
+    validate(csm, csm1);
+    resetClusterState();
+    csm1.initState(clusterState);
+    csm = (ClusterStateMessage) ClusterStateMessageFactory.createNextAvailableGlobalTransactionIDMessage(clusterState);
+    csm1 = writeAndRead(csm);
+    validate(csm, csm1);
+
+    // OBJECT_ID
+    resetClusterState();
+    modifyClusterState();
+    csm = (ClusterStateMessage) ClusterStateMessageFactory.createNextAvailableObjectIDMessage(clusterState);
+    csm1 = writeAndRead(csm);
+    validate(csm, csm1);
+    resetClusterState();
+    csm1.initState(clusterState);
+    csm = (ClusterStateMessage) ClusterStateMessageFactory.createNextAvailableObjectIDMessage(clusterState);
+    csm1 = writeAndRead(csm);
+    validate(csm, csm1);
+
+    // NEW_CONNECTION_CREATED
+    resetClusterState();
+    csm = (ClusterStateMessage) ClusterStateMessageFactory.createNewConnectionCreatedMessage(connectionID);
+    csm1 = writeAndRead(csm);
+    validate(csm, csm1);
+    resetClusterState();
+    csm1.initState(clusterState);
+    assertEquals(connectionID, clusterState.getAllConnections().iterator().next());
+
+    // CONNECTION_DESTROYED
+    resetClusterState();
+    csm = (ClusterStateMessage) ClusterStateMessageFactory.createConnectionDestroyedMessage(connectionID);
+    csm1 = writeAndRead(csm);
+    validate(csm, csm1);
+    clusterState.addNewConnection(connectionID);
+    csm1.initState(clusterState);
+    assertEquals(0, clusterState.getAllConnections().size());
+  }
+
 }
