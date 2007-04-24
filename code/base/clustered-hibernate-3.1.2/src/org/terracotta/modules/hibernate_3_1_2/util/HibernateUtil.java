f3eb1ceb5700a2391df4d501781b4a45d20f153d
Antonio Si <asi@terracottatech.com>
2007-04-24 17:16:09 +0000

    Add debug statement.
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@2681 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/clustered-hibernate-3.1.2/src/org/terracotta/modules/hibernate_3_1_2/util/HibernateUtil.java
+++ code/base/clustered-hibernate-3.1.2/src/org/terracotta/modules/hibernate_3_1_2/util/HibernateUtil.java
@@ -1,5 +1,6 @@
 /*
- * All content copyright (c) 2003-2007 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ * All content copyright (c) 2003-2007 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
  */
 package org.terracotta.modules.hibernate_3_1_2.util;
 
@@ -11,18 +12,17 @@ public class HibernateUtil {
   private static final SessionFactory sessionFactory;
 
   static {
-      try {
-          // Create the SessionFactory from hibernate.cfg.xml
-          sessionFactory = new Configuration().configure().buildSessionFactory();
-      } catch (Throwable ex) {
-          // Make sure you log the exception, as it might be swallowed
-          System.err.println("Initial SessionFactory creation failed." + ex);
-          throw new ExceptionInInitializerError(ex);
-      }
+    try {
+      // Create the SessionFactory from hibernate.cfg.xml
+      sessionFactory = new Configuration().configure().buildSessionFactory();
+    } catch (Throwable ex) {
+      ex.printStackTrace(System.err);
+      throw new ExceptionInInitializerError(ex);
+    }
   }
 
   public static SessionFactory getSessionFactory() {
-      return sessionFactory;
+    return sessionFactory;
   }
 
 }
