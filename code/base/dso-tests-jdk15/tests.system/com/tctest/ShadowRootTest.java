118d5e3e530c18403a18dcdb5449abb54eae2895
Hung Huynh <hhuynh@gmail.com>
2007-04-24 22:06:01 +0000

    test for DEV-641: shadowed root
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@2689 7fc7bbf3-cf45-46d4-be06-341739edd864

---
+++ code/base/dso-tests-jdk15/tests.system/com/tctest/ShadowRootTest.java
@@ -0,0 +1,23 @@
+/*
+ * All content copyright (c) 2003-2006 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ */
+package com.tctest;
+
+public class ShadowRootTest extends TransparentTestBase {
+
+  private static final int NODE_COUNT = 1;
+
+  public ShadowRootTest() {
+    this.disableAllUntil("2007-05-01");    
+  }
+  
+  public void setUp() throws Exception {
+    super.setUp();
+    getTransparentAppConfig().setClientCount(NODE_COUNT).setIntensity(1);
+    initializeTestRunner();
+  }
+
+  protected Class getApplicationClass() {
+    return ShadowRootTestApp.class;
+  }
+}
