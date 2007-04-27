d68bfe5cee9ad3ddb576abd2a4285ac02603371b
Hung Huynh <hhuynh@gmail.com>
2007-04-27 17:38:49 +0000

    dummy webclient for testing
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@2743 7fc7bbf3-cf45-46d4-be06-341739edd864

---
+++ code/base/dso-container-tests/tests.base/com/tc/test/server/util/WebClient.java
@@ -0,0 +1,94 @@
+/*
+ * All content copyright (c) 2003-2007 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
+ */
+package com.tc.test.server.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Dummy webclient to test out Http no response exception
+ * This is to be used only in test framework since it sends
+ * along cookies (got from first response) every time regardless
+ * expires, domain, path properties 
+ *
+ * @author hhuynh
+ */
+public class WebClient {
+  private volatile int serial;
+  private Map          cookies = new HashMap();
+
+  public String getResponseAsString(URL url) throws IOException {
+    String modifiedUrl = url.toString();
+    if (modifiedUrl.indexOf("?") > 0) {
+      modifiedUrl += "&_wcserial=" + serial++;
+    } else {
+      modifiedUrl += "?&_wcserial=" + serial++;
+    }
+    System.out.println("[WebClient]: " + modifiedUrl);
+    URL myUrl = new URL(modifiedUrl);
+    HttpURLConnection http = (HttpURLConnection) myUrl.openConnection();
+    http.setRequestProperty("Cookie", getCookies());
+    http.connect();
+    if (http.getResponseCode() != HttpURLConnection.HTTP_OK) {
+      throw new IOException("Response code is not OK: " + http.getResponseMessage());
+    }
+    extractCookies(http);
+    BufferedReader in = new BufferedReader(new InputStreamReader(http.getInputStream()));
+    StringBuffer buffer = new StringBuffer(100);
+    String line;
+    try {
+      while ((line = in.readLine()) != null) {
+        buffer.append(line).append("\n");
+      }
+    } finally {
+      try {
+        if (in != null) in.close();
+      } catch (Exception ignored) { // nop
+      }
+    }
+
+    return buffer.toString().trim();
+  }
+  
+  public int getResponseAsInt(URL url) throws IOException {
+    String response = getResponseAsString(url);
+    return Integer.parseInt(response);
+  }
+
+  private String getCookies() {
+    StringBuffer cookiesString = new StringBuffer(100);
+    for (Iterator it = cookies.entrySet().iterator(); it.hasNext();) {
+      Map.Entry e = (Map.Entry) it.next();
+      cookiesString.append(e.getKey().toString()).append("=").append(e.getValue().toString());
+      if (it.hasNext()) {
+        cookiesString.append("; ");
+      }
+    }
+    return cookiesString.toString();
+  }
+
+  private void extractCookies(URLConnection urlConnect) {
+    Map headerFields = urlConnect.getHeaderFields();
+    List cookieList = (List) headerFields.get("Set-Cookie");
+    if (cookieList != null) {
+      for (Iterator it = cookieList.iterator(); it.hasNext();) {
+        String cookie = (String) it.next();
+        // parse first name=value, ignore the rest
+        cookie = cookie.substring(0, cookie.indexOf(";"));
+        String[] pair = cookie.split("=", 2);
+        cookies.put(pair[0], pair[1]);
+      }
+    }
+  }
+
+}
