ae767cc14a4923022de07be704fbb492fb96f5b7
etsai <etsai@terracottatech.com>
2007-04-28 03:59:22 +0000

    MutateValidateLinkedHashMapTest init checkin
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@2756 7fc7bbf3-cf45-46d4-be06-341739edd864

---
+++ code/base/dso-crash-tests/tests.system/com/tctest/MutateValidateLinkedHashMapTest.java
@@ -0,0 +1,41 @@
+/*
+ * All content copyright (c) 2003-2006 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
+ */
+package com.tctest;
+
+import com.tc.test.activepassive.ActivePassiveCrashMode;
+import com.tc.test.activepassive.ActivePassivePersistenceMode;
+import com.tc.test.activepassive.ActivePassiveSharedDataMode;
+import com.tc.test.activepassive.ActivePassiveTestSetupManager;
+import com.tctest.runner.TransparentAppConfig;
+
+public class MutateValidateLinkedHashMapTest extends TransparentTestBase {
+
+  public static final int      MUTATOR_NODE_COUNT      = 2;
+  public static final int      VALIDATOR_NODE_COUNT    = 1;
+  public static final int      APP_INSTANCE_PER_NODE   = 2;
+  private static final boolean IS_MUTATE_VALIDATE_TEST = true;
+
+  public void doSetUp(TransparentTestIface t) throws Exception {
+    TransparentAppConfig tac = t.getTransparentAppConfig();
+    tac.setClientCount(MUTATOR_NODE_COUNT).setIntensity(1).setValidatorCount(VALIDATOR_NODE_COUNT)
+        .setApplicationInstancePerClientCount(APP_INSTANCE_PER_NODE);
+    t.initializeTestRunner(IS_MUTATE_VALIDATE_TEST);
+  }
+
+  protected Class getApplicationClass() {
+    return MutateValidateLinkedHashMapTestApp.class;
+  }
+
+  public void setupActivePassiveTest(ActivePassiveTestSetupManager setupManager) {
+    setupManager.setServerCount(2);
+    setupManager.setServerCrashMode(ActivePassiveCrashMode.MUTATE_VALIDATE);
+    setupManager.setServerShareDataMode(ActivePassiveSharedDataMode.NETWORK);
+    setupManager.setServerPersistenceMode(ActivePassivePersistenceMode.TEMPORARY_SWAP_ONLY);
+  }
+
+  protected boolean canRunActivePassive() {
+    return true;
+  }
+}
