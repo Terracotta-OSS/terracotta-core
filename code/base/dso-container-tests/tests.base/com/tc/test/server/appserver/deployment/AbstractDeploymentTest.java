c7228be7dba2296af20600c3f77ef49d18bf8f1c
Hung Huynh <hhuynh@gmail.com>
2007-05-04 02:46:24 +0000

    force-kill any strayed appservers
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@2881 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-container-tests/tests.base/com/tc/test/server/appserver/deployment/AbstractDeploymentTest.java
+++ code/base/dso-container-tests/tests.base/com/tc/test/server/appserver/deployment/AbstractDeploymentTest.java
@@ -6,6 +6,7 @@ package com.tc.test.server.appserver.deployment;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import com.tc.process.HeartBeatService;
 import com.tc.test.TCTestCase;
 import com.tc.test.TestConfigObject;
 import com.tc.test.server.tcconfig.StandardTerracottaAppServerConfig;
@@ -67,6 +68,7 @@ public abstract class AbstractDeploymentTest extends TCTestCase {
 
   protected void tearDown() throws Exception {
     ServerManagerUtil.stop(serverManager);
+    HeartBeatService.sendKillSignalToChildren();
     super.tearDown();
   }
 
