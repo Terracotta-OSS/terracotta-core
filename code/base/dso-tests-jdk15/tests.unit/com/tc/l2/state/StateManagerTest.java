d3a9c2736238c9120fa1315737a2e459f1bbdaf8
etsai <etsai@terracottatech.com>
2007-05-10 18:05:43 +0000

    longer sleep before doing verification for StateManagerTest
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@3000 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-tests-jdk15/tests.unit/com/tc/l2/state/StateManagerTest.java
+++ code/base/dso-tests-jdk15/tests.unit/com/tc/l2/state/StateManagerTest.java
@@ -195,7 +195,7 @@ public class StateManagerTest extends TCTestCase {
       elections[i].join();
     }
     
-    Thread.sleep(100);
+    Thread.sleep(500);
     // verification
     int activeCount = 0;
     for(int i=0; i < nodes; ++i) {
@@ -245,7 +245,7 @@ public class StateManagerTest extends TCTestCase {
       elections[i].join();
     }
     
-    Thread.sleep(100);
+    Thread.sleep(500);
     // verification
     int activeCount = 0;
     for(int i=0; i < nodes; ++i) {
@@ -338,7 +338,7 @@ public class StateManagerTest extends TCTestCase {
     for(int i=1; i < nodes; ++i) {
       reElectThreads[i].join();
     }
-    Thread.sleep(100);
+    Thread.sleep(500);
  
     //verify
     activeCount = 0;
