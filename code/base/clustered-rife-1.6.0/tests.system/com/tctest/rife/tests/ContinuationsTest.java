72220ba9e4a420b9cbe27d3fc750558fe504f7da
Jason Voegele <jason@jvoegele.com>
2007-05-15 16:57:07 +0000

    Disabled all tests in com.tctest.rife.tests.ContinuationsTest.
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@3078 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/clustered-rife-1.6.0/tests.system/com/tctest/rife/tests/ContinuationsTest.java
+++ code/base/clustered-rife-1.6.0/tests.system/com/tctest/rife/tests/ContinuationsTest.java
@@ -22,8 +22,7 @@ import com.uwyn.rife.tools.StringUtils;
 
 public class ContinuationsTest extends AbstractTwoServerDeploymentTest {
     public ContinuationsTest() {
-        disableTestUntil("testAllTypes", "2007-05-28");
-        disableTestUntil("testSimpleInterface", "2007-05-28");
+        disableAllUntil("2007-05-28");
     }
 
 	public static Test suite() {
