1fef72e2ff108167cb1f38fb0a23b72840fb93cd
Juris Galang <jgalang@terracottatech.com>
2007-05-17 22:09:47 +0000

    Fix: removed reference to 'distribute' directory.
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@3154 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/buildscripts/postscripts/make-enterprise.rb
+++ code/base/buildscripts/postscripts/make-enterprise.rb
@@ -9,7 +9,7 @@ class BaseCodeTerracottaBuilder <  TerracottaBuilder
   protected
   def postscript(ant, build_environment, product_directory, *args)
     ent_top = FilePath.new(@basedir, '..', '..', '..').canonicalize
-    notices_dir = FilePath.new(ent_top, 'kits', 'source', 'docs', 'distribute', 'notices').canonicalize
+    notices_dir = FilePath.new(ent_top, 'kits', 'source', 'docs', 'notices').canonicalize
     ant.copy(:todir => product_directory.to_s, :overwrite => true) {
       ant.fileset(:dir => notices_dir.to_s, :includes => '*.txt, *.pdf')
     }
