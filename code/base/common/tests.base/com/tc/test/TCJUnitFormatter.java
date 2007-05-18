76d047f0d006433e5569ff9e449e26cb5ed5d748
Hung Huynh <hhuynh@gmail.com>
2007-05-18 03:36:24 +0000

    attempt to fix class not found error in junit formatter
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@3159 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/common/tests.base/com/tc/test/TCJUnitFormatter.java
+++ code/base/common/tests.base/com/tc/test/TCJUnitFormatter.java
@@ -8,8 +8,6 @@ import org.apache.tools.ant.taskdefs.optional.junit.JUnitResultFormatter;
 import org.apache.tools.ant.taskdefs.optional.junit.JUnitTest;
 
 import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -23,26 +21,6 @@ import junit.framework.Test;
 public class TCJUnitFormatter implements JUnitResultFormatter {
 
   private final List  currentExceptions;
-  private static class TestException {
-    private final Test      theTest;
-    private final Throwable exception;
-
-    public TestException(Test theTest, Throwable exception) {
-      this.theTest = theTest;
-      this.exception = exception;
-    }
-
-    public String toString() {
-      StringWriter sw = new StringWriter();
-      PrintWriter pw = new PrintWriter(sw);
-
-      pw.println(this.theTest + " FAILED:");
-      this.exception.printStackTrace(pw);
-
-      pw.flush();
-      return sw.toString();
-    }
-  }
 
   public TCJUnitFormatter() {
     this.currentExceptions = new ArrayList();
