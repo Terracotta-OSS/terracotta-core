a5c6e0bafe1dc7507f02a941084651c8a1866238
Nathaniel Harward <nharward@terracottatech.com>
2007-06-06 02:17:09 +0000

    Merging fix for DEV-709 from the 2.4 branch
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@3497 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/clustered-websphere-6.1.0.7/src/org/terracotta/modules/websphere_6_1/WebAppFilterManagerClassAdapter.java
+++ code/base/clustered-websphere-6.1.0.7/src/org/terracotta/modules/websphere_6_1/WebAppFilterManagerClassAdapter.java
@@ -38,7 +38,9 @@ public class WebAppFilterManagerClassAdapter extends ClassAdapter implements Opc
   /**
    * <pre>
    * private void __tc_loadDSOFilterInfo() {
-   *   if (ClassProcessorHelper.isDSOSessions(webAppConfig.getContextRoot())) {
+   *   // Remove leading slashes
+   *   final String applicationName = webAppConfig.getContextRoot().replaceAll(&quot;&circ;/+&quot;, &quot;&quot;);
+   *   if (ClassProcessorHelper.isDSOSessions(applicationName)) {
    *     FilterConfig dsoFilterConfig = new FilterConfig(&quot;TerracottaSessionFilterConfig&quot;);
    *     dsoFilterConfig.setName(&quot;TerracottaSessionFilter&quot;);
    *     dsoFilterConfig.addInitParameter(&quot;app-server&quot;, &quot;IBM-Websphere&quot;);
@@ -78,51 +80,59 @@ public class WebAppFilterManagerClassAdapter extends ClassAdapter implements Opc
                       "Lcom/ibm/ws/webcontainer/webapp/WebAppConfiguration;");
     mv.visitMethodInsn(INVOKEVIRTUAL, "com/ibm/ws/webcontainer/webapp/WebAppConfiguration", "getContextRoot",
                        "()Ljava/lang/String;");
+    mv.visitLdcInsn("^/+");
+    mv.visitLdcInsn("");
+    mv.visitMethodInsn(INVOKEVIRTUAL, "java/lang/String", "replaceAll",
+                       "(Ljava/lang/String;Ljava/lang/String;)Ljava/lang/String;");
+    mv.visitVarInsn(ASTORE, 1);
+    Label l3 = new Label();
+    mv.visitLabel(l3);
+    mv.visitVarInsn(ALOAD, 1);
     mv.visitMethodInsn(INVOKESTATIC, "com/tc/object/bytecode/hook/impl/ClassProcessorHelper", "isDSOSessions",
                        "(Ljava/lang/String;)Z");
-    Label l3 = new Label();
-    mv.visitJumpInsn(IFEQ, l3);
     Label l4 = new Label();
-    mv.visitLabel(l4);
+    mv.visitJumpInsn(IFEQ, l4);
+    Label l5 = new Label();
+    mv.visitLabel(l5);
     mv.visitTypeInsn(NEW, "com/ibm/ws/webcontainer/filter/FilterConfig");
     mv.visitInsn(DUP);
     mv.visitLdcInsn("TerracottaSessionFilterConfig");
     mv.visitMethodInsn(INVOKESPECIAL, "com/ibm/ws/webcontainer/filter/FilterConfig", "<init>", "(Ljava/lang/String;)V");
-    mv.visitVarInsn(ASTORE, 1);
-    Label l5 = new Label();
-    mv.visitLabel(l5);
-    mv.visitVarInsn(ALOAD, 1);
+    mv.visitVarInsn(ASTORE, 2);
+    Label l6 = new Label();
+    mv.visitLabel(l6);
+    mv.visitVarInsn(ALOAD, 2);
     mv.visitLdcInsn("TerracottaSessionFilter");
     mv
         .visitMethodInsn(INVOKEVIRTUAL, "com/ibm/ws/webcontainer/filter/FilterConfig", "setName",
                          "(Ljava/lang/String;)V");
-    Label l6 = new Label();
-    mv.visitLabel(l6);
-    mv.visitVarInsn(ALOAD, 1);
+    Label l7 = new Label();
+    mv.visitLabel(l7);
+    mv.visitVarInsn(ALOAD, 2);
     mv.visitLdcInsn("app-server");
     mv.visitLdcInsn("IBM-Websphere");
     mv.visitMethodInsn(INVOKEVIRTUAL, "com/ibm/ws/webcontainer/filter/FilterConfig", "addInitParameter",
                        "(Ljava/lang/String;Ljava/lang/String;)V");
-    Label l7 = new Label();
-    mv.visitLabel(l7);
+    Label l8 = new Label();
+    mv.visitLabel(l8);
     mv.visitTypeInsn(NEW, "java/net/URLClassLoader");
     mv.visitInsn(DUP);
     mv.visitInsn(ICONST_0);
     mv.visitTypeInsn(ANEWARRAY, "java/net/URL");
-    Label l8 = new Label();
-    mv.visitLabel(l8);
-    mv.visitMethodInsn(INVOKESTATIC, "com/ibm/ws/webcontainer/util/ThreadContextHelper", "getContextClassLoader",
-                       "()Ljava/lang/ClassLoader;");
     Label l9 = new Label();
     mv.visitLabel(l9);
-    mv.visitMethodInsn(INVOKESPECIAL, "java/net/URLClassLoader", "<init>", "([Ljava/net/URL;Ljava/lang/ClassLoader;)V");
-    mv.visitVarInsn(ASTORE, 2);
+    mv.visitMethodInsn(INVOKESTATIC, "com/ibm/ws/webcontainer/util/ThreadContextHelper", "getContextClassLoader",
+                       "()Ljava/lang/ClassLoader;");
     Label l10 = new Label();
     mv.visitLabel(l10);
-    mv.visitVarInsn(ALOAD, 2);
-    mv.visitTypeInsn(CHECKCAST, "com/tc/object/loaders/NamedClassLoader");
+    mv.visitMethodInsn(INVOKESPECIAL, "java/net/URLClassLoader", "<init>", "([Ljava/net/URL;Ljava/lang/ClassLoader;)V");
+    mv.visitVarInsn(ASTORE, 3);
     Label l11 = new Label();
     mv.visitLabel(l11);
+    mv.visitVarInsn(ALOAD, 3);
+    mv.visitTypeInsn(CHECKCAST, "com/tc/object/loaders/NamedClassLoader");
+    Label l12 = new Label();
+    mv.visitLabel(l12);
     mv.visitLdcInsn("Websphere.");
     mv.visitTypeInsn(NEW, "java/lang/StringBuffer");
     mv.visitInsn(DUP);
@@ -136,36 +146,36 @@ public class WebAppFilterManagerClassAdapter extends ClassAdapter implements Opc
     mv.visitMethodInsn(INVOKEVIRTUAL, "java/lang/StringBuffer", "append",
                        "(Ljava/lang/String;)Ljava/lang/StringBuffer;");
     mv.visitMethodInsn(INVOKEVIRTUAL, "java/lang/StringBuffer", "toString", "()Ljava/lang/String;");
-    Label l12 = new Label();
-    mv.visitLabel(l12);
+    Label l13 = new Label();
+    mv.visitLabel(l13);
     mv.visitMethodInsn(INVOKESTATIC, "com/tc/object/loaders/Namespace", "createLoaderName",
                        "(Ljava/lang/String;Ljava/lang/String;)Ljava/lang/String;");
     mv.visitMethodInsn(INVOKEINTERFACE, "com/tc/object/loaders/NamedClassLoader", "__tc_setClassLoaderName",
                        "(Ljava/lang/String;)V");
-    Label l13 = new Label();
-    mv.visitLabel(l13);
-    mv.visitVarInsn(ALOAD, 2);
+    Label l14 = new Label();
+    mv.visitLabel(l14);
+    mv.visitVarInsn(ALOAD, 3);
     mv.visitTypeInsn(CHECKCAST, "com/tc/object/loaders/NamedClassLoader");
     mv.visitMethodInsn(INVOKESTATIC, "com/tc/object/bytecode/hook/impl/ClassProcessorHelper", "registerGlobalLoader",
                        "(Lcom/tc/object/loaders/NamedClassLoader;)V");
     mv.visitLabel(l0);
-    mv.visitVarInsn(ALOAD, 2);
+    mv.visitVarInsn(ALOAD, 3);
     mv.visitMethodInsn(INVOKESTATIC, "com/tc/object/bytecode/hook/impl/SessionsHelper", "injectClasses",
                        "(Ljava/lang/ClassLoader;)V");
-    Label l14 = new Label();
-    mv.visitJumpInsn(GOTO, l14);
-    mv.visitLabel(l1);
-    mv.visitVarInsn(ASTORE, 3);
     Label l15 = new Label();
-    mv.visitLabel(l15);
+    mv.visitJumpInsn(GOTO, l15);
+    mv.visitLabel(l1);
+    mv.visitVarInsn(ASTORE, 4);
+    Label l16 = new Label();
+    mv.visitLabel(l16);
     mv.visitTypeInsn(NEW, "java/lang/RuntimeException");
     mv.visitInsn(DUP);
     mv.visitTypeInsn(NEW, "java/lang/StringBuffer");
     mv.visitInsn(DUP);
     mv.visitLdcInsn("Unable to inject Terracotta session filter classes into application ");
     mv.visitMethodInsn(INVOKESPECIAL, "java/lang/StringBuffer", "<init>", "(Ljava/lang/String;)V");
-    Label l16 = new Label();
-    mv.visitLabel(l16);
+    Label l17 = new Label();
+    mv.visitLabel(l17);
     mv.visitVarInsn(ALOAD, 0);
     mv.visitFieldInsn(GETFIELD, "com/ibm/ws/webcontainer/filter/WebAppFilterManager", "webAppConfig",
                       "Lcom/ibm/ws/webcontainer/webapp/WebAppConfiguration;");
@@ -174,37 +184,37 @@ public class WebAppFilterManagerClassAdapter extends ClassAdapter implements Opc
     mv.visitMethodInsn(INVOKEVIRTUAL, "java/lang/StringBuffer", "append",
                        "(Ljava/lang/String;)Ljava/lang/StringBuffer;");
     mv.visitMethodInsn(INVOKEVIRTUAL, "java/lang/StringBuffer", "toString", "()Ljava/lang/String;");
-    mv.visitVarInsn(ALOAD, 3);
-    Label l17 = new Label();
-    mv.visitLabel(l17);
+    mv.visitVarInsn(ALOAD, 4);
+    Label l18 = new Label();
+    mv.visitLabel(l18);
     mv.visitMethodInsn(INVOKESPECIAL, "java/lang/RuntimeException", "<init>",
                        "(Ljava/lang/String;Ljava/lang/Throwable;)V");
     mv.visitInsn(ATHROW);
-    mv.visitLabel(l14);
-    mv.visitVarInsn(ALOAD, 1);
+    mv.visitLabel(l15);
+    mv.visitVarInsn(ALOAD, 2);
     mv.visitLdcInsn("com.terracotta.session.SessionFilter");
     mv.visitMethodInsn(INVOKEVIRTUAL, "com/ibm/ws/webcontainer/filter/FilterConfig", "setFilterClassName",
                        "(Ljava/lang/String;)V");
-    Label l18 = new Label();
-    mv.visitLabel(l18);
-    mv.visitVarInsn(ALOAD, 1);
-    mv.visitVarInsn(ALOAD, 2);
-    mv.visitMethodInsn(INVOKEVIRTUAL, "com/ibm/ws/webcontainer/filter/FilterConfig", "setFilterClassLoader",
-                       "(Ljava/lang/ClassLoader;)V");
     Label l19 = new Label();
     mv.visitLabel(l19);
+    mv.visitVarInsn(ALOAD, 2);
+    mv.visitVarInsn(ALOAD, 3);
+    mv.visitMethodInsn(INVOKEVIRTUAL, "com/ibm/ws/webcontainer/filter/FilterConfig", "setFilterClassLoader",
+                       "(Ljava/lang/ClassLoader;)V");
+    Label l20 = new Label();
+    mv.visitLabel(l20);
     mv.visitVarInsn(ALOAD, 0);
     mv.visitFieldInsn(GETFIELD, "com/ibm/ws/webcontainer/filter/WebAppFilterManager", "webAppConfig",
                       "Lcom/ibm/ws/webcontainer/webapp/WebAppConfiguration;");
-    mv.visitVarInsn(ALOAD, 1);
+    mv.visitVarInsn(ALOAD, 2);
     mv.visitMethodInsn(INVOKEVIRTUAL, "com/ibm/ws/webcontainer/webapp/WebAppConfiguration", "addFilterInfo",
                        "(Lcom/ibm/wsspi/webcontainer/filter/IFilterConfig;)V");
-    Label l20 = new Label();
-    mv.visitLabel(l20);
+    Label l21 = new Label();
+    mv.visitLabel(l21);
     mv.visitTypeInsn(NEW, "com/ibm/ws/webcontainer/filter/FilterMapping");
     mv.visitInsn(DUP);
     mv.visitLdcInsn("/*");
-    mv.visitVarInsn(ALOAD, 1);
+    mv.visitVarInsn(ALOAD, 2);
     mv.visitInsn(ACONST_NULL);
     mv
         .visitMethodInsn(
@@ -212,23 +222,24 @@ public class WebAppFilterManagerClassAdapter extends ClassAdapter implements Opc
                          "com/ibm/ws/webcontainer/filter/FilterMapping",
                          "<init>",
                          "(Ljava/lang/String;Lcom/ibm/wsspi/webcontainer/filter/IFilterConfig;Lcom/ibm/wsspi/webcontainer/servlet/IServletConfig;)V");
-    mv.visitVarInsn(ASTORE, 3);
-    Label l21 = new Label();
-    mv.visitLabel(l21);
-    mv.visitVarInsn(ALOAD, 0);
-    mv.visitVarInsn(ALOAD, 3);
-    mv.visitMethodInsn(INVOKESPECIAL, "com/ibm/ws/webcontainer/filter/WebAppFilterManager", "addFilterMapping",
-                       "(Lcom/ibm/ws/webcontainer/filter/FilterMapping;)V");
-    mv.visitLabel(l3);
-    mv.visitInsn(RETURN);
+    mv.visitVarInsn(ASTORE, 4);
     Label l22 = new Label();
     mv.visitLabel(l22);
-    mv.visitLocalVariable("this", "Lcom/ibm/ws/webcontainer/filter/WebAppFilterManager;", null, l2, l22, 0);
-    mv.visitLocalVariable("dsoFilterConfig", "Lcom/ibm/ws/webcontainer/filter/FilterConfig;", null, l5, l3, 1);
-    mv.visitLocalVariable("terracottaDSOSessionsLoader", "Ljava/net/URLClassLoader;", null, l10, l3, 2);
-    mv.visitLocalVariable("e", "Ljava/lang/Exception;", null, l15, l14, 3);
-    mv.visitLocalVariable("dsoFilterMapping", "Lcom/ibm/ws/webcontainer/filter/FilterMapping;", null, l21, l3, 3);
-    mv.visitMaxs(5, 4);
+    mv.visitVarInsn(ALOAD, 0);
+    mv.visitVarInsn(ALOAD, 4);
+    mv.visitMethodInsn(INVOKESPECIAL, "com/ibm/ws/webcontainer/filter/WebAppFilterManager", "addFilterMapping",
+                       "(Lcom/ibm/ws/webcontainer/filter/FilterMapping;)V");
+    mv.visitLabel(l4);
+    mv.visitInsn(RETURN);
+    Label l23 = new Label();
+    mv.visitLabel(l23);
+    mv.visitLocalVariable("this", "Lcom/ibm/ws/webcontainer/filter/WebAppFilterManager;", null, l2, l23, 0);
+    mv.visitLocalVariable("applicationName", "Ljava/lang/String;", null, l3, l23, 1);
+    mv.visitLocalVariable("dsoFilterConfig", "Lcom/ibm/ws/webcontainer/filter/FilterConfig;", null, l6, l4, 2);
+    mv.visitLocalVariable("terracottaDSOSessionsLoader", "Ljava/net/URLClassLoader;", null, l11, l4, 3);
+    mv.visitLocalVariable("e", "Ljava/lang/Exception;", null, l16, l15, 4);
+    mv.visitLocalVariable("dsoFilterMapping", "Lcom/ibm/ws/webcontainer/filter/FilterMapping;", null, l22, l4, 4);
+    mv.visitMaxs(5, 5);
     mv.visitEnd();
   }
 
