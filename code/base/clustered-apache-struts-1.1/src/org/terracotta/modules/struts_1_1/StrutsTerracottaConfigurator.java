0ace10e6189f7fae3654b37e96f0e52baf0c8cd7
Nathaniel Harward <nharward@terracottatech.com>
2007-06-08 01:33:26 +0000

    Update for Struts 1.1 config module.  svn merge -c 3566 https://svn.terracotta.org/repo/tc/dso/branches/2.4
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@3567 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/clustered-apache-struts-1.1/src/org/terracotta/modules/struts_1_1/StrutsTerracottaConfigurator.java
+++ code/base/clustered-apache-struts-1.1/src/org/terracotta/modules/struts_1_1/StrutsTerracottaConfigurator.java
@@ -4,48 +4,16 @@
  */
 package org.terracotta.modules.struts_1_1;
 
-import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.BundleException;
-import org.osgi.framework.ServiceReference;
+import org.terracotta.modules.configuration.TerracottaConfiguratorModule;
 
 import com.tc.object.config.StandardDSOClientConfigHelper;
-import com.tc.object.config.TransparencyClassSpec;
 
-public final class StrutsTerracottaConfigurator implements BundleActivator {
+public final class StrutsTerracottaConfigurator extends TerracottaConfiguratorModule {
 
-  private static final String INCLUDE_TAG_CLASSNAME = "org.apache.struts.taglib.bean.IncludeTag";
-
-  public void start(final BundleContext context) throws Exception {
-    final ServiceReference configHelperRef = context.getServiceReference(StandardDSOClientConfigHelper.class.getName());
-    if (configHelperRef != null) {
-      final StandardDSOClientConfigHelper configHelper = (StandardDSOClientConfigHelper) context
-          .getService(configHelperRef);
-      addStrutsInstrumentation(configHelper);
-      context.ungetService(configHelperRef);
-    } else {
-      throw new BundleException("Expected the " + StandardDSOClientConfigHelper.class.getName()
-          + " service to be registered, was unable to find it");
-    }
-  }
-
-  public void stop(final BundleContext context) throws Exception {
-    // Ignore this, we don't need to stop anything
-  }
-
-  private void addStrutsInstrumentation(final StandardDSOClientConfigHelper configHelper) {
-    // Hack for honoring transient in Struts action classes
-    TransparencyClassSpec spec = configHelper.getOrCreateSpec("org.apache.struts.action.ActionForm");
-    spec.setHonorTransient(true);
-    spec = configHelper.getOrCreateSpec("org.apache.struts.action.ActionMappings");
-    spec.setHonorTransient(true);
-    spec = configHelper.getOrCreateSpec("org.apache.struts.action.ActionServletWrapper");
-    spec.setHonorTransient(true);
-    spec = configHelper.getOrCreateSpec("org.apache.struts.action.DynaActionFormClass");
-    spec.setHonorTransient(true);
-
-    // Hack for Struts <bean:include> tag
-    configHelper.addCustomAdapter(INCLUDE_TAG_CLASSNAME, new IncludeTagAdapter());
+  protected void addInstrumentation(BundleContext context, StandardDSOClientConfigHelper configHelper) {
+    configHelper.addCustomAdapter("org.apache.struts.taglib.bean.IncludeTag", new IncludeTagAdapter());
+    super.addInstrumentation(context, configHelper);
   }
 
 }
