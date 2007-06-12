f045f1323bbab67cc4c3e66f44d49067e3bbe2dd
etsai <etsai@terracottatech.com>
2007-06-12 04:18:12 +0000

    test case for large transaction, CDV-253
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@3626 7fc7bbf3-cf45-46d4-be06-341739edd864

---
+++ code/base/dso-crash-tests/tests.system/com/tctest/HashMapBatchTxnTest.java
@@ -0,0 +1,27 @@
+/*
+ * All content copyright (c) 2003-2006 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
+ */
+package com.tctest;
+
+/*
+ * Test case for CDV-253
+ */
+
+public class HashMapBatchTxnTest extends TransparentTestBase {
+
+  private static final int NODE_COUNT = 4;
+  
+  public HashMapBatchTxnTest() {
+  }
+
+  public void doSetUp(TransparentTestIface t) throws Exception {
+    t.getTransparentAppConfig().setClientCount(NODE_COUNT);
+    t.initializeTestRunner();
+  }
+
+  protected Class getApplicationClass() {
+    return HashMapBatchTxnTestApp.class;
+  }
+ 
+}
