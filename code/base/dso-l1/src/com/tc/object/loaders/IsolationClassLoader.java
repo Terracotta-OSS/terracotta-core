482aeea8f0c7f1fbc6d01c42aeace2f67933be8b
Tim Eck <timeck@gmail.com>
2007-06-28 22:17:33 +0000

    merge 4092 from 2.4 branch
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@4093 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-l1/src/com/tc/object/loaders/IsolationClassLoader.java
+++ code/base/dso-l1/src/com/tc/object/loaders/IsolationClassLoader.java
@@ -4,6 +4,11 @@
  */
 package com.tc.object.loaders;
 
+import org.apache.commons.io.IOUtils;
+
+import com.tc.asm.ClassReader;
+import com.tc.asm.ClassVisitor;
+import com.tc.asm.ClassWriter;
 import com.tc.object.ClientObjectManager;
 import com.tc.object.bytecode.Manager;
 import com.tc.object.bytecode.ManagerImpl;
@@ -13,9 +18,13 @@ import com.tc.object.bytecode.hook.impl.PreparedComponentsFromL2Connection;
 import com.tc.object.config.DSOClientConfigHelper;
 import com.tc.object.tx.ClientTransactionManager;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Constructor;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * DSO Class loader for internal testing. The main purpose of this loader is to force test classes to be be defined
@@ -26,8 +35,9 @@ public class IsolationClassLoader extends URLClassLoader implements NamedClassLo
 
   private final Manager               manager;
   private final DSOClientConfigHelper config;
-  private final HashMap               onLoadErrors;
+  private final Map                   onLoadErrors;
   private final StandardClassProvider classProvider;
+  private final Map                   adapters      = new HashMap();
 
   public IsolationClassLoader(DSOClientConfigHelper config, PreparedComponentsFromL2Connection connectionComponents) {
     this(config, true, null, null, connectionComponents);
@@ -71,16 +81,56 @@ public class IsolationClassLoader extends URLClassLoader implements NamedClassLo
   public Class loadClass(String name) throws ClassNotFoundException {
     throwIfNeeded(name);
 
+    Class c = findLoadedClass(name);
+    if (c != null) { return c; }
+
     if (name.startsWith("com.tc.")) {
       return SYSTEM_LOADER.loadClass(name);
     } else {
+      if (adapters.containsKey(name)) { return adaptClass(name); }
+
       return super.loadClass(name);
+    }
+  }
+
+  private Class adaptClass(String name) throws ClassNotFoundException {
+    byte[] orig = getBytes(name);
+
+    ClassReader cr = new ClassReader(orig);
+    ClassWriter cw = new ClassWriter(cr, ClassWriter.COMPUTE_MAXS);
+
+    Class adapterClass = (Class) adapters.get(name);
+
+    try {
+      Constructor cstr = adapterClass.getConstructor(new Class[] { ClassVisitor.class });
+      cstr.setAccessible(true);
+      ClassVisitor cv = (ClassVisitor) cstr.newInstance(new Object[] { cw });
+      cr.accept(cv, ClassReader.SKIP_FRAMES);
+
+      byte[] adapted = cw.toByteArray();
+
+      return defineClass(name, adapted, 0, adapted.length);
+    } catch (Exception e) {
+      throw new ClassNotFoundException(name, e);
+    }
+  }
+
+  private byte[] getBytes(String name) throws ClassNotFoundException {
+    InputStream is = super.getResourceAsStream(name.replace('.', '/').concat(".class"));
+    if (is == null) { throw new ClassNotFoundException(name); }
+
+    try {
+      return IOUtils.toByteArray(is);
+    } catch (IOException ioe) {
+      throw new ClassNotFoundException(name, ioe);
+    } finally {
+      IOUtils.closeQuietly(is);
     }
   }
 
   private void throwIfNeeded(String name) throws ClassNotFoundException {
     // throw exception if one is registered
-    final String t = (String)onLoadErrors.get(name);
+    final String t = (String) onLoadErrors.get(name);
     if (t != null) {
       ClassNotFoundException rv = new ClassNotFoundException(t);
       throw rv;
@@ -105,5 +155,10 @@ public class IsolationClassLoader extends URLClassLoader implements NamedClassLo
   protected Class findClass(String name) throws ClassNotFoundException {
     throwIfNeeded(name);
     return super.findClass(name);
+  }
+
+  public void addAdapter(String name, Class adapterClass) {
+    Object prev = adapters.put(name, adapterClass);
+    if (prev != null) { throw new AssertionError("adapter already exists for " + name); }
   }
 }
