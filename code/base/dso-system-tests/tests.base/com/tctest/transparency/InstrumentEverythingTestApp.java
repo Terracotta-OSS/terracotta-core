8390ddc51c3065832859c6a8c91c402e55fe2663
Nathaniel Harward <nharward@terracottatech.com>
2007-07-17 00:45:55 +0000

    MNK-283 [com.tctest.transparency.InstrumentEverythingTest - test]: Changed verification stage to synchronize on the root.  My guess is that faulting objects in can cause the local HashMap instance to think it has been modified when it may not actually have been by the app.
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@4451 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-system-tests/tests.base/com/tctest/transparency/InstrumentEverythingTestApp.java
+++ code/base/dso-system-tests/tests.base/com/tctest/transparency/InstrumentEverythingTestApp.java
@@ -1,5 +1,6 @@
 /*
- * All content copyright (c) 2003-2006 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ * All content copyright (c) 2003-2006 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
  */
 package com.tctest.transparency;
 
@@ -65,7 +66,9 @@ public class InstrumentEverythingTestApp extends AbstractErrorCatchingTransparen
       List local = new ArrayList();
       addALotOfObjects(local);
       moveToStageAndWait(INTERMEDIATE);
-      verify(local, root);
+      synchronized (root) {
+        verify(local, root);
+      }
     }
     printDetails();
     moveToStage(END);
