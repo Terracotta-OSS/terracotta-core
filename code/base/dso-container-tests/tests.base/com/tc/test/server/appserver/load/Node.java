7ca2c7dd91169472fb8d94bc805e500ca48e6862
Hung Huynh <hhuynh@gmail.com>
2007-07-24 16:59:34 +0000

    use httpunit for better error reporting
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@4614 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-container-tests/tests.base/com/tc/test/server/appserver/load/Node.java
+++ code/base/dso-container-tests/tests.base/com/tc/test/server/appserver/load/Node.java
@@ -6,27 +6,26 @@ package com.tc.test.server.appserver.load;
 
 import EDU.oswego.cs.dl.util.concurrent.SynchronizedRef;
 
+import com.meterware.httpunit.WebConversation;
+import com.meterware.httpunit.WebResponse;
 import com.tc.logging.TCLogger;
 import com.tc.logging.TCLogging;
-import com.tc.test.server.util.WebClient;
 import com.tc.util.concurrent.ThreadUtil;
 
-import java.io.IOException;
-import java.net.ConnectException;
 import java.net.URL;
 import java.util.Random;
 
 import junit.framework.Assert;
 
 public class Node implements Runnable {
-  protected static final TCLogger logger = TCLogging.getLogger(Node.class);
-  protected final WebClient[]     clients;
-  protected final long            duration;
-  protected final int             numRequests[];
-  protected final SynchronizedRef error  = new SynchronizedRef(null);
-  protected final URL[]           mutateUrls;
-  protected final URL[]           validateUrls;
-  protected final Random          random = new Random();
+  protected static final TCLogger   logger = TCLogging.getLogger(Node.class);
+  protected final long              duration;
+  protected final int               numRequests[];
+  protected final SynchronizedRef   error  = new SynchronizedRef(null);
+  protected final URL[]             mutateUrls;
+  protected final URL[]             validateUrls;
+  protected final Random            random = new Random();
+  protected final WebConversation[] conversations;
 
   public Node(URL mutateUrl, URL validateUrl, int numSessions, long duration) {
     this(new URL[] { mutateUrl }, new URL[] { validateUrl }, numSessions, duration);
@@ -35,18 +34,17 @@ public class Node implements Runnable {
   public Node(URL[] mutateUrls, URL[] validateUrls, int numSessions, long duration) {
     this.mutateUrls = mutateUrls;
     this.validateUrls = validateUrls;
-    clients = createClients(numSessions);
+    conversations = createConversations(numSessions);
     this.duration = duration;
-    this.numRequests = new int[clients.length];
+    this.numRequests = new int[numSessions];
   }
 
-  private WebClient[] createClients(int numSessions) {
-    WebClient[] _clients = new WebClient[numSessions];
-    for (int i = 0; i < numSessions; i++) {
-      _clients[i] = new WebClient();
+  private WebConversation[] createConversations(int count) {
+    WebConversation[] wc = new WebConversation[count];
+    for (int i = 0; i < count; i++) {
+      wc[i] = new WebConversation();
     }
-
-    return _clients;
+    return wc;
   }
 
   public void checkError() throws Throwable {
@@ -64,21 +62,26 @@ public class Node implements Runnable {
     }
   }
 
-  private void validate() throws ConnectException, IOException {
-    for (int i = 0; i < clients.length; i++) {
+  private void validate() throws Exception {
+    for (int i = 0; i < conversations.length; i++) {
       int expect = numRequests[i];
       if (expect == 0) { throw new AssertionError("No requests were ever made for client " + i); }
 
-      WebClient client = clients[i];
+      WebConversation wc = conversations[i];
 
       for (int u = 0; u < validateUrls.length; u++) {
-        int actual = client.getResponseAsInt(validateUrls[u]);
-        Assert.assertEquals(getSessionID(client), expect, actual);
+        int actual = getResponseAsInt(wc, validateUrls[u]);
+        Assert.assertEquals(getSessionID(wc), expect, actual);
         logger.info("validated value of " + expect + " for client " + i + " on " + validateUrls[u]);
         // Recording the request that was just made. This is needed for RequestCountTest.
         numRequests[i]++;
       }
     }
+  }
+
+  private int getResponseAsInt(WebConversation wc, URL url) throws Exception {
+    WebResponse response = wc.getResponse(url.toString());
+    return Integer.parseInt(response.getText().trim());
   }
 
   private void makeRequests() throws Exception {
@@ -87,15 +90,15 @@ public class Node implements Runnable {
     int session = 0;
     final long end = System.currentTimeMillis() + duration;
     while (System.currentTimeMillis() <= end) {
-      WebClient client = clients[session];
+      WebConversation wc = conversations[session];
       URL mutateUrl = numURLS == 1 ? mutateUrls[0] : mutateUrls[random.nextInt(mutateUrls.length)];
 
       final long start = System.currentTimeMillis();
       try {
-        int newVal = client.getResponseAsInt(mutateUrl);
+        int newVal = getResponseAsInt(wc, mutateUrl);
         numRequests[session]++;
-        Assert.assertEquals(getSessionID(client), numRequests[session], newVal);
-        session = (session + 1) % clients.length;
+        Assert.assertEquals(getSessionID(wc), numRequests[session], newVal);
+        session = (session + 1) % conversations.length;
         ThreadUtil.reallySleep(random.nextInt(5) + 1);
       } catch (Exception e) {
         logger.error("Elapsed time for failed request was " + (System.currentTimeMillis() - start) + " millis, url = "
@@ -105,8 +108,8 @@ public class Node implements Runnable {
     }
   }
 
-  private String getSessionID(WebClient client) {
-    return (String) client.getCookies().get("jsessionid");
+  private String getSessionID(WebConversation wc) {
+    return wc.getCookieValue("JSESSIONID");
   }
 
 }
