7f1c26a8922bda20ee75423f2e51f19b5fd56893
Saravanan Subbiah <ssubbiah@terracottatech.com>
2007-07-24 05:24:10 +0000

    Adding more debug info
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@4605 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-l2/src/com/tc/objectserver/impl/ObjectRequestManagerImpl.java
+++ code/base/dso-l2/src/com/tc/objectserver/impl/ObjectRequestManagerImpl.java
@@ -98,6 +98,7 @@ public class ObjectRequestManagerImpl implements ObjectRequestManager, ServerTra
     logger.info("Processing Pending Lookups = " + pendingRequests.size());
     for (Iterator iter = pendingRequests.iterator(); iter.hasNext();) {
       PendingRequest request = (PendingRequest) iter.next();
+      logger.info("Processing pending Looking up : " + request.getResponseContext());
       objectManager.lookupObjectsAndSubObjectsFor(request.getResponseContext().getChannelID(), request
           .getResponseContext(), request.getMaxReachableObjects());
     }
