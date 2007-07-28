bc80a0f4f78118199dd6fd5da97681a7c307a2e1
Hung Huynh <hhuynh@gmail.com>
2007-07-28 20:14:51 +0000

    fixed scope
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@4705 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-tests-jdk15/tests.system/com/tctest/LocalObjectStateTest.java
+++ code/base/dso-tests-jdk15/tests.system/com/tctest/LocalObjectStateTest.java
@@ -5,7 +5,7 @@ package com.tctest;
 
 public class LocalObjectStateTest extends TransparentTestBase {
 
-  private static final int NODE_COUNT = 2;
+  public static final int NODE_COUNT = 2;
 
   public void doSetUp(TransparentTestIface t) throws Exception {
     t.getTransparentAppConfig().setClientCount(NODE_COUNT);
