d1c0e9d2620dee5bc6f76f25946c963127766d2d
Hung Huynh <hhuynh@gmail.com>
2007-08-08 22:10:08 +0000

    test for DEV-865
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@4877 7fc7bbf3-cf45-46d4-be06-341739edd864

---
+++ code/base/dso-system-tests/tests.system/com/tctest/CloneExceptionTest.java
@@ -0,0 +1,25 @@
+/*
+ * All content copyright (c) 2003-2006 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
+ */
+package com.tctest;
+
+public class CloneExceptionTest extends TransparentTestBase {
+
+  private static final int NODE_COUNT = 1;
+
+  public CloneExceptionTest() {
+    // DEV-865
+    disableAllUntil("2007-11-10");
+  }
+
+  public void setUp() throws Exception {
+    super.setUp();
+    getTransparentAppConfig().setClientCount(NODE_COUNT).setIntensity(1);
+    initializeTestRunner();
+  }
+
+  protected Class getApplicationClass() {
+    return CloneExceptionTestApp.class;
+  }
+}
