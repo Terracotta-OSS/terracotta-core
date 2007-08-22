bafc9ddc85a805a3ae0ee3a1435b06cf80f32d18
Hung Huynh <hhuynh@gmail.com>
2007-08-22 01:43:02 +0000

    use ant to get jre bundle instead of open-uri
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@5184 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/buildscripts/extensions/bundled-jres.rb
+++ code/base/buildscripts/extensions/bundled-jres.rb
@@ -11,22 +11,17 @@ module BundledJREs
     processor_type = @build_environment.processor_type.downcase
     jre_filename   = "jre-with-jdk-#{version}-#{os_type}.tar.gz"
     jre_url        = "#{@static_resources.jre_url}/#{os_type}/#{jre_filename}" 
-    jre_srcdir     = FilePath.new('.tc-build-cache', 'bundled-jre', 'sun', os_type).ensure_directory
+    jre_srcdir     = FilePath.new(ENV['HOME'], '.tc').ensure_directory
     jre_srcfile    = FilePath.new(jre_srcdir, jre_filename)
     begin
-      unless File.exist?(jre_srcfile.to_s)
-        open(jre_url) do |src|
-          File.open(jre_srcfile.to_s, 'w') do |dest|
-            while data = src.read(8192) do 
-              dest.write(data) 
-            end
-          end
-        end 
+      unless File.exist?(jre_srcfile.to_s)        
+        ant.get(:src => jre_url, :dest => jre_srcfile.to_s)
       end
       destdir = FilePath.new(product_directory, directory).ensure_directory
       ant.untar(:src => jre_srcfile, :dest => destdir.to_s, :compression => 'gzip', :overwrite => true) 
-    rescue
-      puts :warn, "Unable to retrieve JRE bundle: `#{jre_filename}'"
+    rescue      
+      puts "Unable to retrieve JRE bundle: `#{jre_filename}'"
+      puts "Caught exception: #{$!}"
     end 
   end
 end
