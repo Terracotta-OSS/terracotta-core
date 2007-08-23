fcacc98b69d7174430d86bc1527257ca28a3dcf9
Tim Eck <timeck@gmail.com>
2007-08-23 06:26:28 +0000

    another instance where logging junk needs to be added (should fix a bunch of spring+weblogic failures)
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@5233 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-spring-tests/tests.base/com/tctest/spring/integrationtests/SpringDeploymentTest.java
+++ code/base/dso-spring-tests/tests.base/com/tctest/spring/integrationtests/SpringDeploymentTest.java
@@ -1,8 +1,11 @@
 /*
- * All content copyright (c) 2003-2007 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ * All content copyright (c) 2003-2007 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
  */
 package com.tctest.spring.integrationtests;
 
+import org.apache.commons.logging.LogFactory;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.BeanFactory;
 
 import com.tc.test.TestConfigObject;
@@ -13,11 +16,15 @@ public class SpringDeploymentTest extends AbstractDeploymentTest {
 
   protected DeploymentBuilder makeDeploymentBuilder(String warFileName) {
     DeploymentBuilder builder = getServerManager().makeDeploymentBuilder(warFileName);
-    
+
+    // All spring tests need these I guess
+    builder.addDirectoryOrJARContainingClass(LogFactory.class); // commons-logging
+    builder.addDirectoryOrJARContainingClass(Logger.class); // log4j
+
     builder.addDirectoryOrJARContainingClassOfSelectedVersion(BeanFactory.class,
                                                               new String[] { TestConfigObject.SPRING_VARIANT }); // springframework
-    
+
     return builder;
   }
-  
+
 }
