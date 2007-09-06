b5d6f89d6be070aa494e0aa0ede500865bc82806
Tim Eck <timeck@gmail.com>
2007-09-06 23:29:39 +0000

    merge 5466 from 2.4
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@5467 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-container-tests/tests.base/com/tc/test/server/appserver/weblogic9x/Weblogic9xAppServer.java
+++ code/base/dso-container-tests/tests.base/com/tc/test/server/appserver/weblogic9x/Weblogic9xAppServer.java
@@ -5,9 +5,13 @@
 package com.tc.test.server.appserver.weblogic9x;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.tools.ant.BuildListener;
+import org.apache.tools.ant.taskdefs.Java;
+import org.apache.tools.ant.types.Path;
 import org.codehaus.cargo.container.InstalledLocalContainer;
 import org.codehaus.cargo.container.State;
 import org.codehaus.cargo.container.configuration.LocalConfiguration;
+import org.codehaus.cargo.container.internal.AntContainerExecutorThread;
 import org.codehaus.cargo.container.weblogic.WebLogic9xInstalledLocalContainer;
 
 import com.tc.test.server.appserver.AppServerParameters;
@@ -19,6 +23,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Vector;
 
 /**
  * Weblogic9x AppServer implementation
@@ -49,6 +54,56 @@ public final class Weblogic9xAppServer extends CargoAppServer {
 
     public TCWebLogic9xInstalledLocalContainer(LocalConfiguration configuration) {
       super(configuration);
+    }
+
+    public void doStop(Java java) throws Exception {
+      // Use the weblogic scripting interface to stop WLS 9.2. Sometimes we get RMI exceptions trying to stop WLS9.2
+      // using the 8.1 stop method. The stop scripts supplied with version 9 use this scripting stuff so I doing the
+      // same here.
+
+      // Hack --> remove the extra build listener that gets added to the ant project
+      Vector buildListeners = java.getProject().getBuildListeners();
+      if (buildListeners.size() > 1) {
+        BuildListener listener = (BuildListener) buildListeners.get(buildListeners.size() - 1);
+        java.getProject().removeBuildListener(listener);
+      }
+
+      File serverDir = new File(getHome(), "server");
+      Path classpath = java.createClasspath();
+      classpath.createPathElement().setLocation(new File(serverDir, "lib/weblogic_sp.jar"));
+      classpath.createPathElement().setLocation(new File(serverDir, "lib/weblogic.jar"));
+      java.setClassname("weblogic.WLST");
+      java.createArg().setValue(createShutdownScript().getAbsolutePath());
+      AntContainerExecutorThread webLogicRunner = new AntContainerExecutorThread(java);
+      webLogicRunner.start();
+    }
+
+    private File createShutdownScript() throws IOException {
+      String port = getConfiguration().getPropertyValue("cargo.servlet.port");
+      String user = getConfiguration().getPropertyValue("cargo.weblogic.administrator.user");
+      String passwd = getConfiguration().getPropertyValue("cargo.weblogic.administrator.password");
+
+      File tmp = File.createTempFile("wls92shutdown", ".py");
+      tmp.deleteOnExit();
+
+      FileOutputStream out = null;
+
+      try {
+        out = new FileOutputStream(tmp);
+
+        String connect = "connect(url='t3://localhost:" + port + "',adminServerName='AdminServer',username='" + user
+                         + "',password='" + passwd + "')\n";
+        out.write(connect.getBytes("UTF-8"));
+        out.write("shutdown(name='AdminServer',entityType='Server',force='true',block='true')\n".getBytes("UTF-8"));
+        out.write("exit()\n".getBytes("UTF-8"));
+        out.flush();
+      } finally {
+        if (out != null) {
+          out.close();
+        }
+      }
+
+      return tmp;
     }
 
     protected void setState(State state) {
