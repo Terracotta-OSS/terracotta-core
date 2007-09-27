bec9f5ce90d2e85a509434bc3fdedb974a95fb48
Hung Huynh <hhuynh@gmail.com>
2007-09-27 23:48:00 +0000

    test for custom adapted class
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@5722 7fc7bbf3-cf45-46d4-be06-341739edd864

---
+++ code/base/dso-system-tests/tests.system/com/tctest/CustomClassAdapterTest.java
@@ -0,0 +1,24 @@
+/*
+ * All content copyright (c) 2003-2006 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
+ */
+package com.tctest;
+
+public class CustomClassAdapterTest extends TransparentTestBase {
+
+  private static final int NODE_COUNT = 2;
+
+  public CustomClassAdapterTest() {
+    disableAllUntil("2008-01-15");
+  }
+  
+  public void doSetUp(TransparentTestIface t) throws Exception {
+    t.getTransparentAppConfig().setClientCount(NODE_COUNT);
+    t.initializeTestRunner();
+  }
+
+  protected Class getApplicationClass() {
+    return CustomClassAdapterTestApp.class;
+  }
+
+}
