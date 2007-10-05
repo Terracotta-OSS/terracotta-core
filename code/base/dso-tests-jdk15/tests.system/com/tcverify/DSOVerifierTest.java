9f7d37ecccd03e7775188da8b0f7340f6df6ce53
Tim Eck <timeck@gmail.com>
2007-10-05 20:14:19 +0000

    keep output in files as well (trying to debug DSOVerifierCDV424Test failure under ibm jdk)
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@5825 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-tests-jdk15/tests.system/com/tcverify/DSOVerifierTest.java
+++ code/base/dso-tests-jdk15/tests.system/com/tcverify/DSOVerifierTest.java
@@ -4,6 +4,7 @@
  */
 package com.tcverify;
 
+import org.apache.commons.io.output.TeeOutputStream;
 import org.apache.commons.lang.ArrayUtils;
 
 import com.tc.config.schema.builder.InstrumentedClassConfigBuilder;
@@ -18,14 +19,16 @@ import com.tc.config.schema.test.LockConfigBuilderImpl;
 import com.tc.config.schema.test.RootConfigBuilderImpl;
 import com.tc.config.schema.test.TerracottaConfigBuilder;
 import com.tc.process.LinkedJavaProcess;
-import com.tc.process.StreamCollector;
+import com.tc.process.StreamCopier;
 import com.tc.server.TCServerImpl;
 import com.tc.test.TCTestCase;
 import com.tc.test.TestConfigObject;
 import com.tc.util.PortChooser;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.StringReader;
@@ -97,10 +100,20 @@ public class DSOVerifierTest extends TCTestCase {
     p1.getOutputStream().close();
     p2.getOutputStream().close();
 
-    StreamCollector p1StdOut = new StreamCollector(p1.getInputStream());
-    StreamCollector p2StdOut = new StreamCollector(p2.getInputStream());
-    StreamCollector p1StdErr = new StreamCollector(p1.getErrorStream());
-    StreamCollector p2StdErr = new StreamCollector(p2.getErrorStream());
+    FileOutputStream p1o = new FileOutputStream(getTempFile("p1out.log"));
+    FileOutputStream p1e = new FileOutputStream(getTempFile("p1err.log"));
+    FileOutputStream p2o = new FileOutputStream(getTempFile("p2out.log"));
+    FileOutputStream p2e = new FileOutputStream(getTempFile("p2err.log"));
+
+    ByteArrayOutputStream p1oBytes = new ByteArrayOutputStream();
+    ByteArrayOutputStream p1eBytes = new ByteArrayOutputStream();
+    ByteArrayOutputStream p2oBytes = new ByteArrayOutputStream();
+    ByteArrayOutputStream p2eBytes = new ByteArrayOutputStream();
+
+    StreamCopier p1StdOut = new StreamCopier(p1.getInputStream(), new TeeOutputStream(p1oBytes, p1o));
+    StreamCopier p1StdErr = new StreamCopier(p1.getErrorStream(), new TeeOutputStream(p1eBytes, p1e));
+    StreamCopier p2StdOut = new StreamCopier(p2.getInputStream(), new TeeOutputStream(p2oBytes, p2o));
+    StreamCopier p2StdErr = new StreamCopier(p2.getErrorStream(), new TeeOutputStream(p2eBytes, p2e));
     p1StdOut.start();
     p1StdErr.start();
     p2StdOut.start();
@@ -114,10 +127,10 @@ public class DSOVerifierTest extends TCTestCase {
     p2StdOut.join();
     p2StdErr.join();
 
-    String p1Out = p1StdOut.toString();
-    String p2Out = p2StdOut.toString();
-    String p1Err = p1StdErr.toString();
-    String p2Err = p2StdErr.toString();
+    String p1Out = new String(p1oBytes.toByteArray());
+    String p1Err = new String(p1eBytes.toByteArray());
+    String p2Out = new String(p2oBytes.toByteArray());
+    String p2Err = new String(p2eBytes.toByteArray());
 
     String compound = "Process 1 output:\n\n" + p1Out + "\n\nProcess 1 error:\n\n" + p1Err
                       + "\n\nProcess 2 output:\n\n" + p2Out + "\n\nProcess 2 error:\n\n" + p2Err + "\n\n";
@@ -131,6 +144,7 @@ public class DSOVerifierTest extends TCTestCase {
       fail(compound);
     }
   }
+
 
   protected String getMainClass() {
     return DSOVerifier.class.getName();
