fa2e3a967cfa4dcf41a9e5cba8b8b582bdb7706a
Tim Eck <timeck@gmail.com>
2007-10-08 21:49:31 +0000

    add exclude that let's WAS 6.1.0.0 start even if *..* is included (CDV-429)
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@5850 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/clustered-websphere-6.1.0.7/src/org/terracotta/modules/websphere_6_1/WebsphereTerracottaConfigurator.java
+++ code/base/clustered-websphere-6.1.0.7/src/org/terracotta/modules/websphere_6_1/WebsphereTerracottaConfigurator.java
@@ -7,6 +7,9 @@ import org.terracotta.modules.configuration.TerracottaConfiguratorModule;
 public final class WebsphereTerracottaConfigurator extends TerracottaConfiguratorModule {
 
   protected final void addInstrumentation(final BundleContext context) {
+  	// Without this exclude, WAS 6.1.0.0 will fail to start with StackOverflowError (CDV-429)
+    configHelper.addPermanentExcludePattern("org.eclipse.osgi.framework.internal.core.FrameworkSecurityManager");
+
     configHelper.addCustomAdapter("com.ibm.ws.classloader.JarClassLoader", new JarClassLoaderAdapter());
     configHelper.addCustomAdapter("com.ibm.ws.classloader.ClassGraph", new ClassGraphAdapter());
     configHelper.addCustomAdapter("com.ibm.ws.webcontainer.filter.WebAppFilterManager",
