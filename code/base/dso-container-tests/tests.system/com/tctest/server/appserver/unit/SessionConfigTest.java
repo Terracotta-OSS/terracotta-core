4c018537e372b0ef5255cdc38b8d659e6f9163ba
Hung Huynh <hhuynh@gmail.com>
2007-10-11 02:04:22 +0000

    more testcases for WebAppConfig
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@5904 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-container-tests/tests.system/com/tctest/server/appserver/unit/SessionConfigTest.java
+++ code/base/dso-container-tests/tests.system/com/tctest/server/appserver/unit/SessionConfigTest.java
@@ -7,23 +7,37 @@ package com.tctest.server.appserver.unit;
 import com.meterware.httpunit.WebConversation;
 import com.meterware.httpunit.WebResponse;
 import com.tc.test.server.appserver.AppServerFactory;
-import com.tc.test.server.appserver.deployment.AbstractOneServerDeploymentTest;
+import com.tc.test.server.appserver.deployment.AbstractDeploymentTest;
+import com.tc.test.server.appserver.deployment.Deployment;
 import com.tc.test.server.appserver.deployment.DeploymentBuilder;
+import com.tc.test.server.appserver.deployment.ServerTestSetup;
 import com.tc.test.server.appserver.deployment.WebApplicationServer;
 import com.tc.test.server.util.TcConfigBuilder;
 import com.tctest.webapp.servlets.SessionConfigServlet;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import junit.framework.Test;
 
 /**
- * Test CookieEnabled=false
+ * Test session-descriptor setting
+ * 
+ * http://edocs.bea.com/wls/docs81/webapp/weblogic_xml.html#1038173
+ * http://edocs.bea.com/wls/docs90/webapp/weblogic_xml.html#1071982
+ * 
  * @author hhuynh
  */
-public class SessionConfigTest extends AbstractOneServerDeploymentTest {
-  private static final String CONTEXT = "SessionConfigTest";
-  private static final String MAPPING = "app";
+public class SessionConfigTest extends AbstractDeploymentTest {
+  private static final String  RESOURCE_ROOT = "/com/tctest/server/appserver/unit/sessionconfigtest";
+  private static final String  CONTEXT       = "SessionConfigTest";
+  private static final String  MAPPING       = "app";
+
+  private Deployment           deployment;
+  private WebApplicationServer server;
+
+  private Map                  descriptors   = new HashMap();
 
   public SessionConfigTest() {
     if (AppServerFactory.getCurrentAppServerId() != AppServerFactory.WEBLOGIC) {
@@ -32,57 +46,100 @@ public class SessionConfigTest extends AbstractOneServerDeploymentTest {
   }
 
   public static Test suite() {
-    return new SessionConfigTestSetup();
+    return new ServerTestSetup(SessionConfigTest.class);
   }
 
+  // CookieEnabled = false
   public void testCookieDisabled() throws Exception {
+    descriptors.put("wl81", "weblogic81a.xml");
+    descriptors.put("wl92", "weblogic92a.xml");
+    setupAppServer();
+
     WebConversation conversation = new WebConversation();
 
-    WebResponse response1 = request(server0, "testcase=CookiesEnabled&hit=0", conversation);
-    assertEquals("OK", response1.getText().trim());
+    WebResponse response = request(server, "testcase=testCookieDisabled&hit=0", conversation);
+    assertEquals("OK", response.getText().trim());
 
-    response1 = request(server0, "testcase=CookiesEnabled&hit=1", conversation);
-    assertEquals("OK", response1.getText().trim());
-
+    response = request(server, "testcase=testCookieDisabled&hit=1", conversation);
+    assertEquals("OK", response.getText().trim());
   }
 
-  private WebResponse request(WebApplicationServer server, String params, WebConversation con) throws Exception {
-    return server.ping("/" + CONTEXT + "/" + MAPPING + "?" + params, con);
+  // CookieEnabled = false, URLRewritingEnabled = false
+  public void testUrlRewritingDisabled() throws Exception {
+    descriptors.put("wl81", "weblogic81b.xml");
+    descriptors.put("wl92", "weblogic92b.xml");
+    setupAppServer();
+
+    WebResponse response = request(server, "testcase=testUrlRewritingDisabled", new WebConversation());
+    System.out.println("Response: " + response.getText());
+    assertEquals("OK", response.getText().trim());
   }
 
-  private static class SessionConfigTestSetup extends OneServerTestSetup {
+  // TrackingEnabled = false
+  public void testTrackingDisabled() throws Exception {
+    descriptors.put("wl81", "weblogic81c.xml");
+    descriptors.put("wl92", "weblogic92c.xml");
+    setupAppServer();
 
-    public SessionConfigTestSetup() {
-      super(SessionConfigTest.class, CONTEXT);
+    WebConversation wc = new WebConversation();
+    WebResponse response = request(server, "testcase=testTrackingDisabled&hit=0", wc);
+    System.out.println("Response: " + response.getText());
+    assertEquals("OK", response.getText().trim());
+    
+    response = request(server, "testcase=testTrackingDisabled&hit=1", wc);
+    System.out.println("Response: " + response.getText());
+    assertEquals("OK", response.getText().trim());    
+  }
+  
+  // IDLength = 69 -- only applicable to Weblogic
+  public void testIdLength() throws Exception {
+    if (AppServerFactory.getCurrentAppServerId() != AppServerFactory.WEBLOGIC) {
+      return;
+    }
+    descriptors.put("wl81", "weblogic81d.xml");
+    descriptors.put("wl92", "weblogic92d.xml");
+    setupAppServer();
+
+    WebConversation wc = new WebConversation();
+    WebResponse response = request(server, "", wc);    
+    assertEquals("OK", response.getText().trim());
+    int idLength = wc.getCookieValue("JSESSIONID").indexOf("!");
+    System.out.println(wc.getCookieValue("JSESSIONID") + ", length = " + idLength);
+  }
+
+  private WebResponse request(WebApplicationServer appserver, String params, WebConversation con) throws Exception {
+    return appserver.ping("/" + CONTEXT + "/" + MAPPING + "?" + params, con);
+  }
+
+  private void setupAppServer() throws Exception {
+    TcConfigBuilder tcConfigBuilder = new TcConfigBuilder();
+    tcConfigBuilder.addWebApplication(CONTEXT);
+
+    deployment = makeDeployment();
+    server = makeWebApplicationServer(tcConfigBuilder);
+    server.addWarDeployment(deployment, CONTEXT);
+    server.start();
+  }
+
+  private Deployment makeDeployment() throws Exception {
+    DeploymentBuilder builder = makeDeploymentBuilder(CONTEXT + ".war");
+    builder.addServlet("SessionConfigServlet", "/" + MAPPING + "/*", SessionConfigServlet.class, null, false);
+
+    // add container specific descriptor
+    switch (AppServerFactory.getCurrentAppServerId()) {
+      case AppServerFactory.WEBLOGIC:
+        if (AppServerFactory.getCurrentAppServerMajorVersion().equals("8")) {
+          builder.addResourceFullpath(RESOURCE_ROOT, (String) descriptors.get("wl81"), "WEB-INF/weblogic.xml");
+        }
+        if (AppServerFactory.getCurrentAppServerMajorVersion().equals("9")) {
+          builder.addResourceFullpath(RESOURCE_ROOT, (String) descriptors.get("wl92"), "WEB-INF/weblogic.xml");
+        }
+        break;
+      default:
+        break;
     }
 
-    protected void configureWar(DeploymentBuilder builder) {
-      builder.addServlet("SessionConfigServlet", "/" + MAPPING + "/*", SessionConfigServlet.class, null, false);
-      // add container specific descriptor
-
-      switch (AppServerFactory.getCurrentAppServerId()) {
-        case AppServerFactory.WEBLOGIC:
-          if (AppServerFactory.getCurrentAppServerMajorVersion().equals("8")) {
-            builder.addResourceFullpath("/com/tctest/server/appserver/unit/sessionconfigtest", "weblogic81a.xml",
-                                        "WEB-INF/weblogic.xml");
-          }
-          if (AppServerFactory.getCurrentAppServerMajorVersion().equals("9")) {
-            builder.addResourceFullpath("/com/tctest/server/appserver/unit/sessionconfigtest", "weblogic92a.xml",
-                                        "WEB-INF/weblogic.xml");
-          }
-          break;
-        case AppServerFactory.TOMCAT:
-          builder.addResourceFullpath("/com/tctest/server/appserver/unit/sessionconfigtest", "contexta.xml",
-                                      "META-INF/context.xml");
-          break;
-        default:
-          break;
-      }
-    }
-
-    protected void configureTcConfig(TcConfigBuilder clientConfig) {
-      clientConfig.addWebApplication(CONTEXT);
-    }
+    return builder.makeDeployment();
   }
 
 }
--- code/base/dso-container-tests/tests.system/com/tctest/webapp/servlets/SessionConfigServlet.java
+++ code/base/dso-container-tests/tests.system/com/tctest/webapp/servlets/SessionConfigServlet.java
@@ -1,5 +1,6 @@
 /*
- * All content copyright (c) 2003-2007 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ * All content copyright (c) 2003-2007 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
  */
 package com.tctest.webapp.servlets;
 
@@ -12,22 +13,37 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 public final class SessionConfigServlet extends HttpServlet {
-  
+
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     HttpSession session = request.getSession(true);
     response.setContentType("text/html");
     PrintWriter out = response.getWriter();
-    
+
     String testCase = request.getParameter("testcase");
-    if ("CookiesEnabled".equals(testCase)) {
+    if ("testCookieDisabled".equals(testCase)) {
       // cookie enable set to false so we should get no cookies
       // on second hit (hit=1)
       String hit = request.getParameter("hit");
       if ("1".equals(hit) && request.getCookies() != null) {
         out.println("Still receive cookie when CookiesEnabled=false");
       }
+    } else if ("testUrlRewritingDisabled".equals(testCase)) {
+      String originalUrl = request.getRequestURI() + "/index.html";
+      String encodedUrl = response.encodeURL(originalUrl);
+      if (!originalUrl.equals(encodedUrl)) {
+        out.println("encodeUrl succeeded: " + encodedUrl);
+      }
+    } else if ("testTrackingDisabled".equals(testCase)) {
+      String hit = request.getParameter("hit");
+      if ("1".equals(hit) && request.getCookies() != null) {
+        out.println("Still receive cookie when TrackingDisabled=false");
+      }
+      String originalUrl = request.getRequestURI() + "/index.html";
+      String encodedUrl = response.encodeURL(originalUrl);
+      if (!originalUrl.equals(encodedUrl)) {
+        out.println("encodeUrl succeeded: " + encodedUrl);
+      }
     }
-    
     out.println("OK");
   }
 }
