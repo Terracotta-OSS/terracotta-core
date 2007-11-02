d0a8cfa0ca500d07c61c358de4eaff9e774acaef
Hung Huynh <hhuynh@gmail.com>
2007-11-02 22:29:14 +0000

    DEV-1094: Tim's fix for race condition
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@6176 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/demos/pojo/chatter/src/demo/chatter/ChatManager.java
+++ code/demos/pojo/chatter/src/demo/chatter/ChatManager.java
@@ -15,64 +15,80 @@ import javax.swing.DefaultListModel;
  *@author    Terracotta, Inc.
  */
 class ChatManager {
-	private transient ChatListener listener;
+  private transient ChatListener listener;
 
-	private List messages;
+  private List messages;
 
-	private DefaultListModel listModel;
+  private DefaultListModel listModel;
 
-	private Map map;
+  private Map map;
 
-	public ChatManager() {
-		listModel = new DefaultListModel();
-		messages = new Vector();
-		map = new HashMap();
-	}
+  public ChatManager() {
+    listModel = new DefaultListModel();
+    messages = new Vector();
+    map = new HashMap();
+  }
 
-	public Message[] getMessages() {
-		return (Message[]) messages.toArray(new Message[0]);
-	}
+  public Message[] getMessages() {
+    return (Message[]) messages.toArray(new Message[0]);
+  }
 
-	public Object[] getCurrentUsers() {
-		synchronized (listModel) {
-			return listModel.toArray();
-		}
-	}
+  public Object[] getCurrentUsers() {
+    synchronized (listModel) {
+      return listModel.toArray();
+    }
+  }
 
-	public void send(User user, String message) {
-		Message msg = new Message(user, message);
-		synchronized (messages) {
-			messages.add(msg);
-		}
-		sendNewMessageEvent(msg);
-	}
+  public void send(User user, String message) {
+    Message msg = new Message(user, message);
+    synchronized (messages) {
+      messages.add(msg);
+    }
+    sendNewMessageEvent(msg);
+  }
 
-	public void registerUser(User user) {
-		this.listener = user;
-		synchronized (listModel) {
-			listModel.addElement(user);
-			map.put(user.getNodeId(), user);
-		}
-		sendNewUserEvent(user.getName());
-	}
+  public void registerUser(User user) {
 
-	public String removeUser(String nodeId) {
-		synchronized (listModel) {
-			if (map.containsKey(nodeId)) {
-				User removedUser = (User) map.get(nodeId);
-				listModel.removeElement(removedUser);
-				return removedUser.getName();
-			} else {
-				return "";
-			}
-		}
-	}
+    synchronized(Lock.lock) {
+      System.err.println("setting listener from " + listener + " to " + user);
+      this.listener = user;
+    }
 
-	private synchronized void sendNewUserEvent(String username) {
-		this.listener.newUser(username);
-	}
+    synchronized (listModel) {
+      listModel.addElement(user);
+      map.put(user.getNodeId(), user);
+    }
+    sendNewUserEvent(user.getName());
+  }
 
-	private synchronized void sendNewMessageEvent(Message message) {
-		this.listener.newMessage(message);
-	}
+  public String removeUser(String nodeId) {
+    synchronized (listModel) {
+      if (map.containsKey(nodeId)) {
+        User removedUser = (User) map.get(nodeId);
+        listModel.removeElement(removedUser);
+        return removedUser.getName();
+      } else {
+        return "";
+      }
+    }
+  }
+
+  private synchronized void sendNewUserEvent(String username) {
+    synchronized(Lock.lock) {
+      if (this.listener == null) {
+        System.err.println("listener is NULL!!!!");
+        return;
+      }
+      this.listener.newUser(username);
+    }
+  }
+
+  private synchronized void sendNewMessageEvent(Message message) {
+    this.listener.newMessage(message);
+  }
+
+  static class Lock {
+    final static Object lock = new Object();
+  }
+
 }
