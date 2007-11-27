2f7af3092ad876dec0600ba0844e3cd09f68342d
Antonio Si <asi@terracottatech.com>
2007-11-27 20:45:29 +0000

    Minor changes to the test.
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@6382 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/clustered-ehcache-commons-1.0/tests.base/com/tctest/EhcacheGlobalEvictionTestApp.java
+++ code/base/clustered-ehcache-commons-1.0/tests.base/com/tctest/EhcacheGlobalEvictionTestApp.java
@@ -141,7 +141,7 @@ public abstract class EhcacheGlobalEvictionTestApp extends ServerCrashingAppBase
       barrier.barrier();
       
       if (index == 0) {
-        Thread.sleep(24000);
+        Thread.sleep(20000);
 
         Assert.assertTrue(cache.isExpired(new Element("key04", "val04")));
         Assert.assertTrue(cache.isExpired(new Element("key05", "val05")));
