5a6b80471bd0ce94f4a27148750ef64f706fc055
Alex Miller <alex@terracottatech.com>
2008-02-02 05:23:00 +0000

    timebomb till Monday
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@6903 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-container-tests/tests.system/com/tctest/management/RequestCountTest.java
+++ code/base/dso-container-tests/tests.system/com/tctest/management/RequestCountTest.java
@@ -42,7 +42,7 @@ public final class RequestCountTest extends AbstractDeploymentTest {
   private TcConfigBuilder       configBuilder;
 
   public RequestCountTest() {
-    // disableAllUntil("2008-01-15");
+    disableAllUntil("2008-02-04");
   }
 
   public static Test suite() {
