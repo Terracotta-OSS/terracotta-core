7053e434214648ccd3e7d2615aca920d632e0057
Abhishek Singh <asingh@terracottatech.com>
2008-02-21 03:30:02 +0000

    Added dso-container-tests/tests.system/com/tctest/externall1/StandardLoaderApp.java Missing File in previous commit.
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@7076 7fc7bbf3-cf45-46d4-be06-341739edd864

---
+++ code/base/dso-container-tests/tests.system/com/tctest/externall1/StandardLoaderApp.java
@@ -0,0 +1,30 @@
+/*
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
+ */
+package com.tctest.externall1;
+
+import com.tctest.webapp.servlets.StandardLoaderServlet;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StandardLoaderApp {
+
+  private final Map sharedMap = new HashMap();
+
+  public static void main(String[] args) {
+     new StandardLoaderApp().doTest();
+  }
+  
+  private void doTest() {
+    Object obj = sharedMap.get("1");
+    if (obj instanceof StandardLoaderServlet.Inner) {
+      System.out.println("OK");
+    }
+    else {
+      System.exit(1);
+    }
+  }
+
+}
