16cdaaf720b7cb0c2ed859c4509a6609e28d5064
Nabib El-Rahman <nelrahma@terracottatech.com>
2008-03-01 02:01:56 +0000

    added dump handler to common-api
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@7213 7fc7bbf3-cf45-46d4-be06-341739edd864

---
+++ code/base/common-api/src/com/tc/logging/DumpHandler.java
@@ -0,0 +1,16 @@
+/*
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ */
+package com.tc.logging;
+import java.io.Writer;
+
+public interface DumpHandler {
+  
+  String dump();
+
+  void dump(Writer writer);
+  
+  void dumpToLogger();
+
+
+}
