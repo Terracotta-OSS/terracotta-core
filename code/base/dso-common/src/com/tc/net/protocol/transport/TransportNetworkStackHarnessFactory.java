d13f4b14df178814b42db7a12514158bb939ca0e
Raghu Singh <rsingh@terracottatech.com>
2008-03-08 02:18:00 +0000

    http://jira.terracotta.org/jira/browse/CDV-533
    This checkin takes care of the scenario when l1.reconnect.enabled is set in client and is unset in server or vice versa. It forces client to log the error and quit.
    Also added the unit test for it.
    
    
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@7307 7fc7bbf3-cf45-46d4-be06-341739edd864

---
+++ code/base/dso-common/src/com/tc/net/protocol/transport/TransportNetworkStackHarnessFactory.java
@@ -0,0 +1,56 @@
+/*
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
+ */
+package com.tc.net.protocol.transport;
+
+import com.tc.net.protocol.AbstractNetworkStackHarness;
+import com.tc.net.protocol.NetworkStackHarness;
+import com.tc.net.protocol.NetworkStackHarnessFactory;
+import com.tc.net.protocol.tcm.MessageChannelInternal;
+import com.tc.net.protocol.tcm.ServerMessageChannelFactory;
+
+public class TransportNetworkStackHarnessFactory implements NetworkStackHarnessFactory {
+
+  public NetworkStackHarness createServerHarness(ServerMessageChannelFactory channelFactory,
+                                                 MessageTransport transport,
+                                                 MessageTransportListener[] transportListeners) {
+    return new TransportNetworkStackHarness(channelFactory, transport);
+  }
+
+  public NetworkStackHarness createClientHarness(MessageTransportFactory transportFactory,
+                                                 MessageChannelInternal channel,
+                                                 MessageTransportListener[] transportListeners) {
+    return new TransportNetworkStackHarness(transportFactory, channel);
+  }
+
+  private class TransportNetworkStackHarness extends AbstractNetworkStackHarness {
+
+    TransportNetworkStackHarness(ServerMessageChannelFactory channelFactory, MessageTransport transport) {
+      super(channelFactory, transport);
+    }
+
+    TransportNetworkStackHarness(MessageTransportFactory transportFactory, MessageChannelInternal channel) {
+      super(transportFactory, channel);
+    }
+
+    protected void connectStack() {
+      transport.setReceiveLayer(null);
+      
+      // XXX: this is super ugly, but...
+      if (transport instanceof ClientMessageTransport) {
+        ClientMessageTransport cmt = (ClientMessageTransport) transport;
+        ClientConnectionEstablisher cce = cmt.getConnectionEstablisher();
+        ConnectionWatcher cw = new ConnectionWatcher(cmt, channel, cce);
+        transport.addTransportListener(cw);
+      } else {
+        transport.addTransportListener(channel);
+      }
+    }
+
+    protected void createIntermediateLayers() {
+      // no intermediate layers to create
+    }
+
+  }
+}
