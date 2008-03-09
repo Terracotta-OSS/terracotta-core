559c5f658a141afda6dd5da2a2b85c00f7adccfc
Saravanan Subbiah <ssubbiah@terracottatech.com>
2008-03-09 01:15:04 +0000

    Removal of Eclipse warning broke the build
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@7328 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-container-tests/tests.base/com/tc/test/server/appserver/deployment/TempDirectoryUtil.java
+++ code/base/dso-container-tests/tests.base/com/tc/test/server/appserver/deployment/TempDirectoryUtil.java
@@ -1,9 +1,9 @@
 /*
- * All content copyright (c) 2003-2006 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ * All content copyright (c) 2003-2006 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
  */
 package com.tc.test.server.appserver.deployment;
 
-import com.tc.exception.TCRuntimeException;
 import com.tc.test.TempDirectoryHelper;
 
 import java.io.File;
@@ -16,15 +16,11 @@ public class TempDirectoryUtil {
   public static File getTempDirectory(Class type) throws IOException {
     return getTempDirectoryHelper(type).getDirectory();
   }
-  
+
   protected static synchronized TempDirectoryHelper getTempDirectoryHelper(Class type) {
     if (tempDirectoryHelper == null) {
-      try {
-        PropertiesHackForRunningInEclipse.initializePropertiesWhenRunningInEclipse();
-        tempDirectoryHelper = new TempDirectoryHelper(type, true);
-      } catch (IOException ioe) {
-        throw new TCRuntimeException("Can't get configuration for temp directory", ioe);
-      }
+      PropertiesHackForRunningInEclipse.initializePropertiesWhenRunningInEclipse();
+      tempDirectoryHelper = new TempDirectoryHelper(type, true);
     }
 
     return tempDirectoryHelper;
