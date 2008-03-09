20e434394e7e07831a7963c822ba6567a94bf33c
Geert Bevin <gbevin@terracottatech.com>
2008-03-09 10:58:03 +0000

    Fixes to lock guarding.
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@7337 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/common/src/com/tc/util/concurrent/FileLockGuard.java
+++ code/base/common/src/com/tc/util/concurrent/FileLockGuard.java
@@ -3,6 +3,9 @@
  */
 package com.tc.util.concurrent;
 
+import com.tc.logging.CustomerLogging;
+import com.tc.logging.TCLogger;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
@@ -11,52 +14,57 @@ import java.nio.channels.FileLock;
 
 /**
  * This class allows a code section to be guarded by a file lock.
- *
+ * <p/>
  * The guarding is guaranteed both when the lock file is used within the
  * same JVM as well as when several JVMs are being used. This extends the
  * behavior of the standard JDK {@link FileLock} class, which only guards
  * when several JVMs are concurrently using the lock.
  */
 public abstract class FileLockGuard {
-    public static void guard(File lockFile, Guarded guarded) throws IOException, InnerException {
+  private final static TCLogger logger = CustomerLogging.getDSOGenericLogger();
+
+  public static void guard(File lockFile, Guarded guarded) throws IOException, InnerException {
+    if (logger.isInfoEnabled()) {
+      logger.info("Guarding through lock file '" + lockFile.getAbsolutePath() + "', guarded instance : "+guarded+".");
+    }
+    // synchronize on the interned string of the lock file for in-vm guarding
+    synchronized (lockFile.getAbsolutePath().intern()) {
       RandomAccessFile raf = new RandomAccessFile(lockFile, "rw");
       FileChannel channel = raf.getChannel();
       // lock on a file for multi-vm guarding
       FileLock lock = channel.lock();
-      // synchronize on the interned string of the lock file for in-vm guarding
-      synchronized (lockFile.getAbsolutePath().intern()) {
-        try {
-          guarded.execute();
-        } finally {
-          lock.release();
-        }
-      }
-    }
-
-    /**
-     * Abstract class that needs to be extended by implementing the code
-     * section that needs to be guarded in the {@link FileLockGuard.Guarded#execute()}
-     * method.
-     */
-    public static abstract class Guarded {
-      public abstract void execute() throws InnerException;
-    }
-
-    /**
-     * This exception can be thrown within the guarded code section. The main
-     * purpose is to be able to catch it outside the {@link FileLockGuard#guard(File, FileLockGuard.Guarded)}
-     * method call so that the original inner exception can be retrieved and
-     * re-thrown.
-     */
-    public static class InnerException extends Exception {
-      private final Throwable innerException;
-
-      public InnerException(Throwable innerException) {
-        this.innerException = innerException;
-      }
-
-      public Throwable getInnerException() {
-        return innerException;
+      try {
+        guarded.execute();
+      } finally {
+        lock.release();
       }
     }
   }
+
+  /**
+   * Abstract class that needs to be extended by implementing the code
+   * section that needs to be guarded in the {@link FileLockGuard.Guarded#execute()}
+   * method.
+   */
+  public static abstract class Guarded {
+    public abstract void execute() throws InnerException;
+  }
+
+  /**
+   * This exception can be thrown within the guarded code section. The main
+   * purpose is to be able to catch it outside the {@link FileLockGuard#guard(File, FileLockGuard.Guarded)}
+   * method call so that the original inner exception can be retrieved and
+   * re-thrown.
+   */
+  public static class InnerException extends Exception {
+    private final Throwable innerException;
+
+    public InnerException(Throwable innerException) {
+      this.innerException = innerException;
+    }
+
+    public Throwable getInnerException() {
+      return innerException;
+    }
+  }
+}
