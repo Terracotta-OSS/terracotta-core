ed74ff6d84685e2fe7f1ec83238bcd5ff8edbeeb
Juris Galang <jgalang@terracottatech.com>
2008-03-17 21:12:20 +0000

    got rid of extrau quotes
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@7553 7fc7bbf3-cf45-46d4-be06-341739edd864

--- kits/skeletons/shared-base/common/bin/boot-jar-path.bat
+++ kits/skeletons/shared-base/common/bin/boot-jar-path.bat
@@ -24,7 +24,7 @@ rem
 
 if not defined JAVA_HOME set JAVA_HOME="%TC_INSTALL_DIR%\jre"
 if defined JAVA_HOME set JAVA_HOME="%JAVA_HOME:%"=%"
-if not exist %JAVA_HOME%" set JAVA_HOME=%TC_INSTALL_DIR%\jre
+if not exist %JAVA_HOME% set JAVA_HOME=%TC_INSTALL_DIR%\jre
 FOR %%i IN (%JAVA_HOME%) DO SET JAVA_HOME=%%~fsi
 
 set JAVACMD=%JAVA_HOME%\bin\java
--- kits/skeletons/shared-base/common/bin/cvt.bat
+++ kits/skeletons/shared-base/common/bin/cvt.bat
@@ -12,7 +12,7 @@ for %%i in ("%TC_INSTALL_DIR%") do set TC_INSTALL_DIR=%%~fsi
 
 if not defined JAVA_HOME set JAVA_HOME="%TC_INSTALL_DIR%\jre"
 if defined JAVA_HOME set JAVA_HOME="%JAVA_HOME:%"=%"
-if not exist %JAVA_HOME%" set JAVA_HOME=%TC_INSTALL_DIR%\jre
+if not exist %JAVA_HOME% set JAVA_HOME=%TC_INSTALL_DIR%\jre
 FOR %%i IN (%JAVA_HOME%) DO SET JAVA_HOME=%%~fsi
 
 set CLASSPATH=%TC_INSTALL_DIR%\lib\tc.jar
