7670a48572cb397e6fef13a2dc411ee4523becd0
Nabib El-Rahman <nelrahma@terracottatech.com>
2008-03-28 01:15:47 +0000

    put some print statements in the test to see when during initializing list, or reading list does that test fail in the monkeys.
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@7727 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-system-tests/tests.system/com/tctest/MutualReferenceCollectionTest.java
+++ code/base/dso-system-tests/tests.system/com/tctest/MutualReferenceCollectionTest.java
@@ -73,14 +73,20 @@ public class MutualReferenceCollectionTest extends TransparentTestBase {
       if (barrier.barrier() == 0) {
         synchronized (firstList) {
           for (int i = 0; i < 5000; i++) {
-            firstList.add(new Object());
+           firstList.add(new Object());
+           if((i % 100) == 0) {
+             System.out.println(i + "entries has been intialized for firstList thus far, for thread " + Thread.currentThread());
+           }
           }
           // reference secondList
           firstList.add(secondList);
         }
         synchronized (secondList) {
           for (int i = 1; i < 5000; i++) {
-            secondList.add(new Object());
+           secondList.add(new Object());
+           if((i % 100) == 0) {
+             System.out.println(i + "entries has been intialized for secondList thus far, for thread " + Thread.currentThread());
+           }
           }
           // reference firstList
           secondList.add(firstList);
@@ -117,8 +123,13 @@ public class MutualReferenceCollectionTest extends TransparentTestBase {
       private void doWork() throws Throwable {
         // iterate through the list to force faulting
         synchronized (list) {
+          int i = 0;
           for (Iterator it = list.iterator(); it.hasNext();) {
             it.next();
+            if((i % 100) == 0) {
+              System.out.println(i + "entries has been read thus far for " + Thread.currentThread());
+            }
+            i++;
             Thread.sleep(5 + (int) (Math.random() * 10));
           }
         }
