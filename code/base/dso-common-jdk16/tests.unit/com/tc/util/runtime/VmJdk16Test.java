12323f50d1f9fdaea8a6c5e673c2cda78d8d1620
Nabib El-Rahman <nelrahma@terracottatech.com>
2008-03-30 21:20:33 +0000

    run the test only of the JRE running the test is JRE 1.6.
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@7793 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-common-jdk16/tests.unit/com/tc/util/runtime/VmJdk16Test.java
+++ code/base/dso-common-jdk16/tests.unit/com/tc/util/runtime/VmJdk16Test.java
@@ -1,5 +1,6 @@
 /*
- * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
  */
 package com.tc.util.runtime;
 
@@ -11,12 +12,14 @@ import junit.framework.TestCase;
 public class VmJdk16Test extends TestCase {
 
   public void testValidateJdk16() {
-    assertFalse(Vm.isJDK14());
-    assertFalse(Vm.isJDK15());
-    assertTrue(Vm.isJDK15Compliant());
-    assertTrue(Vm.isJDK16());
-    assertTrue(Vm.isJDK16Compliant());
-    assertFalse(Vm.isJDK17());
+    if (Vm.isJDK16()) {
+      assertFalse(Vm.isJDK14());
+      assertFalse(Vm.isJDK15());
+      assertTrue(Vm.isJDK15Compliant());
+      assertTrue(Vm.isJDK16());
+      assertTrue(Vm.isJDK16Compliant());
+      assertFalse(Vm.isJDK17());
+    }
   }
-  
+
 }
