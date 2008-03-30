b9715959c8528af33e67cb46de09381d5022e8ef
Hung Huynh <hhuynh@gmail.com>
2008-03-30 17:54:25 +0000

    disable ThreadDumpUtilJdk15Test
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@7784 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-common-jdk15/tests.unit/com/tc/runtime/ThreadDumpUtilJdk15Test.java
+++ code/base/dso-common-jdk15/tests.unit/com/tc/runtime/ThreadDumpUtilJdk15Test.java
@@ -1,24 +1,29 @@
 /*
- * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
  */
 package com.tc.runtime;
 
+import com.tc.test.TCTestCase;
 import com.tc.util.Assert;
 import com.tc.util.runtime.ThreadDumpUtil;
 import com.tc.util.runtime.ThreadDumpUtilJdk15;
 
-import junit.framework.TestCase;
+public class ThreadDumpUtilJdk15Test extends TCTestCase {
 
-public class ThreadDumpUtilJdk15Test extends TestCase {
+  public ThreadDumpUtilJdk15Test() {
+    disableAllUntil("2008-04-15");
+  }
 
   public void testThreadDump15() {
-    if (true) return;
     String st = ThreadDumpUtilJdk15.getThreadDump();
-    Assert.eval("The text \"Full thread dump \" should be present in the thread dump", st.indexOf("Full thread dump ") >= 0);
+    Assert.eval("The text \"Full thread dump \" should be present in the thread dump",
+                st.indexOf("Full thread dump ") >= 0);
   }
 
   public void testThreadDump14() {
     String st = ThreadDumpUtil.getThreadDump();
-    Assert.eval("The text \"Full thread dump \" should be present in the thread dump", st.indexOf("Full thread dump ") >= 0);
+    Assert.eval("The text \"Full thread dump \" should be present in the thread dump",
+                st.indexOf("Full thread dump ") >= 0);
   }
 }
