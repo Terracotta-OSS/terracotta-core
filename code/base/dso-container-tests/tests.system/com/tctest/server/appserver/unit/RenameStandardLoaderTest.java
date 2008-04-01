5fec47e4fdfeb146c9ac6545224cf48f953344ad
Hung Huynh <hhuynh@gmail.com>
2008-04-01 19:23:05 +0000

    MNK-483: timebomb
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@7841 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-container-tests/tests.system/com/tctest/server/appserver/unit/RenameStandardLoaderTest.java
+++ code/base/dso-container-tests/tests.system/com/tctest/server/appserver/unit/RenameStandardLoaderTest.java
@@ -33,7 +33,7 @@ public class RenameStandardLoaderTest extends AbstractOneServerDeploymentTest {
 
   public RenameStandardLoaderTest() {
     if (appServerInfo().getId() == AppServerInfo.WEBSPHERE) {
-      disableAllUntil("2008-04-31");
+      disableAllUntil("2008-05-15");
     }
   }
 
