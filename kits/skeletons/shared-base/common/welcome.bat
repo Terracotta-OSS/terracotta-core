3fc30abfd58e716990ddc32f8ba1208983aa37a0
Juris Galang <jgalang@terracottatech.com>
2008-04-01 01:07:51 +0000

    merged from 2.6
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@7830 7fc7bbf3-cf45-46d4-be06-341739edd864

--- kits/skeletons/shared-base/common/welcome.bat
+++ kits/skeletons/shared-base/common/welcome.bat
@@ -7,13 +7,16 @@ rem  All rights reserved.
 rem
 
 setlocal
-set TC_INSTALL_DIR=%~d0%~p0
+set TC_INSTALL_DIR=%~d0%~p0.
 set TC_INSTALL_DIR="%TC_INSTALL_DIR:"=%"
 
 if not defined JAVA_HOME set JAVA_HOME="%TC_INSTALL_DIR%\jre"
 set JAVA_HOME="%JAVA_HOME:"=%"
 
-set CLASSPATH=%TC_INSTALL_DIR%lib\tc.jar
-set JAVA_OPTS=-Dtc.install-root=%TC_INSTALL_DIR% %JAVA_OPTS%
-start "Terraotta Welcome" /B %JAVA_HOME%\bin\java %JAVA_OPTS% -cp %CLASSPATH% com.tc.welcome.WelcomeFrame %*
+set CLASSPATH=%TC_INSTALL_DIR%\lib\tc.jar
+
+set OPTS=-Djava.awt.Window.locationByPlatform=true
+set OPTS=%OPTS% -Dtc.install-root=%TC_INSTALL_DIR%
+set JAVA_OPTS=%OPTS% %JAVA_OPTS%
+start "Terracotta Welcome" /B %JAVA_HOME%\bin\java %JAVA_OPTS% -cp %CLASSPATH% com.tc.welcome.WelcomeFrame %*
 endlocal
