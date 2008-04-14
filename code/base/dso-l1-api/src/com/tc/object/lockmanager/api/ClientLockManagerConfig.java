8dfe87dd2c03c2d10f60ce5bafdc72a79c2695b1
Nabib El-Rahman <nelrahma@terracottatech.com>
2008-04-14 23:45:20 +0000

    fix compilation error
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@8188 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-l1-api/src/com/tc/object/lockmanager/api/ClientLockManagerConfig.java
+++ code/base/dso-l1-api/src/com/tc/object/lockmanager/api/ClientLockManagerConfig.java
@@ -11,16 +11,3 @@ public interface ClientLockManagerConfig {
   public long getTimeoutInterval();
 
 }
-/*
- * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
- */
-package com.tc.object.lockmanager.api;
-
-public interface ClientLockManagerConfig {
-  
-  public static final long DEFAULT_TIMEOUT_INTERVAL = 6000;
-  
-  
-  public long getTimeoutInterval();
-
-}
