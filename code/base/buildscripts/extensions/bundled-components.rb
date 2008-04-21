778d82247e4612bbeb19786761c0c487aac6b2f5
Hung Huynh <hhuynh@gmail.com>
2008-04-21 21:05:13 +0000

    merge 8276 from 2.6
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@8277 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/buildscripts/extensions/bundled-components.rb
+++ code/base/buildscripts/extensions/bundled-components.rb
@@ -43,7 +43,7 @@ module BundledComponents
     jarfile = FilePath.new(destdir, "tc.jar")
     if File.exist?(libdir.to_s)
       ant.jar(:destfile => jarfile.to_s, :basedir => runtime_classes_dir.to_s) do
-        classpath = 'svt.jar '
+        classpath = ''
         libfiles  = Dir.entries(libdir.to_s).delete_if { |item| /\.jar$/ !~ item }
         classpath << "#{libfiles.first} "
         libfiles[1..-2].each { |item| classpath << "#{item} " }
