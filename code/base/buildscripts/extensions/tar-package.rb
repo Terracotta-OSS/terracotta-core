aa9d16bbe863d65182c57e85f689914b33de8806
Hung Huynh <hhuynh@gmail.com>
2008-04-23 02:39:12 +0000

    DEV-1573: all add DLL to the fileset that needs exec permission while tar gzip the package
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@8301 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/buildscripts/extensions/tar-package.rb
+++ code/base/buildscripts/extensions/tar-package.rb
@@ -9,7 +9,7 @@ class BaseCodeTerracottaBuilder <  TerracottaBuilder
   def make_package(srcdir, destdir, filename, install_name, internal_name)
     ant.tar(:destfile => FilePath.new("#{destdir}", "#{filename}.tar.gz").to_s, :compression => 'gzip', :longfile => 'gnu') do
       ant.tarfileset(:dir => srcdir.to_s, :prefix => install_name, :excludes => "**/*.sh **/*.bat **/*.exe **/bin/** **/libexec/**") 
-      ant.tarfileset(:dir => srcdir.to_s, :prefix => install_name, :includes => "**/*.sh **/*.bat **/*.exe **/bin/** **/libexec/**", :mode => 755) 
+      ant.tarfileset(:dir => srcdir.to_s, :prefix => install_name, :includes => "**/*.dll **/*.sh **/*.bat **/*.exe **/bin/** **/libexec/**", :mode => 755) 
     end
   end
 end
