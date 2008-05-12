a147315118590df05c01ae690dbee89e5de4ecaa
Hung Huynh <hhuynh@gmail.com>
2008-05-12 01:05:07 +0000

    fixed occasional failure of not being to clean up in Windows.
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@8503 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-l1/tests.unit/com/tc/plugins/ModulesLoaderTest.java
+++ code/base/dso-l1/tests.unit/com/tc/plugins/ModulesLoaderTest.java
@@ -1,5 +1,6 @@
 /*
- * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
  */
 package com.tc.plugins;
 
@@ -30,43 +31,46 @@ public class ModulesLoaderTest extends BaseDSOTestCase {
 
   /**
    * Helper method to check that an error message contains some expected text
+   * 
    * @param t The error, will verify has non-null message
    * @param expectedText Expected text in message
    */
   private void checkErrorMessageContainsText(Throwable t, String expectedText) {
     String message = t.getMessage();
     Assert.assertNotNull("Expected non-null error message", message);
-    Assert.assertTrue("Expected error message to contain '" + expectedText + "' but was '" + message + "'", 
-                      message.indexOf(expectedText) >= 0);
-  }  
+    Assert.assertTrue("Expected error message to contain '" + expectedText + "' but was '" + message + "'", message
+        .indexOf(expectedText) >= 0);
+  }
 
+  protected boolean cleanTempDir() {
+    return false;
+  }
+  
   /**
-   * Test that missing bundle will cause ModulesLoader to throw exception
-   * and also that the message thrown 
+   * Test that missing bundle will cause ModulesLoader to throw exception and also that the message thrown
    */
   public void testMissingModule() throws Exception {
     String nonexistentBundle = "IDoNotExistThereforeIAmNot";
     String nonexistentVersion = "1.0.0";
-    
+
     DSOClientConfigHelper configHelper = configHelper();
     configHelper.addModule(nonexistentBundle, nonexistentVersion);
     ClassProvider classProvider = new MockClassProvider();
-   
+
     try {
       Modules modules = configHelper.getModulesForInitialization();
       EmbeddedOSGiRuntime osgiRuntime = EmbeddedOSGiRuntime.Factory.createOSGiRuntime(modules);
       ModulesLoader.initModules(osgiRuntime, configHelper, classProvider, modules.getModuleArray(), false);
       Assert.fail("Should get exception on missing bundle");
 
-    } catch(BundleException e) {
+    } catch (BundleException e) {
       checkErrorMessageContainsText(e, nonexistentBundle);
       checkErrorMessageContainsText(e, nonexistentVersion);
     }
   }
-  
+
   /**
-   * CDV-552 - when a module with a bad bundle version is loaded, tell the user
-   * which module caused startup to fail.
+   * CDV-552 - when a module with a bad bundle version is loaded, tell the user which module caused startup to fail.
    */
   public void testBadModuleBundleManifestVersion() throws Exception {
     String badGroupId = "org.terracotta.modules";
@@ -74,76 +78,77 @@ public class ModulesLoaderTest extends BaseDSOTestCase {
     String badVersion = "1.0.0.SNAPSHOT";
     String badSymbolicName = badGroupId + "." + badArtifactId;
     String badSymbolicVersion = "TedNugent";
-    
+
     // Create bundle jar based on these attributes
-    // Note: on windows, when this osgi startup fails, the jar file is left 
-    // open and prevents deletion.  So, for this particular test, we write 
+    // Note: on windows, when this osgi startup fails, the jar file is left
+    // open and prevents deletion. So, for this particular test, we write
     // instead to the java temp dir and mark to delete on exit
     File tempDir = new File(System.getProperty("java.io.tmpdir"));
-    File generatedJar1 = createBundle(tempDir, badGroupId, badArtifactId, badVersion, 
-                                      badSymbolicName, badSymbolicVersion, 
-                                      null, null);
+    File generatedJar1 = createBundle(tempDir, badGroupId, badArtifactId, badVersion, badSymbolicName,
+                                      badSymbolicVersion, null, null);
     generatedJar1.deleteOnExit();
-    
+
     EmbeddedOSGiRuntime osgiRuntime = null;
     try {
       DSOClientConfigHelper configHelper = configHelper();
-      
+
       // Add temp dir to list of repository locations to pick up bundle above
-      configHelper.addRepository(tempDir.toURL().toString());
+      configHelper.addRepository(tempDir.getAbsolutePath());
       configHelper.addModule(badArtifactId, badVersion);
       ClassProvider classProvider = new MockClassProvider();
-     
+
       try {
         Modules modules = configHelper.getModulesForInitialization();
         osgiRuntime = EmbeddedOSGiRuntime.Factory.createOSGiRuntime(modules);
-        ModulesLoader.initModules(osgiRuntime, configHelper, classProvider, modules.getModuleArray(), false);        
+        ModulesLoader.initModules(osgiRuntime, configHelper, classProvider, modules.getModuleArray(), false);
         Assert.fail("Should get exception on invalid bundle");
-        
-      } catch(BundleException e) {
+
+      } catch (BundleException e) {
         checkErrorMessageContainsText(e, badGroupId);
         checkErrorMessageContainsText(e, badArtifactId);
         checkErrorMessageContainsText(e, badVersion);
       }
-      
+
     } finally {
       shutdownAndCleanUpJars(osgiRuntime, null);
     }
   }
-    
-  private File createBundle(File repositoryRootDir, String groupId, String artifactId, String version, String bundleName, String bundleVersion, String requiredBundles, String tcXml) throws IOException {
+
+  private File createBundle(File repositoryRootDir, String groupId, String artifactId, String version,
+                            String bundleName, String bundleVersion, String requiredBundles, String tcXml)
+      throws IOException {
     File groupLocation = new File(repositoryRootDir, groupId.replace('.', File.separatorChar));
     File nameLocation = new File(groupLocation, artifactId);
     File versionLocation = new File(nameLocation, version);
     versionLocation.mkdirs();
     File bundleFile = new File(versionLocation, artifactId + "-" + version + ".jar");
-    
+
     Map manifestAttributes = new HashMap();
     manifestAttributes.put("Bundle-SymbolicName", bundleName);
     manifestAttributes.put("Bundle-Version", bundleVersion);
-    if(requiredBundles != null) {
+    if (requiredBundles != null) {
       manifestAttributes.put("RequireBundle", requiredBundles);
     }
-    
+
     createBundleWithManifest(bundleFile.getAbsolutePath(), manifestAttributes, tcXml);
-    
+
     return bundleFile;
   }
-  
+
   private void createBundleWithManifest(String jarLocation, Map manifestProps, String tcXml) throws IOException {
     Manifest manifest = new Manifest();
     Attributes attributes = manifest.getMainAttributes();
     Iterator attrIter = manifestProps.entrySet().iterator();
     attributes.put(Name.MANIFEST_VERSION, "1.0");
-    while(attrIter.hasNext()) {
-      Map.Entry entry = (Map.Entry)attrIter.next();
-      attributes.putValue((String)entry.getKey(), (String)entry.getValue());
+    while (attrIter.hasNext()) {
+      Map.Entry entry = (Map.Entry) attrIter.next();
+      attributes.putValue((String) entry.getKey(), (String) entry.getValue());
     }
-    
+
     FileOutputStream fstream = new FileOutputStream(jarLocation);
     JarOutputStream stream = new JarOutputStream(fstream, manifest);
 
-    if(tcXml != null) {
+    if (tcXml != null) {
       stream.putNextEntry(new JarEntry("terracotta.xml"));
       OutputStreamWriter writer = new OutputStreamWriter(stream, "utf-8");
       writer.write(tcXml);
@@ -153,30 +158,7 @@ public class ModulesLoaderTest extends BaseDSOTestCase {
 
     stream.close();
   }
-  
-//  /**
-//   * Test that using a non-file repository will throw an exception, alerting the user.
-//   * See DEV-1176.
-//   */
-//  public void testNonFileRepository() throws Exception {
-//    String repo = "http://terracotta.org";
-//    
-//    DSOClientConfigHelper configHelper = configHelper();
-//    ClassProvider classProvider = new MockClassProvider();
-//   
-//    try {
-//      configHelper.addRepository(repo);
-//      Modules modules = configHelper.getModulesForInitialization();
-//      EmbeddedOSGiRuntime osgiRuntime = EmbeddedOSGiRuntime.Factory.createOSGiRuntime(modules);
-//      ModulesLoader.initModules(osgiRuntime, configHelper, classProvider, modules.getModuleArray(), false);
-//      Assert.fail("Should get exception on non-file repository");
-//
-//    } catch(BundleException e) {
-//      checkErrorMessageContainsText(e, repo);
-//      checkErrorMessageContainsText(e, "file");
-//    }
-//  }
-  
+
   /**
    * Test that using a windows file path with spaces does not throw an exception
    */
@@ -185,7 +167,7 @@ public class ModulesLoaderTest extends BaseDSOTestCase {
 
     DSOClientConfigHelper configHelper = configHelper();
     ClassProvider classProvider = new MockClassProvider();
-   
+
     EmbeddedOSGiRuntime osgiRuntime = null;
     try {
       configHelper.addRepository(repo);
@@ -198,47 +180,45 @@ public class ModulesLoaderTest extends BaseDSOTestCase {
   }
 
   /**
-   * CDV-553 - when a module with a bad terracotta.xml is loaded, throw an error
-   * and identify the module
+   * CDV-553 - when a module with a bad terracotta.xml is loaded, throw an error and identify the module
    */
   public void testBadModuleXmlThrowsError() throws Exception {
     String badGroupId = "org.terracotta.modules";
     String badArtifactId = "badxml";
     String badVersion = "1.0.0";
     String badSymbolicName = badGroupId + "." + badArtifactId;
-    
+
     // Create bundle jar based on these attributes
     File tempDir = getTempDirectory();
-    File generatedJar1 = createBundle(tempDir, badGroupId, badArtifactId, badVersion, 
-                                      badSymbolicName, badVersion, 
+    File generatedJar1 = createBundle(tempDir, badGroupId, badArtifactId, badVersion, badSymbolicName, badVersion,
                                       null, TC_CONFIG_MISSING_FIRST_CHAR);
-    
+
     EmbeddedOSGiRuntime osgiRuntime = null;
     try {
       DSOClientConfigHelper configHelper = configHelper();
-      
+
       // Add temp dir to list of repository locations to pick up bundle above
-      configHelper.addRepository(tempDir.toURL().toString());
+      configHelper.addRepository(tempDir.getAbsolutePath());
       configHelper.addModule(badArtifactId, badVersion);
       ClassProvider classProvider = new MockClassProvider();
-     
+
       try {
         Modules modules = configHelper.getModulesForInitialization();
         osgiRuntime = EmbeddedOSGiRuntime.Factory.createOSGiRuntime(modules);
         ModulesLoader.initModules(osgiRuntime, configHelper, classProvider, modules.getModuleArray(), false);
         Assert.fail("Should get exception on invalid bundle");
-        
-      } catch(BundleException e) {
+
+      } catch (BundleException e) {
         checkErrorMessageContainsText(e, badGroupId);
         checkErrorMessageContainsText(e, badArtifactId);
         checkErrorMessageContainsText(e, badVersion);
       }
-      
+
     } finally {
       shutdownAndCleanUpJars(osgiRuntime, new File[] { generatedJar1 });
     }
   }
-  
+
   /**
    * Test catch and throw of ConfigSetupException due to root with no field or expression
    */
@@ -247,64 +227,60 @@ public class ModulesLoaderTest extends BaseDSOTestCase {
     String badArtifactId = "badconfig";
     String badVersion = "1.0.0";
     String badSymbolicName = badGroupId + "." + badArtifactId;
-    
+
     // Create bundle jar based on these attributes
     File tempDir = getTempDirectory();
-    File generatedJar1 = createBundle(tempDir, badGroupId, badArtifactId, badVersion, 
-                                      badSymbolicName, badVersion, 
+    File generatedJar1 = createBundle(tempDir, badGroupId, badArtifactId, badVersion, badSymbolicName, badVersion,
                                       null, TC_CONFIG_NO_ROOT_FIELD_OR_EXPR);
-    
+
     EmbeddedOSGiRuntime osgiRuntime = null;
     try {
       DSOClientConfigHelper configHelper = configHelper();
-      
+
       // Add temp dir to list of repository locations to pick up bundle above
-      configHelper.addRepository(tempDir.toURL().toString());
+      configHelper.addRepository(tempDir.getAbsolutePath());
       configHelper.addModule(badArtifactId, badVersion);
       ClassProvider classProvider = new MockClassProvider();
-     
+
       try {
         Modules modules = configHelper.getModulesForInitialization();
         osgiRuntime = EmbeddedOSGiRuntime.Factory.createOSGiRuntime(modules);
         ModulesLoader.initModules(osgiRuntime, configHelper, classProvider, modules.getModuleArray(), false);
         Assert.fail("Should get exception on invalid config");
-        
-      } catch(BundleException e) {
+
+      } catch (BundleException e) {
         checkErrorMessageContainsText(e, badGroupId);
         checkErrorMessageContainsText(e, badArtifactId);
         checkErrorMessageContainsText(e, badVersion);
-        checkErrorMessageContainsText(e, "no_expr");     // check root name is in message
+        checkErrorMessageContainsText(e, "no_expr"); // check root name is in message
       }
-      
+
     } finally {
       shutdownAndCleanUpJars(osgiRuntime, new File[] { generatedJar1 });
     }
   }
-  
-  private static final String TC_CONFIG_MISSING_FIRST_CHAR = "?xml version=\"1.0\" encoding=\"UTF-8\" ?>" + 
-    "<xml-fragment>" + 
-    "</xml-fragment>";
-  
-  private static final String TC_CONFIG_NO_ROOT_FIELD_OR_EXPR = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>" + 
-  "<xml-fragment>" +
-  "<roots>" + 
-  "<root><root-name>no_expr</root-name></root>" + 
-  "</roots>" + 
-  "</xml-fragment>";
+
+  private static final String TC_CONFIG_MISSING_FIRST_CHAR    = "?xml version=\"1.0\" encoding=\"UTF-8\" ?>"
+                                                                + "<xml-fragment>" + "</xml-fragment>";
+
+  private static final String TC_CONFIG_NO_ROOT_FIELD_OR_EXPR = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>"
+                                                                + "<xml-fragment>" + "<roots>"
+                                                                + "<root><root-name>no_expr</root-name></root>"
+                                                                + "</roots>" + "</xml-fragment>";
 
   private void shutdownAndCleanUpJars(EmbeddedOSGiRuntime osgiRuntime, File[] jars) {
     // Shutdown and wait for open jar references to get cleaned up
-    if(osgiRuntime != null) {
+    if (osgiRuntime != null) {
       osgiRuntime.shutdown();
       try {
         Thread.sleep(2000);
-      } catch(InterruptedException e) {
+      } catch (InterruptedException e) {
         // ignore
       }
     }
-    
-    if(jars != null) {
-      for(int i=0; i<jars.length; i++) {
+
+    if (jars != null) {
+      for (int i = 0; i < jars.length; i++) {
         jars[i].delete();
       }
     }
