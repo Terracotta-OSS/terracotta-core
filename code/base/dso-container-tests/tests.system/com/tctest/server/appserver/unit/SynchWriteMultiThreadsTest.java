d227b8b2c89affc8654f0b39002c47ada5103d60
Hung Huynh <hhuynh@gmail.com>
2008-05-29 01:41:02 +0000

    set synchronous-write to false to force the test to fail to prove its sanity
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@8662 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-container-tests/tests.system/com/tctest/server/appserver/unit/SynchWriteMultiThreadsTest.java
+++ code/base/dso-container-tests/tests.system/com/tctest/server/appserver/unit/SynchWriteMultiThreadsTest.java
@@ -115,7 +115,7 @@ public class SynchWriteMultiThreadsTest extends AbstractTwoServerDeploymentTest 
     }
 
     protected void configureTcConfig(TcConfigBuilder clientConfig) {
-      clientConfig.addWebApplication(CONTEXT, true);
+      clientConfig.addWebApplication(CONTEXT, false);
     }
   }
 }
