d41d08f94da4a832399b86b4296a2c120d8a9330
Nabib El-Rahman <nelrahma@terracottatech.com>
2008-05-31 01:27:00 +0000

    system test to validate lock held bug in clientlockmanager
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@8696 7fc7bbf3-cf45-46d4-be06-341739edd864

---
+++ code/base/dso-system-tests/tests.base/com/tctest/ClientGCLockHeldApp.java
@@ -0,0 +1,144 @@
+/*
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
+ */
+package com.tctest;
+
+import EDU.oswego.cs.dl.util.concurrent.BrokenBarrierException;
+import EDU.oswego.cs.dl.util.concurrent.CyclicBarrier;
+import EDU.oswego.cs.dl.util.concurrent.SynchronizedInt;
+
+import com.tc.object.config.ConfigVisitor;
+import com.tc.object.config.DSOClientConfigHelper;
+import com.tc.object.config.TransparencyClassSpec;
+import com.tc.object.config.spec.CyclicBarrierSpec;
+import com.tc.object.config.spec.SynchronizedIntSpec;
+import com.tc.simulator.app.ApplicationConfig;
+import com.tc.simulator.listener.ListenerProvider;
+import com.tctest.runner.AbstractErrorCatchingTransparentApp;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class ClientGCLockHeldApp extends AbstractErrorCatchingTransparentApp {
+
+  private static final int MINUTES_TEST_RUN = 10;
+
+  final List               lockList         = new ArrayList();
+  final List               lockObj          = new ArrayList();
+  CyclicBarrier            barrier;
+
+  public ClientGCLockHeldApp(String appId, ApplicationConfig cfg, ListenerProvider listenerProvider) {
+    super(appId, cfg, listenerProvider);
+  }
+
+  public static void visitL1DSOConfig(ConfigVisitor visitor, DSOClientConfigHelper config) {
+    String testClass = ClientGCLockHeldApp.class.getName();
+    TransparencyClassSpec spec = config.getOrCreateSpec(testClass);
+    spec.addRoot("lockList", "lockList");
+    spec.addRoot("lockObj", "lockObj");
+    spec.addRoot("barrier", "barrier");
+    String methodExpression = "* " + testClass + "*.*(..)";
+    config.addWriteAutolock(methodExpression);
+    new SynchronizedIntSpec().visit(visitor, config);
+    new CyclicBarrierSpec().visit(visitor, config);
+  }
+
+  public void runTest() throws BrokenBarrierException, InterruptedException {
+    setCyclicBarrier();
+    runGCTest();
+  }
+
+  private void setCyclicBarrier() {
+    int participationCount = getParticipantCount();
+    log("Participation Count = " + participationCount);
+    barrier = new CyclicBarrier(participationCount);
+  }
+
+  static DateFormat formatter = new SimpleDateFormat("hh:mm:ss,S");
+
+  private static void log(String message) {
+    System.err.println(Thread.currentThread().getName() + " :: "
+                       + formatter.format(new Date(System.currentTimeMillis())) + " : " + message);
+  }
+
+  public void runGCTest() throws BrokenBarrierException, InterruptedException {
+
+    barrier.barrier();
+
+    // start a thread that holds a lock forever.
+
+    // add to list to make shared;
+    RunForeverThread thread = new RunForeverThread();
+    thread.start();
+
+    Stopwatch stopwatch = new Stopwatch().start();
+
+    for (int i = 0; stopwatch.getElapsedTime() < (1000 * 60 * MINUTES_TEST_RUN); i++) {
+      SynchronizedInt counter = new SynchronizedInt(0);
+      synchronized (lockList) {
+        lockList.add(counter);
+      }
+      counter.increment();
+    }
+
+  }
+  
+  public void holdLock() {
+    synchronized (lockObj) {
+      try {
+        Thread.currentThread().join();
+        System.out.println("should not reach here");
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  private class RunForeverThread extends Thread { 
+    
+    public void run() {
+      holdLock();
+    } 
+
+  }
+
+  private static class Stopwatch {
+    private long    startTime = -1;
+    private long    stopTime  = -1;
+    private boolean running   = false;
+
+    public Stopwatch start() {
+      startTime = System.currentTimeMillis();
+      running = true;
+      return this;
+    }
+
+    public Stopwatch stop() {
+      stopTime = System.currentTimeMillis();
+      running = false;
+      return this;
+    }
+
+    public long getElapsedTime() {
+      if (startTime == -1) { return 0; }
+      if (running) {
+        return System.currentTimeMillis() - startTime;
+      } else {
+        return stopTime - startTime;
+      }
+    }
+
+    public Stopwatch reset() {
+      startTime = -1;
+      stopTime = -1;
+      running = false;
+      return this;
+    }
+
+  }
+
+}
