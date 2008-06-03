38cfd2112f0b0e5fcf14dc5606f882cb5ab267c1
Hung Huynh <hhuynh@gmail.com>
2008-06-03 22:36:37 +0000

    MNK-512: disabled MutualReferenceCollectionTest
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@8732 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-system-tests/tests.system/com/tctest/MutualReferenceCollectionTest.java
+++ code/base/dso-system-tests/tests.system/com/tctest/MutualReferenceCollectionTest.java
@@ -26,6 +26,10 @@ import java.util.List;
 public class MutualReferenceCollectionTest extends TransparentTestBase {
   private static final int NODE_COUNT = 3;
 
+  public MutualReferenceCollectionTest() {
+    // MNK-512
+    disableAllUntil("2008-06-30");
+  }
   
   public void doSetUp(TransparentTestIface t) throws Exception {
     t.getTransparentAppConfig().setClientCount(NODE_COUNT);
