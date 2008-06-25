1be5fbb3376b337ac78b98dfa3fcdd13fdd6bb65
Manoj Govindassamy <mgovinda@terracottatech.com>
2008-06-25 08:04:55 +0000

    More tests for ObjectIDSetIterator remove - few corner cases added.
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@8926 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-common/tests.unit/com/tc/util/ObjectIDSetTest.java
+++ code/base/dso-common/tests.unit/com/tc/util/ObjectIDSetTest.java
@@ -21,6 +21,7 @@ import java.util.ConcurrentModificationException;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Random;
 import java.util.Set;
 import java.util.SortedSet;
@@ -356,5 +357,110 @@ public class ObjectIDSetTest extends TCTestCase {
     oidSetIterator = oidSet.iterator();
     long totalElements = iterateElements(oidSetIterator);
     assertEquals((visitedCount - removedCount), totalElements);
+  }
+
+  public void testObjectIDSetIteratorRemoveSpecailCases() {
+    List longList = new ArrayList();
+    longList.add(new ObjectID(25));
+    longList.add(new ObjectID(26));
+    longList.add(new ObjectID(27));
+    longList.add(new ObjectID(28));
+    longList.add(new ObjectID(9));
+    longList.add(new ObjectID(13));
+    longList.add(new ObjectID(12));
+    longList.add(new ObjectID(14));
+    longList.add(new ObjectID(18));
+    longList.add(new ObjectID(2));
+    longList.add(new ObjectID(23));
+    longList.add(new ObjectID(47));
+    longList.add(new ObjectID(35));
+    longList.add(new ObjectID(10));
+    longList.add(new ObjectID(1));
+    longList.add(new ObjectID(4));
+    longList.add(new ObjectID(15));
+    longList.add(new ObjectID(8));
+    longList.add(new ObjectID(56));
+    longList.add(new ObjectID(11));
+    longList.add(new ObjectID(10));
+    longList.add(new ObjectID(33));
+    longList.add(new ObjectID(17));
+    longList.add(new ObjectID(29));
+    longList.add(new ObjectID(19));
+    // Data : 1 2 4 8 9 10 11 12 13 14 15 17 18 19 23 25 26 27 28 29 33 35 47 56
+
+    /**
+     * ObjectIDSet { (oids:ranges) = 24:10 , compression ratio = 1.0 } [ Range(1,2) Range(4,4) Range(8,15) Range(17,19)
+     * Range(23,23) Range(25,29) Range(33,33) Range(35,35) Range(47,47) Range(56,56)]
+     */
+
+    Set objectIDSet = new SetCreatorImpl().create(longList);
+    int totalElements = longList.size() - 1;
+
+    Iterator i = objectIDSet.iterator();
+    assertEquals(totalElements, iterateElements(i));
+
+    List longSortList = new ArrayList();
+    i = objectIDSet.iterator();
+    while (i.hasNext()) {
+      longSortList.add(i.next());
+    }
+
+    // remove first element in a range. eg: 8 from (8,15)
+    removeElementFromIterator(objectIDSet.iterator(), totalElements, longSortList.indexOf(new ObjectID(8)) + 1, 9);
+    objectIDSet.add(new ObjectID(8)); // get back to original state
+
+    // remove last element in a range. eg: 19 from (17,19)
+    removeElementFromIterator(objectIDSet.iterator(), totalElements, longSortList.indexOf(new ObjectID(19)) + 1, 23);
+    objectIDSet.add(new ObjectID(19));
+
+    // remove the only element in the range. eg: 33 from (33,33)
+    removeElementFromIterator(objectIDSet.iterator(), totalElements, longSortList.indexOf(new ObjectID(33)) + 1, 35);
+    objectIDSet.add(new ObjectID(33));
+
+    // remove the least element
+    removeElementFromIterator(objectIDSet.iterator(), totalElements, longSortList.indexOf(new ObjectID(1)) + 1, 2);
+    objectIDSet.add(new ObjectID(1));
+
+    // remove the max element; element will be removed, but while going to next element, exception expected
+    try {
+      removeElementFromIterator(objectIDSet.iterator(), totalElements, longSortList.indexOf(new ObjectID(56)) + 1, -99);
+      throw new AssertionError("XXX we are suppose to go somewhere else ... ");
+    } catch (NoSuchElementException noSE) {
+      System.out.println("XXX Got the expected Exception " + noSE);
+    } finally {
+      objectIDSet.add(new ObjectID(56));
+    }
+
+    // remove the non existing element; exception expected
+    try {
+      removeElementFromIterator(objectIDSet.iterator(), totalElements, longSortList.indexOf(new ObjectID(16)) + 1, -99);
+      throw new AssertionError("XXX we are suppose to go somewhere else ... ");
+    } catch (IllegalStateException ise) {
+      System.out.println("XXX Got the expected Exception " + ise);
+    }
+
+    i = objectIDSet.iterator();
+    assertEquals(5, iterateElements(i, 5));
+    objectIDSet.add(new ObjectID(99));
+    try {
+      assertEquals(5, iterateElements(i, 1));
+      throw new AssertionError("XXX we are suppose to go somewhere else ... ");
+    } catch (ConcurrentModificationException cme) {
+      System.out.println("XXX Got the expected Exception " + cme);
+
+    } finally {
+      objectIDSet.remove(new ObjectID(99));
+    }
+
+  }
+
+  private void removeElementFromIterator(Iterator i, int totalElements, long indexOfRemoveElement,
+                                         int nextExpectedElement) {
+    long visitedElements = 0;
+    visitedElements += iterateElements(i, indexOfRemoveElement);
+    i.remove();
+    assertEquals(nextExpectedElement, ((ObjectID) i.next()).toLong());
+    visitedElements += iterateElements(i);
+    assertEquals(visitedElements, totalElements - 1);
   }
 }
