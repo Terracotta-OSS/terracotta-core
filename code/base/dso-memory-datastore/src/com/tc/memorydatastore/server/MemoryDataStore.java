d3cc2919d13e635fabaf5e80fa6eb0c78667cf97
Tim Eck <timeck@gmail.com>
2008-06-27 20:47:59 +0000

    removed unused inner class
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@8969 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-memory-datastore/src/com/tc/memorydatastore/server/MemoryDataStore.java
+++ code/base/dso-memory-datastore/src/com/tc/memorydatastore/server/MemoryDataStore.java
@@ -1,5 +1,6 @@
 /*
- * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
  */
 package com.tc.memorydatastore.server;
 
@@ -16,8 +17,6 @@ import java.util.Set;
 import java.util.TreeMap;
 
 public class MemoryDataStore {
-  private final static int MAX_TRIE_LEVEL = 8;
-
   private final Map        store          = new HashMap();
 
   public MemoryDataStore() {
@@ -59,13 +58,6 @@ public class MemoryDataStore {
     return dataStore;
   }
 
-  /*
-   * private ByteTrie getDataStore(String dataStoreName) { ByteTrie dataStore =
-   * (ByteTrie) this.store.get(dataStoreName); if (dataStore == null) {
-   * dataStore = new ByteTrie(); this.store.put(dataStoreName, dataStore); }
-   * return dataStore; }
-   */
-
   private static class ByteArrayObjectWrapper implements Comparable {
     private byte[] value;
 
@@ -74,8 +66,7 @@ public class MemoryDataStore {
     }
 
     public int hashCode() {
-      if (value == null)
-        return 0;
+      if (value == null) return 0;
 
       int result = 1;
       for (int i = 0; i < value.length; i++) {
@@ -86,12 +77,8 @@ public class MemoryDataStore {
     }
 
     public boolean equals(Object obj) {
-      if (obj == null) {
-        return false;
-      }
-      if (!(obj instanceof ByteArrayObjectWrapper)) {
-        return false;
-      }
+      if (obj == null) { return false; }
+      if (!(obj instanceof ByteArrayObjectWrapper)) { return false; }
       return Arrays.equals(this.value, ((ByteArrayObjectWrapper) obj).value);
     }
 
@@ -105,9 +92,7 @@ public class MemoryDataStore {
     }
 
     public int compareTo(Object obj) {
-      if (obj == null) {
-        throw new NullPointerException();
-      }
+      if (obj == null) { throw new NullPointerException(); }
       Assert.assertTrue(obj instanceof ByteArrayObjectWrapper);
 
       ByteArrayObjectWrapper objWrapper = (ByteArrayObjectWrapper) obj;
@@ -121,27 +106,19 @@ public class MemoryDataStore {
         if (value[i] == b[i]) {
           continue;
         }
-        if (value[i] < b[i]) {
-          return -1;
-        }
-        if (value[i] > b[i]) {
-          return 1;
-        }
+        if (value[i] < b[i]) { return -1; }
+        if (value[i] > b[i]) { return 1; }
       }
 
-      if (value.length == b.length) {
-        return 0;
-      }
-      if (value.length > b.length) {
-        return 1;
-      }
+      if (value.length == b.length) { return 0; }
+      if (value.length > b.length) { return 1; }
       return -1;
     }
 
     public boolean startsWith(ByteArrayObjectWrapper obj) {
       byte[] b = obj.value;
       if (value.length < b.length) { return false; }
-      
+
       for (int i = 0; i < b.length; i++) {
         if (value[i] == b[i]) {
           continue;
@@ -161,29 +138,25 @@ public class MemoryDataStore {
     }
 
     public void put(byte[] key, byte[] value) {
-      if (key.length <= 0) {
-        return;
-      }
+      if (key.length <= 0) { return; }
       map.put(new ByteArrayObjectWrapper(key), value);
     }
 
     public byte[] get(byte[] key) {
-      if (key.length <= 0) {
-        return null;
-      }
+      if (key.length <= 0) { return null; }
 
       return (byte[]) map.get(new ByteArrayObjectWrapper(key));
     }
 
     public Collection getAll(byte[] key) {
       if (key.length <= 0) {
-        //return null;
+        // return null;
       }
 
-      //long startTime = System.currentTimeMillis();
+      // long startTime = System.currentTimeMillis();
       ByteArrayObjectWrapper wrappedKey = new ByteArrayObjectWrapper(key);
       Map subMap = map.tailMap(wrappedKey);
-      //System.err.println("Num of potentials in getAll: " + subMap.size());
+      // System.err.println("Num of potentials in getAll: " + subMap.size());
 
       Collection returnValues = new ArrayList();
 
@@ -198,29 +171,25 @@ public class MemoryDataStore {
           break;
         }
       }
-      //System.err.println("Num to returns in getAll: " + returnValues.size());
-      //long endTime = System.currentTimeMillis();
-      //System.err.println("Time spent in getAll: " + (endTime - startTime) + " ms");
+      // System.err.println("Num to returns in getAll: " + returnValues.size());
+      // long endTime = System.currentTimeMillis();
+      // System.err.println("Time spent in getAll: " + (endTime - startTime) + " ms");
 
       return returnValues;
     }
 
     public byte[] remove(byte[] key) {
-      if (key.length <= 0) {
-        return null;
-      }
+      if (key.length <= 0) { return null; }
 
       return (byte[]) map.remove(new ByteArrayObjectWrapper(key));
     }
 
     public int removeAll(byte[] key) {
-      if (key.length <= 0) {
-        return 0;
-      }
+      if (key.length <= 0) { return 0; }
 
       ByteArrayObjectWrapper wrappedKey = new ByteArrayObjectWrapper(key);
       Map subMap = map.tailMap(wrappedKey);
-      //System.err.println("Num of potentials in getAll: " + subMap.size());
+      // System.err.println("Num of potentials in getAll: " + subMap.size());
 
       int returnValue = 0;
       Set entrySet = subMap.entrySet();
@@ -235,171 +204,10 @@ public class MemoryDataStore {
         }
       }
 
-      //System.err.println("Num to remove: " + returnValue);
+      // System.err.println("Num to remove: " + returnValue);
 
       return returnValue;
     }
-  }
-
-  private static class ByteTrie {
-    private final static int     NUM_OF_ENTRIES = 256;
-    private final ByteTrieNode[] head           = new ByteTrieNode[NUM_OF_ENTRIES];
-
-    public ByteTrie() {
-      super();
-    }
-
-    public void put(byte[] key, byte[] value) {
-      if (key.length <= 0) {
-        return;
-      }
-
-      ByteTrieNode node = getNode(key);
-      node.put(key, value);
-    }
-
-    public byte[] get(byte[] key) {
-      if (key.length <= 0) {
-        return null;
-      }
-
-      ByteTrieNode node = getNode(key);
-      return node.get(key);
-    }
-
-    public Collection getAll(byte[] key) {
-      if (key.length <= 0) {
-        return null;
-      }
-
-      ByteTrieNode node = getNode(key);
-      return node.getAll();
-    }
-
-    public byte[] remove(byte[] key) {
-      if (key.length <= 0) {
-        return null;
-      }
-
-      ByteTrieNode node = getNode(key);
-      return node.remove(key);
-    }
-
-    public int removeAll(byte[] key) {
-      if (key.length <= 0) {
-        return 0;
-      }
-
-      ByteTrieNode node = getNode(key);
-      return node.removeAll(key);
-    }
-
-    public void dumpTrie() {
-      for (int i = 0; i < head.length; i++) {
-        if (head[i] != null) {
-          System.err.println("head[" + i + "]: " + head[i]);
-          head[i].dumpTrie(2);
-        }
-      }
-    }
-
-    private ByteTrieNode getNode(byte[] key) {
-      ByteTrieNode node = getNode(head, key[0]);
-
-      int level = (MAX_TRIE_LEVEL == -1 || key.length < MAX_TRIE_LEVEL) ? key.length : MAX_TRIE_LEVEL;
-      for (int i = 1; i < level; i++) {
-        node = getNode(node.next, key[i]);
-      }
-      return node;
-    }
-
-    private ByteTrieNode getNode(ByteTrieNode[] nodes, byte b) {
-      ByteTrieNode node = nodes[b - 1];
-      if (node == null) {
-        node = new ByteTrieNode();
-        nodes[b - 1] = node;
-      }
-      return node;
-    }
-
-    private static class ByteTrieNode {
-      private final ByteTrieNode[] next        = new ByteTrieNode[NUM_OF_ENTRIES];
-      private final Map            dataEntries = new HashMap();
-
-      public void put(byte[] key, byte[] value) {
-        dataEntries.put(new ByteArrayObjectWrapper(key), value);
-      }
-
-      public byte[] get(byte[] key) {
-        return (byte[]) dataEntries.get(new ByteArrayObjectWrapper(key));
-      }
-
-      public Collection getAll() {
-        Collection returnValue = new ArrayList();
-        Set entrySet = dataEntries.entrySet();
-        for (Iterator i = entrySet.iterator(); i.hasNext();) {
-          Map.Entry entry = (Map.Entry) i.next();
-          ByteArrayObjectWrapper key = (ByteArrayObjectWrapper) entry.getKey();
-          TCByteArrayKeyValuePair keyValuePair = new TCByteArrayKeyValuePair(key.value, (byte[]) entry.getValue());
-          returnValue.add(keyValuePair);
-        }
-        for (int i = 0; i < next.length; i++) {
-          if (next[i] != null) {
-            returnValue.addAll(next[i].getAll());
-          }
-        }
-        return returnValue;
-      }
-
-      public byte[] remove(byte[] key) {
-        return (byte[]) dataEntries.remove(new ByteArrayObjectWrapper(key));
-      }
-
-      public int removeAll(byte[] key) {
-        int numOfRemove = getNumOfChilds();
-        dataEntries.clear();
-        for (int i = 0; i < next.length; i++) {
-          next[i] = null;
-        }
-        return numOfRemove;
-      }
-
-      public void dumpTrie(int numOfSpaces) {
-        printSpace(numOfSpaces);
-        System.err.println("Size of data: " + dataEntries.size());
-        Set keySet = dataEntries.keySet();
-        for (Iterator i = keySet.iterator(); i.hasNext();) {
-          Object key = i.next();
-          printSpace(numOfSpaces);
-          System.err.println("key: " + key);
-        }
-
-        for (int i = 0; i < next.length; i++) {
-          if (next[i] != null) {
-            printSpace(numOfSpaces);
-            System.err.println("next[" + i + "]: " + next[i]);
-            next[i].dumpTrie(numOfSpaces + 2);
-          }
-        }
-      }
-
-      private int getNumOfChilds() {
-        int numOfChilds = 0;
-        for (int i = 0; i < next.length; i++) {
-          if (next[i] != null) {
-            numOfChilds += next[i].getNumOfChilds();
-          }
-        }
-        return dataEntries.size() + numOfChilds;
-      }
-
-      private void printSpace(int numOfSpaces) {
-        for (int i = 0; i < numOfSpaces; i++) {
-          System.err.print(' ');
-        }
-      }
-    }
-
   }
 
 }
