eec6f9756800f3cb3cac251369171b9faaf5a9fb
Jason Voegele <jason@jvoegele.com>
2008-07-08 17:21:15 +0000

    Removed clustered-* modules that have been ported to the Forge.
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@9074 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/clustered-cglib-2.1.3/src/org/terracotta/modules/cglib_2_1_3/object/dna/impl/CGLibBulkBeanInstance.java
+++
@@ -1,8 +0,0 @@
-/*
- * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
- */
-package org.terracotta.modules.cglib_2_1_3.object.dna.impl;
-
-public interface CGLibBulkBeanInstance {
-  // This is a marker interface to indicate an instance is a cglib shared BulkBean instance.
-}
