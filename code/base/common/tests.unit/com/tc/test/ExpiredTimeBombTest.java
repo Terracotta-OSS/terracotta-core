19e0e6ddcb32bd6cd953ed05f5dd8091e54ccd6a
Hung Huynh <hhuynh@gmail.com>
2008-07-15 21:55:21 +0000

    fixed error message
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@9230 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/common/tests.unit/com/tc/test/ExpiredTimeBombTest.java
+++ code/base/common/tests.unit/com/tc/test/ExpiredTimeBombTest.java
@@ -22,6 +22,6 @@ public class ExpiredTimeBombTest extends TCTestCase {
       // expected
       return;
     }
-    fail("shouldn't have thrown exception when timebomb expired");
+    fail("should have thrown exception when timebomb expired");
   }
 }
