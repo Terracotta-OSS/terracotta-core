1bdebaf8ffda141fc8bfdc6bd81b38facea7bf2b
Tim Eck <timeck@gmail.com>
2008-07-16 19:29:54 +0000

    rework low mem workaround and apply to all load tests
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@9255 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-container-tests/tests.system/com/tctest/server/appserver/load/LowMemWorkaround.java
+++ code/base/dso-container-tests/tests.system/com/tctest/server/appserver/load/LowMemWorkaround.java
@@ -4,21 +4,33 @@
  */
 package com.tctest.server.appserver.load;
 
+import org.hyperic.sigar.Mem;
+import org.hyperic.sigar.Sigar;
+import org.hyperic.sigar.SigarException;
+
 import com.tc.test.AppServerInfo;
 import com.tc.text.Banner;
-import com.tc.util.runtime.Os;
-import com.tc.util.runtime.SystemMemory;
 
 public class LowMemWorkaround {
+  // This number isn't exact of course but it is appropriate for our monkey environments
+  private static final long TWO_GIGABYTES = 2000000000L;
+
   static int computeNumberOfNodes(int defaultNum, AppServerInfo appServerInfo) {
-    if (Os.isSolaris() && appServerInfo.getId() == AppServerInfo.GLASSFISH) {
-      long mem = SystemMemory.getTotalSystemMemory();
-      if (mem < (SystemMemory.GB * 2)) {
-        Banner.warnBanner("Using 2 nodes (instead of " + defaultNum + ") since this machine has limited memory (" + mem
-                          + ")");
+    try {
+      Sigar sigar = new Sigar();
+
+      Mem mem = sigar.getMem();
+
+      long memTotal = mem.getTotal();
+      if (memTotal < TWO_GIGABYTES) {
+        Banner.warnBanner("Using 2 nodes (instead of " + defaultNum + ") since this machine has limited memory ("
+                          + memTotal + ")");
         return 2;
       }
+
+      return defaultNum;
+    } catch (SigarException se) {
+      throw new RuntimeException(se);
     }
-    return defaultNum;
   }
 }
