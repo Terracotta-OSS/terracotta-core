86fd8ff04afebbecf407b52192fe90b388ac88bd
Geert Bevin <gbevin@terracottatech.com>
2008-07-18 10:16:00 +0000

    DEV-1137 : Visibility into Client Memory Bloat
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@9318 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-l2/src/com/tc/statistics/retrieval/actions/SRAL1ReferenceCount.java
+++ code/base/dso-l2/src/com/tc/statistics/retrieval/actions/SRAL1ReferenceCount.java
@@ -3,19 +3,32 @@
  */
 package com.tc.statistics.retrieval.actions;
 
+import com.tc.net.groups.NodeID;
 import com.tc.objectserver.l1.api.ClientStateManager;
 import com.tc.statistics.StatisticData;
 import com.tc.statistics.StatisticRetrievalAction;
 import com.tc.statistics.StatisticType;
+import com.tc.util.Assert;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+/**
+ */
 public class SRAL1ReferenceCount implements StatisticRetrievalAction {
 
-  public SRAL1ReferenceCount(ClientStateManager clientStateManager) {
-    // dummy
+  public final static String ACTION_NAME = "l1 reference count";
+
+  private final ClientStateManager clientStateManager;
+
+  public SRAL1ReferenceCount(final ClientStateManager clientStateManager) {
+    Assert.assertNotNull("clientStateManager", clientStateManager);
+    this.clientStateManager = clientStateManager;
   }
 
   public String getName() {
-    return "i suck";
+    return ACTION_NAME;
   }
 
   public StatisticType getType() {
@@ -23,7 +36,14 @@ public class SRAL1ReferenceCount implements StatisticRetrievalAction {
   }
 
   public StatisticData[] retrieveStatisticData() {
-    return new StatisticData[] { new StatisticData("i suck", 1L) };
+    List<StatisticData> data = new ArrayList<StatisticData>();
+    synchronized (clientStateManager) {
+      Set<NodeID> nodeIDs = clientStateManager.getConnectedClientIDs();
+      for (final NodeID nodeID : nodeIDs) {
+        data.add(new StatisticData(ACTION_NAME, nodeID.toString(), (long)clientStateManager.getReferenceCount(nodeID)));
+      }
+    }
+    StatisticData[] result = new StatisticData[data.size()];
+    return data.toArray(result);
   }
-
 }
