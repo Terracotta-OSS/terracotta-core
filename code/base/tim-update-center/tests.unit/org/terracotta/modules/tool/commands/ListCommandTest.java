9c03094bfc93705f7485581dc02d89760d091ed9
Hung Huynh <hhuynh@gmail.com>
2008-07-19 06:35:24 +0000

    MNK-648: timebomb
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@9357 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/tim-update-center/tests.unit/org/terracotta/modules/tool/commands/ListCommandTest.java
+++ code/base/tim-update-center/tests.unit/org/terracotta/modules/tool/commands/ListCommandTest.java
@@ -1,13 +1,18 @@
 /*
- * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
  */
 package org.terracotta.modules.tool.commands;
-
 
 /**
  * Test case for {@link ListCommand}.
  */
 public class ListCommandTest extends AbstractCommandTestCase {
+  
+  public ListCommandTest() {
+    // MNK-648
+    this.disableAllUntil("2008-08-15");
+  }
 
   @Override
   protected void setUp() throws Exception {
@@ -18,9 +23,9 @@ public class ListCommandTest extends AbstractCommandTestCase {
    * Test method for {@link org.terracotta.modules.tool.commands.ListCommand#execute()}.
    */
   public final void testExecute() {
-//    command.execute(null);
-//    assertOutMatches("TIM packages for Terracotta " + ProductInfo.getInstance().version());
-//    assertEquals(0, commandErr.toString().length());
+    // command.execute(null);
+    // assertOutMatches("TIM packages for Terracotta " + ProductInfo.getInstance().version());
+    // assertEquals(0, commandErr.toString().length());
   }
 
   @Override
