7f36ec17926a02cd0b7d5d212be9757833de0a13
Hung Huynh <hhuynh@gmail.com>
2008-07-19 06:35:36 +0000

    extends TCTestCase
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@9358 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/tim-update-center/tests.unit/org/terracotta/modules/tool/commands/AbstractCommandTestCase.java
+++ code/base/tim-update-center/tests.unit/org/terracotta/modules/tool/commands/AbstractCommandTestCase.java
@@ -1,22 +1,23 @@
 /*
- * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
  */
 package org.terracotta.modules.tool.commands;
+
+import com.tc.test.TCTestCase;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import junit.framework.TestCase;
-
 /**
  * Abstract base class for command test cases.
  */
-public abstract class AbstractCommandTestCase extends TestCase {
+public abstract class AbstractCommandTestCase extends TCTestCase {
   protected AbstractCommand command;
-  protected StringWriter commandOut;
-  protected StringWriter commandErr;
+  protected StringWriter    commandOut;
+  protected StringWriter    commandErr;
 
   protected void setUp() throws Exception {
     super.setUp();
