0002fffd18311f6feeb279ba93f4b66198badb8e
Alex Miller <alex@terracottatech.com>
2008-07-24 18:33:30 +0000

    DEV-1742 - add new tcbuild capability to inject a resource into a jar before deploying to Maven.  Using this to inject the build-data.txt into tc.jar, and the api jars, so that tc-maven-plugin can get to the version info.
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@9475 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/buildscripts/postscripts/maven-deploy.rb
+++ code/base/buildscripts/postscripts/maven-deploy.rb
@@ -18,15 +18,34 @@ class BaseCodeTerracottaBuilder <  TerracottaBuilder
       args.each do |arg|
         next unless arg
         if arg['file']
-          file = FilePath.new(product_directory, interpolate(arg['file'])).to_s
+          file = FilePath.new(product_directory, interpolate(arg['file']))
         else
-          file = FilePath.new(arg['srcfile']).to_s
+          file = FilePath.new(arg['srcfile'])
+        end
+        
+        if arg['inject']
+          # Copy jar to tmp jar in same dir
+          replacement_file = FilePath.new(file.directoryname) << file.filename + '.tmp'
+          @ant.copy(:tofile => replacement_file.to_s, :file => file.to_s)
+          file = replacement_file
+          
+          # Inject resource into jar
+          inject_file = FilePath.new(product_directory, interpolate(arg['inject']))
+          @ant.jar(:destfile => replacement_file.to_s, 
+            :update => 'true',
+            :basedir => inject_file.directoryname,
+            :includes => inject_file.filename)
         end
 
         artifact = arg['artifact']
         version = arg[MAVEN_VERSION_CONFIG_KEY] || config_source[MAVEN_VERSION_CONFIG_KEY] ||
           config_source['version'] || build_environment.version
-        maven.deploy_file(file, artifact, version, arg['pom'])
+        maven.deploy_file(file.to_s, artifact, version, arg['pom'])
+        
+        # clean up injected file if it existed
+        if arg['inject'] 
+          file.delete
+        end
       end
     end
   end
