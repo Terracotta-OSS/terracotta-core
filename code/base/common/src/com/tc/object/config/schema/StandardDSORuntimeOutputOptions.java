1df4bec35fec28210b9e20f74945283e662a9cf5
Abhishek Singh <asingh@terracottatech.com>
2008-07-24 12:40:16 +0000

    CDV-731: deprecate usage of <caller> in <runtime-output-options>
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@9472 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/common/src/com/tc/object/config/schema/StandardDSORuntimeOutputOptions.java
+++ code/base/common/src/com/tc/object/config/schema/StandardDSORuntimeOutputOptions.java
@@ -7,6 +7,9 @@ package com.tc.object.config.schema;
 import com.tc.config.schema.BaseNewConfigObject;
 import com.tc.config.schema.context.ConfigContext;
 import com.tc.config.schema.dynamic.BooleanConfigItem;
+import com.tc.logging.CustomerLogging;
+import com.tc.logging.TCLogger;
+import com.tc.logging.TCLogging;
 import com.terracottatech.config.DsoClientData;
 
 /**
@@ -18,6 +21,9 @@ public class StandardDSORuntimeOutputOptions extends BaseNewConfigObject impleme
   private final BooleanConfigItem doCaller;
   private final BooleanConfigItem doFullStack;
 
+  private static final TCLogger consoleLogger = CustomerLogging.getConsoleLogger();
+  private static final TCLogger logger = TCLogging.getLogger(StandardDSORuntimeOutputOptions.class);
+
   public StandardDSORuntimeOutputOptions(ConfigContext context) {
     super(context);
 
@@ -26,6 +32,14 @@ public class StandardDSORuntimeOutputOptions extends BaseNewConfigObject impleme
     this.doAutoLockDetails = this.context.booleanItem("debugging/runtime-output-options/auto-lock-details");
     this.doCaller = this.context.booleanItem("debugging/runtime-output-options/caller");
     this.doFullStack = this.context.booleanItem("debugging/runtime-output-options/full-stack");
+
+    if (doCaller.getBoolean()) {
+      //CDV-731: deprecate use of <caller> in <runtime-output-options>
+      final String msg = "<caller> element in <runtime-output-options> is deprecated. " +
+                         "Please set the value of <caller> to false in your tc-config file to remove this warning.";
+      consoleLogger.warn(msg);
+      logger.warn(msg);
+    }
   }
 
   public BooleanConfigItem doAutoLockDetails() {
