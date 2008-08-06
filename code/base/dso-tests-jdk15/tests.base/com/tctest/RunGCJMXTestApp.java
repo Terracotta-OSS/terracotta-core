cefca3b78c365dfc8c34fe069503abe9f118a558
Nabib El-Rahman <nelrahma@terracottatech.com>
2008-08-06 23:10:40 +0000

    DEV-1794, convert UnsupportedOperationException to warnings, with test.
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@9625 7fc7bbf3-cf45-46d4-be06-341739edd864

---
+++ code/base/dso-tests-jdk15/tests.base/com/tctest/RunGCJMXTestApp.java
@@ -0,0 +1,73 @@
+/*
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
+ */
+package com.tctest;
+
+import com.tc.management.JMXConnectorProxy;
+import com.tc.management.beans.L2MBeanNames;
+import com.tc.management.beans.object.ObjectManagementMonitorMBean;
+import com.tc.simulator.app.ApplicationConfig;
+import com.tc.simulator.listener.ListenerProvider;
+import com.tctest.runner.AbstractTransparentApp;
+
+import javax.management.MBeanServerConnection;
+import javax.management.MBeanServerInvocationHandler;
+import javax.management.remote.JMXConnector;
+
+public class RunGCJMXTestApp extends AbstractTransparentApp {
+
+  public static final String           CONFIG_FILE = "config-file";
+  public static final String           PORT_NUMBER = "port-number";
+  public static final String           HOST_NAME   = "host-name";
+  public static final String           JMX_PORT    = "jmx-port";
+
+  private final ApplicationConfig      config;
+
+  private MBeanServerConnection        mbsc        = null;
+  private JMXConnector                 jmxc;
+  private ObjectManagementMonitorMBean objectMBean;
+
+  public RunGCJMXTestApp(String appId, ApplicationConfig config, ListenerProvider listenerProvider) {
+    super(appId, config, listenerProvider);
+    this.config = config;
+  }
+
+  public void run() {
+
+    for (int i = 0; i < 10; i++) {
+      runGC();
+      if (!config.getServerControl().isRunning()) {
+        notifyError("call to objectManagementMonitor.runGC() should not stop server");
+      }
+    }
+
+  }
+
+  private void connect() throws Exception {
+    System.out.println("connecting to jmx server....");
+    jmxc = new JMXConnectorProxy("localhost", Integer.parseInt(config.getAttribute(JMX_PORT)));
+    mbsc = jmxc.getMBeanServerConnection();
+    System.out.println("obtained mbeanserver connection");
+    objectMBean = (ObjectManagementMonitorMBean) MBeanServerInvocationHandler
+        .newProxyInstance(mbsc, L2MBeanNames.OBJECT_MANAGEMENT, ObjectManagementMonitorMBean.class, false);
+
+  }
+
+  private void disconnect() throws Exception {
+    if (jmxc != null) {
+      jmxc.close();
+    }
+  }
+
+  private void runGC() {
+    try {
+      connect();
+      objectMBean.runGC();
+      disconnect();
+    } catch (Exception e) {
+      notifyError("should not throw exception when calling objectManagementMonitor.runGC()");
+    }
+  }
+
+}
--- code/base/management-api/src/com/tc/management/beans/object/ObjectManagementMonitor.java
+++ code/base/management-api/src/com/tc/management/beans/object/ObjectManagementMonitor.java
@@ -1,5 +1,6 @@
 /*
- * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
  */
 package com.tc.management.beans.object;
 
@@ -9,112 +10,108 @@ import com.tc.management.AbstractTerracottaMBean;
 
 import javax.management.NotCompliantMBeanException;
 
-public class ObjectManagementMonitor extends AbstractTerracottaMBean implements
-		ObjectManagementMonitorMBean {
+public class ObjectManagementMonitor extends AbstractTerracottaMBean implements ObjectManagementMonitorMBean {
 
-	private static final TCLogger logger = TCLogging
-			.getLogger(ObjectManagementMonitor.class);
+  private static final TCLogger  logger = TCLogging.getLogger(ObjectManagementMonitor.class);
 
-	private volatile GCComptroller gcController;
+  private volatile GCComptroller gcController;
 
-	private final GCRunner gcRunner;
+  private final GCRunner         gcRunner;
 
-	public ObjectManagementMonitor() throws NotCompliantMBeanException {
-		super(ObjectManagementMonitorMBean.class, false);
+  public ObjectManagementMonitor() throws NotCompliantMBeanException {
+    super(ObjectManagementMonitorMBean.class, false);
 
-		gcRunner = new GCRunner() {
-			private boolean isRunning = false;
+    gcRunner = new GCRunner() {
+      private boolean isRunning = false;
 
-			public void run() {
-				setRunningState();
-				gcController.startGC();
-				setStopState();
-			}
+      public void run() {
+        setRunningState();
+        gcController.startGC();
+        setStopState();
+      }
 
-			private synchronized void setRunningState() {
-				if (isRunning) {
-					throw new UnsupportedOperationException(
-							"Cannot run GC because GC is already running.");
-				}
-				isRunning = true;
-				logger.info("Running GC.");
-			}
+      private synchronized void setRunningState() {
+        if (isRunning) {
+          logger.warn("Cannot run GC because GC is already running.");
+          return;
+        }
+        isRunning = true;
+        logger.info("Running GC.");
+      }
 
-			private synchronized void setStopState() {
-				if (!isRunning) {
-					throw new UnsupportedOperationException(
-							"Cannot stop GC because GC is not running.");
-				}
-				isRunning = false;
-				logger.info("GC finished.");
-			}
+      private synchronized void setStopState() {
+        if (!isRunning) {
+          logger.warn("Cannot stop GC because GC is not running.");
+          return;
+        }
+        isRunning = false;
+        logger.info("GC finished.");
+      }
 
-			public synchronized boolean isGCRunning() {
-				return isRunning;
-			}
-		};
-	}
+      public synchronized boolean isGCRunning() {
+        return isRunning;
+      }
+    };
+  }
 
-	public boolean isGCRunning() {
-		return gcRunner.isGCRunning();
-	}
+  public boolean isGCRunning() {
+    return gcRunner.isGCRunning();
+  }
 
-	public synchronized void runGC() {
-		if (!isEnabled()) {
-			throw new UnsupportedOperationException(
-					"Cannot run GC because mBean is not enabled.");
-		}
-		if (gcController == null) {
-			throw new RuntimeException("Failure: see log for more information");
-		}
-		
-		if (!gcController.isGCStarted()) {
-			throw new UnsupportedOperationException(
-					"Cannot run GC externally because this server "
-							+ "is in PASSIVE state and GC is disabled.");
-		}
-		// XXX::Note:: There is potencially a rare here, one could check to see
-		// if it is disabled and before GC is started it could be disabled. In
-		// which case it will not be run, just logged in the log file.
-		if (gcController.isGCDisabled()) {
-			throw new UnsupportedOperationException(
-					"Cannot run GC externally because PASSIVE server(s) are currently synching state "
-							+ "with this ACTIVE server and GC is disabled.");
-		}
-		if (isGCRunning()) {
-			throw new UnsupportedOperationException(
-					"Cannot run GC because GC is already running.");
-		}
+  public synchronized void runGC() {
+    if (!isEnabled()) {
+      logger.warn("Cannot run GC because mBean is not enabled.");
+      return;
+    }
+    if (gcController == null) {
+      logger.warn("Failure: see log for more information, gcController is null");
+      return;
+    }
 
-		Thread gcRunnerThread = new Thread(gcRunner);
-		gcRunnerThread.setName("GCRunnerThread");
-		gcRunnerThread.start();
-	}
+    if (!gcController.isGCStarted()) {
+      logger.warn("Cannot run GC externally because this server " + "is in PASSIVE state and GC is disabled.");
+      return;
+    }
+    // XXX::Note:: There is potencially a rare here, one could check to see
+    // if it is disabled and before GC is started it could be disabled. In
+    // which case it will not be run, just logged in the log file.
+    if (gcController.isGCDisabled()) {
+      logger.warn("Cannot run GC externally because PASSIVE server(s) are currently synching state "
+                  + "with this ACTIVE server and GC is disabled.");
+      return;
+    }
+    if (isGCRunning()) {
+      logger.warn("Cannot run GC because GC is already running.");
+      return;
+    }
 
-	public synchronized void reset() {
-		// nothing to reset
-	}
+    Thread gcRunnerThread = new Thread(gcRunner);
+    gcRunnerThread.setName("GCRunnerThread");
+    gcRunnerThread.start();
+  }
 
-	public void registerGCController(GCComptroller controller) {
-		if (isEnabled()) {
-			if (gcController != null) {
-				logger
-						.warn("Registering new gc-controller while one already registered. Old : "
-								+ gcController);
-			}
-			gcController = controller;
-		}
-	}
+  public synchronized void reset() {
+    // nothing to reset
+  }
 
-	public static interface GCComptroller {
-		void startGC();
+  public void registerGCController(GCComptroller controller) {
+    if (isEnabled()) {
+      if (gcController != null) {
+        logger.warn("Registering new gc-controller while one already registered. Old : " + gcController);
+      }
+      gcController = controller;
+    }
+  }
 
-		boolean isGCDisabled();
+  public static interface GCComptroller {
+    void startGC();
 
-		boolean isGCStarted();
-	}
+    boolean isGCDisabled();
 
-	static interface GCRunner extends Runnable {
-		boolean isGCRunning();
-	}
+    boolean isGCStarted();
+  }
+
+  static interface GCRunner extends Runnable {
+    boolean isGCRunning();
+  }
 }
