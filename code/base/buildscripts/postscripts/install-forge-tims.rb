cd2f9a1892c04796882dc85fdc4c1b60256a40e8
Jason Voegele <jason@jvoegele.com>
2008-08-12 19:22:52 +0000

    Added further diagnostic info to determine reason for tim-get failure.
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@9683 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/buildscripts/postscripts/install-forge-tims.rb
+++ code/base/buildscripts/postscripts/install-forge-tims.rb
@@ -10,7 +10,15 @@ class BaseCodeTerracottaBuilder <  TerracottaBuilder
   # - Install TIMs from the Forge into the kit using the tim-get tool.
   def postscript(ant, build_environment, product_directory, *args)
     return if @no_demo
+
     tim_get = FilePath.new(product_directory, 'bin', 'tim-get').script_extension
+    unless File.exist?(tim_get.to_s)
+      raise("Cannot find tim-get executable at expected location: #{tim_get}")
+    end
+    unless File.executable?(tim_get.to_s)
+      raise("tim-get script exists, but is not executable")
+    end
+
     args.each do |tim|
       puts("***** Installing #{tim}")
       unless system(tim_get.to_s, 'install', tim)
