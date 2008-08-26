1b3515a4986eaf862ebadf66fe3f8a8d5dcd64db
Hung Huynh <hhuynh@gmail.com>
2008-08-26 22:50:20 +0000

    attempt to make abnormal test failures more generic
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@9876 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/buildscripts/testrun_record.rb
+++ code/base/buildscripts/testrun_record.rb
@@ -345,6 +345,21 @@ class SubtreeTestRunRecord
   end
     
   private
+  
+  def create_abnormal_junit_report(filename, classname)
+    File.open(filename, "w") do |file|
+      file << "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+      file << "<testsuite errors=\"0\" failures=\"1\" name=\"#{classname.escape(:xml_attribute)}\" tests=\"0\" time=\"0.000\">\n"
+      file << "<testcase classname=\"#{classname.xml_escape}\" name='test' time='0.0'>\n"
+      file << ("  <failure type='junit.framework.AssertionFailedError' message=\"Failed abnormally\">\n") % classname.xml_escape(true)
+      file << ("      Failed abnormally\n") % classname.xml_escape
+      file << "   </failure>\n"
+      file << "</testcase>\n"
+      file << "<system-out/><system-err/>\n"
+      file << "</testsuite>\n"
+    end
+  end
+  
   # Reads in all the XML files for tests in this subtree, analyzes them, and stores the
   # data in this object. This only will happen once (lazy-init), so it's always safe to
   # call this function. 
@@ -376,6 +391,7 @@ class SubtreeTestRunRecord
                 @total_suites_passed += 1 unless testsuite_run.failed?
               rescue => e
                 STDERR.puts("Test #{file_classname} failed abnormally. Result file can't be parsed. Check log for exception.")
+                create_abnormal_junit_report(filename, file_classname)
                 testsuite_run = UnparseableTestSuiteRunRecord.new(filename, file_classname, e)
                 @testsuites[file_classname] = testsuite_run
                 @total_suites += 1
