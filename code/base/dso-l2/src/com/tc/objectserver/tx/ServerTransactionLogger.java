72499117f87a8ac1d8528d3b574ab336214c049c
Saravanan Subbiah <ssubbiah@terracottatech.com>
2008-09-11 01:56:05 +0000

    Using atomic ints instead of reqular syncs, since this synchronization hides some issues and makes it tough to reproduce
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@10028 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-l2/src/com/tc/objectserver/tx/ServerTransactionLogger.java
+++ code/base/dso-l2/src/com/tc/objectserver/tx/ServerTransactionLogger.java
@@ -10,18 +10,22 @@ import com.tc.object.tx.ServerTransactionID;
 
 import java.util.Collection;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class ServerTransactionLogger implements ServerTransactionListener {
 
-  private final TCLogger                       logger;
-  private final ServerTransactionManagerConfig config;
+  private final TCLogger      logger;
 
-  private long                                 outStandingTxns = 0;
-  private long                                 last            = 0;
+  private final AtomicInteger outStandingTxns = new AtomicInteger(0);
+  private final AtomicLong    last            = new AtomicLong(0);
+  private final boolean       verboseLogging;
+  private final boolean       printStatsEnabled;
 
   public ServerTransactionLogger(TCLogger logger, ServerTransactionManagerConfig config) {
     this.logger = logger;
-    this.config = config;
+    this.verboseLogging = config.isVerboseLogging();
+    this.printStatsEnabled = config.isPrintStatsEnabled();
   }
 
   public void addResentServerTransactionIDs(Collection stxIDs) {
@@ -37,44 +41,44 @@ public class ServerTransactionLogger implements ServerTransactionListener {
   }
 
   public void incomingTransactions(NodeID source, Set serverTxnIDs) {
-    if (config.isVerboseLogging()) logger.info("incomingTransactions: " + source + ", " + serverTxnIDs);
+    if (verboseLogging) logger.info("incomingTransactions: " + source + ", " + serverTxnIDs);
     incrementOutStandingTxns(serverTxnIDs.size());
   }
 
-  private synchronized void incrementOutStandingTxns(int count) {
-    outStandingTxns += count;
+  private void incrementOutStandingTxns(int count) {
+    int current = outStandingTxns.addAndGet(count);
     if (needToLogStats()) {
-      logStats();
+      logStats(current);
     }
   }
 
   private synchronized void decrementOutStandingTxns(int count) {
-    outStandingTxns -= count;
+    int current = outStandingTxns.addAndGet(-count);
     if (needToLogStats()) {
-      logStats();
+      logStats(current);
     }
   }
 
   private boolean needToLogStats() {
-    if (!config.isPrintStatsEnabled()) return false;
+    if (!printStatsEnabled) return false;
     long now = System.currentTimeMillis();
-    boolean log = (now - last) > 1000;
+    boolean log = (now - last.get()) > 1000;
     if (log) {
-      last = now;
+      last.set(now);
     }
     return log;
   }
 
-  private void logStats() {
-    logger.info("Number of pending transactions in the System : " + outStandingTxns);
+  private void logStats(int current) {
+    logger.info("Number of pending transactions in the System : " + current);
   }
 
   public void transactionApplied(ServerTransactionID stxID) {
-    if (config.isVerboseLogging()) logger.info("transactionApplied: " + stxID);
+    if (verboseLogging) logger.info("transactionApplied: " + stxID);
   }
 
   public void transactionCompleted(ServerTransactionID stxID) {
-    if (config.isVerboseLogging()) logger.info("transactionCompleted: " + stxID);
+    if (verboseLogging) logger.info("transactionCompleted: " + stxID);
     decrementOutStandingTxns(1);
   }
 
