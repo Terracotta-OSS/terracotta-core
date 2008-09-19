6dee637c8afe6f55ccf0dae5871c0c5aa436fce7
Nabib El-Rahman <nelrahma@terracottatech.com>
2008-09-19 23:50:26 +0000

    oops. wrong branch.
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@10173 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-system-tests/tests.base/com/tctest/GenericMapTestApp.java
+++ code/base/dso-system-tests/tests.base/com/tctest/GenericMapTestApp.java
@@ -183,10 +183,6 @@ public class GenericMapTestApp extends GenericTestApp {
 
     // unreachable
   }
-  
-  void testFailure() {
-    throw new AssertionError("see if this test is failing!!!");
-  }
 
   void testBasicUnSynchronizedPut(Map map, boolean validate, int v) {
     // if (map instanceof Hashtable) { return; }
