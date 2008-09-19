78899a7157598b8f4e029d4c38fb8b02448745c1
Saravanan Subbiah <ssubbiah@terracottatech.com>
2008-09-19 04:24:27 +0000

    Using the new interface callBackOnResentTxnsInSystemCompletion() in ServerTransactionManager instead of book keeping locally
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@10147 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-l2/src/com/tc/objectserver/gtx/GlobalTransactionIDLowWaterMarkProvider.java
+++ code/base/dso-l2/src/com/tc/objectserver/gtx/GlobalTransactionIDLowWaterMarkProvider.java
@@ -6,34 +6,20 @@ package com.tc.objectserver.gtx;
 
 import com.tc.logging.TCLogger;
 import com.tc.logging.TCLogging;
-import com.tc.net.groups.NodeID;
 import com.tc.object.gtx.GlobalTransactionID;
 import com.tc.object.gtx.GlobalTransactionManager;
-import com.tc.object.tx.ServerTransactionID;
-import com.tc.objectserver.tx.ServerTransactionListener;
 import com.tc.objectserver.tx.ServerTransactionManager;
-import com.tc.util.State;
+import com.tc.objectserver.tx.TxnsInSystemCompletionLister;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
-
-// XXX::TODO:: Clean this class to use the new callBackOnResentTxnsInSystemCompletion() interface in transactionManager.
-public class GlobalTransactionIDLowWaterMarkProvider implements GlobalTransactionManager, ServerTransactionListener {
+public class GlobalTransactionIDLowWaterMarkProvider implements GlobalTransactionManager {
 
   private static final TCLogger             logger              = TCLogging
                                                                     .getLogger(GlobalTransactionIDLowWaterMarkProvider.class);
-  private static final State                INITIAL             = new State("INITAL");
-  private static final State                STARTED             = new State("STARTED");
 
   private final ServerTransactionManager    transactionManager;
   private final GlobalTransactionManager    gtxm;
 
   private volatile GlobalTransactionManager lwmProvider;
-  private State                             state               = INITIAL;
-
-  private final Set                         resentTxns          = new HashSet();
 
   private final GlobalTransactionManager    NULL_GLOBAL_TXN_MGR = new GlobalTransactionManager() {
                                                                   public GlobalTransactionID getLowGlobalTransactionIDWatermark() {
@@ -48,63 +34,20 @@ public class GlobalTransactionIDLowWaterMarkProvider implements GlobalTransactio
     this.lwmProvider = NULL_GLOBAL_TXN_MGR;
   }
 
-  // XXX::TODO:: Clean this class to use the new callBackOnResentTxnsInSystemCompletion() interface in
-  // transactionManager.
   public void goToActiveMode() {
-    transactionManager.addTransactionListener(this);
+    transactionManager.callBackOnResentTxnsInSystemCompletion(new TxnsInSystemCompletionLister() {
+      public void onCompletion() {
+        switchLWMProvider();
+      }
+    });
   }
 
   public GlobalTransactionID getLowGlobalTransactionIDWatermark() {
     return lwmProvider.getLowGlobalTransactionIDWatermark();
   }
 
-  public synchronized void addResentServerTransactionIDs(Collection stxIDs) {
-    resentTxns.addAll(stxIDs);
-  }
-
-  public synchronized void clearAllTransactionsFor(NodeID deadNode) {
-    for (Iterator i = resentTxns.iterator(); i.hasNext();) {
-      ServerTransactionID sid = (ServerTransactionID) i.next();
-      if (sid.getSourceID().equals(deadNode)) {
-        i.remove();
-      }
-    }
-    switchLWMProviderIfReady();
-  }
-
-  private void switchLWMProviderIfReady() {
-    if (resentTxns.isEmpty() && state == STARTED) {
-      logger.info("Switching GlobalTransactionID Low Water mark provider since all resent transactions are applied");
-      this.lwmProvider = gtxm;
-      this.transactionManager.removeTransactionListener(this);
-    }
-  }
-
-  public void incomingTransactions(NodeID source, Set serverTxnIDs) {
-    // NOP
-  }
-
-  public void transactionApplied(ServerTransactionID stxID) {
-    // NOP
-  }
-
-  public synchronized void transactionCompleted(ServerTransactionID stxID) {
-    resentTxns.remove(stxID);
-    switchLWMProviderIfReady();
-  }
-
-  public synchronized void transactionManagerStarted(Set cids) {
-    state = STARTED;
-    removeAllExceptFrom(cids);
-    switchLWMProviderIfReady();
-  }
-
-  private void removeAllExceptFrom(Set cids) {
-    for (Iterator i = resentTxns.iterator(); i.hasNext();) {
-      ServerTransactionID sid = (ServerTransactionID) i.next();
-      if (!cids.contains(sid.getSourceID())) {
-        i.remove();
-      }
-    }
+  private void switchLWMProvider() {
+    logger.info("Switching GlobalTransactionID Low Water mark provider since all resent transactions are applied");
+    this.lwmProvider = gtxm;
   }
 }
