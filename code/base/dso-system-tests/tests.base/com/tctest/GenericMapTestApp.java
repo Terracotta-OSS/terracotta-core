43436bdfbc043c7b6af3a052079a758d46641d2e
Nabib El-Rahman <nelrahma@terracottatech.com>
2008-09-19 23:22:57 +0000

    see if this test fails in monkeys.
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@10172 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-system-tests/tests.base/com/tctest/GenericMapTestApp.java
+++ code/base/dso-system-tests/tests.base/com/tctest/GenericMapTestApp.java
@@ -44,6 +44,7 @@ import java.util.Set;
 import java.util.TreeMap;
 import java.util.Map.Entry;
 
+
 public class GenericMapTestApp extends GenericTestApp {
 
   private static final int LITERAL_VARIANT   = 1;
@@ -67,7 +68,7 @@ public class GenericMapTestApp extends GenericTestApp {
 
   protected void setupTestObject(String test) {
     List maps = new ArrayList();
-
+    
     maps.add(new HashMap());
     maps.add(new Hashtable());
     maps.add(new TreeMap(new NullTolerantComparator()));
@@ -181,6 +182,10 @@ public class GenericMapTestApp extends GenericTestApp {
     }
 
     // unreachable
+  }
+  
+  void testFailure() {
+    throw new AssertionError("see if this test is failing!!!");
   }
 
   void testBasicUnSynchronizedPut(Map map, boolean validate, int v) {
