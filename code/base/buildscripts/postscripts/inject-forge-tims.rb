593c78c45e30fdfdebd466a9f8bb822335daf3d7
Juris Galang <jgalang@terracottatech.com>
2008-09-25 23:40:12 +0000

    added some debug statements
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@10253 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/buildscripts/postscripts/inject-forge-tims.rb
+++ code/base/buildscripts/postscripts/inject-forge-tims.rb
@@ -17,21 +17,29 @@ class BaseCodeTerracottaBuilder <  TerracottaBuilder
     timnames = args[0]['tims']
     timnames.each do |entry|
       output = tim_get_output(:list, entry).split("\n")
-      output.delete_if { |line| !line.index(entry) }
+      output.delete_if { |line| 
+        puts "[xxx] #{line}"
+        !line.index(entry) 
+      }
       data = output.last
-      data.gsub!(/[()]/, '')
-      data = data.split
-      data.shift
-      tims[data.first] = data.last
+      unless data.nil?
+        data.gsub!(/[()]/, '')
+        data = data.split
+        data.shift
+        tims[data.first] = data.last
+      end
     end
 
+    puts "WARNING: Unable to locate all of the TIMs listed in the distribution config - only some TIM info will be injected." if !tims.empty? && timnames.size != tims.size
+    puts "WARNING: Unable to locate any TIMs - no TIM info will be injected." if tims.empty?
+    
     dirnames = args[1]['dest']
     dirnames.each do |entry|
       srcdir = FilePath.new(product_directory, *entry.split('/'))
       Find.find(srcdir.to_s) do |path|
         next unless FileTest.file?(path) && File.basename(path) =~ /^tc-config.xml$/
         config = File.read(path)
-        tims.each do |k, v|
+          tims.each do |k, v|
           value = "<module name=\"#{k}\" version=\"#{v}\"/>"
           regex = /<module *name *= *"#{Regexp.escape(k)}" *version *= *"@tim.version@" *\/>/
           File.open(path, 'w') {|f| f.write(config) } if config.gsub!(regex, value)
