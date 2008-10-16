50dee4135cd20da4a54742d74e5c9899ca95914e
Hung Huynh <hhuynh@gmail.com>
2008-10-16 02:24:27 +0000

    add support for tc-config
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@10501 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/deploy/src/com/tc/server/util/ServerStat.java
+++ code/base/deploy/src/com/tc/server/util/ServerStat.java
@@ -4,11 +4,18 @@
  */
 package com.tc.server.util;
 
+import org.apache.xmlbeans.XmlException;
+
 import com.tc.cli.CommandLineBuilder;
+import com.tc.config.Loader;
 import com.tc.management.JMXConnectorProxy;
 import com.tc.management.beans.L2MBeanNames;
 import com.tc.management.beans.TCServerInfoMBean;
+import com.terracottatech.config.Server;
+import com.terracottatech.config.TcConfigDocument;
+import com.terracottatech.config.TcConfigDocument.TcConfig;
 
+import java.io.File;
 import java.io.IOException;
 
 import javax.management.MBeanServerConnection;
@@ -16,6 +23,8 @@ import javax.management.MBeanServerInvocationHandler;
 
 public class ServerStat {
   private static final String   UNKNOWN = "unknown";
+  private static final String   NEWLINE = System.getProperty("line.separator");
+
   public String                 host;
   public int                    port;
   private JMXConnectorProxy     jmxProxy;
@@ -60,25 +69,25 @@ public class ServerStat {
   }
 
   public String getHealth() {
-    if (!connected) return "failed to connect at port " + port;
+    if (!connected) return "FAILED to connect";
     return infoBean.getHealthStatus();
   }
 
   public String toString() {
-    String newline = System.getProperty("line.separator");
     StringBuilder sb = new StringBuilder();
-    sb.append("- " + host + ":").append(newline);
-    sb.append("  health: " + getHealth()).append(newline);
-    sb.append("  role: " + getRole()).append(newline);
-    sb.append("  state: " + getState()).append(newline);
-    sb.append("  jmxport: " + port).append(newline);
+    sb.append("- " + host + ":").append(NEWLINE);
+    sb.append("  health: " + getHealth()).append(NEWLINE);
+    sb.append("  role: " + getRole()).append(NEWLINE);
+    sb.append("  state: " + getState()).append(NEWLINE);
+    sb.append("  jmxport: " + port).append(NEWLINE);
     return sb.toString();
   }
 
   public static void main(String[] args) {
-    String usage = "    server-stat.sh -s host1,host2\n" +
-                   "    server-stat.sh -s host1:9520,host2:9520\n" +
-                   "    server-stat.sh -f /path/to/tc-config.xml\n";
+    String usage = "    server-stat.sh -s host1,host2" + NEWLINE + 
+                   "    server-stat.sh -s host1:9520,host2:9520" + NEWLINE +
+                   "    server-stat.sh -f /path/to/tc-config.xml" + NEWLINE;
+    
     CommandLineBuilder commandLineBuilder = new CommandLineBuilder(ServerStat.class.getName(), args);
 
     commandLineBuilder.addOption("s", true, "Terracotta server list (comma separated)", String.class, false, "list");
@@ -94,6 +103,33 @@ public class ServerStat {
     String hostList = commandLineBuilder.getOptionValue('s');
     String configFile = commandLineBuilder.getOptionValue('f');
 
+    if (configFile != null) {
+      handleConfigFile(configFile);
+    } else {
+      handleList(hostList);
+    }
+  }
+
+  private static void handleConfigFile(String configFile) {
+    TcConfigDocument tcConfigDocument = null;
+    try {
+      tcConfigDocument = new Loader().parse(new File(configFile));
+    } catch (IOException e) {
+      System.err.println("Error reading " + configFile + ". Error: " + e.getMessage());
+      System.exit(1);
+    } catch (XmlException e) {
+      System.err.println("Error parsing " + configFile + ". Error: " + e.getMessage());
+      System.exit(1);
+    }
+    TcConfig tcConfig = tcConfigDocument.getTcConfig();
+    Server[] servers = tcConfig.getServers().getServerArray();
+    for (int i = 0; i < servers.length; i++) {
+      ServerStat stat = new ServerStat(servers[i].getHost(), servers[i].getJmxPort());
+      System.out.println(stat.toString());
+    }
+  }
+
+  private static void handleList(String hostList) {
     if (hostList == null) {
       printStat("localhost:9520");
     } else {
@@ -103,13 +139,9 @@ public class ServerStat {
         System.out.println();
       }
     }
-    
-    if (configFile != null) {
-      //
-    }
   }
-  
-  public static void printStat(String info) {
+
+  private static void printStat(String info) {
     String host = "localhost";
     int port = 9520;
     if (info.indexOf(':') > 0) {
