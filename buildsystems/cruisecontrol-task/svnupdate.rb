b85f397e94c944f5104d9858cdbe34a256fabb1b
Hung Huynh <hhuynh@gmail.com>
2008-11-12 23:25:42 +0000

    revert 10802
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@10803 7fc7bbf3-cf45-46d4-be06-341739edd864

--- buildsystems/cruisecontrol-task/svnupdate.rb
+++ buildsystems/cruisecontrol-task/svnupdate.rb
@@ -6,8 +6,8 @@ require 'yaml'
 
 class SvnUpdate
 
-  def initialize(build_target)
-    @build_target = build_target
+  def initialize(monkey_name)
+    @monkey_name = monkey_name
 
     @os_topdir = File.join(File.expand_path(File.dirname(__FILE__)), "..", "..")
     @is_ee_branch = @os_topdir =~ /community/ ? true : false
@@ -95,12 +95,7 @@ class SvnUpdate
     log "path: #{path}"
     log "current rev: #{current_rev}"
     log "good_rev: #{good_rev}"
-	
-	# only update to HEAD if it's a 
-	#   - monkey-police running "check_compile" target
-	#   - or if it's a test monkey
-	#   - or there's no good_rev info
-    if @build_target == "check_compile" || @build_target == "check_monkeytest" || good_rev == 0
+    if @monkey_name == "monkey-police" || @monkey_name == "test-monkey" || good_rev == 0
       svn_update_with_error_tolerant(path, "HEAD")
       return
     end
