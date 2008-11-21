1e57ab78d642b874b7f5f9c53c36973fc1a5ab93
Gary Keim <gkeim@terracottatech.com>
2008-11-21 22:52:41 +0000

    Merge of change 10895 from 2.7 branch.
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@10897 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/ui-eclipse/src/org/terracotta/dso/editors/RuntimeLoggingPanel.java
+++ code/base/ui-eclipse/src/org/terracotta/dso/editors/RuntimeLoggingPanel.java
@@ -1,5 +1,6 @@
 /*
- * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
  */
 package org.terracotta.dso.editors;
 
@@ -17,9 +18,7 @@ import org.terracotta.ui.util.SWTUtil;
 import com.terracottatech.config.DsoClientDebugging;
 import com.terracottatech.config.RuntimeLogging;
 
-public class RuntimeLoggingPanel extends ConfigurationEditorPanel
-  implements XmlObjectStructureListener
-{
+public class RuntimeLoggingPanel extends ConfigurationEditorPanel implements XmlObjectStructureListener {
   private DsoClientDebugging m_dsoClientDebugging;
   private RuntimeLogging     m_runtimeLogging;
   private Layout             m_layout;
@@ -33,7 +32,7 @@ public class RuntimeLoggingPanel extends ConfigurationEditorPanel
   public void ensureXmlObject() {
     super.ensureXmlObject();
 
-    if(m_runtimeLogging == null) {
+    if (m_runtimeLogging == null) {
       removeListeners();
       m_runtimeLogging = m_dsoClientDebugging.addNewRuntimeLogging();
       updateChildren();
@@ -46,15 +45,14 @@ public class RuntimeLoggingPanel extends ConfigurationEditorPanel
   }
 
   public boolean hasAnySet() {
-    return m_runtimeLogging != null &&
-          (m_runtimeLogging.isSetLockDebug()              ||
-           m_runtimeLogging.isSetWaitNotifyDebug()        ||
-           m_runtimeLogging.isSetDistributedMethodDebug() ||
-           m_runtimeLogging.isSetNewObjectDebug());
+    return m_runtimeLogging != null
+           && (m_runtimeLogging.isSetLockDebug() || m_runtimeLogging.isSetDistributedMethodDebug()
+               || m_runtimeLogging.isSetFieldChangeDebug() || m_runtimeLogging.isSetWaitNotifyDebug()
+               || m_runtimeLogging.isSetNewObjectDebug() || m_runtimeLogging.isSetNonPortableDump());
   }
 
   private void testRemoveRuntimeLogging() {
-    if(!hasAnySet() && m_dsoClientDebugging.getRuntimeLogging() != null) {
+    if (!hasAnySet() && m_dsoClientDebugging.getRuntimeLogging() != null) {
       m_dsoClientDebugging.unsetRuntimeLogging();
       m_runtimeLogging = null;
       fireXmlObjectStructureChanged();
@@ -68,21 +66,21 @@ public class RuntimeLoggingPanel extends ConfigurationEditorPanel
   }
 
   private void addListeners() {
-    ((XmlBooleanToggle)m_layout.m_lockDebugCheck.getData()).addXmlObjectStructureListener(this);
-    ((XmlBooleanToggle)m_layout.m_distributedMethodDebugCheck.getData()).addXmlObjectStructureListener(this);
-    ((XmlBooleanToggle)m_layout.m_fieldChangeDebugCheck.getData()).addXmlObjectStructureListener(this);
-    ((XmlBooleanToggle)m_layout.m_nonPortableDumpCheck.getData()).addXmlObjectStructureListener(this);
-    ((XmlBooleanToggle)m_layout.m_waitNotifyDebugCheck.getData()).addXmlObjectStructureListener(this);
-    ((XmlBooleanToggle)m_layout.m_newObjectDebugCheck.getData()).addXmlObjectStructureListener(this);
+    ((XmlBooleanToggle) m_layout.m_lockDebugCheck.getData()).addXmlObjectStructureListener(this);
+    ((XmlBooleanToggle) m_layout.m_distributedMethodDebugCheck.getData()).addXmlObjectStructureListener(this);
+    ((XmlBooleanToggle) m_layout.m_fieldChangeDebugCheck.getData()).addXmlObjectStructureListener(this);
+    ((XmlBooleanToggle) m_layout.m_nonPortableDumpCheck.getData()).addXmlObjectStructureListener(this);
+    ((XmlBooleanToggle) m_layout.m_waitNotifyDebugCheck.getData()).addXmlObjectStructureListener(this);
+    ((XmlBooleanToggle) m_layout.m_newObjectDebugCheck.getData()).addXmlObjectStructureListener(this);
   }
 
   private void removeListeners() {
-    ((XmlBooleanToggle)m_layout.m_lockDebugCheck.getData()).removeXmlObjectStructureListener(this);
-    ((XmlBooleanToggle)m_layout.m_distributedMethodDebugCheck.getData()).removeXmlObjectStructureListener(this);
-    ((XmlBooleanToggle)m_layout.m_fieldChangeDebugCheck.getData()).removeXmlObjectStructureListener(this);
-    ((XmlBooleanToggle)m_layout.m_nonPortableDumpCheck.getData()).removeXmlObjectStructureListener(this);
-    ((XmlBooleanToggle)m_layout.m_waitNotifyDebugCheck.getData()).removeXmlObjectStructureListener(this);
-    ((XmlBooleanToggle)m_layout.m_newObjectDebugCheck.getData()).removeXmlObjectStructureListener(this);
+    ((XmlBooleanToggle) m_layout.m_lockDebugCheck.getData()).removeXmlObjectStructureListener(this);
+    ((XmlBooleanToggle) m_layout.m_distributedMethodDebugCheck.getData()).removeXmlObjectStructureListener(this);
+    ((XmlBooleanToggle) m_layout.m_fieldChangeDebugCheck.getData()).removeXmlObjectStructureListener(this);
+    ((XmlBooleanToggle) m_layout.m_nonPortableDumpCheck.getData()).removeXmlObjectStructureListener(this);
+    ((XmlBooleanToggle) m_layout.m_waitNotifyDebugCheck.getData()).removeXmlObjectStructureListener(this);
+    ((XmlBooleanToggle) m_layout.m_newObjectDebugCheck.getData()).removeXmlObjectStructureListener(this);
   }
 
   private void updateChildren() {
@@ -94,8 +92,7 @@ public class RuntimeLoggingPanel extends ConfigurationEditorPanel
     setEnabled(true);
 
     m_dsoClientDebugging = dsoClientDebugging;
-    m_runtimeLogging     = m_dsoClientDebugging != null ?
-                           m_dsoClientDebugging.getRuntimeLogging() : null;
+    m_runtimeLogging = m_dsoClientDebugging != null ? m_dsoClientDebugging.getRuntimeLogging() : null;
 
     updateChildren();
     addListeners();
@@ -107,12 +104,12 @@ public class RuntimeLoggingPanel extends ConfigurationEditorPanel
     m_dsoClientDebugging = null;
     m_runtimeLogging = null;
 
-    ((XmlBooleanToggle)m_layout.m_lockDebugCheck.getData()).tearDown();
-    ((XmlBooleanToggle)m_layout.m_distributedMethodDebugCheck.getData()).tearDown();
-    ((XmlBooleanToggle)m_layout.m_fieldChangeDebugCheck.getData()).tearDown();
-    ((XmlBooleanToggle)m_layout.m_nonPortableDumpCheck.getData()).tearDown();
-    ((XmlBooleanToggle)m_layout.m_waitNotifyDebugCheck.getData()).tearDown();
-    ((XmlBooleanToggle)m_layout.m_newObjectDebugCheck.getData()).tearDown();
+    ((XmlBooleanToggle) m_layout.m_lockDebugCheck.getData()).tearDown();
+    ((XmlBooleanToggle) m_layout.m_distributedMethodDebugCheck.getData()).tearDown();
+    ((XmlBooleanToggle) m_layout.m_fieldChangeDebugCheck.getData()).tearDown();
+    ((XmlBooleanToggle) m_layout.m_nonPortableDumpCheck.getData()).tearDown();
+    ((XmlBooleanToggle) m_layout.m_waitNotifyDebugCheck.getData()).tearDown();
+    ((XmlBooleanToggle) m_layout.m_newObjectDebugCheck.getData()).tearDown();
 
     setEnabled(false);
   }
@@ -149,17 +146,17 @@ public class RuntimeLoggingPanel extends ConfigurationEditorPanel
     }
 
     void setRuntimeLogging(RuntimeLogging runtimeLogging) {
-      ((XmlBooleanToggle)m_lockDebugCheck.getData()).setup(runtimeLogging);
-      ((XmlBooleanToggle)m_distributedMethodDebugCheck.getData()).setup(runtimeLogging);
-      ((XmlBooleanToggle)m_fieldChangeDebugCheck.getData()).setup(runtimeLogging);
-      ((XmlBooleanToggle)m_nonPortableDumpCheck.getData()).setup(runtimeLogging);
-      ((XmlBooleanToggle)m_waitNotifyDebugCheck.getData()).setup(runtimeLogging);
-      ((XmlBooleanToggle)m_newObjectDebugCheck.getData()).setup(runtimeLogging);
+      ((XmlBooleanToggle) m_lockDebugCheck.getData()).setup(runtimeLogging);
+      ((XmlBooleanToggle) m_distributedMethodDebugCheck.getData()).setup(runtimeLogging);
+      ((XmlBooleanToggle) m_fieldChangeDebugCheck.getData()).setup(runtimeLogging);
+      ((XmlBooleanToggle) m_nonPortableDumpCheck.getData()).setup(runtimeLogging);
+      ((XmlBooleanToggle) m_waitNotifyDebugCheck.getData()).setup(runtimeLogging);
+      ((XmlBooleanToggle) m_newObjectDebugCheck.getData()).setup(runtimeLogging);
     }
 
     private Layout(Composite parent) {
       parent.setLayout(new GridLayout());
-      
+
       Group runtimeLoggingGroup = new Group(parent, SWT.SHADOW_NONE);
       runtimeLoggingGroup.setText(RUNTIME_LOGGING);
       GridLayout gridLayout = new GridLayout();
@@ -171,7 +168,7 @@ public class RuntimeLoggingPanel extends ConfigurationEditorPanel
       m_lockDebugCheck = new Button(runtimeLoggingGroup, SWT.CHECK);
       m_lockDebugCheck.setText(LOCK_DEBUG);
       initBooleanField(m_lockDebugCheck, RuntimeLogging.class, "lock-debug");
-      
+
       m_distributedMethodDebugCheck = new Button(runtimeLoggingGroup, SWT.CHECK);
       m_distributedMethodDebugCheck.setText(DISTRIBUTED_METHOD_DEBUG);
       initBooleanField(m_distributedMethodDebugCheck, RuntimeLogging.class, "distributed-method-debug");
@@ -179,15 +176,15 @@ public class RuntimeLoggingPanel extends ConfigurationEditorPanel
       m_fieldChangeDebugCheck = new Button(runtimeLoggingGroup, SWT.CHECK);
       m_fieldChangeDebugCheck.setText(FIELD_CHANGE_DEBUG);
       initBooleanField(m_fieldChangeDebugCheck, RuntimeLogging.class, "field-change-debug");
-      
+
       m_nonPortableDumpCheck = new Button(runtimeLoggingGroup, SWT.CHECK);
       m_nonPortableDumpCheck.setText(NON_PORTABLE_DUMP);
       initBooleanField(m_nonPortableDumpCheck, RuntimeLogging.class, "non-portable-dump");
-      
+
       m_waitNotifyDebugCheck = new Button(runtimeLoggingGroup, SWT.CHECK);
       m_waitNotifyDebugCheck.setText(WAIT_NOTIFY_DEBUG);
       initBooleanField(m_waitNotifyDebugCheck, RuntimeLogging.class, "wait-notify-debug");
-      
+
       m_newObjectDebugCheck = new Button(runtimeLoggingGroup, SWT.CHECK);
       m_newObjectDebugCheck.setText(NEW_OBJECT_DEBUG);
       initBooleanField(m_newObjectDebugCheck, RuntimeLogging.class, "new-object-debug");
