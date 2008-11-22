d4c288b28fa03c7119e868c0ead84b4e94a21c97
etsai <etsai@terracottatech.com>
2008-11-22 17:31:37 +0000

    A logic missing if coordinatorGrpId is zero
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@10901 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/common/src/com/tc/config/schema/L2ConfigForL1Object.java
+++ code/base/common/src/com/tc/config/schema/L2ConfigForL1Object.java
@@ -165,6 +165,7 @@ public class L2ConfigForL1Object implements L2ConfigForL1 {
     this.l2DataByGroup = new ObjectArrayConfigItem[keys.size()];
 
     int l2DataByGroupPosition = 0;
+    boolean isCoordinatorSet = false;
     for (Iterator iter = keys.iterator(); iter.hasNext();) {
       Integer key = (Integer) iter.next();
       List l2DataList = (List) this.l2DataByGroupId.get(key);
@@ -174,11 +175,12 @@ public class L2ConfigForL1Object implements L2ConfigForL1 {
         L2Data data = (L2Data) iterator.next();
         l2DataArray[position++] = data;
       }
-      if (l2DataByGroupPosition != coordinatorGrpId) {
+      if ((l2DataByGroupPosition != coordinatorGrpId) || isCoordinatorSet) {
         setL2DataInGrp(l2DataByGroupPosition + 1, l2DataArray);
         l2DataByGroupPosition++;
       } else {
         setL2DataInGrp(0, l2DataArray);
+        isCoordinatorSet = true;
       }
     }
   }
