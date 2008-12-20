ff7b7da5c98153e0c5893b542e1ef1e67df2ffb4
Chris Dennis <chris.w.dennis@gmail.com>
2008-12-20 04:45:11 +0000

    JavaDoc for ExceptionTableOrderingMethodAdapter
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@11131 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-l1/src/com/tc/object/bytecode/ExceptionTableOrderingMethodAdapter.java
+++ code/base/dso-l1/src/com/tc/object/bytecode/ExceptionTableOrderingMethodAdapter.java
@@ -12,18 +12,38 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
+/**
+ * Provides correct sorting of exception handlers based on their catch ranges.
+ * <p>
+ * This fixes a hypothesized bug in ASM.  An exception handler nested within an existing handler via instrumentation can
+ * result in the outer handler having its entry before the inner handler in the method's exception table.  In the case
+ * of an intersection of the two catch types (both handlers are potential candidates for catching the exception) the
+ * inner handler added by instrumentation will not be executed, the exception will be caught by the outer handler first
+ * (See CDV-391).
+ */
 public class ExceptionTableOrderingMethodAdapter extends MethodAdapter {
 
+  /**
+   * Assembled list of method exception handlers.
+   */
   private final List handlers = new ArrayList();
 
   public ExceptionTableOrderingMethodAdapter(MethodVisitor mv) {
     super(mv);
   }
 
+  /**
+   * Caches try/catch blocks in a local data structure.
+   */
   public void visitTryCatchBlock(Label start, Label end, Label handler, String type) {
     handlers.add(new Handler(start, end, handler, type, handlers.size()));
   }
 
+  /**
+   * Sorts the local list of exception handlers and then visits them in the correct order.
+   * <p>
+   * Following this it obviously calls <code>super.visitMax(...)</code>
+   */
   public void visitMaxs(int maxStack, int maxLocals) {
     Collections.sort(handlers);
     for (Iterator it = handlers.iterator(); it.hasNext(); ) {
@@ -33,8 +53,20 @@ public class ExceptionTableOrderingMethodAdapter extends MethodAdapter {
     super.visitMaxs(maxStack, maxLocals);
   }
   
+  /**
+   * Represents an exception handler.
+   */
   private static class Handler implements Comparable {
 
+    /**
+     * Constructs a Handler instance with the given properties.
+     * 
+     * @param start Label representing the start of the try block (inclusive).
+     * @param end Label representing the end of the try block (exclusive).
+     * @param handler Label representing the start of the handler code.
+     * @param type String representation of the internal Java type caught by this handler (<code>null</code> for finally).
+     * @param index integer representing the position in the original visited order. 
+     */
     public Handler(Label start, Label end, Label handler, String type, int index) {
       this.start = start;
       this.end = end;
@@ -42,6 +74,7 @@ public class ExceptionTableOrderingMethodAdapter extends MethodAdapter {
       this.desc = type;
       this.index = index;
     }
+    
     /**
      * Beginning of the exception handler's scope (inclusive).
      */
@@ -59,19 +92,31 @@ public class ExceptionTableOrderingMethodAdapter extends MethodAdapter {
 
     /**
      * Internal name of the type of exceptions handled by this handler, or
-     * <tt>null</tt> to catch any exceptions.
+     * <code>null</code> to catch any exceptions.
      */
     private final String desc;
 
     /**
-     * Visited Order.
+     * Handlers position in the original visited order.
      */
     private final int index;
 
+    /**
+     * Visit the supplied MethodVisitor with this try/catch block.
+     */
     void accept(MethodVisitor mv) {
       mv.visitTryCatchBlock(start, end, handler, desc);
     }
 
+    /**
+     * Defines the natural ordering of try blocks.  We order the exception table by the following rules:
+     * <ol>
+     * <li>Non-intersecting try blocks are ordered in the table by their bytecode position.</li>
+     * <li>Blocks entirely contained by other blocks precede their enclosers in the table</li>
+     * <li>Blocks that partially intersect will throw <code>IllegalStateException</code>.  They cannot be generated from
+     * Java source code.</li>
+     * </ol>
+     */
     public int compareTo(Object o) {
       Handler h0 = this;
       Handler h1 = (Handler) o;
@@ -95,14 +140,16 @@ public class ExceptionTableOrderingMethodAdapter extends MethodAdapter {
         int h1Size = h1.end.getOffset() - h1.start.getOffset();
 
         if (h0Size == h1Size) {
+          //identically scoped - revert to visited order
           return h0.index - h1.index;
         } else {
+          //tightest scope comes first
           return h0Size - h1Size;
         }
       }
 
       //screwed up
-      throw new IllegalStateException("Non-Compliant Handler Ranges : " + h0 + " and " + h1);
+      throw new IllegalStateException("Handler Ranges Not Language Compliant: " + h0 + " and " + h1);
     }
   }
 }
