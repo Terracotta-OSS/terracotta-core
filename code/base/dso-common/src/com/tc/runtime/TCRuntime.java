dd03a1c941bf4b1111792decb8c3523e0d09f178
Nihit Purwar <npurwar@terracottatech.com>
2009-01-30 13:04:31 +0000

    Removed dso-l1-jdk15 and dso-common-jdk15
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@11443 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-common/src/com/tc/runtime/TCRuntime.java
+++ code/base/dso-common/src/com/tc/runtime/TCRuntime.java
@@ -1,15 +1,14 @@
 /*
- * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
  */
 package com.tc.runtime;
 
 import com.tc.properties.TCProperties;
-import com.tc.properties.TCPropertiesImpl;
 import com.tc.properties.TCPropertiesConsts;
+import com.tc.properties.TCPropertiesImpl;
 import com.tc.util.Assert;
 import com.tc.util.runtime.Vm;
-
-import java.lang.reflect.Constructor;
 
 public class TCRuntime {
 
@@ -29,34 +28,12 @@ public class TCRuntime {
 
     if (Vm.isJDK15Compliant()) {
       if (props.getBoolean(TCPropertiesConsts.MEMORY_MONITOR_FORCEBASIC)) {
-        memoryManager = getMemoryManagerJdk15Basic();
+        memoryManager = new TCMemoryManagerJdk15Basic();
       } else {
-        memoryManager = getMemoryManagerJdk15PoolMonitor();
+        memoryManager = new TCMemoryManagerJdk15PoolMonitor();
       }
     } else {
-      memoryManager = new TCMemoryManagerJdk14();
-    }
-  }
-
-  private static JVMMemoryManager getMemoryManagerJdk15PoolMonitor() {
-    return getMemoryManagerJdk15("com.tc.runtime.TCMemoryManagerJdk15PoolMonitor");
-  }
-
-  private static JVMMemoryManager getMemoryManagerJdk15Basic() {
-    return getMemoryManagerJdk15("com.tc.runtime.TCMemoryManagerJdk15Basic");
-  }
-
-  /*
-   * XXX::This method is intentionally written using Reflection so that we dont have any 1.5 dependences.
-   * TODO:: Figure a better way to do this.
-   */
-  private static JVMMemoryManager getMemoryManagerJdk15(String className) {
-    try {
-      Class c =  Class.forName(className);
-      Constructor constructor = c.getConstructor(new Class[0]);
-      return (JVMMemoryManager) constructor.newInstance(new Object[0]);
-    } catch (Exception e) {
-      throw new AssertionError(e);
+      throw new RuntimeException("JVMMemoryManager require JRE 1.5 or greater");
     }
   }
 }
