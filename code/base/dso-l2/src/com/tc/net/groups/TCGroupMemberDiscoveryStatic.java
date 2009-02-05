d7c5387d2c9a184bbd19d7fca1174f631c92e314
etsai <etsai@terracottatech.com>
2009-02-05 22:53:00 +0000

    DEV-2346, catch all throwable to prevent tc-group-comm from dying
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@11520 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-l2/src/com/tc/net/groups/TCGroupMemberDiscoveryStatic.java
+++ code/base/dso-l2/src/com/tc/net/groups/TCGroupMemberDiscoveryStatic.java
@@ -27,21 +27,22 @@ import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class TCGroupMemberDiscoveryStatic implements TCGroupMemberDiscovery {
-  private static final TCLogger                    logger                          = TCLogging
-                                                                                       .getLogger(TCGroupMemberDiscoveryStatic.class);
+  private static final TCLogger                    logger          = TCLogging
+                                                                       .getLogger(TCGroupMemberDiscoveryStatic.class);
   private final static long                        DISCOVERY_INTERVAL_MS;
   static {
-    DISCOVERY_INTERVAL_MS = TCPropertiesImpl.getProperties().getLong(TCPropertiesConsts.L2_NHA_TCGROUPCOMM_DISCOVERY_INTERVAL);
+    DISCOVERY_INTERVAL_MS = TCPropertiesImpl.getProperties()
+        .getLong(TCPropertiesConsts.L2_NHA_TCGROUPCOMM_DISCOVERY_INTERVAL);
   }
 
-  private final AtomicBoolean                      running                         = new AtomicBoolean(false);
-  private final AtomicBoolean                      stopAttempt                     = new AtomicBoolean(false);
-  private final Map<String, DiscoveryStateMachine> nodeStateMap                    = Collections
-                                                                                       .synchronizedMap(new HashMap<String, DiscoveryStateMachine>());
+  private final AtomicBoolean                      running         = new AtomicBoolean(false);
+  private final AtomicBoolean                      stopAttempt     = new AtomicBoolean(false);
+  private final Map<String, DiscoveryStateMachine> nodeStateMap    = Collections
+                                                                       .synchronizedMap(new HashMap<String, DiscoveryStateMachine>());
   private final TCGroupManagerImpl                 manager;
   private Node                                     local;
-  private Integer                                  joinedNodes                     = 0;
-  private Integer                                  connectingCount                 = 0;
+  private Integer                                  joinedNodes     = 0;
+  private Integer                                  connectingCount = 0;
 
   public TCGroupMemberDiscoveryStatic(TCGroupManagerImpl manager) {
     this.manager = manager;
@@ -94,6 +95,10 @@ public class TCGroupMemberDiscoveryStatic implements TCGroupMemberDiscovery {
     } catch (IOException e) {
       stateMachine.connetIOException();
       stateMachine.loggerWarn("Node:" + node + " " + e);
+    } catch (Throwable t) {
+      // catch all throwables to prevent discover from dying
+      stateMachine.throwableException();
+      stateMachine.loggerWarn("Node:" + node + " " + t);
     } finally {
       decConnectingCount();
     }
@@ -212,6 +217,7 @@ public class TCGroupMemberDiscoveryStatic implements TCGroupMemberDiscovery {
     private final DiscoveryState STATE_CONNECT_TIMEOUT = new ConnectTimeoutState();
     private final DiscoveryState STATE_MAX_CONNECTION  = new MaxConnExceedState();
     private final DiscoveryState STATE_IO_EXCEPTION    = new IOExceptionState();
+    private final DiscoveryState STATE_THROWABLE_EXCEP = new ThrowableExceptionState();
     private final DiscoveryState STATE_UNKNOWN_HOST    = new UnknownHostState();
     private final DiscoveryState STATE_MEMBER_IN_GROUP = new MemberInGroupState();
 
@@ -304,6 +310,10 @@ public class TCGroupMemberDiscoveryStatic implements TCGroupMemberDiscovery {
 
     void connetIOException() {
       badConnect(STATE_IO_EXCEPTION);
+    }
+
+    void throwableException() {
+      badConnect(STATE_THROWABLE_EXCEP);
     }
 
     synchronized void unknownHost() {
@@ -447,6 +457,15 @@ public class TCGroupMemberDiscoveryStatic implements TCGroupMemberDiscovery {
     private class IOExceptionState extends BadState {
       public IOExceptionState() {
         super("IO-Exception");
+      }
+    }
+
+    /*
+     * ThrowableExceptionState --
+     */
+    private class ThrowableExceptionState extends BadState {
+      public ThrowableExceptionState() {
+        super("Connection-Throwable");
       }
     }
 
