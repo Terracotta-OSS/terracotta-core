8a9f9dbe290ae8f246201a912d4591b5456d5b12
Hung Huynh <hhuynh@gmail.com>
2009-02-07 04:10:15 +0000

    alert when branch name is mismatched between OS and EE branches
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@11546 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/buildscripts/build-tc.rb
+++ code/base/buildscripts/build-tc.rb
@@ -79,6 +79,16 @@ class BaseCodeTerracottaBuilder < TerracottaBuilder
 
     # Load the XMLBeans task, so we can use it to process config files when needed by that target.
     ant.taskdef(:name => 'xmlbean', :classname => 'org.apache.xmlbeans.impl.tool.XMLBean')
+
+    # print warning if branh name doesn't match between OSS and EE branch
+    branch = @build_environment.current_branch
+    unless @build_environment.ee_svninfo.url =~ /#{branch}$/
+      STDERR.puts("*" * 80)
+      STDERR.puts("  Branch name '#{branch}' doesn't match between EE and OS branches.")
+      STDERR.puts("  EE URL: #{@build_environment.ee_svninfo.url}")
+      STDERR.puts("  OS URL: #{@build_environment.os_svninfo.url} ")
+      STDERR.puts("*" * 80)
+    end
   end
 
 
