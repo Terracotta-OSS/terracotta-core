5be61ed3d143bb6d860acedf3a21228c7554913d
Jason Voegele <jason@jvoegele.com>
2009-02-10 19:05:04 +0000

    Update trunk to 3.0.0-SNAPSHOT.
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@11612 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/eclipse2maven.rb
+++ code/base/eclipse2maven.rb
@@ -21,7 +21,7 @@ POM_DEF = <<POM
   <parent>
     <groupId>org.terracotta</groupId>
     <artifactId>parent</artifactId>
-    <version>2.8.0-SNAPSHOT</version>
+    <version>3.0.0-SNAPSHOT</version>
   </parent>
   <artifactId>${artifactId}</artifactId>
   ${dependencies}
