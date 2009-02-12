aafdc2d083b3ea24108d89e5a367a23479b611ff
Hung Huynh <hhuynh@gmail.com>
2009-02-12 16:31:34 +0000

    move jruby to a cache so different branches can use the same copy
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@11638 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/tcbuild.bat
+++ code/base/tcbuild.bat
@@ -12,13 +12,13 @@ if "x%ANT_HOME%"=="x" (
 
 setlocal
 
+set jruby_version=jruby-1.1.2-20090208
 set build_system_dir=%~dp0..\..\buildsystems
-set JRUBY_HOME=%build_system_dir%\jruby-1.1.2-20090208
+set cache_dir=c:\temp\tc
+set JRUBY_HOME=%cache_dir%\%jruby_version%
 
 if not exist "%JRUBY_HOME%" (
-	echo --------------------------------------------------------------------------------
-	echo LOADING JRUBY USING IVY
-	echo.
+	echo JRuby not found at %JRUBY_HOME%. Installing...
 	call %ANT_HOME%\bin\ant.bat -buildfile %build_system_dir%\install-jruby-build.xml
 )
 
