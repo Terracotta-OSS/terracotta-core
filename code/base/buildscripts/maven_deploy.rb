fc1b5cdd1df89e6450f3f2c35e3b2c193798b110
Jason Voegele <jason@jvoegele.com>
2009-02-19 21:18:36 +0000

    Auto-detect packaging type when deploying Maven artifacts and packaging is not explicitly specified.  (And it is never explicitly specified so this becomes rather important.)
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@11784 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/buildscripts/maven_deploy.rb
+++ code/base/buildscripts/maven_deploy.rb
@@ -3,7 +3,6 @@ class MavenDeploy
   include MavenConstants
 
   def initialize(options = {})
-    @packaging = options[:packaging] || 'jar'
     @generate_pom = options.boolean(:generate_pom, true)
     @group_id = options[:group_id] || DEFAULT_GROUP_ID
     @repository_url = options[:repository_url] || MAVEN_REPO_LOCAL
@@ -11,10 +10,12 @@ class MavenDeploy
     @snapshot = options.boolean(:snapshot)
   end
 
-  def deploy_file(file, artifact_id, version, pom_file = nil, dry_run = false)
+  def deploy_file(file, artifact_id, version, pom_file = nil, packaging = nil, dry_run = false)
     unless File.exist?(file)
       raise("Bad 'file' argument passed to deploy_file.  File does not exist: #{file}")
     end
+
+    packaging ||= file =~ /pom.*\.xml$/ ? 'pom' : File.extname(file)
 
     command = dry_run ? ['echo'] : []
     command << FilePath.new('mvn').batch_extension.to_s << '-B' << '-N'
@@ -28,7 +29,7 @@ class MavenDeploy
     end
 
     command_args = {
-      'packaging' => @packaging,
+      'packaging' => packaging,
       'groupId' => @group_id,
       'artifactId' => artifact_id,
       'file' => file,
