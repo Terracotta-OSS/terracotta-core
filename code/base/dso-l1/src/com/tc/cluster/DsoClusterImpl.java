ba31d18b373c32992c745d154d642210bf236327
Walter Harley <wharley@terracottatech.com>
2009-02-21 00:25:52 +0000

    redo change 11800, which got accidentally blown away by 11812
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@11815 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-l1/src/com/tc/cluster/DsoClusterImpl.java
+++ code/base/dso-l1/src/com/tc/cluster/DsoClusterImpl.java
@@ -12,6 +12,7 @@ import com.tc.object.ClusterMetaDataManager;
 import com.tc.object.ObjectID;
 import com.tc.object.bytecode.Manageable;
 import com.tc.object.bytecode.TCMap;
+import com.tc.util.Assert;
 
 import java.util.Arrays;
 import java.util.Collection;
@@ -22,8 +23,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArrayList;
-
-import junit.framework.Assert;
 
 public class DsoClusterImpl implements DsoClusterInternal {
 
