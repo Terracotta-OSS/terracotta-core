059656ef074142d13fda8b74fa2e3bdac373ded6
Hung Huynh <hhuynh@gmail.com>
2009-02-28 01:47:55 +0000

    INT-638: separate extra jars that needed inside jruby into a folder. Update to jruby-1.1.6
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@11912 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/tcbuild.bat
+++ code/base/tcbuild.bat
@@ -10,12 +10,20 @@ if "x%ANT_HOME%"=="x" (
   cmd /c exit /b 1
 )
 
-setlocal
+setlocal enabledelayedexpansion
 
-set jruby_version=jruby-1.1.2-20090208
+set jruby_version=jruby-1.1.6
 set build_system_dir=%~dp0..\..\buildsystems
+set old_CD=%CD%
+cd %build_system_dir%
+set build_system_dir=%CD%
+cd %old_CD%
 set cache_dir=c:\temp\tc
 set JRUBY_HOME=%cache_dir%\%jruby_version%
+set extra_jruby_cp=%CLASSPATH%
+FOR %%F IN (%build_system_dir%\jruby-lib\*.jar) DO (
+  SET extra_jruby_cp=!extra_jruby_cp!;%%F%
+)
 
 if not exist "%cache_dir%" (
   mkdir %cache_dir%
@@ -25,6 +33,8 @@ if not exist "%JRUBY_HOME%" (
 	echo JRuby not found at %JRUBY_HOME%. Installing...
 	call %ANT_HOME%\bin\ant.bat -buildfile %build_system_dir%\install-jruby-build.xml
 )
+
+set CLASSPATH=%extra_jruby_cp%
 
 echo.
 echo --------------------------------------------------------------------------------
