bcb2da25504939869f6b41808b82b84201392e60
Hung Huynh <hhuynh@gmail.com>
2009-03-10 22:38:28 +0000

    don't swallow stack trace on fatal error
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@12067 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/tim-get-tool/src/org/terracotta/tools/cli/TIMGetTool.java
+++ code/base/tim-get-tool/src/org/terracotta/tools/cli/TIMGetTool.java
@@ -44,7 +44,7 @@ public class TIMGetTool {
       }
       System.exit(1);
     } catch (Exception e) {
-      System.err.println("Internal error: " + e.getMessage());
+      e.printStackTrace();
       System.exit(2);
     }
   }
