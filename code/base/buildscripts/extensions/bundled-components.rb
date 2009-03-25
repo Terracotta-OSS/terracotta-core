2e452cceb1d29697969d4f01973f9c4ec46d854e
Hung Huynh <hhuynh@gmail.com>
2009-03-25 17:10:53 +0000

    add title to javadoc to show version
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@12316 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/buildscripts/extensions/bundled-components.rb
+++ code/base/buildscripts/extensions/bundled-components.rb
@@ -106,11 +106,13 @@ module BundledComponents
           a_module.subtree(src).copy_classes(@build_results, runtime_classes_dir, ant, :excludes => excludes)
           if javadoc
             puts "Generating javadoc for #{a_module.name}"
-            javadoc_destdir = FilePath.new(File.dirname(destdir.to_s), "docs", "javadoc").ensure_directory
+            javadoc_destdir = FilePath.new(File.dirname(destdir.to_s), "docs", "api", @config_source['api.version'], "javadoc").ensure_directory
+            title = "Terracotta API version #{@config_source['api.version']}"
             ant.javadoc(:destdir => javadoc_destdir.to_s,
               :author => true, :version => true, :use => true, :defaultexcludes => "true",
+              :header => title,
               :bottom => "<i>All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.</i>",
-              :doctitle => "Terracotta API", :windowtitle => "Terracotta API Documentation") do
+              :doctitle => title, :windowtitle => title) do
               ant.packageset(:dir => a_module.name + '/src', :defaultexcludes => true) do
                 ant.include(:name => 'com/tc/**')
               end
