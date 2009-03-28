13ca19135919818a1d8eec8e16ebeab708dfde51
etsai <etsai@terracottatech.com>
2009-03-28 10:42:02 +0000

    enable LinkedBlockingQueueSpawningActiveActiveTest to simulate DEV-2609 issue
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@12364 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-system-tests/tests.base/com/tctest/LinkedBlockingQueueSpawningTestApp.java
+++ code/base/dso-system-tests/tests.base/com/tctest/LinkedBlockingQueueSpawningTestApp.java
@@ -4,7 +4,7 @@
  */
 package com.tctest;
 
-import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
 
 import com.tc.exception.TCRuntimeException;
@@ -12,19 +12,15 @@ import com.tc.object.bytecode.Manageable;
 import com.tc.object.config.ConfigVisitor;
 import com.tc.object.config.DSOClientConfigHelper;
 import com.tc.object.config.TransparencyClassSpec;
-import com.tc.process.StreamCopier;
+import com.tc.objectserver.control.ExtraL1ProcessControl;
 import com.tc.simulator.app.ApplicationConfig;
 import com.tc.simulator.listener.ListenerProvider;
 import com.tctest.runner.AbstractTransparentApp;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.net.URL;
 import java.util.ArrayList;
-import java.util.Enumeration;
 import java.util.List;
-import java.util.Properties;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -34,12 +30,12 @@ import java.util.concurrent.atomic.AtomicLong;
 import junit.framework.Assert;
 
 public class LinkedBlockingQueueSpawningTestApp extends AbstractTransparentApp {
-  private static final Logger                        LOG                = Logger
+  private static final Logger                        logger             = Logger
                                                                             .getLogger(LinkedBlockingQueueSpawningTestApp.class);
   public static String                               CONFIG_FILE        = "config-file";
 
   private static final long                          SPAWN_SLEEP_PERIOD = 1000;
-  private static final int                           SPAWN_COUNT        = 10;
+  private static final int                           SPAWN_COUNT        = 6;
   private static final int                           PRODUCERS          = 5;
   private static final int                           CONSUMERS          = 3;
   private static final int                           LBQ_CAPACITY       = 300;
@@ -54,33 +50,31 @@ public class LinkedBlockingQueueSpawningTestApp extends AbstractTransparentApp {
   private static final AtomicLong                    outCounter         = new AtomicLong(0L);
 
   // end roots
+  private final ApplicationConfig                    config;
 
   public LinkedBlockingQueueSpawningTestApp(String appId, ApplicationConfig cfg, ListenerProvider listenerProvider) {
     super(appId, cfg, listenerProvider);
-
-    System.setProperty("tc.config", cfg.getAttribute(CONFIG_FILE));
-
-    System.out.println("XXX Test for spawning LBQClient " + SPAWN_COUNT + " times and sleep " + SPAWN_SLEEP_PERIOD
-                       + "ms in between.");
+    this.config = cfg;
+    logger.info("XXX Test for spawning LBQClient " + SPAWN_COUNT + " times and sleep " + SPAWN_SLEEP_PERIOD
+                + "ms in between.");
   }
 
   public void run() {
 
-    Thread spawnerThread = new Thread(new Spawner(), "Spawner");
-    spawnerThread.start();
     try {
-      spawnerThread.join();
-    } catch (InterruptedException e) {
+      for (int i = 0; i < SPAWN_COUNT; ++i) {
+        spawnNewClientAndWaitForCompletion("" + i, LBQClient.class);
+      }
+    } catch (Exception e) {
       throw new TCRuntimeException(e);
     }
+
   }
 
   public static void visitL1DSOConfig(ConfigVisitor visitor, DSOClientConfigHelper config) {
     String testClass = LinkedBlockingQueueSpawningTestApp.class.getName();
     TransparencyClassSpec spec = config.getOrCreateSpec(testClass);
-
-    config.addIncludePattern(testClass + "$*");
-
+    config.addIncludePattern(testClass + "*");
     String methodExpression = "* " + testClass + "*.*(..)";
     config.addWriteAutolock(methodExpression);
     methodExpression = "* " + WorkItem.class.getName() + "*.*(..)";
@@ -91,16 +85,39 @@ public class LinkedBlockingQueueSpawningTestApp extends AbstractTransparentApp {
     config.addWriteAutolock(methodExpression);
     methodExpression = "* " + Consumer.class.getName() + "*.*(..)";
     config.addWriteAutolock(methodExpression);
-    methodExpression = "* " + Spawner.class.getName() + "*.*(..)";
-    config.addWriteAutolock(methodExpression);
-    methodExpression = "* " + Utils.class.getName() + "*.*(..)";
-    config.addWriteAutolock(methodExpression);
 
     spec.addRoot("queue", "queue");
     spec.addRoot("counter", "counter");
     spec.addRoot("outCounter", "outCounter");
     spec.addRoot("inputLock", "inputLock");
     spec.addRoot("outputLock", "outputLock");
+  }
+
+  protected ExtraL1ProcessControl spawnNewClientAndWaitForCompletion(String clientID, Class clientClass)
+      throws Exception {
+    final String hostName = "localhost";
+    final int port = 0;
+    final File configFile = new File(config.getAttribute(CONFIG_FILE));
+    File workingDir = new File(configFile.getParentFile(), "client-" + clientID);
+    FileUtils.forceMkdir(workingDir);
+
+    List jvmArgs = new ArrayList();
+    addTestTcPropertiesFile(jvmArgs);
+    ExtraL1ProcessControl client = new ExtraL1ProcessControl(hostName, port, clientClass, configFile.getAbsolutePath(),
+                                                             new String[0], workingDir, jvmArgs);
+    client.start();
+    client.mergeSTDERR();
+    client.mergeSTDOUT();
+    Assert.assertEquals("Client exits with error", 0, client.waitFor());
+    return client;
+  }
+
+  protected final void addTestTcPropertiesFile(List jvmArgs) {
+    URL url = getClass().getResource("/com/tc/properties/tests.properties");
+    if (url == null) { return; }
+    String pathToTestTcProperties = url.getPath();
+    if (pathToTestTcProperties == null || pathToTestTcProperties.equals("")) { return; }
+    jvmArgs.add("-Dcom.tc.properties=" + pathToTestTcProperties);
   }
 
   public static class WorkItem {
@@ -116,10 +133,11 @@ public class LinkedBlockingQueueSpawningTestApp extends AbstractTransparentApp {
   }
 
   public static class LBQClient {
-    private final ExecutorService pool = Executors.newCachedThreadPool();
+    private final ExecutorService pool     = Executors.newCachedThreadPool();
 
     private final long            timeout;
     private long                  runtime;
+    private int                   exitCode = 0;
 
     public LBQClient(long runtime) {
 
@@ -130,8 +148,8 @@ public class LinkedBlockingQueueSpawningTestApp extends AbstractTransparentApp {
 
     public void run() {
       try {
-        LOG.info("Starting... with counter=" + counter.get() + " counter:"
-                 + ((Manageable) counter).__tc_managed().getObjectID());
+        logger.info("Starting... with counter=" + counter.get() + " counter:"
+                    + ((Manageable) counter).__tc_managed().getObjectID());
 
         for (int i = 0; i < PRODUCERS; i++) {
           pool.execute(new Producer(this, timeout));
@@ -142,8 +160,13 @@ public class LinkedBlockingQueueSpawningTestApp extends AbstractTransparentApp {
         }
 
       } catch (Throwable e) {
+        exitCode = 1;
         throw new RuntimeException(e);
       }
+    }
+
+    public void setFail() {
+      exitCode = 1;
     }
 
     public void await() {
@@ -154,23 +177,24 @@ public class LinkedBlockingQueueSpawningTestApp extends AbstractTransparentApp {
           pool.awaitTermination(runtime + 200L, TimeUnit.MILLISECONDS);
         }
       } catch (InterruptedException e) {
-        LOG.error("Got interrupted while waiting for all threads to finish");
-        System.exit(1); // not a graceful exit
+        logger.error("Got interrupted while waiting for all threads to finish");
+        exitCode = 1;
       }
     }
 
     public static void main(String[] args) {
       Runtime.getRuntime().addShutdownHook(new Thread() {
         public void run() {
-          LOG.info(LBQClient.class.getName() + " shutdown.");
+          logger.info(LBQClient.class.getName() + " shutdown.");
         }
       });
 
       final LBQClient t = new LBQClient(CLIENT_RUNTIME);
       t.run();
       t.await();
+      logger.info("One client done. exit=" + t.exitCode);
+      System.exit(t.exitCode);
     }
-
   }
 
   public static class Producer implements Runnable {
@@ -186,9 +210,10 @@ public class LinkedBlockingQueueSpawningTestApp extends AbstractTransparentApp {
           synchronized (inputLock) {
             WorkItem d = new WorkItem(counter.getAndIncrement());
             queue.put(d);
-            LOG.info("XXX produce " + d.getID());
+            if (d.getID() % 100 == 0) {
+              logger.info("XXX produce " + d.getID());
+            }
           }
-
           Thread.sleep(3);
         }
       } catch (Throwable e) {
@@ -204,9 +229,11 @@ public class LinkedBlockingQueueSpawningTestApp extends AbstractTransparentApp {
 
   public static class Consumer implements Runnable {
     private long timeout;
+    private final LBQClient client;
 
-    public Consumer(LBQClient driver, long timeout) {
+    public Consumer(LBQClient client, long timeout) {
       this.timeout = timeout;
+      this.client = client;
     }
 
     public void run() {
@@ -215,93 +242,20 @@ public class LinkedBlockingQueueSpawningTestApp extends AbstractTransparentApp {
           synchronized (outputLock) {
             WorkItem data = queue.take();
             Assert.assertEquals("Sequence mismatch!", outCounter.getAndIncrement(), data.getID());
-            LOG.info("XXX consume " + data.getID());
+            if (data.getID() % 100 == 0) {
+              logger.info("XXX consume " + data.getID());
+            }
           }
         }
       } catch (Throwable e) {
+        client.setFail();
         e.printStackTrace();
-        System.exit(1);
+        throw new TCRuntimeException(e);
       }
     }
 
     private boolean canRun() {
       return timeout == 0L || System.currentTimeMillis() < timeout;
-    }
-  }
-
-  public class Spawner implements Runnable {
-
-    private List<String> spawnL1cmd() {
-      List<String> cmdList = new ArrayList<String>();
-      cmdList.add(Utils.getProperty("java.home") + "/bin/java");
-      Enumeration<?> e = System.getProperties().propertyNames();
-      while (e.hasMoreElements()) {
-        String prop = (String) e.nextElement();
-        if (prop.startsWith("tc.") || prop.startsWith("lbq.")) {
-          cmdList.add("-D" + prop + "=" + quoteIfNeeded(Utils.getProperty(prop)));
-        }
-      }
-      cmdList.add("-Xbootclasspath/p:" + Utils.getProperty("sun.boot.class.path").split(File.pathSeparator)[0]);
-      cmdList.add("-Dtc.nodeName=spawn");
-      cmdList.add("-cp");
-      cmdList.add(quoteIfNeeded(Utils.getProperty("java.class.path")));
-      cmdList.add(com.tctest.LinkedBlockingQueueSpawningTestApp.LBQClient.class.getName());
-      return cmdList;
-    }
-
-    private Process createNewL1() throws IOException {
-      List<String> cmdList = spawnL1cmd();
-      LOG.info("Spawn cmd: " + cmdList);
-      Process p = Runtime.getRuntime().exec(cmdList.toArray(new String[0]), null,
-                                            new File(Utils.getProperty("user.dir")));
-      Utils.merge(p.getInputStream(), System.out);
-      Utils.merge(p.getErrorStream(), System.err);
-      return p;
-    }
-
-    private String quoteIfNeeded(String path) {
-      if (path.indexOf(" ") > 0) { return "\"" + path + "\""; }
-      return path;
-    }
-
-    public void run() {
-      try {
-        for (int i = 0; i < SPAWN_COUNT; ++i) {
-          LOG.info("Spawning LBQLongRunning...");
-          Process l1 = createNewL1();
-          int returnedCode = l1.waitFor();
-          IOUtils.closeQuietly(l1.getInputStream());
-          IOUtils.closeQuietly(l1.getOutputStream());
-          IOUtils.closeQuietly(l1.getErrorStream());
-          l1.destroy();
-
-          if (returnedCode != 0) {
-            LOG.error("L1 returned code: " + returnedCode);
-            System.exit(-1);
-          } else {
-            LOG.info("L1 finished.");
-          }
-          Thread.sleep(SPAWN_SLEEP_PERIOD);
-        }
-      } catch (Exception e) {
-        LOG.error(e);
-      }
-    }
-  }
-
-  public static class Utils {
-    private static final Properties props = new Properties();
-    static {
-      props.putAll(System.getProperties());
-    }
-
-    public static String getProperty(String key) {
-      return props.getProperty(key);
-    }
-
-    public static void merge(InputStream in, OutputStream out) {
-      StreamCopier sc = new StreamCopier(in, out);
-      sc.start();
     }
   }
 
