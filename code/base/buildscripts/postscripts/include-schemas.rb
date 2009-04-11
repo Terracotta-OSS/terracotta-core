7150993b1a411a941689bc455281a53b481c6380
Hung Huynh <hhuynh@gmail.com>
2009-04-11 00:52:56 +0000

    refactor schema generator code
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@12498 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/buildscripts/postscripts/include-schemas.rb
+++ code/base/buildscripts/postscripts/include-schemas.rb
@@ -29,38 +29,52 @@ class BaseCodeTerracottaBuilder <  TerracottaBuilder
     end
 
     unless @static_resources.docflex_home.nil?
-      destdir = FilePath.new(product_directory, args[0].split('/')).ensure_directory
-      srcdir = @static_resources.config_schema_source_directory(@module_set)
-
-      ant.copy(:todir => destdir.to_s) do
-        ant.fileset(:dir => srcdir.to_s, :includes => 'terracotta*.xsd')
+      tmp_schema_dir = File.join(@distribution_results.build_dir.to_s, "schema")
+      puts "creating schema in temp schema dir: #{tmp_schema_dir}"
+      create_schema(tmp_schema_dir)
+      args.each do | arg |
+        puts "arg #{arg}"
+        destdir = FilePath.new(product_directory, *arg.split('/')).ensure_directory
+        puts "copy schema into #{destdir.to_s}"
+        ant.copy(:todir => destdir.to_s) do
+          ant.fileset(:dir => tmp_schema_dir)
+        end
       end
-      
-      docflex           = FilePath.new(@static_resources.docflex_home)
-      docflex_lib       = FilePath.new(docflex, "lib")
-      docflex_template  = FilePath.new(docflex, "templates", "XSDDoc", "FramedDoc.tpl")
-      docflex_classpath = "#{docflex_lib.to_s}/xercesImpl.jar:#{docflex_lib.to_s}/xml-apis.jar:#{docflex_lib.to_s}/docflex-xml-re.jar"
-      xsd_file, schema_version  = extract_schema_version(destdir)
+      puts "Deleting tmp schema dir..."
+      FileUtils.rm_rf(tmp_schema_dir)
+    end
+  end
 
-      ant.java(
-        :classname   => 'com.docflex.xml.Generator',
-        :classpath   => docflex_classpath,
-        :fork        => true,
-        :failonerror => true,
-        :dir         => destdir.to_s,
-        :maxmemory   => '512m') do
-        ant.jvmarg(:value => "-Djava.awt.headless=true")
-        ant.arg(:line => "-template #{docflex_template.canonicalize.to_s}")
-        ant.arg(:line => "-format HTML")
-        ant.arg(:line => "-p:docTitle=\"Terracotta Configuration Schema Version #{schema_version}\"")
-        ant.arg(:line => "-d #{destdir.to_s}")
-        ant.arg(:line => "-f index.html")
-        ant.arg(:line => "-nodialog=true")
-        ant.arg(:line => "-quiet=true")
-        ant.arg(:line => "-launchviewer=false")
-        ant.arg(:line => File.basename(xsd_file.to_s))
-      end
+  def create_schema(destdir)
+    srcdir = @static_resources.config_schema_source_directory(@module_set)
+
+    ant.copy(:todir => destdir.to_s) do
+      ant.fileset(:dir => srcdir.to_s, :includes => 'terracotta*.xsd')
     end
 
+    docflex           = FilePath.new(@static_resources.docflex_home)
+    docflex_lib       = FilePath.new(docflex, "lib")
+    docflex_template  = FilePath.new(docflex, "templates", "XSDDoc", "FramedDoc.tpl")
+    docflex_classpath = "#{docflex_lib.to_s}/xercesImpl.jar:#{docflex_lib.to_s}/xml-apis.jar:#{docflex_lib.to_s}/docflex-xml-re.jar"
+    xsd_file, schema_version  = extract_schema_version(destdir)
+
+    ant.java(
+      :classname   => 'com.docflex.xml.Generator',
+      :classpath   => docflex_classpath,
+      :fork        => true,
+      :failonerror => true,
+      :dir         => destdir.to_s,
+      :maxmemory   => '512m') do
+      ant.jvmarg(:value => "-Djava.awt.headless=true")
+      ant.arg(:line => "-template #{docflex_template.canonicalize.to_s}")
+      ant.arg(:line => "-format HTML")
+      ant.arg(:line => "-p:docTitle=\"Terracotta Configuration Schema Version #{schema_version}\"")
+      ant.arg(:line => "-d #{destdir.to_s}")
+      ant.arg(:line => "-f index.html")
+      ant.arg(:line => "-nodialog=true")
+      ant.arg(:line => "-quiet=true")
+      ant.arg(:line => "-launchviewer=false")
+      ant.arg(:line => File.basename(xsd_file.to_s))
+    end
   end
 end
