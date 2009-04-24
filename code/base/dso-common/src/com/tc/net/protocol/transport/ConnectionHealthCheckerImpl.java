426958a59c6616d3ddf4e3017564ba0f0aba640a
etsai <etsai@terracottatech.com>
2009-04-24 04:10:06 +0000

    revert 12556
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@12559 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-common/src/com/tc/net/protocol/transport/ConnectionHealthCheckerImpl.java
+++ code/base/dso-common/src/com/tc/net/protocol/transport/ConnectionHealthCheckerImpl.java
@@ -182,8 +182,6 @@ public class ConnectionHealthCheckerImpl implements ConnectionHealthChecker {
 
             if (!connContext.probeIfAlive()) {
               // Connection is dead. Disconnect the transport.
-              logger.error("Declared connection dead " + mtb.getConnectionId() + " due to long idle time "
-                           + mtb.getConnection().getIdleReceiveTime());
               mtb.disconnect();
               connectionIterator.remove();
             }
