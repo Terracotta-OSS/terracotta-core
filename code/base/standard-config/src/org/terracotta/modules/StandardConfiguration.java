a1b1d61eaa5a571f66ffa0d69ba5331727ccd8a2
Hung Huynh <hhuynh@gmail.com>
2009-05-01 00:31:05 +0000

    revert  12620:12617 12609:12607
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@12625 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/standard-config/src/org/terracotta/modules/StandardConfiguration.java
+++ code/base/standard-config/src/org/terracotta/modules/StandardConfiguration.java
@@ -11,88 +11,53 @@ import com.tc.object.config.TransparencyClassSpec;
 
 public class StandardConfiguration extends TerracottaConfiguratorModule {
 
-  protected void addInstrumentation(final BundleContext context) {
-    super.addInstrumentation(context);
-    configFileTypes();
-    configEventTypes();
-    configExceptionTypes();
-    configArrayTypes();
-    configUnsafe();
-    configTroveTypes();
-    configJavaProxyTypes();
-    configBackportConcurrentTypes();
-  }
+    protected void addInstrumentation(final BundleContext context) {
+        super.addInstrumentation(context);
+        configFileTypes();
+        configEventTypes();
+        configExceptionTypes();
+        configArrayTypes();
+        configUnsafe();
+    }
 
-  private void configTroveTypes() {
-    configHelper.addIncludePattern("gnu.trove..*", false, false, true);
-  }
+    private void configUnsafe() {
+        TransparencyClassSpec spec;
 
-  private void configJavaProxyTypes() {
-    configHelper.addIncludePattern("java.lang.reflect.Proxy", false, false, false);
-    // TODO remove if we find a better way using ProxyApplicator etc.
-    configHelper.addIncludePattern("$Proxy..*", false, false, true);
-  }
+        spec = getOrCreateSpec("sun.misc.Unsafe");
+        spec.setCustomClassAdapter(new UnsafeAdapter());
+        spec.markPreInstrumented();
 
-  private void configBackportConcurrentTypes() {
-    configHelper.addIncludePattern("edu.emory.mathcs.backport.java.util.AbstractCollection", false, false, false);
-    configHelper.addIncludePattern("edu.emory.mathcs.backport.java.util.AbstractQueue", false, false, false);
-    configHelper.addIncludePattern("edu.emory.mathcs.backport.java.util.concurrent.LinkedBlockingQueue", false, false,
-                                   false);
-    configHelper.addIncludePattern("edu.emory.mathcs.backport.java.util.concurrent.LinkedBlockingQueue$Node", false,
-                                   false, false);
-    configHelper.addIncludePattern("edu.emory.mathcs.backport.java.util.concurrent.FutureTask", false, false, false);
+        spec = getOrCreateSpec("com.tcclient.util.DSOUnsafe");
+        spec.setCustomClassAdapter(new DSOUnsafeAdapter());
+        spec.markPreInstrumented();
+    }
 
-    configHelper.addIncludePattern("edu.emory.mathcs.backport.java.util.concurrent.ConcurrentLinkedQueue", false,
-                                   false, false);
-    configHelper.addIncludePattern("edu.emory.mathcs.backport.java.util.concurrent.PriorityBlockingQueue", false,
-                                   false, false);
-    configHelper.addIncludePattern("edu.emory.mathcs.backport.java.util.concurrent.ArrayBlockingQueue", false, false,
-                                   false);
-    configHelper.addIncludePattern("edu.emory.mathcs.backport.java.util.concurrent.CopyOnWriteArrayList", false, false,
-                                   false);
-    configHelper.addIncludePattern("edu.emory.mathcs.backport.java.util.concurrent.ConcurrentHashMap", false, false,
-                                   false);
+    private void configArrayTypes() {
+        final TransparencyClassSpec spec = getOrCreateSpec("java.util.Arrays");
+        spec.addDoNotInstrument("copyOfRange");
+        spec.addDoNotInstrument("copyOf");
+        getOrCreateSpec("java.util.Arrays$ArrayList");
+    }
 
-  }
+    private void configFileTypes() {
+        final TransparencyClassSpec spec = getOrCreateSpec("java.io.File");
+        spec.setHonorTransient(true);
+    }
 
-  private void configUnsafe() {
-    TransparencyClassSpec spec;
+    private void configEventTypes() {
+        getOrCreateSpec("java.util.EventObject");
+    }
 
-    spec = getOrCreateSpec("sun.misc.Unsafe");
-    spec.setCustomClassAdapter(new UnsafeAdapter());
-    spec.markPreInstrumented();
-
-    spec = getOrCreateSpec("com.tcclient.util.DSOUnsafe");
-    spec.setCustomClassAdapter(new DSOUnsafeAdapter());
-    spec.markPreInstrumented();
-  }
-
-  private void configArrayTypes() {
-    final TransparencyClassSpec spec = getOrCreateSpec("java.util.Arrays");
-    spec.addDoNotInstrument("copyOfRange");
-    spec.addDoNotInstrument("copyOf");
-    getOrCreateSpec("java.util.Arrays$ArrayList");
-  }
-
-  private void configFileTypes() {
-    final TransparencyClassSpec spec = getOrCreateSpec("java.io.File");
-    spec.setHonorTransient(true);
-  }
-
-  private void configEventTypes() {
-    getOrCreateSpec("java.util.EventObject");
-  }
-
-  private void configExceptionTypes() {
-    getOrCreateSpec("java.lang.Exception");
-    getOrCreateSpec("java.lang.RuntimeException");
-    getOrCreateSpec("java.lang.InterruptedException");
-    getOrCreateSpec("java.awt.AWTException");
-    getOrCreateSpec("java.io.IOException");
-    getOrCreateSpec("java.io.FileNotFoundException");
-    getOrCreateSpec("java.lang.Error");
-    getOrCreateSpec("java.util.ConcurrentModificationException");
-    getOrCreateSpec("java.util.NoSuchElementException");
-  }
+    private void configExceptionTypes() {
+        getOrCreateSpec("java.lang.Exception");
+        getOrCreateSpec("java.lang.RuntimeException");
+        getOrCreateSpec("java.lang.InterruptedException");
+        getOrCreateSpec("java.awt.AWTException");
+        getOrCreateSpec("java.io.IOException");
+        getOrCreateSpec("java.io.FileNotFoundException");
+        getOrCreateSpec("java.lang.Error");
+        getOrCreateSpec("java.util.ConcurrentModificationException");
+        getOrCreateSpec("java.util.NoSuchElementException");
+    }
 
 }
