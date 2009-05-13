ca1b4665ccde1c5727f0afeab04458c38e801e87
Hung Huynh <hhuynh@gmail.com>
2009-05-13 01:05:10 +0000

    revert 12682
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@12697 7fc7bbf3-cf45-46d4-be06-341739edd864

---
+++ code/base/dso-spring/src/com/tcspring/ConversationLockProtocol.java
@@ -0,0 +1,30 @@
+/*
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ */
+package com.tcspring;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.tc.aspectwerkz.joinpoint.StaticJoinPoint;
+
+/**
+ * Used to replace org.springframework.webflow.conversation.impl.UtilConcurrentConversationLock
+ * 
+ * @author Eugene Kuleshov
+ */
+public class ConversationLockProtocol {
+  private final transient Log logger = LogFactory.getLog(getClass());
+
+  // a hack to work around class cast exception for call advice
+  public Object replaceUtilConversationLock(StaticJoinPoint jp) throws Throwable {
+    Object o = jp.proceed();
+    if(o.getClass().getName().equals("org.springframework.webflow.conversation.impl.UtilConcurrentConversationLock")) {
+      logger.info("Creating distributed ConversationLock");
+      return new DSOConversationLock();
+    }
+    return o;
+
+  }
+
+}
---
+++ code/base/dso-spring/src/com/tcspring/DSOConversationLock.java
@@ -0,0 +1,25 @@
+/*
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ */
+package com.tcspring;
+
+import org.springframework.webflow.conversation.impl.ConversationLock;
+
+import com.tc.object.bytecode.Manager;
+import com.tc.object.bytecode.ManagerUtil;
+
+/**
+ * Clustered conversation lock 
+ * 
+ * @author Eugene Kuleshov
+ */
+class DSOConversationLock implements ConversationLock {
+
+  public void lock() {
+    ManagerUtil.monitorEnter(this, Manager.LOCK_TYPE_WRITE);
+  }
+
+  public void unlock() {
+    ManagerUtil.monitorExit(this);
+  }
+}
