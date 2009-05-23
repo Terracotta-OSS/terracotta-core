41fee0d1fecabe78f7a5d14c99a56962697b97f4
Tim Eck <timeck@gmail.com>
2009-05-23 04:56:33 +0000

    fix busted DsoFinalMethodTest
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@12769 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-l1/tests.base/com/tc/object/TestClassFactory.java
+++ code/base/dso-l1/tests.base/com/tc/object/TestClassFactory.java
@@ -14,6 +14,7 @@ import com.tc.object.loaders.LoaderDescription;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.Collections;
 import java.util.List;
 
 public class TestClassFactory implements TCClassFactory {
@@ -187,11 +188,11 @@ public class TestClassFactory implements TCClassFactory {
     }
 
     public List<Method> getPostCreateMethods() {
-      throw new ImplementMe();
+      return Collections.EMPTY_LIST;
     }
 
     public List<Method> getPreCreateMethods() {
-      throw new ImplementMe();
+      return Collections.EMPTY_LIST;
     }
   }
 
