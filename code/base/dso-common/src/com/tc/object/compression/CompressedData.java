c92cfebd8126ce5b88553c00c59147a1903987e0
Tim Eck <timeck@gmail.com>
2009-06-01 17:58:09 +0000

    add final
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@12823 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-common/src/com/tc/object/compression/CompressedData.java
+++ code/base/dso-common/src/com/tc/object/compression/CompressedData.java
@@ -6,33 +6,32 @@ package com.tc.object.compression;
 
 public class CompressedData {
 
-  private byte[] data;
-  private int compressedSize; // how much of data is really data
-  
-  private int uncompressedSize; 
+  private final byte[] data;
+  private final int    compressedSize;  // how much of data is really data
+  private final int    uncompressedSize;
 
   public CompressedData(byte[] data, int compressedSize, int uncompressedSize) {
     this.data = data;
     this.compressedSize = compressedSize;
     this.uncompressedSize = uncompressedSize;
   }
-  
+
   public CompressedData(byte[] data, int uncompressedSize) {
     this.data = data;
     this.compressedSize = data.length;
     this.uncompressedSize = uncompressedSize;
   }
-  
+
   public byte[] getCompressedData() {
     return data;
   }
-  
+
   public int getCompressedSize() {
     return this.compressedSize;
   }
-  
+
   public int getUncompressedSize() {
     return this.uncompressedSize;
   }
-  
+
 }
