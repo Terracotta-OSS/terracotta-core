41d6dc14372b88d759cc2ec9bf32f11b9c25cb20
Hung Huynh <hhuynh@gmail.com>
2009-08-04 23:39:31 +0000

    CDV-1345: put hibernate cache rerference-config.xml to hibernate folder
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@13327 7fc7bbf3-cf45-46d4-be06-341739edd864

---
+++ code/base/buildscripts/postscripts/extract-hibernate-cache-reference.rb
@@ -0,0 +1,60 @@
+#
+# All content copyright (c) 2003-2008 Terracotta, Inc.,
+# except as may otherwise be noted in a separate copyright notice.
+# All rights reserved
+#
+
+require 'tmpdir'
+require 'fileutils'
+
+class BaseCodeTerracottaBuilder <  TerracottaBuilder
+  protected
+
+  # - Install TIMs from the Forge into the kit using the tim-get tool.
+  def postscript(ant, build_environment, product_directory, *args)
+    return if @no_demo
+
+    # first and only argument is a hash
+    arg = args[0]
+
+    dest = File.join(product_directory.to_s, arg['dest'])
+    agent_pattern = arg['agent_pattern']
+    provider_pattern = arg['provider_pattern']
+    reference_file = arg['reference-file']
+    work_dir = File.join(Dir.tmpdir, "tcbuild-extract")
+
+    # look up the agent jar
+    agent_jar = nil
+    Dir.chdir(dest) do |path|
+      Dir.glob(agent_pattern + "*.jar") { |filename|
+        agent_jar = File.join(path, filename)
+      }
+      fail("Can't find agent jar with pattern #{agent_pattern}") unless File.exists?(agent_jar)
+    end
+
+    # extract provider from agent jar
+    ant.unzip(:src => agent_jar, :dest => work_dir) do
+      ant.patternset(:includes => "**/" + provider_pattern + "*.jar")
+    end
+
+    # look up provider jar
+    provider_jar = nil
+    Dir.chdir(File.join(work_dir, "TIMs")) do |path|
+      Dir.glob(provider_pattern + "*.jar") { |filename|
+        provider_jar = File.join(path, filename)
+      }
+      fail("Can't find provider jar with pattern #{provider_pattern}") unless File.exists?(provider_jar)
+    end
+
+    # extract reference-config.xml from provider jar
+    ant.unzip(:src => provider_jar, :dest => work_dir) do
+      ant.patternset(:includes => reference_file)
+    end
+
+    # copy it over to dest
+    FileUtils.cp File.join(work_dir, reference_file), dest
+
+    # clean up
+    FileUtils.rm_rf(work_dir)
+  end
+end
