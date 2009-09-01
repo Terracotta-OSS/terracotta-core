c3d0b6f1278756cd27b0af218ba5349b5c3c7dc5
Gary Keim <gkeim@terracottatech.com>
2009-09-01 00:47:15 +0000

    Merge of change 13527 from 3.1 branch.
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@13528 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-l2/src/com/tc/objectserver/mgmt/FacadeUtil.java
+++ code/base/dso-l2/src/com/tc/objectserver/mgmt/FacadeUtil.java
@@ -1,5 +1,6 @@
 /*
- * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
  */
 package com.tc.objectserver.mgmt;
 
@@ -7,10 +8,19 @@ import com.tc.object.dna.impl.ClassInstance;
 import com.tc.object.dna.impl.UTF8ByteDataHolder;
 
 public class FacadeUtil {
+  /**
+   * If this is returned it means the field has been removed from its parent type, and so no longer exists in the server
+   * state instance for that parent type.
+   */
+  public static final String UNKNOWN_TYPE = "unknown";
 
   public static String getFieldType(Object value) {
     // XXX: this is kinda wrong actually...we'll end up returning "Integer" for "int" fields and what not.
-    return getShortClassName(value.getClass().getName());
+    if (value != null) {
+      return getShortClassName(value.getClass().getName());
+    } else {
+      return UNKNOWN_TYPE;
+    }
   }
 
   private static String getShortClassName(String className) {
