cf53f9a6769d5a4aca8a3500933800fac214c8a7
etsai <etsai@terracottatech.com>
2009-10-18 21:04:52 +0000

    DEV-3444, merge 13855 to trunk
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@13856 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-common/src/com/tc/object/net/DSOChannelManagerImpl.java
+++ code/base/dso-common/src/com/tc/object/net/DSOChannelManagerImpl.java
@@ -23,6 +23,7 @@ import com.tc.object.msg.BatchTransactionAcknowledgeMessage;
 import com.tc.object.msg.ClientHandshakeAckMessage;
 import com.tc.util.concurrent.CopyOnWriteArrayMap;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -107,7 +108,26 @@ public class DSOChannelManagerImpl implements DSOChannelManager, DSOChannelManag
   }
 
   public TCConnection[] getAllActiveClientConnections() {
-    return connectionManager.getAllActiveConnections();
+    TCConnection[] connections = connectionManager.getAllActiveConnections();
+    ArrayList activeChannelConnections = new ArrayList(connections.length);
+    Set activeConnectionAddresses = getActiveChannelRemoteAddress();
+    for (TCConnection connection : connections) {
+      if(activeConnectionAddresses.contains(connection.getRemoteAddress())) {
+      activeChannelConnections.add(connection); 
+    }
+    }
+    return (TCConnection[]) activeChannelConnections.toArray(new TCConnection[activeChannelConnections.size()]);
+  }
+
+  private Set getActiveChannelRemoteAddress() {
+    Set activeConnectionAddresses = new HashSet();
+    synchronized (activeChannels) {
+      for (Iterator i = activeChannels.values().iterator(); i.hasNext();) {
+        MessageChannel channel = (MessageChannel) i.next();
+        activeConnectionAddresses.add(channel.getRemoteAddress());
+      }
+    }
+    return activeConnectionAddresses;
   }
 
   public void makeChannelActive(final ClientID clientID, final boolean persistent) {
