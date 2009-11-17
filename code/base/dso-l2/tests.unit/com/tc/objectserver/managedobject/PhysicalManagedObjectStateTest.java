8f9875dc0306a3b50618f97cf78d2aa4dcff03e8
Nabib El-Rahman <nelrahma@terracottatech.com>
2009-11-17 23:51:52 +0000

    DEV-3406, implement hasNoReferences for all Physical objects
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@13988 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-l2/tests.unit/com/tc/objectserver/managedobject/PhysicalManagedObjectStateTest.java
+++ code/base/dso-l2/tests.unit/com/tc/objectserver/managedobject/PhysicalManagedObjectStateTest.java
@@ -19,5 +19,37 @@ public class PhysicalManagedObjectStateTest extends AbstractTestManagedObjectSta
 
     basicTestUnit(className, ManagedObjectState.PHYSICAL_TYPE, cursor, 2);
   }
+  
+  public void testObjectPhysicalHasReferences() throws Exception {
+    String className = "com.tc.objectserver.managedobject.PhysicalManagedObjectStateTest";
+    TestDNACursor cursor = new TestDNACursor();
 
+    cursor.addPhysicalAction("field1", new ObjectID(2002), true);
+    cursor.addPhysicalAction("field2", new ObjectID(2003), true);
+    cursor.addPhysicalAction("field3", new Integer(33), false);
+
+
+    
+    ManagedObjectState state = createManagedObjectState(className, cursor);
+    System.out.println("state: " + state);
+    assertFalse(state.hasNoReferences());
+  }
+  
+  public void testObjectPhysicalHasNoReferences() throws Exception {
+    String className = "com.tc.objectserver.managedobject.PhysicalManagedObjectStateTest";
+    TestDNACursor cursor = new TestDNACursor();
+
+    cursor.addPhysicalAction("field1", new Integer(2002), false);
+    cursor.addPhysicalAction("field2", new Integer(2003), false);
+    cursor.addPhysicalAction("field3", new Integer(33), false);
+
+
+    
+    ManagedObjectState state = createManagedObjectState(className, cursor);
+    System.out.println("state: " + state);
+    assertTrue(state.hasNoReferences());
+  }
+
+
+  
 }
