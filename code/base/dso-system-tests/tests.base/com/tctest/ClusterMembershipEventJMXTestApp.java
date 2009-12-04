cc82d9659b0304eae67d5f22774827d3e40fa6cd
Gary Keim <gkeim@terracottatech.com>
2009-12-04 03:06:55 +0000

    Merge of change 14127 from 3.2 branch.
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@14128 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-system-tests/tests.base/com/tctest/ClusterMembershipEventJMXTestApp.java
+++ code/base/dso-system-tests/tests.base/com/tctest/ClusterMembershipEventJMXTestApp.java
@@ -6,18 +6,20 @@ package com.tctest;
 
 import com.tc.exception.ExceptionHelperImpl;
 import com.tc.exception.RuntimeExceptionHelper;
+import com.tc.management.TerracottaManagement;
+import com.tc.management.beans.L1MBeanNames;
 import com.tc.management.exposed.TerracottaClusterMBean;
+import com.tc.object.bytecode.ManagerUtil;
 import com.tc.object.config.ConfigVisitor;
 import com.tc.object.config.DSOClientConfigHelper;
 import com.tc.simulator.app.ApplicationConfig;
 import com.tc.simulator.listener.ListenerProvider;
 import com.tc.util.Assert;
+import com.tc.util.UUID;
 import com.tctest.runner.AbstractTransparentApp;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import javax.management.MBeanServer;
@@ -30,29 +32,14 @@ import javax.management.NotificationListener;
 import javax.management.ObjectName;
 import javax.management.RuntimeMBeanException;
 
-public class ClusterMembershipEventJMXTestApp extends AbstractTransparentApp implements NotificationListener {
-
-  public static final String CONFIG_FILE    = "config-file";
-  public static final String PORT_NUMBER    = "port-number";
-  public static final String HOST_NAME      = "host-name";
-
-  // private final ApplicationConfig config;
-
-  private MBeanServer        server         = null;
-  private ObjectName         clusterBean    = null;
-  private final List         clusterBeanBag = new ArrayList();
-  private final Map          eventsCount    = new HashMap();
+public class ClusterMembershipEventJMXTestApp extends AbstractTransparentApp {
+  private MBeanServer server         = null;
+  private ObjectName  clusterBean    = null;
+  private final List  clusterBeanBag = new ArrayList();
 
   public ClusterMembershipEventJMXTestApp(final String appId, final ApplicationConfig config,
                                           final ListenerProvider listenerProvider) {
     super(appId, config, listenerProvider);
-    // this.config = config;
-
-    try {
-      registerMBeanListener();
-    } catch (Exception ex) {
-      ex.printStackTrace();
-    }
   }
 
   public static void visitL1DSOConfig(final ConfigVisitor visitor, final DSOClientConfigHelper config) {
@@ -64,6 +51,7 @@ public class ClusterMembershipEventJMXTestApp extends AbstractTransparentApp imp
 
   public void run() {
     try {
+      locateClusterBean();
       runTest();
     } catch (Throwable t) {
       notifyError(t);
@@ -72,7 +60,7 @@ public class ClusterMembershipEventJMXTestApp extends AbstractTransparentApp imp
 
   private void runTest() throws Throwable {
     TerracottaClusterMBean cluster = (TerracottaClusterMBean) MBeanServerInvocationHandler
-        .newProxyInstance(server, clusterBean, TerracottaClusterMBean.class, false);
+        .newProxyInstance(server, clusterBean, TerracottaClusterMBean.class, true);
     ExceptionHelperImpl helper = new ExceptionHelperImpl();
     helper.addHelper(new RuntimeExceptionHelper());
 
@@ -82,7 +70,6 @@ public class ClusterMembershipEventJMXTestApp extends AbstractTransparentApp imp
     } catch (UnsupportedOperationException e) {
       // expected
     } catch (RuntimeMBeanException e) {
-      e.printStackTrace();
       Assert.assertTrue(helper.getUltimateCause(e) instanceof UnsupportedOperationException);
     }
 
@@ -104,105 +91,81 @@ public class ClusterMembershipEventJMXTestApp extends AbstractTransparentApp imp
       Assert.assertTrue(helper.getUltimateCause(e) instanceof UnsupportedOperationException);
     }
 
-    // Disabling entire test since JMX event notifications have been deprecated
-    /*
-     * config.getServerControl().crash(); while (config.getServerControl().isRunning()) { Thread.sleep(5000); } // this
-     * sleep should be longer than l1-reconnect timeout Thread.sleep(30 1000); config.getServerControl().start(); while
-     * (!config.getServerControl().isRunning()) { Thread.sleep(5000); } echo("Server restarted successfully.");
-     * spawnNewClient(); synchronized (eventsCount) { while (eventsCount.size() < 4) { eventsCount.wait(3 60 1000); }
-     * Assert.assertTrue(((Integer) eventsCount.get("com.tc.cluster.event.nodeDisconnected")).intValue() >= 1);
-     * Assert.assertTrue(((Integer) eventsCount.get("com.tc.cluster.event.nodeConnected")).intValue() >= 1);
-     * Assert.assertTrue(((Integer) eventsCount.get("com.tc.cluster.event.thisNodeDisconnected")).intValue() >= 1);
-     * Assert.assertTrue(((Integer) eventsCount.get("com.tc.cluster.event.thisNodeConnected")).intValue() >= 1); }
-     */
+    NotificationListener listener = new NotificationListener() {
+      public void handleNotification(final Notification notification, final Object handback) {
+        /**/
+      }
+    };
+
+    try {
+      server.addNotificationListener(clusterBean, listener, null, clusterBean);
+      Assert.fail("Expected UnsupportedOperationException");
+    } catch (UnsupportedOperationException e) {
+      // expected
+    }
   }
 
-  private void registerMBeanListener() throws Exception {
+  private void locateClusterBean() throws Exception {
     List servers = MBeanServerFactory.findMBeanServer(null);
-    if (servers.size() == 0) { throw new RuntimeException("No bean server found!"); }
-    echo("Mbean Servers found: " + servers.size());
+    if (servers.size() == 0) { throw new RuntimeException("No mean server found!"); }
 
-    // our *star* bean for membership events
-    ObjectName on = new ObjectName("org.terracotta:type=Terracotta Cluster,name=Terracotta Cluster Bean,*");
+    clusterBean = TerracottaManagement.addNodeInfo(L1MBeanNames.CLUSTER_BEAN_PUBLIC, new UUID(ManagerUtil.getUUID()));
     for (int i = 0; i < servers.size(); i++) {
       MBeanServer mbeanServer = (MBeanServer) servers.get(i);
-      Set<ObjectName> onSet = mbeanServer.queryNames(on, null);
-      if (onSet.size() > 0) {
+      if (mbeanServer.isRegistered(clusterBean)) {
         server = mbeanServer;
-        clusterBean = onSet.iterator().next();
         break;
       }
     }
 
     Assert.assertNotNull(server);
 
-    // The MBeanServerDelegate emits notifications about
-    // registration/unregistration of MBeans
-    ObjectName delegateName = ObjectName.getInstance("JMImplementation:type=MBeanServerDelegate");
+    // Fairly certain L1 beans are registered early. The assertion above also indicates
+    // the bean must already be registered. Finally, if all this were necessary, such logic would
+    // need to be run against each MBeanServer found, not just the one that we already determined
+    // contains the bean.
 
-    // listener for newly registered MBeans
-    NotificationListener listener = new NotificationListener() {
-      public void handleNotification(final Notification notification, final Object handback) {
+    if (false) {
+      ObjectName delegateName = ObjectName.getInstance("JMImplementation:type=MBeanServerDelegate");
+
+      // listener for newly registered MBeans;
+      // only clusterBean will ever be notified for due to the filter below
+      NotificationListener listener = new NotificationListener() {
+        public void handleNotification(final Notification notification, final Object handback) {
+          synchronized (clusterBeanBag) {
+            clusterBeanBag.add(handback);
+            clusterBeanBag.notifyAll();
+          }
+        }
+      };
+
+      // filter to let only clusterBean passed through
+      NotificationFilter filter = new NotificationFilter() {
+        public boolean isNotificationEnabled(final Notification notification) {
+          if (notification.getType().equals("JMX.mbean.registered")
+              && ((MBeanServerNotification) notification).getMBeanName().equals(clusterBean)) return true;
+          return false;
+        }
+      };
+
+      // add our listener for clusterBean's registration
+      server.addNotificationListener(delegateName, listener, filter, clusterBean);
+
+      // because of race condition, clusterBean might already have registered
+      // before we registered the listener
+      Set allObjectNames = server.queryNames(null, null);
+
+      if (!allObjectNames.contains(clusterBean)) {
         synchronized (clusterBeanBag) {
-          clusterBeanBag.add(handback);
-          clusterBeanBag.notifyAll();
+          while (clusterBeanBag.isEmpty()) {
+            clusterBeanBag.wait();
+          }
         }
       }
-    };
 
-    // filter to let only clusterBean passed through
-    NotificationFilter filter = new NotificationFilter() {
-      public boolean isNotificationEnabled(final Notification notification) {
-        if (notification.getType().equals("JMX.mbean.registered")
-            && ((MBeanServerNotification) notification).getMBeanName().equals(clusterBean)) return true;
-        return false;
-      }
-    };
-
-    // add our listener for clusterBean's registration
-    server.addNotificationListener(delegateName, listener, filter, clusterBean);
-
-    // because of race condition, clusterBean might already have registered
-    // before we registered the listener
-    Set allObjectNames = server.queryNames(null, null);
-
-    if (!allObjectNames.contains(clusterBean)) {
-      synchronized (clusterBeanBag) {
-        while (clusterBeanBag.isEmpty()) {
-          clusterBeanBag.wait();
-        }
-      }
+      // clusterBean is now registered, no need to listen for it
+      server.removeNotificationListener(delegateName, listener);
     }
-
-    // clusterBean is now registered, no need to listen for it
-    server.removeNotificationListener(delegateName, listener);
-
-    // now that we have the clusterBean, add listener for membership events
-    try {
-      echo("server=" + server + ", clusterBean=" + clusterBean);
-      server.addNotificationListener(clusterBean, this, null, clusterBean);
-      // Assert.fail("Expected UnsupportedOperationException");
-    } catch (UnsupportedOperationException e) {
-      // expected
-    }
-  }
-
-  public void handleNotification(final Notification notification, final Object handback) {
-    synchronized (eventsCount) {
-      String msgType = notification.getType();
-      if (eventsCount.containsKey(msgType)) {
-        Integer count = (Integer) eventsCount.get(msgType);
-        eventsCount.put(msgType, new Integer(count.intValue() + 1));
-      } else {
-        eventsCount.put(msgType, new Integer(1));
-      }
-      echo("type=" + notification.getType() + ", message=" + notification.getMessage());
-      eventsCount.notifyAll();
-    }
-  }
-
-  private static void echo(final String msg) {
-    System.out.println(msg);
   }
 
   public static class L1Client {
@@ -210,18 +173,4 @@ public class ClusterMembershipEventJMXTestApp extends AbstractTransparentApp imp
       // nothing to do
     }
   }
-  /*
-   * private ExtraL1ProcessControl spawnNewClient() throws Exception { final String hostName =
-   * config.getAttribute(HOST_NAME); final int port = Integer.parseInt(config.getAttribute(PORT_NUMBER)); final File
-   * configFile = new File(config.getAttribute(CONFIG_FILE)); File workingDir = new File(configFile.getParentFile(),
-   * "client-0"); FileUtils.forceMkdir(workingDir); List jvmArgs = new ArrayList(); addTestTcPropertiesFile(jvmArgs);
-   * ExtraL1ProcessControl client = new ExtraL1ProcessControl(hostName, port, L1Client.class, configFile
-   * .getAbsolutePath(), new String[0], workingDir, jvmArgs); client.start(); client.mergeSTDERR();
-   * client.mergeSTDOUT(); client.waitFor(); System.err.println("\n### Started New Client"); return client; } private
-   * void addTestTcPropertiesFile(final List jvmArgs) { URL url =
-   * getClass().getResource("/com/tc/properties/tests.properties"); if (url == null) { return; } String
-   * pathToTestTcProperties = url.getPath(); if (pathToTestTcProperties == null || pathToTestTcProperties.equals("")) {
-   * return; } System.err.println("\n##### -Dcom.tc.properties=" + pathToTestTcProperties);
-   * jvmArgs.add("-Dcom.tc.properties=" + pathToTestTcProperties); }
-   */
 }
