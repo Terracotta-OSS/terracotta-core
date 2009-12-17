82cdc560e310d0db59de8baa19c9e3a44dbe5b83
etsai <etsai@terracottatech.com>
2009-12-17 07:31:00 +0000

    DEV-3554, merge 14235 from 3.2
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@14236 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-common/src/com/tc/net/groups/StripeIDMismatchGroupMessage.java
+++ code/base/dso-common/src/com/tc/net/groups/StripeIDMismatchGroupMessage.java
@@ -16,6 +16,7 @@ public class StripeIDMismatchGroupMessage extends AbstractGroupMessage {
   public static final int ERROR_NOT_CLUSTER_MEMBER = 1;
   public static final int ERROR_MISMATCH_STRIPEID  = 2;
   public static final int ERROR_MISMATCH_GROUPID   = 3;
+  public static final int MISMATCH_TEMPORARY       = 4;
 
   private int             errorType;
   private String          reason;
