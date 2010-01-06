45cd0b907e59493860c809d9ee377b17e3c2f473
Tim Eck <timeck@gmail.com>
2010-01-06 00:37:14 +0000

    allow valve attributes
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@14259 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-container-tests/tests.base/com/tc/test/server/appserver/ValveDefinition.java
+++ code/base/dso-container-tests/tests.base/com/tc/test/server/appserver/ValveDefinition.java
@@ -3,10 +3,14 @@
  */
 package com.tc.test.server.appserver;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class ValveDefinition {
 
-  private final String className;
+  private final String              className;
+  private final Map<String, String> attributes = new LinkedHashMap<String, String>();
 
   public ValveDefinition(String className) {
     this.className = className;
@@ -16,8 +20,23 @@ public class ValveDefinition {
     return className;
   }
 
-  public String toXml() {
-    return "<Valve className=\"" + getClassName() + "\"/>";
+  public void setAttribute(String name, String value) {
+    attributes.put(name, value);
   }
 
+  @Override
+  public String toString() {
+    return toXml();
+  }
+
+  public String toXml() {
+    String xml = "<Valve className=\"" + getClassName() + "\"";
+    for (Entry<String, String> attr : attributes.entrySet()) {
+      xml += " " + attr.getKey() + "=\"" + attr.getValue() + "\"";
+    }
+
+    xml += "/>";
+
+    return xml;
+  }
 }
