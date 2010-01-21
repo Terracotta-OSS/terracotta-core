d024db7707af93e23799c2ace2cea54bf32936a5
Jason Voegele <jason@jvoegele.com>
2010-01-21 16:38:23 +0000

    DEV-3664 DEV-3665: Move tools under platform and remove tools/sessions from kit.
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@14339 7fc7bbf3-cf45-46d4-be06-341739edd864

--- kits/skeletons/shared-base/common/tools/sessions/configurator-sandbox/bin/stop-tc-server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-#  All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
-
-cd `dirname "$0"`/../../..
-SANDBOX="`pwd`/sessions/configurator-sandbox/$1"
-../bin/stop-tc-server.sh -f "${SANDBOX}"/tc-config.xml
