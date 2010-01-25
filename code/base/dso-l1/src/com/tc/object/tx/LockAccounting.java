52b1c647f9a2912b286eae15d2ffb08a07b27c7a
Nihit Purwar <npurwar@terracottatech.com>
2010-01-25 09:32:03 +0000

    lock accounting cleanup
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@14363 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-l1/src/com/tc/object/tx/LockAccounting.java
+++ code/base/dso-l1/src/com/tc/object/tx/LockAccounting.java
@@ -20,151 +20,59 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 public class LockAccounting {
 
-  private final CopyOnWriteArrayList<TxnRemovedListener> listeners      = new CopyOnWriteArrayList<TxnRemovedListener>();
-  private final Map<TransactionID, Set<LockID>>          tx2Locks       = new HashMap();
-  private final Map<LockID, Set<TransactionID>>          lock2Txs       = new HashMap();
+  private final TxnLocksBookKeeping regularTxnLocks   = new TxnLocksBookKeeping();
+  private final TxnLocksBookKeeping syncWriteTxnLocks = new TxnLocksBookKeeping();
 
-  // for sync write
-  private final Map<LockID, Set<TransactionID>>          syncLock2Txs   = new HashMap();
-  private final Map<TransactionID, Set<LockID>>          syncTxns2Locks = new HashMap();
-
-  public synchronized Object dump() {
-    return "LockAccounting:\ntx2Locks=" + tx2Locks + "\nlock2Txs=" + lock2Txs + "/LockAccounting";
+  public Object dump() {
+    return toString();
   }
 
   public String toString() {
-    return "LockAccounting[tx2Locks=" + tx2Locks + ", lock2Txs=" + lock2Txs + "]";
+    StringBuilder builder = new StringBuilder();
+    builder.append("Lock Accounting:\n");
+    builder.append("Regular Lock Accounting ");
+    builder.append(regularTxnLocks.toString());
+    builder.append("\n");
+    builder.append("Sync Write Lock Accounting ");
+    builder.append(syncWriteTxnLocks.toString());
+    builder.append("\n");
+    return builder.toString();
   }
 
-  public synchronized void add(TransactionID txID, Collection lockIDs, boolean isSyncWrite) {
-    getOrCreateSetFor(txID, tx2Locks).addAll(lockIDs);
-    for (Iterator i = lockIDs.iterator(); i.hasNext();) {
-      LockID lid = (LockID) i.next();
-      getOrCreateSetFor(lid, lock2Txs).add(txID);
-      if (isSyncWrite) {
-        addSyncTxns(txID, lid);
-      }
+  public void add(TransactionID txID, Collection lockIDs, boolean isSyncWrite) {
+    regularTxnLocks.add(txID, lockIDs);
+    if (isSyncWrite) {
+      syncWriteTxnLocks.add(txID, lockIDs);
     }
   }
 
-  private void addSyncTxns(TransactionID txID, LockID lid) {
-    synchronized (syncTxns2Locks) {
-      getOrCreateSetFor(lid, syncLock2Txs).add(txID);
-      getOrCreateSetFor(txID, syncTxns2Locks).add(lid);
-    }
-  }
-
-  public synchronized Collection getTransactionsFor(LockID lockID) {
-    Set rv = new HashSet();
-    Set toAdd = lock2Txs.get(lockID);
-    if (toAdd != null) {
-      rv.addAll(toAdd);
-    }
-    return rv;
+  public Collection getTransactionsFor(LockID lockID) {
+    return regularTxnLocks.getTransactionsFor(lockID);
   }
 
   public Collection getSyncWriteTransactionsFor(LockID lockID) {
-    synchronized (syncTxns2Locks) {
-      Set rv = new HashSet();
-      Set toAdd = syncLock2Txs.get(lockID);
-      if (toAdd != null) {
-        rv.addAll(toAdd);
-      }
-      return rv;
-    }
+    return syncWriteTxnLocks.getTransactionsFor(lockID);
   }
 
   public void transactionRecvdByServer(TransactionID txID) {
-    synchronized (syncTxns2Locks) {
-      Set lockIDs = getSetFor(txID, syncTxns2Locks);
-      if (lockIDs != null) {
-        // this may be null if there are phantom acknowledgements caused by server restart.
-        for (Iterator i = lockIDs.iterator(); i.hasNext();) {
-          LockID lid = (LockID) i.next();
-          Set txIDs = getSetFor(lid, syncLock2Txs);
-          if (txIDs != null) {
-            txIDs.remove(txID);
-            if (txIDs.isEmpty()) {
-              syncLock2Txs.remove(lid);
-            }
-          }
-        }
-      }
-      syncTxns2Locks.remove(txID);
-    }
+    syncWriteTxnLocks.acknowledge(txID, true);
   }
 
   // This method returns a set of lockIds that has no more transactions to wait for
-  public synchronized Set acknowledge(TransactionID txID) {
-    // added so that if we have still not received the ack for sync write
-    if (syncTxns2Locks.containsKey(txID)) {
+  public Set acknowledge(TransactionID txID) {
+    if (syncWriteTxnLocks.containsTransaction(txID)) {
       transactionRecvdByServer(txID);
     }
 
-    Set completedLockIDs = null;
-    Set lockIDs = getSetFor(txID, tx2Locks);
-    if (lockIDs != null) {
-      // this may be null if there are phantom acknowledgments caused by server restart.
-      for (Iterator i = lockIDs.iterator(); i.hasNext();) {
-        LockID lid = (LockID) i.next();
-        Set txIDs = getOrCreateSetFor(lid, lock2Txs);
-        if (!txIDs.remove(txID)) throw new AssertionError("No lock=>transaction found for " + lid + ", " + txID);
-        if (txIDs.isEmpty()) {
-          lock2Txs.remove(lid);
-          if (completedLockIDs == null) {
-            completedLockIDs = new HashSet();
-          }
-          completedLockIDs.add(lid);
-        }
-      }
-    }
-    removeTxn(txID);
-    return (completedLockIDs == null ? Collections.EMPTY_SET : completedLockIDs);
+    return regularTxnLocks.acknowledge(txID, false);
   }
 
-  public synchronized boolean isEmpty() {
-    return tx2Locks.isEmpty() && lock2Txs.isEmpty();
-  }
-
-  private static Set getSetFor(Object key, Map m) {
-    return (Set) m.get(key);
-  }
-
-  private static Set getOrCreateSetFor(Object key, Map m) {
-    Set rv = getSetFor(key, m);
-    if (rv == null) {
-      rv = new HashSet();
-      m.put(key, rv);
-    }
-    return rv;
-  }
-
-  private void removeTxn(TransactionID txnID) {
-    tx2Locks.remove(txnID);
-    notifyTxnRemoved(txnID);
-  }
-
-  private void notifyTxnRemoved(TransactionID txnID) {
-    for (TxnRemovedListener l : listeners) {
-      l.txnRemoved(txnID);
-    }
+  public boolean isEmpty() {
+    return regularTxnLocks.isEmpty();
   }
 
   public void waitAllCurrentTxnCompleted() {
-    TxnRemovedListener listener;
-    Latch latch = new Latch();
-    synchronized (this) {
-      Set currentTxnSet = new HashSet(tx2Locks.keySet());
-      listener = new TxnRemovedListener(currentTxnSet, latch);
-      listeners.add(listener);
-    }
-    try {
-      latch.acquire();
-    } catch (InterruptedException e) {
-      throw new TCRuntimeException(e);
-    } finally {
-      listeners.remove(listener);
-    }
+    regularTxnLocks.waitAllCurrentTxnCompleted();
   }
 
   private static class TxnRemovedListener {
@@ -186,6 +94,111 @@ public class LockAccounting {
       this.latch.release();
     }
 
+  }
+
+  private static class TxnLocksBookKeeping {
+    private final CopyOnWriteArrayList<TxnRemovedListener> listeners = new CopyOnWriteArrayList<TxnRemovedListener>();
+
+    private final Map<TransactionID, Set<LockID>>          tx2Locks  = new HashMap();
+    private final Map<LockID, Set<TransactionID>>          lock2Txs  = new HashMap();
+
+    public synchronized void add(TransactionID txID, Collection lockIDs) {
+      getOrCreateSetFor(txID, tx2Locks).addAll(lockIDs);
+      for (Iterator i = lockIDs.iterator(); i.hasNext();) {
+        LockID lid = (LockID) i.next();
+        getOrCreateSetFor(lid, lock2Txs).add(txID);
+      }
+    }
+
+    // This method returns a set of lockIds that has no more transactions to wait for
+    public synchronized Set acknowledge(TransactionID txID, boolean isTxnRecvdByServerAck) {
+      Set completedLockIDs = null;
+      Set lockIDs = getSetFor(txID, tx2Locks);
+      if (lockIDs != null) {
+        // this may be null if there are phantom acknowledgments caused by server restart.
+        for (Iterator i = lockIDs.iterator(); i.hasNext();) {
+          LockID lid = (LockID) i.next();
+          Set txIDs = getOrCreateSetFor(lid, lock2Txs);
+          boolean removed = txIDs.remove(txID);
+          if (!removed && !isTxnRecvdByServerAck) throw new AssertionError("No lock=>transaction found for " + lid
+                                                                           + ", " + txID);
+          if (txIDs.isEmpty()) {
+            lock2Txs.remove(lid);
+            if (completedLockIDs == null) {
+              completedLockIDs = new HashSet();
+            }
+            completedLockIDs.add(lid);
+          }
+        }
+      }
+      removeTxn(txID);
+      return (completedLockIDs == null ? Collections.EMPTY_SET : completedLockIDs);
+    }
+
+    public synchronized Collection getTransactionsFor(LockID lockID) {
+      Set rv = new HashSet();
+      Set toAdd = lock2Txs.get(lockID);
+      if (toAdd != null) {
+        rv.addAll(toAdd);
+      }
+      return rv;
+    }
+
+    public void waitAllCurrentTxnCompleted() {
+      TxnRemovedListener listener;
+      Latch latch = new Latch();
+      synchronized (this) {
+        Set currentTxnSet = new HashSet(tx2Locks.keySet());
+        listener = new TxnRemovedListener(currentTxnSet, latch);
+        listeners.add(listener);
+      }
+      try {
+        latch.acquire();
+      } catch (InterruptedException e) {
+        throw new TCRuntimeException(e);
+      } finally {
+        listeners.remove(listener);
+      }
+    }
+
+    public synchronized boolean isEmpty() {
+      return tx2Locks.isEmpty() && lock2Txs.isEmpty();
+    }
+
+    /**
+     * This method is not synchronized
+     */
+    public boolean containsTransaction(TransactionID txID) {
+      return tx2Locks.containsKey(txID);
+    }
+
+    private static Set getSetFor(Object key, Map m) {
+      return (Set) m.get(key);
+    }
+
+    private static Set getOrCreateSetFor(Object key, Map m) {
+      Set rv = getSetFor(key, m);
+      if (rv == null) {
+        rv = new HashSet();
+        m.put(key, rv);
+      }
+      return rv;
+    }
+
+    private void removeTxn(TransactionID txnID) {
+      tx2Locks.remove(txnID);
+      notifyTxnRemoved(txnID);
+    }
+
+    private void notifyTxnRemoved(TransactionID txnID) {
+      for (TxnRemovedListener l : listeners) {
+        l.txnRemoved(txnID);
+      }
+    }
+
+    public String toString() {
+      return "[tx2Locks=" + tx2Locks + ", lock2Txs=" + lock2Txs + "]";
+    }
   }
 
 }
