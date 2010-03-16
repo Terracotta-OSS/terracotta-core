a3be605986dad316f70965ae0416642f2829c237
Geert Bevin <gbevin@terracottatech.com>
2010-03-16 17:18:37 +0000

    Added some more Sigar testing
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@14726 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/sigarstats/tests.unit/com/tc/statistics/retrieval/SigarUtilTest.java
+++ code/base/sigarstats/tests.unit/com/tc/statistics/retrieval/SigarUtilTest.java
@@ -1,5 +1,8 @@
 package com.tc.statistics.retrieval;
 
+import org.hyperic.sigar.Mem;
+import org.hyperic.sigar.Sigar;
+
 import junit.framework.TestCase;
 
 public class SigarUtilTest extends TestCase {
@@ -8,5 +11,12 @@ public class SigarUtilTest extends TestCase {
     SigarUtil.sigarInit();
     Class sigarClass = Class.forName("org.hyperic.sigar.Sigar");
     assertEquals(sigarClass.getName(), "org.hyperic.sigar.Sigar");
+  }
+
+  public void testSigarMem() throws Exception{
+    Sigar sigar = SigarUtil.newSigar();
+    Mem mem = sigar.getMem();
+    System.out.println("Total memory: " + mem.getTotal());
+    assertTrue(mem.getTotal() > 0);
   }
 }
