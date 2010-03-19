1a0f1558dd4222e4606b14645cdee6b7e2373c66
Manoj Govindassamy <mgovinda@terracottatech.com>
2010-03-19 20:18:01 +0000

    MNK-1546 : fix for monkey
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@14747 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-system-tests/tests.system/com/tctest/jdk15/LinkedBlockingQueueInterruptTakeTest.java
+++ code/base/dso-system-tests/tests.system/com/tctest/jdk15/LinkedBlockingQueueInterruptTakeTest.java
@@ -1,18 +1,24 @@
 /*
- * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
  */
 package com.tctest.jdk15;
 
+import com.tc.properties.TCProperties;
+import com.tc.properties.TCPropertiesConsts;
+import com.tc.properties.TCPropertiesImpl;
 import com.tc.util.runtime.Os;
 import com.tc.util.runtime.Vm;
 import com.tctest.TransparentTestBase;
 import com.tctest.TransparentTestIface;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 public class LinkedBlockingQueueInterruptTakeTest extends TransparentTestBase {
 
   private static final int NODE_COUNT = 3;
+  private boolean          reconnectOriginalValue;
 
   public LinkedBlockingQueueInterruptTakeTest() {
     // MNK-527
@@ -26,8 +32,26 @@ public class LinkedBlockingQueueInterruptTakeTest extends TransparentTestBase {
     t.initializeTestRunner();
   }
 
+  @Override
+  protected void setExtraJvmArgs(ArrayList jvmArgs) {
+    super.setExtraJvmArgs(jvmArgs);
+    TCProperties tcProps = TCPropertiesImpl.getProperties();
+    reconnectOriginalValue = tcProps.getBoolean(TCPropertiesConsts.L2_L1RECONNECT_ENABLED);
+    tcProps.setProperty(TCPropertiesConsts.L2_L1RECONNECT_ENABLED, "false");
+    System.setProperty("com.tc." + TCPropertiesConsts.L2_L1RECONNECT_ENABLED, "false");
+    jvmArgs.add("-Dcom.tc." + TCPropertiesConsts.L2_L1RECONNECT_ENABLED + "=false");
+  }
+
   protected Class getApplicationClass() {
     return LinkedBlockingQueueInterruptTakeTestApp.class;
+  }
+
+  @Override
+  protected void tearDown() throws Exception {
+    super.tearDown();
+    TCProperties tcProps = TCPropertiesImpl.getProperties();
+    tcProps.setProperty(TCPropertiesConsts.L2_L1RECONNECT_ENABLED, "" + reconnectOriginalValue);
+    System.setProperty("com.tc." + TCPropertiesConsts.L2_L1RECONNECT_ENABLED, "" + reconnectOriginalValue);
   }
 
 }
