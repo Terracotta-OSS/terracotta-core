6d511d961c78240f2f7076a02d3a3db6b73e37c2
Raghu Singh <rsingh@terracottatech.com>
2010-04-24 04:05:21 +0000

    monkey fix
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@15004 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-common/src/com/tc/util/runtime/ThreadIDMapJdk15.java
+++ code/base/dso-common/src/com/tc/util/runtime/ThreadIDMapJdk15.java
@@ -11,13 +11,15 @@ import java.util.Map;
 import java.util.WeakHashMap;
 
 public class ThreadIDMapJdk15 implements ThreadIDMap {
-  private final Map<Long, ThreadID> id2ThreadIDMap = new WeakHashMap<Long, ThreadID>();
+  private final Map<Thread, ThreadID> threadIDMap    = new WeakHashMap<Thread, ThreadID>();
+  private final Map<Long, ThreadID>   id2ThreadIDMap = new WeakHashMap<Long, ThreadID>();
 
   public synchronized void addTCThreadID(final ThreadID tcThreadID) {
     final Thread currentThread = Thread.currentThread();
     final Long javaThreadID = new Long(currentThread.getId());
-    Object prev = this.id2ThreadIDMap.put(javaThreadID, tcThreadID);
+    Object prev = threadIDMap.put(currentThread, tcThreadID);
     Assert.assertNull(javaThreadID + " should not exist before", prev);
+    prev = this.id2ThreadIDMap.put(javaThreadID, tcThreadID);
   }
 
   public synchronized ThreadID getTCThreadID(final Long javaThreadID) {
@@ -26,7 +28,7 @@ public class ThreadIDMapJdk15 implements ThreadIDMap {
 
   /** For testing only - not in interface */
   public synchronized int getSize() {
-    return this.id2ThreadIDMap.size();
+    return this.threadIDMap.size();
   }
 
 }
