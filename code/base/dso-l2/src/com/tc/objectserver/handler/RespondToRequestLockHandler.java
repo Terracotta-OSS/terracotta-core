761677ad59c17f0635c59da8e0b5a0343b42e134
Manoj Govindassamy <mgovinda@terracottatech.com>
2010-05-26 01:44:40 +0000

    better lock send fail message logging
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@15333 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-l2/src/com/tc/objectserver/handler/RespondToRequestLockHandler.java
+++ code/base/dso-l2/src/com/tc/objectserver/handler/RespondToRequestLockHandler.java
@@ -72,8 +72,7 @@ public class RespondToRequestLockHandler extends AbstractEventHandler {
       send(responseMessage);
 
     } catch (NoSuchChannelException e) {
-      logger.info("Failed to send lock response message:" + lrc.getLockID() + " to:" + cid
-                  + " because the session is dead.");
+      logger.info("Failed to send lock message:" + lrc + " to:" + cid + " because the session is dead.");
       return;
     }
   }
