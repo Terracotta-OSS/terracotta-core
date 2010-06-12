24c1d52ee85bb782ee14c4541fdb80ace638485b
Gary Keim <gkeim@terracottatech.com>
2010-06-12 01:05:30 +0000

    Reworked the sessions presentation to acknowledge that there is state spread across a collection of L1 MBeans, such as statisticsEnabled, allowing the user to manipulate that state either on a per-node or cluster-wide basis. Also factored out a useful base class for all presentation models.
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@15522 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/deploy/src/com/tc/admin/common/TreeComboBox.java
+++ code/base/deploy/src/com/tc/admin/common/TreeComboBox.java
@@ -25,7 +25,6 @@ import java.awt.event.ItemListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
-import java.util.Enumeration;
 
 import javax.swing.AbstractAction;
 import javax.swing.AbstractButton;
@@ -51,7 +50,6 @@ import javax.swing.tree.DefaultTreeSelectionModel;
 import javax.swing.tree.ExpandVetoException;
 import javax.swing.tree.TreeCellRenderer;
 import javax.swing.tree.TreeModel;
-import javax.swing.tree.TreeNode;
 import javax.swing.tree.TreePath;
 
 public class TreeComboBox extends JPanel implements TreeWillExpandListener, TreeModelListener {
@@ -458,29 +456,7 @@ public class TreeComboBox extends JPanel implements TreeWillExpandListener, Tree
   }
 
   private void expandTree() {
-    expandAll(tree, true);
-  }
-
-  private static void expandAll(JTree tree, boolean expand) {
-    TreeNode root = (TreeNode) tree.getModel().getRoot();
-    expandAll(tree, new TreePath(root), expand);
-  }
-
-  private static void expandAll(JTree tree, TreePath parent, boolean expand) {
-    TreeNode node = (TreeNode) parent.getLastPathComponent();
-    if (node.getChildCount() >= 0) {
-      for (Enumeration e = node.children(); e.hasMoreElements();) {
-        TreeNode n = (TreeNode) e.nextElement();
-        TreePath path = parent.pathByAddingChild(n);
-        expandAll(tree, path, expand);
-      }
-    }
-
-    if (expand) {
-      tree.expandPath(parent);
-    } else {
-      tree.collapsePath(parent);
-    }
+    XTree.expandAll(tree, true);
   }
 
   public void treeNodesChanged(TreeModelEvent e) {
