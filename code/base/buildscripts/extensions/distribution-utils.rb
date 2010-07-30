12b620d720e4c0807e3192fefc2d18e7247bc6c0
Hung Huynh <hhuynh@gmail.com>
2010-07-30 01:45:59 +0000

    fixed mapping
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@16031 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/buildscripts/extensions/distribution-utils.rb
+++ code/base/buildscripts/extensions/distribution-utils.rb
@@ -158,7 +158,7 @@ module DistributionUtils
   VARIABLE_MAP = {
     'tim-api.version' => lambda { @config_source['tim-api.version'] || 'unknown' },
     'kitversion' => 'kit.version',
-    'version' => :version,
+    'version' => 'version',
     'branch' => :current_branch,
     'platform' => lambda { @build_environment.os_family.downcase },
     'revision' => :os_revision,
