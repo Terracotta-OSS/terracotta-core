9f5a4036572098f9a703ade900bdc3d73cc5e85d
Manoj Govindassamy <mgovinda@terracottatech.com>
2010-09-23 00:59:17 +0000

    removing the debug stats from offheap object db as these are available in dev-console
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@16329 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/common-api/src/com/tc/properties/TCPropertiesConsts.java
+++ code/base/common-api/src/com/tc/properties/TCPropertiesConsts.java
@@ -841,7 +841,6 @@ public interface TCPropertiesConsts {
    * </code>
    ********************************************************************************************************************/
 
-  public static final String   L2_OFFHEAP_CACHE_STATS_ENABLED                                  = "l2.offHeapCache.stats.enabled";
   public static final String   L2_OFFHEAP_CACHE_MAP_PERCENTAGE                                 = "l2.offHeapCache.map.percentage";
   public static final String   L2_OFFHEAP_OBJECT_CACHE_INITIAL_DATASIZE                        = "l2.offHeapCache.object.initialDataSize";
   public static final String   L2_OFFHEAP_OBJECT_CACHE_TABLESIZE                               = "l2.offHeapCache.object.tableSize";
