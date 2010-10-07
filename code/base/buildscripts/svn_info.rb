7c4ad2371a40ef0c746476568d85fd964efe3f41
Hung Huynh <hhuynh@gmail.com>
2010-10-07 22:28:26 +0000

    correctly converting file separator for Cygwin
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@16418 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/buildscripts/svn_info.rb
+++ code/base/buildscripts/svn_info.rb
@@ -7,8 +7,9 @@ class SvnInfo
     @svninfo["Last Changed Date"] = "unknown"
     @svninfo["URL"] = "unknown"  
     if dir
-      svn_info = `svn info "#{dir.to_shell_escaped_s}" 2>&1`
-      if $? == 0 && !svn_info.blank?        
+      dir = dir.gsub("\\", "/").to_shell_escaped_s
+      svn_info = `svn info "#{dir}" 2>&1`
+      if $? == 0 && !svn_info.blank?
         begin
           @svninfo.merge!(YAML::load(svn_info))
           @valid = true
