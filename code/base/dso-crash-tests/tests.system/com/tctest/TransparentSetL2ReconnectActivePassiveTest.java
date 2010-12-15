d2d31d6fd0507099fe467d13bfb4d0742d6f7555
Manoj Govindassamy <mgovinda@terracottatech.com>
2010-12-15 22:15:45 +0000

    MNK-2016 : raising the L2 reconnect window for solaris boxes
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@16852 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-crash-tests/tests.system/com/tctest/TransparentSetL2ReconnectActivePassiveTest.java
+++ code/base/dso-crash-tests/tests.system/com/tctest/TransparentSetL2ReconnectActivePassiveTest.java
@@ -4,35 +4,61 @@
  */
 package com.tctest;
 
+import com.tc.properties.TCProperties;
+import com.tc.properties.TCPropertiesConsts;
+import com.tc.properties.TCPropertiesImpl;
 import com.tc.test.MultipleServersCrashMode;
 import com.tc.test.MultipleServersPersistenceMode;
 import com.tc.test.MultipleServersSharedDataMode;
 import com.tc.test.activepassive.ActivePassiveTestSetupManager;
 import com.tc.test.proxyconnect.ProxyConnectManager;
+import com.tc.util.runtime.Os;
 
-public class TransparentSetL2ReconnectActivePassiveTest extends ActivePassiveTransparentTestBase implements TestConfigurator {
+import java.util.ArrayList;
+
+public class TransparentSetL2ReconnectActivePassiveTest extends ActivePassiveTransparentTestBase implements
+    TestConfigurator {
   private static final int NODE_COUNT           = 3;
   private static final int EXECUTION_COUNT      = 3;
   private static final int LOOP_ITERATION_COUNT = 400;
 
+  @Override
   protected Class getApplicationClass() {
     return TransparentSetTestApp.class;
   }
 
+  @Override
   public void doSetUp(TransparentTestIface t) throws Exception {
     t.getTransparentAppConfig().setClientCount(NODE_COUNT).setApplicationInstancePerClientCount(EXECUTION_COUNT)
         .setIntensity(LOOP_ITERATION_COUNT);
     t.initializeTestRunner();
   }
 
+  @Override
   protected boolean enableL2Reconnect() {
     return true;
   }
 
+  @Override
+  protected void setL2ReconnectTimout(final ArrayList jvmArgs, int timeoutMilliSecond) {
+
+    if (Os.isSolaris()) {
+      String timeoutString = Integer.toString(10000);
+      TCProperties tcProps = TCPropertiesImpl.getProperties();
+      tcProps.setProperty(TCPropertiesConsts.L2_NHA_TCGROUPCOMM_RECONNECT_TIMEOUT, timeoutString);
+      System.setProperty("com.tc." + TCPropertiesConsts.L2_NHA_TCGROUPCOMM_RECONNECT_TIMEOUT, timeoutString);
+      jvmArgs.add("-Dcom.tc." + TCPropertiesConsts.L2_NHA_TCGROUPCOMM_RECONNECT_TIMEOUT + "=" + timeoutString);
+    } else {
+      super.setL2ReconnectTimout(jvmArgs, timeoutMilliSecond);
+    }
+  }
+
+  @Override
   protected boolean canRunL2ProxyConnect() {
     return true;
   }
-  
+
+  @Override
   protected void setupL2ProxyConnectTest(ProxyConnectManager[] managers) {
     /*
      * subclass can overwrite to change the test parameters.
@@ -43,6 +69,7 @@ public class TransparentSetL2ReconnectActivePassiveTest extends ActivePassiveTra
     }
   }
 
+  @Override
   public void setupActivePassiveTest(ActivePassiveTestSetupManager setupManager) {
     setupManager.setServerCount(2);
     setupManager.setServerCrashMode(MultipleServersCrashMode.CONTINUOUS_ACTIVE_CRASH);
