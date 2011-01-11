154ac28cf7d270c17c76ba81799a0bf68b92e16c
Manoj Govindassamy <mgovinda@terracottatech.com>
2011-01-11 00:07:22 +0000

    DEV-5129, MNK-2084 : Server should not close the channel when sending a Client Handshake Refuse Message
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@16941 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-l2/src/com/tc/objectserver/handler/ClientHandshakeHandler.java
+++ code/base/dso-l2/src/com/tc/objectserver/handler/ClientHandshakeHandler.java
@@ -14,6 +14,7 @@ import com.tc.object.msg.ClientHandshakeMessage;
 import com.tc.objectserver.core.api.ServerConfigurationContext;
 import com.tc.objectserver.handshakemanager.ClientHandshakeException;
 import com.tc.objectserver.handshakemanager.ServerClientHandshakeManager;
+import com.tc.objectserver.handshakemanager.ServerClientModeInCompatibleException;
 import com.tc.operatorevent.TerracottaOperatorEvent;
 import com.tc.operatorevent.TerracottaOperatorEventFactory;
 
@@ -37,26 +38,29 @@ public class ClientHandshakeHandler extends AbstractEventHandler {
       this.handshakeManager.notifyClientConnect(clientMsg);
     } catch (ClientHandshakeException e) {
       getLogger().error("Handshake Error : ", e);
-      this.handshakeManager.notifyClientRejected(clientMsg, e.getMessage());
       MessageChannel c = clientMsg.getChannel();
       getLogger().error("Closing channel " + c.getChannelID() + " because of previous errors");
       c.close();
+    } catch (ServerClientModeInCompatibleException e) {
+      getLogger().error("Handshake Error : ", e);
+      this.handshakeManager.notifyClientRejected(clientMsg, e.getMessage());
+      // client should go away after reading this message and the channel should get closed there by.
     }
   }
 
-  private void checkCompatibility(boolean isEnterpriseClient) throws ClientHandshakeException {
+  private void checkCompatibility(boolean isEnterpriseClient) throws ServerClientModeInCompatibleException {
     if (this.isEnterpriseServer && !isEnterpriseClient) {
       TerracottaOperatorEvent handshakeRefusedEvent = TerracottaOperatorEventFactory
           .createHandShakeRejectedEvent(OPEN_SOURCE, ENTERPRISE);
       this.opEventLogger.fireOperatorEvent(handshakeRefusedEvent);
-      throw new ClientHandshakeException("An " + OPEN_SOURCE + " client can not connect to an " + ENTERPRISE
-                                         + " Server, Connection refused.");
+      throw new ServerClientModeInCompatibleException("An " + OPEN_SOURCE + " client can not connect to an "
+                                                         + ENTERPRISE + " Server, Connection refused.");
     } else if (!this.isEnterpriseServer && isEnterpriseClient) {
       TerracottaOperatorEvent handshakeRefusedEvent = TerracottaOperatorEventFactory
           .createHandShakeRejectedEvent(ENTERPRISE, OPEN_SOURCE);
       this.opEventLogger.fireOperatorEvent(handshakeRefusedEvent);
-      throw new ClientHandshakeException("An " + ENTERPRISE + " client can not connect to an " + OPEN_SOURCE
-                                         + " Server, Connection refused.");
+      throw new ServerClientModeInCompatibleException("An " + ENTERPRISE + " client can not connect to an "
+                                                         + OPEN_SOURCE + " Server, Connection refused.");
     }
   }
 
