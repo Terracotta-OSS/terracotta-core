d5b9efc4d13a8a576caacab730d6127aabbe50a9
Nihit Purwar <npurwar@terracottatech.com>
2011-01-19 07:08:09 +0000

    DEV-5214
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@16996 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-common/src/com/tc/object/cache/CacheConfigImpl.java
+++ code/base/dso-common/src/com/tc/object/cache/CacheConfigImpl.java
@@ -8,24 +8,58 @@ import com.tc.properties.TCProperties;
 
 public class CacheConfigImpl implements CacheConfig {
 
-  private final int     leastCount;
-  private final int     percentageToEvict;
-  private final long    sleepInterval;
-  private final int     criticalThreshold;
-  private final int     threshold;
-  private final boolean monitorOldGenOnly;
-  private final boolean loggingEnabled;
-  private final int     criticalObjectThreshold;
+  private final int         leastCount;
+  private final int         percentageToEvict;
+  private final long        sleepInterval;
+  private final int         criticalThreshold;
+  private final int         threshold;
+  private final boolean     monitorOldGenOnly;
+  private final boolean     loggingEnabled;
+  private final int         criticalObjectThreshold;
+
+  private static final int  THRESHOLD                                = 70;
+  private static final int  CRITICAL_THRESHOLD                       = 90;
+
+  private static final int  TEMP_SWAP_OFFHEAP_2GB_THRESHOLD          = 50;
+  private static final int  TEMP_SWAP_OFFHEAP_2GB_CRITICAL_THRESHOLD = 70;
+
+  private static final long TWO_GB                                   = 2 * 1024 * 1024 * 1024L;
+
+  public CacheConfigImpl(TCProperties cacheManagerProperties, boolean isPersistent, boolean isOffHeap) {
+    this(cacheManagerProperties, cacheManagerProperties.getInt("threshold",
+                                                               getDefaultThreshold(isPersistent, isOffHeap)),
+         cacheManagerProperties.getInt("criticalThreshold", getDefaultCriticalThreshold(isPersistent, isOffHeap)));
+  }
 
   public CacheConfigImpl(TCProperties cacheManagerProperties) {
-    leastCount = cacheManagerProperties.getInt("leastCount");
-    percentageToEvict = cacheManagerProperties.getInt("percentageToEvict");
-    sleepInterval = cacheManagerProperties.getLong("sleepInterval");
-    criticalThreshold = cacheManagerProperties.getInt("criticalThreshold");
-    threshold = cacheManagerProperties.getInt("threshold");
-    monitorOldGenOnly = cacheManagerProperties.getBoolean("monitorOldGenOnly");
-    loggingEnabled = cacheManagerProperties.getBoolean("logging.enabled");
-    criticalObjectThreshold = cacheManagerProperties.getInt("criticalObjectThreshold");
+    this(cacheManagerProperties, cacheManagerProperties.getInt("threshold"), cacheManagerProperties
+        .getInt("criticalThreshold"));
+  }
+
+  public CacheConfigImpl(TCProperties cacheManagerProperties, int threshold, int criticalThreshold) {
+    this.leastCount = cacheManagerProperties.getInt("leastCount");
+    this.percentageToEvict = cacheManagerProperties.getInt("percentageToEvict");
+    this.sleepInterval = cacheManagerProperties.getLong("sleepInterval");
+    this.monitorOldGenOnly = cacheManagerProperties.getBoolean("monitorOldGenOnly");
+    this.loggingEnabled = cacheManagerProperties.getBoolean("logging.enabled");
+    this.criticalObjectThreshold = cacheManagerProperties.getInt("criticalObjectThreshold");
+    this.threshold = threshold;
+    this.criticalThreshold = criticalThreshold;
+  }
+
+  private static int getDefaultThreshold(boolean isPersistent, boolean isOffHeap) {
+    if (!isPersistent && isOffHeap && getMaxMemory() <= TWO_GB) { return TEMP_SWAP_OFFHEAP_2GB_THRESHOLD; }
+    return THRESHOLD;
+  }
+
+  private static long getMaxMemory() {
+    Runtime runtime = Runtime.getRuntime();
+    return runtime.maxMemory();
+  }
+
+  private static int getDefaultCriticalThreshold(boolean isPersistent, boolean isOffHeap) {
+    if (!isPersistent && isOffHeap && getMaxMemory() <= TWO_GB) { return TEMP_SWAP_OFFHEAP_2GB_CRITICAL_THRESHOLD; }
+    return CRITICAL_THRESHOLD;
   }
 
   public int getLeastCount() {
