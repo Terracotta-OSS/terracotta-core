c26b716b954e4e331d6ec45a03876d649054d6de
Manoj Govindassamy <mgovinda@terracottatech.com>
2011-02-17 19:45:31 +0000

    logging unexpected message in dummy hccontext
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@17195 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-common/src/com/tc/net/protocol/transport/ConnectionHealthCheckerContextDummyImpl.java
+++ code/base/dso-common/src/com/tc/net/protocol/transport/ConnectionHealthCheckerContextDummyImpl.java
@@ -4,6 +4,9 @@
  */
 package com.tc.net.protocol.transport;
 
+import com.tc.logging.TCLogger;
+import com.tc.logging.TCLogging;
+
 /**
  * A Dummy HealthChecker Context. Doesn't send any probe signals to peers. This is just a dumb context, keeps mouth shut
  * for all the PING probes it gets.
@@ -11,10 +14,13 @@ package com.tc.net.protocol.transport;
  * @author Manoj
  */
 public class ConnectionHealthCheckerContextDummyImpl implements ConnectionHealthCheckerContext {
+  private static final TCLogger logger = TCLogging.getLogger(ConnectionHealthCheckerContextDummyImpl.class);
 
   public boolean receiveProbe(HealthCheckerProbeMessage message) {
-    if (message.isPing()) { return true; }
-    throw new AssertionError("Dummy HealthCheckerContext. Unexpected Probe Message received " + message);
+    if (!message.isPing()) {
+      logger.error("Unexpected probe message received by Dummy HealthCheckerContext: " + message);
+    }
+    return true;
   }
 
   public boolean probeIfAlive() {
