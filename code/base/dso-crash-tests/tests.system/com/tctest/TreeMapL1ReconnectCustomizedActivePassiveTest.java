a1d1341829e41e6acc728b8fa3c130d56cbba0c0
Manoj Govindassamy <mgovinda@terracottatech.com>
2011-03-01 20:50:01 +0000

    test fix
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@17251 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-crash-tests/tests.system/com/tctest/TreeMapL1ReconnectCustomizedActivePassiveTest.java
+++ code/base/dso-crash-tests/tests.system/com/tctest/TreeMapL1ReconnectCustomizedActivePassiveTest.java
@@ -4,6 +4,7 @@
  */
 package com.tctest;
 
+import com.tc.stats.DSOMBean;
 import com.tc.test.MultipleServersCrashMode;
 import com.tc.test.MultipleServersPersistenceMode;
 import com.tc.test.MultipleServersSharedDataMode;
@@ -11,38 +12,44 @@ import com.tc.test.activepassive.ActivePassiveServerManager;
 import com.tc.test.activepassive.ActivePassiveTestSetupManager;
 import com.tc.test.restart.RestartTestHelper;
 import com.tc.util.Assert;
+import com.tc.util.concurrent.ThreadUtil;
 import com.tc.util.runtime.Os;
 
 public class TreeMapL1ReconnectCustomizedActivePassiveTest extends ActivePassiveTransparentTestBase {
 
   private static final int NODE_COUNT = 2;
 
+  @Override
   public void doSetUp(TransparentTestIface t) throws Exception {
     t.getTransparentAppConfig().setClientCount(NODE_COUNT).setIntensity(1);
     t.initializeTestRunner();
   }
 
+  @Override
   protected Class getApplicationClass() {
     return TreeMapTestApp.class;
   }
-  
+
+  @Override
   protected boolean enableL1Reconnect() {
     return true;
   }
-  
+
   @Override
   protected long getRestartInterval(RestartTestHelper helper) {
-    if(Os.isSolaris() || Memory.isMemoryLow()) {
+    if (Os.isSolaris() || Memory.isMemoryLow()) {
       return super.getRestartInterval(helper) * 3;
     } else {
       return super.getRestartInterval(helper);
     }
   }
 
+  @Override
   protected boolean canRunCrash() {
     return true;
   }
 
+  @Override
   public void setupActivePassiveTest(ActivePassiveTestSetupManager setupManager) {
     setupManager.setServerCount(2);
     setupManager.setServerCrashMode(MultipleServersCrashMode.AP_CUSTOMIZED_CRASH);
@@ -50,6 +57,7 @@ public class TreeMapL1ReconnectCustomizedActivePassiveTest extends ActivePassive
     setupManager.setServerPersistenceMode(MultipleServersPersistenceMode.PERMANENT_STORE);
   }
 
+  @Override
   protected void customizeActivePassiveTest(final ActivePassiveServerManager manager) throws Exception {
 
     System.out.println("XXX Start active server[0]");
@@ -61,6 +69,13 @@ public class TreeMapL1ReconnectCustomizedActivePassiveTest extends ActivePassive
 
       public void run() {
         try {
+
+          System.out.println("XXX Waiting for " + NODE_COUNT + "clients to connect");
+          while (getClientsForServer(manager, 0) != NODE_COUNT) {
+            System.err.println(".");
+            ThreadUtil.reallySleep(5000);
+          }
+
           Thread.sleep(5000);
           activeIndex = manager.getAndUpdateActiveIndex();
           Assert.assertTrue(activeIndex == 0);
@@ -73,6 +88,12 @@ public class TreeMapL1ReconnectCustomizedActivePassiveTest extends ActivePassive
           activeIndex = manager.getAndUpdateActiveIndex();
           Assert.assertTrue(activeIndex == 0);
 
+          System.out.println("XXX Waiting for " + NODE_COUNT + " clients to reconnect");
+          while (getClientsForServer(manager, activeIndex) != NODE_COUNT) {
+            System.err.println(".");
+            ThreadUtil.reallySleep(5000);
+          }
+
           System.out.println("XXX Start passive server[1]");
           manager.startServer(1);
           Thread.sleep(1000);
@@ -84,27 +105,40 @@ public class TreeMapL1ReconnectCustomizedActivePassiveTest extends ActivePassive
           activeIndex = manager.getAndUpdateActiveIndex();
           Assert.assertTrue(activeIndex == 1);
 
+          System.out.println("XXX Waiting for " + NODE_COUNT + " clients to switch over");
+          while (getClientsForServer(manager, activeIndex) != NODE_COUNT) {
+            System.err.println(".");
+            ThreadUtil.reallySleep(5000);
+          }
+
           manager.cleanupServerDB(0);
           System.out.println("XXX Start passive server[0]");
           manager.startServer(0);
           Thread.sleep(1000);
-          manager.waitServerIsPassiveStandby(0, 20);//waitServerIsPassiveStandby(0, 20);
-          
-          while(true) {
+          manager.waitServerIsPassiveStandby(0, 20);// waitServerIsPassiveStandby(0, 20);
+
+          while (true) {
             Thread.sleep(20000);
             int crashedIndex = activeIndex;
             System.out.println("XXX Stop active server[" + activeIndex + "]");
             manager.crashActive();
-            
+
             Thread.sleep(3000);
             manager.cleanupServerDB(crashedIndex);
             System.out.println("XXX Start passive server[" + crashedIndex + "]");
             manager.startServer(crashedIndex);
             Thread.sleep(1000);
             manager.waitServerIsPassiveStandby(crashedIndex, 20);
-            
+
             activeIndex = manager.getAndUpdateActiveIndex();
             Assert.assertTrue(activeIndex != crashedIndex);
+
+            System.out.println("XXX Waiting for " + NODE_COUNT + " clients with active");
+            while (getClientsForServer(manager, activeIndex) != NODE_COUNT) {
+              System.err.println(".");
+              ThreadUtil.reallySleep(5000);
+            }
+
           }
 
         } catch (Exception e) {
@@ -116,4 +150,17 @@ public class TreeMapL1ReconnectCustomizedActivePassiveTest extends ActivePassive
     apThread.start();
   }
 
+  private int getClientsForServer(ActivePassiveServerManager activePassiveServerManager, int serverIndex)
+      throws Exception {
+    try {
+      DSOMBean mbean = activePassiveServerManager.getDsoMBean(serverIndex);
+      for (Object obj : mbean.getClients()) {
+        System.out.println("XXX CLIENT BEAN : " + obj);
+      }
+      return mbean.getClients().length;
+    } catch (Exception e) {
+      System.err.println("Not able to get DSO Mbean for server index " + serverIndex + " : " + e);
+    }
+    return 0;
+  }
 }
