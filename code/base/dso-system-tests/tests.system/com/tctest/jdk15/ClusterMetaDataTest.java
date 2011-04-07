476cd1e775a17869fc37cc41c39ff373240c91a0
Tim Wu <tim.wu@terracottatech.com>
2011-04-07 04:39:31 +0000

    MNK-2235: Adding some extra logging to see if we can track down this failure.
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@17464 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-system-tests/tests.system/com/tctest/jdk15/ClusterMetaDataTest.java
+++ code/base/dso-system-tests/tests.system/com/tctest/jdk15/ClusterMetaDataTest.java
@@ -8,8 +8,14 @@ import com.tctest.ClusterMetaDataTestApp;
 import com.tctest.TransparentTestBase;
 import com.tctest.TransparentTestIface;
 
+import java.util.ArrayList;
 
 public class ClusterMetaDataTest extends TransparentTestBase {
+
+  @Override
+  protected void setExtraJvmArgs(ArrayList jvmArgs) {
+    jvmArgs.add("-Dcom.tc.cachemanager.enable.logging=true");
+  }
 
   @Override
   public void doSetUp(final TransparentTestIface t) throws Exception {
