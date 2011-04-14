440affbc7154490f3392d6975391ec9b5eda5489
Abhishek Singh <asingh@terracottatech.com>
2011-04-14 05:18:54 +0000

    fix tests
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@17484 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-l2/tests.unit/com/tc/objectserver/managedobject/ConcurrentDistributedServerMapManagedObjectStateTest.java
+++ code/base/dso-l2/tests.unit/com/tc/objectserver/managedobject/ConcurrentDistributedServerMapManagedObjectStateTest.java
@@ -34,7 +34,7 @@ public class ConcurrentDistributedServerMapManagedObjectStateTest extends Abstra
 
     dnaWriter = new TestDNAWriter();
     state.dehydrate(new ObjectID(1), dnaWriter, DNAType.L1_FAULT); // Only dehydrate the fields
-    Assert.assertEquals(8, dnaWriter.getActionCount()); // only the physical fields are dehydrated
+    Assert.assertEquals(9, dnaWriter.getActionCount()); // only the physical fields are dehydrated
 
     final TestDNACursor cursor3 = dnaWriter.getDNACursor();
     final ConcurrentDistributedServerMapManagedObjectState state3 = (ConcurrentDistributedServerMapManagedObjectState) createManagedObjectState(CDSM_DSO_CLASSNAME,
@@ -60,6 +60,8 @@ public class ConcurrentDistributedServerMapManagedObjectStateTest extends Abstra
     cursor.addPhysicalAction(ConcurrentDistributedServerMapManagedObjectState.INVALIDATE_ON_CHANGE,
                              Boolean.valueOf(false), false);
     cursor.addPhysicalAction(ConcurrentDistributedServerMapManagedObjectState.CACHE_NAME_FIELDNAME, "bob", false);
+    cursor.addPhysicalAction(ConcurrentDistributedServerMapManagedObjectState.LOCAL_CACHE_ENABLED_FIELDNAME,
+                             Boolean.valueOf(true), false);
 
     for (int i = 0; i < 500; i++) {
       cursor.addLogicalAction(SerializationUtil.PUT, new Object[] { "key-" + i, new ObjectID(1000 + i) });
