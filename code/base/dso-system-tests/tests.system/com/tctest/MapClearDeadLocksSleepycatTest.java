e3f3ec88c6211c17eaa8be9e69713c2751816c19
Nihit Purwar <npurwar@terracottatech.com>
2011-04-28 10:23:19 +0000

    debug for MNK-2385
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@17661 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-system-tests/tests.system/com/tctest/MapClearDeadLocksSleepycatTest.java
+++ code/base/dso-system-tests/tests.system/com/tctest/MapClearDeadLocksSleepycatTest.java
@@ -7,6 +7,8 @@ package com.tctest;
 import com.tc.config.schema.setup.TestConfigurationSetupManagerFactory;
 import com.terracottatech.config.PersistenceMode;
 
+import java.util.ArrayList;
+
 /**
  * This test was written specifically to expose a dead lock in sleepcat in persistence map
  */
@@ -15,18 +17,32 @@ public class MapClearDeadLocksSleepycatTest extends TransparentTestBase {
   private static final int NODE_COUNT    = 2;
   private static final int THREADS_COUNT = 2;
 
+  @Override
   public void doSetUp(TransparentTestIface t) throws Exception {
     t.getTransparentAppConfig().setClientCount(NODE_COUNT).setApplicationInstancePerClientCount(THREADS_COUNT)
         .setIntensity(1);
     t.initializeTestRunner();
   }
 
+  @Override
   protected Class getApplicationClass() {
     return MapClearDeadLocksSleepycatTestApp.class;
   }
-  
+
+  @Override
   protected void setupConfig(TestConfigurationSetupManagerFactory configFactory) {
     configFactory.setPersistenceMode(PersistenceMode.PERMANENT_STORE);
+  }
+
+  @Override
+  protected void setExtraJvmArgs(ArrayList jvmArgs) {
+    super.setExtraJvmArgs(jvmArgs);
+    jvmArgs.add("-Dcom.tc.l2.derbydb.derby.locks.deadlockTrace=true");
+  }
+
+  @Override
+  protected boolean useExternalProcess() {
+    return true;
   }
 
 }
