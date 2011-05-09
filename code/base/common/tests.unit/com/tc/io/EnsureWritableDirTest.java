689fada6e0aba9b1f6b85248879b324768ee6f2c
Manoj Govindassamy <mgovinda@terracottatech.com>
2011-05-09 21:38:33 +0000

    MNK-2375 : EnsureWritableDirTest  fix
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@17712 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/common/tests.unit/com/tc/io/EnsureWritableDirTest.java
+++ code/base/common/tests.unit/com/tc/io/EnsureWritableDirTest.java
@@ -1,10 +1,12 @@
 /*
- * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
  */
 package com.tc.io;
 
 import com.tc.test.TCTestCase;
 import com.tc.test.TempDirectoryHelper;
+import com.tc.util.concurrent.ThreadUtil;
 import com.tc.util.io.TCFileUtils;
 import com.tc.util.io.TCFileUtils.EnsureWritableDirReporter;
 import com.tc.util.runtime.Os;
@@ -14,8 +16,8 @@ import java.io.IOException;
 import java.util.Random;
 
 public class EnsureWritableDirTest extends TCTestCase {
-  
-  private Random random = new Random();
+
+  private final Random random = new Random();
 
   private File createTmpDir() throws IOException {
     File tmp_dir_parent = new TempDirectoryHelper(getClass(), false).getDirectory();
@@ -37,7 +39,7 @@ public class EnsureWritableDirTest extends TCTestCase {
         public void reportFailedCreate(File dir, Exception e) {
           fail("null directory should NPE");
         }
-  
+
         public void reportReadOnly(File dir, Exception e) {
           fail("null directory should NPE");
         }
@@ -48,7 +50,7 @@ public class EnsureWritableDirTest extends TCTestCase {
     }
     assertTrue(caughtNPE);
   }
-  
+
   /**
    * Test: ensureWritableDir() of an existing, writable dir should succeed
    */
@@ -58,13 +60,14 @@ public class EnsureWritableDirTest extends TCTestCase {
       public void reportFailedCreate(File dir, Exception e) {
         fail();
       }
+
       public void reportReadOnly(File dir, Exception e) {
         fail();
       }
     });
     assertTrue(result);
   }
-  
+
   /**
    * Test: ensureWritableDir() of nonexisting dir that can be created should succeed
    */
@@ -75,28 +78,37 @@ public class EnsureWritableDirTest extends TCTestCase {
       public void reportFailedCreate(File dir, Exception e) {
         fail();
       }
+
       public void reportReadOnly(File dir, Exception e) {
         fail();
       }
     });
     assertTrue(result);
   }
-  
+
   /**
-   * Test: ensureWritableDir() of a read-only dir should fail and call appropriate reporter function.
-   * Note that creating a read-only dir in Java is fraught, because there is no way (before
-   * Java 6) of setting it back to writable. So we're stuck with trying to delete a read-only
-   * dir, at the end of the test. Whether this works is system-dependent. This test may not
-   * be able to run on all platforms.
+   * Test: ensureWritableDir() of a read-only dir should fail and call appropriate reporter function. Note that creating
+   * a read-only dir in Java is fraught, because there is no way (before Java 6) of setting it back to writable. So
+   * we're stuck with trying to delete a read-only dir, at the end of the test. Whether this works is system-dependent.
+   * This test may not be able to run on all platforms.
    */
   public void testReadOnlyDir() throws Exception {
     File tmpDir = createTmpDir();
-    tmpDir.setReadOnly();
+    int count = 0;
+    while (!tmpDir.setReadOnly()) {
+      ThreadUtil.reallySleep(2000);
+      if (count++ > 5) {
+        System.err.println("XXX Couldn't make " + tmpDir.getAbsolutePath() + " redable.");
+        return;
+      }
+    }
+
     final int calls[] = new int[] { 0 };
     boolean result = TCFileUtils.ensureWritableDir(tmpDir, new EnsureWritableDirReporter() {
       public void reportFailedCreate(File dir, Exception e) {
         fail();
       }
+
       public void reportReadOnly(File dir, Exception e) {
         ++calls[0];
       }
@@ -104,19 +116,16 @@ public class EnsureWritableDirTest extends TCTestCase {
     assertFalse(result);
     assertEquals(1, calls[0]);
   }
-  
+
   /**
-   * Test: ensureWritableDir() of non-existent dir whose parent is read-only should fail
-   * and call appropriate reporter function.
-   * As with {@link #testReadOnlyDir()}, this test is awkward because there's no way
-   * to make the directory be writable again after making it read-only.
+   * Test: ensureWritableDir() of non-existent dir whose parent is read-only should fail and call appropriate reporter
+   * function. As with {@link #testReadOnlyDir()}, this test is awkward because there's no way to make the directory be
+   * writable again after making it read-only.
    */
   public void testReadOnlyParent() throws Exception {
     // On Windows, the read-only attribute of a directory is ignored except when deleting
     // the directory itself, so this test is inapplicable.
-    if (Os.isWindows()) {
-      return;
-    }
+    if (Os.isWindows()) { return; }
     File tmpDir = createTmpDir();
     tmpDir.setReadOnly();
     File child = new File(tmpDir, "child");
@@ -125,6 +134,7 @@ public class EnsureWritableDirTest extends TCTestCase {
       public void reportFailedCreate(File dir, Exception e) {
         ++calls[0];
       }
+
       public void reportReadOnly(File dir, Exception e) {
         fail();
       }
@@ -132,10 +142,9 @@ public class EnsureWritableDirTest extends TCTestCase {
     assertFalse(result);
     assertEquals(1, calls[0]);
   }
-  
+
   /**
-   * Test: ensureWritableDir() of a non-directory file should fail and call appropriate
-   * reporter function.
+   * Test: ensureWritableDir() of a non-directory file should fail and call appropriate reporter function.
    */
   public void testNonDirFile() throws Exception {
     File tmpDir = createTmpDir();
@@ -146,6 +155,7 @@ public class EnsureWritableDirTest extends TCTestCase {
       public void reportFailedCreate(File dir, Exception e) {
         ++calls[0];
       }
+
       public void reportReadOnly(File dir, Exception e) {
         fail();
       }
