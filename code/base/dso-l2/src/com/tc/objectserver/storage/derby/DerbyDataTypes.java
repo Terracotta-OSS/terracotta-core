cce0501087b24accc62f8447b0d6c750c2fdbefa
Nihit Purwar <npurwar@terracottatech.com>
2011-05-13 22:58:34 +0000

    making value size as 1 GB
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@17745 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-l2/src/com/tc/objectserver/storage/derby/DerbyDataTypes.java
+++ code/base/dso-l2/src/com/tc/objectserver/storage/derby/DerbyDataTypes.java
@@ -6,7 +6,7 @@ package com.tc.objectserver.storage.derby;
 public interface DerbyDataTypes {
   String TC_STRING           = "VARCHAR (32672)";
   String TC_BYTE_ARRAY_KEY   = "VARCHAR (32672) FOR BIT DATA";
-  String TC_BYTE_ARRAY_VALUE = "BLOB(16M)";
+  String TC_BYTE_ARRAY_VALUE = "BLOB(1024M)";
   String TC_LONG             = "BIGINT";
   String TC_INT              = "INT";
 }
