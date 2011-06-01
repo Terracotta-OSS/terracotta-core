7626b4f7f766968040bf669811346e8ca3bdc946
Tim Wu <tim.wu@terracottatech.com>
2011-06-01 00:43:31 +0000

    MNK-2444: Fixed the test to work as intended, but disabled it until ClusterState persistence issue is dealt with.
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@17843 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-crash-tests/tests.system/com/tctest/TreeMapPassiveClrDirtyDbActivePassiveTest.java
+++ code/base/dso-crash-tests/tests.system/com/tctest/TreeMapPassiveClrDirtyDbActivePassiveTest.java
@@ -4,17 +4,27 @@
  */
 package com.tctest;
 
-import com.tc.cli.command.BaseCommand;
-import com.tc.cli.command.SetDbCleanCommand;
+import com.tc.config.schema.builder.DSOApplicationConfigBuilder;
+import com.tc.config.schema.test.ConfigBuilderFactoryImpl;
+import com.tc.lcp.LinkedJavaProcess;
+import com.tc.object.config.DSOApplicationConfig;
+import com.tc.object.config.DSOApplicationConfigImpl;
+import com.tc.objectserver.control.ExtraL1ProcessControl;
+import com.tc.objectserver.storage.util.SetDbClean;
+import com.tc.simulator.app.ApplicationConfig;
+import com.tc.simulator.listener.ListenerProvider;
 import com.tc.test.MultipleServersCrashMode;
 import com.tc.test.MultipleServersPersistenceMode;
 import com.tc.test.MultipleServersSharedDataMode;
 import com.tc.test.activepassive.ActivePassiveServerManager;
 import com.tc.test.activepassive.ActivePassiveTestSetupManager;
 import com.tc.util.Assert;
+import com.tctest.runner.AbstractTransparentApp;
 
 import java.io.File;
-import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /*
  * DEV-2011. For the case, both active and passive go down. But active goes down for good. Passive restores data and
@@ -22,74 +32,107 @@ import java.io.OutputStreamWriter;
  */
 public class TreeMapPassiveClrDirtyDbActivePassiveTest extends ActivePassiveTransparentTestBase {
 
-  private static final int NODE_COUNT = 2;
+  private static final int NODE_COUNT = 1;
 
+  public TreeMapPassiveClrDirtyDbActivePassiveTest() {
+    disableAllUntil("2011-08-01");
+  }
+
+  @Override
   public void doSetUp(TransparentTestIface t) throws Exception {
-    t.getTransparentAppConfig().setClientCount(NODE_COUNT).setIntensity(1);
+    t.getTransparentAppConfig().setClientCount(NODE_COUNT);
     t.initializeTestRunner();
   }
 
+  @Override
   protected Class getApplicationClass() {
-    return TreeMapTestApp.class;
+    return DummyTestApp.class;
   }
-  
-//  protected boolean enableL1Reconnect() {
-//    return true;
-//  }
 
+  @Override
   public void setupActivePassiveTest(ActivePassiveTestSetupManager setupManager) {
     setupManager.setServerCount(2);
     setupManager.setServerCrashMode(MultipleServersCrashMode.AP_CUSTOMIZED_CRASH);
-    setupManager.setServerShareDataMode(MultipleServersSharedDataMode.DISK);
+    setupManager.setServerShareDataMode(MultipleServersSharedDataMode.NETWORK);
     setupManager.setServerPersistenceMode(MultipleServersPersistenceMode.PERMANENT_STORE);
   }
 
-  protected void customizeActivePassiveTest(final ActivePassiveServerManager manager) throws Exception {
+  @Override
+  protected DSOApplicationConfigBuilder createDsoApplicationConfig() {
+    System.out.println("Creating dso app config");
+    DSOApplicationConfig cfg = new DSOApplicationConfigImpl(new ConfigBuilderFactoryImpl());
+    String testClass = TestApp.class.getName();
+    cfg.addIncludePattern(testClass + "*");
+    String methodExpression = "* " + testClass + "*.*(..)";
+    cfg.addWriteAutolock(methodExpression);
 
+    cfg.addRoot("counter", testClass + ".counter");
+    DSOApplicationConfigBuilder dsoAppConfigBuilder = super.createDsoApplicationConfig();
+    cfg.writeTo(dsoAppConfigBuilder);
+    return dsoAppConfigBuilder;
+  }
+
+  public static class DummyTestApp extends AbstractTransparentApp {
+    public DummyTestApp(String appId, ApplicationConfig config, ListenerProvider listenerProvider) {
+      super(appId, config, listenerProvider);
+    }
+
+    public void run() {
+      // Nothing to do.
+    }
+  }
+
+  public static class TestApp {
+    private static final AtomicInteger counter = new AtomicInteger();
+
+    public static void main(String[] args) {
+      System.out.println("Value " + inc());
+    }
+
+    public static int inc() {
+      return counter.incrementAndGet();
+    }
+  }
+
+  @Override
+  protected void customizeActivePassiveTest(final ActivePassiveServerManager manager) throws Exception {
     System.out.println("XXX Start active server[0]");
     manager.startServer(0);
     manager.getAndUpdateActiveIndex();
-    manager.getAndUpdateActiveIndex();
     Thread.sleep(5000);
-    
+
     System.out.println("XXX Start passive server[1]");
     manager.startServer(1);
     Thread.sleep(1000);
-    manager.waitServerIsPassiveStandby(1, 20);
-    
-    // Allow L1/clients to start, do rest in a thread
-    Thread apThread = new Thread(new Runnable() {
+    Assert.assertTrue(manager.waitServerIsPassiveStandby(1, 60));
 
-      public void run() {
-        int activeIndex;
-        
-        try {
-          // to run a while
-          Thread.sleep(10000);
+    File client1Workspace = new File(getTempDirectory(), "l1-logs" + File.separator + "client1");
+    client1Workspace.mkdirs();
+    ExtraL1ProcessControl client1 = new ExtraL1ProcessControl("localhost", manager.getDsoPort(), TestApp.class,
+                                                              getConfigFileLocation(), Collections.EMPTY_LIST,
+                                                              client1Workspace, Collections.EMPTY_LIST);
 
-          System.out.println("XXX Stop passive server[1]");
-          manager.stopServer(1);
+    client1.start();
+    client1.waitFor();
 
-          System.out.println("XXX Stop active server[0]");
-          manager.stopServer(0);
+    System.out.println("XXX Stop passive server[1]");
+    manager.stopServer(1);
 
-          // clean up passive dirty db
-          System.out.println("XXX Clean passive db dirty bit");
-          String[] args = new String[1];
-          args[0] = manager.getConfigCreator().getDataLocation(1) + File.separator + "objectdb";
-          BaseCommand cmd = new SetDbCleanCommand(new OutputStreamWriter(System.out));
-          cmd.execute(args);
+    System.out.println("XXX Stop active server[0]");
+    manager.stopServer(0);
 
-          System.out.println("XXX Start passive server[1] as active");
-          manager.startServer(1);
-          activeIndex = manager.getAndUpdateActiveIndex();
-          Assert.assertTrue(activeIndex == 1);
-        } catch (Exception e) {
-          System.out.println("customerizeActivePassiveTest: " + e);
-        }
-      }
-    });
-    apThread.setDaemon(true);
-    apThread.start();
+    // clean up passive dirty db
+    System.out.println("XXX Clean passive db dirty bit");
+    LinkedJavaProcess setDbCleanProcess = new LinkedJavaProcess(SetDbClean.class.getName(), Arrays.asList(manager
+        .getConfigCreator().getDataLocation(1) + File.separator + "objectdb"), Collections.EMPTY_LIST);
+    setDbCleanProcess.start();
+    System.out.println("XXX SetDbCleanCommand exited with status " + setDbCleanProcess.waitFor());
+
+    System.out.println("XXX Start passive server[1] as active");
+    manager.startServer(1);
+    Assert.assertEquals(1, manager.getAndUpdateActiveIndex());
+
+    client1.start();
+    Assert.assertEquals(0, client1.waitFor());
   }
 }
