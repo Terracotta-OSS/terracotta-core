a28fc4179744984a6094831fb374ae4e43c390f4
Gary Keim <gkeim@terracottatech.com>
2011-06-02 23:53:28 +0000

    Merge of change 17859 from 3.5 branch.
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@17860 7fc7bbf3-cf45-46d4-be06-341739edd864

--- kits/skeletons/shared-base/common/bin/dev-console.bat
+++ kits/skeletons/shared-base/common/bin/dev-console.bat
@@ -16,6 +16,6 @@ set JAVA_HOME="%JAVA_HOME:"=%"
 set CLASSPATH=%TC_INSTALL_DIR%\lib\tc.jar
 set CLASSPATH="%CLASSPATH:"=%"
 
-set JAVA_OPTS=-Xmx128m -Dtc.install-root=%TC_INSTALL_DIR% %JAVA_OPTS%
+set JAVA_OPTS=-Xmx128m -Dsun.java2d.pmoffscreen=false -Dtc.install-root=%TC_INSTALL_DIR% %JAVA_OPTS%
 start "AdminConsole" /B %JAVA_HOME%\bin\java %JAVA_OPTS% -cp %CLASSPATH% com.tc.admin.AdminClient %*
 endlocal
