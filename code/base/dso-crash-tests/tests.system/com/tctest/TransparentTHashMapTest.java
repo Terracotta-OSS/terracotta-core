56ac36c8b8a829dd83ba9476b70e248236f4ec14
Abhishek Maheshwari <amaheshw@terracottatech.com>
2011-06-29 18:05:30 +0000

    mnk-2567, disabling test for solaris
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@18013 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-crash-tests/tests.system/com/tctest/TransparentTHashMapTest.java
+++ code/base/dso-crash-tests/tests.system/com/tctest/TransparentTHashMapTest.java
@@ -1,21 +1,35 @@
 /*
- * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
  */
 package com.tctest;
+
+import com.tc.util.runtime.Os;
+
+import java.util.Date;
 
 public class TransparentTHashMapTest extends TransparentTestBase {
 
   private static final int NODE_COUNT = 6;
 
+  public TransparentTHashMapTest() {
+    if (Os.isSolaris()) {
+      disableAllUntil(new Date(Long.MAX_VALUE));
+    }
+  }
+
+  @Override
   protected Class getApplicationClass() {
     return TransparentTHashMapTestApp.class;
   }
 
+  @Override
   public void doSetUp(TransparentTestIface t) throws Exception {
     t.getTransparentAppConfig().setClientCount(NODE_COUNT).setIntensity(1);
     t.initializeTestRunner();
   }
 
+  @Override
   protected boolean canRunCrash() {
     return true;
   }
