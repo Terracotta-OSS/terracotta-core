33c705da5af018d3bf150a1c211b32424cec58cf
Tim Eck <timeck@gmail.com>
2011-06-30 19:25:36 +0000

    formatting only
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@18030 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/common-api/src/com/tc/properties/TCPropertiesConsts.java
+++ code/base/common-api/src/com/tc/properties/TCPropertiesConsts.java
@@ -681,7 +681,7 @@ public interface TCPropertiesConsts {
   public static final String EHCACHE_STORAGESTRATEGY_DCV2_EVICT_UNEXPIRED_ENTRIES_ENABLED    = "ehcache.storageStrategy.dcv2.evictUnexpiredEntries.enabled";
   public static final String EHCACHE_STORAGESTRATEGY_DCV2_PERIODICEVICTION_ENABLED           = "ehcache.storageStrategy.dcv2.periodicEviction.enabled";
   public static final String EHCACHE_STORAGESTRATEGY_DCV2_PINSEGMENTS_ENABLED                = "ehcache.storageStrategy.dcv2.pinSegments.enabled";
-  public static final String   EHCACHE_STORAGESTRATEGY_DCV2_EVICTION_OVERSHOOT                 = "ehcache.storageStrategy.dcv2.eviction.overshoot";
+  public static final String EHCACHE_STORAGESTRATEGY_DCV2_EVICTION_OVERSHOOT                 = "ehcache.storageStrategy.dcv2.eviction.overshoot";
   /*********************************************************************************************************************
    * <code>
    * Section : L1 Lock Manager Properties
@@ -867,7 +867,7 @@ public interface TCPropertiesConsts {
    *                                    in the panel before it starts recycling
    *  l2.operator.events.store      -   Number of operator events L2s will store to keep the history of the events
    *  tc.time.sync.threshold        -   Number of second of tolerable system time difference between
-   *                                    two nodes of cluster beyond which and operator event will be thrown 
+   *                                    two nodes of cluster beyond which and operator event will be thrown
    * </code>
    ********************************************************************************************************************/
   public static final String DEV_CONSOLE_MAX_OPERATOR_EVENTS                                 = "dev.console.max.operator.events";
