af3bf2f5bd26420551495561e75d32d8bf658075
Hung Huynh <hhuynh@gmail.com>
2011-07-09 01:50:14 +0000

    add CheckShortTest suite
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/branches/newtrunk@18125 7fc7bbf3-cf45-46d4-be06-341739edd864

---
+++ dso-statistics-tests/src/test/java/com/tctest/statistics/CheckShortTest.java
@@ -0,0 +1,42 @@
+package com.tctest.statistics;
+
+import java.io.File;
+import java.io.FileReader;
+import java.util.List;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Assert;
+
+public class CheckShortTest {
+	
+	@SuppressWarnings("unchecked")
+	public static Test suite()  {
+		String basedir = System.getProperty("basedir");
+		File checkShortList = new File(basedir + File.separator + "checkshort.txt");
+		Assert.assertTrue(checkShortList.exists());
+		
+		FileReader reader = null;
+		try {
+			reader = new FileReader(checkShortList);
+			List<String> tests = IOUtils.readLines(reader);
+			TestSuite suite = new TestSuite();
+			for (String test : tests) {
+				test = test.trim();
+				if (test.startsWith("#") || test.length() == 0) {
+					continue;
+				}
+				suite.addTestSuite((Class<? extends TestCase>) Class.forName(test));
+			}
+			return suite;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		} finally {
+			IOUtils.closeQuietly(reader);
+		}
+	}
+	
+}
