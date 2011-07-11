1f9624faaf990be754c975f8a2ab41e36afbdf84
Abhishek Maheshwari <amaheshw@terracottatech.com>
2011-07-11 14:17:34 +0000

    DEV-4282, adding warning if supplied hostname is localhost
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@18136 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/common-api/src/com/tc/object/config/schema/L2DSOConfigObject.java
+++ code/base/common-api/src/com/tc/object/config/schema/L2DSOConfigObject.java
@@ -18,6 +18,8 @@ import com.tc.config.schema.context.ConfigContext;
 import com.tc.config.schema.defaults.DefaultValueProvider;
 import com.tc.config.schema.dynamic.ParameterSubstituter;
 import com.tc.config.schema.setup.ConfigurationSetupException;
+import com.tc.logging.TCLogger;
+import com.tc.logging.TCLogging;
 import com.tc.util.Assert;
 import com.terracottatech.config.BindPort;
 import com.terracottatech.config.DsoServerData;
@@ -25,9 +27,9 @@ import com.terracottatech.config.GarbageCollection;
 import com.terracottatech.config.Offheap;
 import com.terracottatech.config.Persistence;
 import com.terracottatech.config.PersistenceMode;
+import com.terracottatech.config.PersistenceMode.Enum;
 import com.terracottatech.config.Server;
 import com.terracottatech.config.Servers;
-import com.terracottatech.config.PersistenceMode.Enum;
 import com.terracottatech.config.TcConfigDocument.TcConfig;
 
 import java.io.File;
@@ -36,7 +38,9 @@ import java.io.File;
  * The standard implementation of {@link L2DSOConfig}.
  */
 public class L2DSOConfigObject extends BaseConfigObject implements L2DSOConfig {
+  private static final TCLogger   logger                                = TCLogging.getLogger(L2DSOConfigObject.class);
   private static final String     WILDCARD_IP                           = "0.0.0.0";
+  private static final String     LOCALHOST                             = "localhost";
   public static final short       DEFAULT_JMXPORT_OFFSET_FROM_DSOPORT   = 10;
   public static final short       DEFAULT_GROUPPORT_OFFSET_FROM_DSOPORT = 20;
   public static final int         MIN_PORTNUMBER                        = 0x0FFF;
@@ -67,8 +71,11 @@ public class L2DSOConfigObject extends BaseConfigObject implements L2DSOConfig {
 
     this.bind = server.getBind();
     this.host = server.getHost();
+    if (this.host.equalsIgnoreCase(LOCALHOST)) {
+      logger.warn("The specified hostname \"" + this.host + "\" may not work correctly. Please replace \"" + this.host
+                  + "\" with appropriate hostname in configuration file");
+    }
     this.serverName = server.getName();
-
     this.dsoPort = server.getDsoPort();
     this.l2GroupPort = server.getL2GroupPort();
     if (server.getDso().getPersistence().isSetOffheap()) {
