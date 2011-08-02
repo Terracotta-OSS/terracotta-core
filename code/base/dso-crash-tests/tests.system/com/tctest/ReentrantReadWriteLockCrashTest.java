98e7b8b7c9fe90c496cee8b121e8e4c3c504f926
Tim Wu <tim.wu@terracottatech.com>
2011-08-02 02:04:28 +0000

    MNK-2690: Add a bit of logging to see if we're getting notifications on the L1 side
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@18359 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/dso-crash-tests/tests.system/com/tctest/ReentrantReadWriteLockCrashTest.java
+++ code/base/dso-crash-tests/tests.system/com/tctest/ReentrantReadWriteLockCrashTest.java
@@ -4,11 +4,16 @@
  */
 package com.tctest;
 
+import com.tc.logging.LogLevel;
+import com.tc.logging.LogLevelImpl;
+import com.tc.object.locks.ClientLockManager;
 import com.tc.test.MultipleServersCrashMode;
 import com.tc.test.MultipleServersPersistenceMode;
 import com.tc.test.MultipleServersSharedDataMode;
 import com.tc.test.activepassive.ActivePassiveTestSetupManager;
 import com.tctest.runner.TransparentAppConfig;
+
+import java.util.Map;
 
 public class ReentrantReadWriteLockCrashTest extends ActivePassiveTransparentTestBase {
 
@@ -20,6 +25,11 @@ public class ReentrantReadWriteLockCrashTest extends ActivePassiveTransparentTes
     appConfig.setClientCount(NODE_COUNT);
     t.initializeTestRunner();
     appConfig.setAttribute(ReentrantReadWriteLockTestApp.CRASH_TEST, "true");
+  }
+
+  @Override
+  protected void setL1ClassLoggingLevels(Map<Class<?>, LogLevel> logLevels) {
+    logLevels.put(ClientLockManager.class, LogLevelImpl.DEBUG);
   }
 
   @Override
--- code/base/dso-l1/src/com/tc/object/locks/ClientLockManagerImpl.java
+++ code/base/dso-l1/src/com/tc/object/locks/ClientLockManagerImpl.java
@@ -435,6 +435,10 @@ public class ClientLockManagerImpl implements ClientLockManager, ClientLockManag
         return;
       }
 
+      if (this.logger.isDebugEnabled()) {
+        this.logger.debug("Got a notification for " + lock + " with thread " + thread);
+      }
+
       final ClientLock lockState = getClientLockState(lock);
       if (lockState == null) {
         throw new AssertionError("Server attempting to notify on non-existent lock " + lock);
@@ -534,6 +538,9 @@ public class ClientLockManagerImpl implements ClientLockManager, ClientLockManag
   public void wait(final LockID lock, final WaitListener listener, final Object waitObject) throws InterruptedException {
     waitUntilRunning();
     final ClientLock lockState = getOrCreateClientLockState(lock);
+    if (logger.isDebugEnabled()) {
+      logger.debug(this.threadManager.getThreadID() + " waiting on log " + lock);
+    }
     lockState.wait(this.remoteManager, listener, this.threadManager.getThreadID(), waitObject);
   }
 
@@ -541,6 +548,9 @@ public class ClientLockManagerImpl implements ClientLockManager, ClientLockManag
       throws InterruptedException {
     waitUntilRunning();
     final ClientLock lockState = getOrCreateClientLockState(lock);
+    if (logger.isDebugEnabled()) {
+      logger.debug(this.threadManager.getThreadID() + " waiting on log " + lock);
+    }
     lockState.wait(this.remoteManager, listener, this.threadManager.getThreadID(), waitObject, timeout);
   }
 
