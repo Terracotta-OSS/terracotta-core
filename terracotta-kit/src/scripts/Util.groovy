9d87b1890d267c15b687609635a5d8bdcd5cb26d
Hung Huynh <hhuynh@gmail.com>
2011-08-06 01:38:20 +0000

    moved groovy code from pom into source
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/branches/newtrunk@18398 7fc7bbf3-cf45-46d4-be06-341739edd864

--- terracotta-kit/src/scripts/Util.groovy
+++ terracotta-kit/src/scripts/Util.groovy
@@ -1,7 +1,50 @@
 
 class Util {
-        
+    static final def ant = new AntBuilder()
+    
     static boolean rename(String oldFile, String newFile) {
       return new File(oldFile).renameTo(new File(newFile))  
+    }
+    
+    static void processEhcacheDistribution(project) {
+      def ehcacheVersion = project.properties['ehcache.version']
+      def kitEdition = project.properties['kit.edition']
+      def rootDir = project.properties['rootDir']
+      
+      def rootEhcacheFolder = "/ehcache-core-" + ehcacheVersion
+      if (kitEdition == 'enterprise') {
+        rootEhcacheFolder = "/ehcache-core-ee-" + ehcacheVersion
+      }
+  
+      def currentName =  rootDir + rootEhcacheFolder
+      def newName = rootDir  + "/ehcache"
+      rename(currentName, newName) 
+    }                  
+    
+    static void processQuartzDistribution(project) {
+      def quartzVersion = project.properties['quartz.version']
+      def rootDir = project.properties['rootDir']
+      def kitEdition = project.properties['kit.edition']
+      
+      def currentName =  rootDir + "/quartz-" + quartzVersion
+      def newName = rootDir  + "/quartz"
+      rename(currentName, newName)
+      if (kitEdition == "enterprise") {        
+        ant.replace(dir: newName, includes: "**/example15/instance*.properties", 
+                    token: "TerracottaJobStore", value: "EnterpriseTerracottaJobStore")
+      }
+    }                  
+    
+    static void createFeatureJar(project) {
+      def rootDir = project.properties['rootDir'].replace('\\', '/')
+      def featureJar = "org.terracotta.dso_" + project.properties['eclipse.plugin.version']
+      def destFile = rootDir + "/update/features/" + featureJar + ".jar" 
+      def baseDir =  rootDir + "/update/features"
+      ant.jar(destfile: destFile, basedir: baseDir)
+      ant.delete(file: rootDir + "/update/features/feature.xml")
+    }
+    
+    static void createPluginJar(project) {
+      def rootDir = project.properties['rootDir'].replace('\\', '/')   
     }
 }
