b774ee80403a4e04ca8a9184631cbbbad89c5754
Tim Wu <tim.wu@terracottatech.com>
2011-08-08 21:59:22 +0000

    Add ability to retrieve TCServerInfoMBean.
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@18416 7fc7bbf3-cf45-46d4-be06-341739edd864

--- code/base/simulator/tests.base/com/tc/objectserver/control/ServerMBeanRetriever.java
+++ code/base/simulator/tests.base/com/tc/objectserver/control/ServerMBeanRetriever.java
@@ -6,6 +6,7 @@ package com.tc.objectserver.control;
 import com.tc.admin.common.MBeanServerInvocationProxy;
 import com.tc.management.beans.L2DumperMBean;
 import com.tc.management.beans.L2MBeanNames;
+import com.tc.management.beans.TCServerInfoMBean;
 import com.tc.stats.api.DSOMBean;
 import com.tc.test.JMXUtils;
 import com.tc.util.CallableWaiter;
@@ -17,9 +18,10 @@ import javax.management.MBeanServerConnection;
 import javax.management.remote.JMXConnector;
 
 public class ServerMBeanRetriever {
-  private final String host;
-  private final int    jmxPort;
-  private DSOMBean     dsoMBean;
+  private final String      host;
+  private final int         jmxPort;
+  private DSOMBean          dsoMBean;
+  private TCServerInfoMBean tcServerInfoMBean;
 
   public ServerMBeanRetriever(final String host, final int jmxPort) {
     this.host = host;
@@ -43,6 +45,19 @@ public class ServerMBeanRetriever {
     return dsoMBean;
   }
 
+  public TCServerInfoMBean getTCServerInfoMBean() throws Exception {
+    if (tcServerInfoMBean != null) {
+      try {
+        tcServerInfoMBean.isActive();
+        return tcServerInfoMBean;
+      } catch (Exception e) {
+        // the dsoMBean is dead, re-obtain it.
+      }
+    }
+    tcServerInfoMBean = getTCServerInfoMBean(host, jmxPort);
+    return tcServerInfoMBean;
+  }
+
   public int getJmxPort() {
     return jmxPort;
   }
@@ -55,14 +70,15 @@ public class ServerMBeanRetriever {
     final AtomicReference<DSOMBean> dsoMBeanRef = new AtomicReference<DSOMBean>();
     CallableWaiter.waitOnCallable(new Callable<Boolean>() {
       public Boolean call() throws Exception {
+        JMXConnector jmxConnector = JMXUtils.getJMXConnector(host, jmxPort);
         try {
-          JMXConnector jmxConnector = JMXUtils.getJMXConnector(host, jmxPort);
           MBeanServerConnection msc = jmxConnector.getMBeanServerConnection();
           DSOMBean dsoMBean = MBeanServerInvocationProxy.newMBeanProxy(msc, L2MBeanNames.DSO, DSOMBean.class, false);
           dsoMBean.getLiveObjectCount();
           dsoMBeanRef.set(dsoMBean);
           return true;
         } catch (Exception e) {
+          jmxConnector.close();
           return false;
         }
       }
@@ -76,17 +92,37 @@ public class ServerMBeanRetriever {
     final AtomicReference<L2DumperMBean> l2DumperRef = new AtomicReference<L2DumperMBean>();
     CallableWaiter.waitOnCallable(new Callable<Boolean>() {
       public Boolean call() throws Exception {
+        JMXConnector jmxConnector = JMXUtils.getJMXConnector(host, jmxPort);
         try {
-          JMXConnector jmxConnector = JMXUtils.getJMXConnector(host, jmxPort);
           MBeanServerConnection msc = jmxConnector.getMBeanServerConnection();
           l2DumperRef.set(MBeanServerInvocationProxy
               .newMBeanProxy(msc, L2MBeanNames.DUMPER, L2DumperMBean.class, false));
           return true;
         } catch (Exception e) {
+          jmxConnector.close();
           return false;
         }
       }
     });
     return l2DumperRef.get();
+  }
+
+  public static TCServerInfoMBean getTCServerInfoMBean(final String host, final int jmxPort) throws Exception {
+    final AtomicReference<TCServerInfoMBean> tcServerInfoMBeanRef = new AtomicReference<TCServerInfoMBean>();
+    CallableWaiter.waitOnCallable(new Callable<Boolean>() {
+      public Boolean call() throws Exception {
+        JMXConnector jmxConnector = JMXUtils.getJMXConnector(host, jmxPort);
+        try {
+          MBeanServerConnection msc = jmxConnector.getMBeanServerConnection();
+          tcServerInfoMBeanRef.set(MBeanServerInvocationProxy.newMBeanProxy(msc, L2MBeanNames.TC_SERVER_INFO,
+                                                                            TCServerInfoMBean.class, false));
+          return true;
+        } catch (Exception e) {
+          jmxConnector.close();
+          return false;
+        }
+      }
+    });
+    return tcServerInfoMBeanRef.get();
   }
 }
