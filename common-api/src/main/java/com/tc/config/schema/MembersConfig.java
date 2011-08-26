fd8328c41d0f930616c9c14cdda34dd918514ae3
cruise <cruise@terracottatech.com>
2011-08-26 23:09:43 +0000

    emty source to reimport trunk
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/branches/newtrunk2@18551 7fc7bbf3-cf45-46d4-be06-341739edd864

--- common-api/src/main/java/com/tc/config/schema/MembersConfig.java
+++
@@ -1,9 +0,0 @@
-/*
- * All content copyright (c) 2003-2007 Terracotta, Inc., except as may otherwise be noted in a separate copyright
- * notice. All rights reserved.
- */
-package com.tc.config.schema;
-
-public interface MembersConfig extends Config {
-  String[] getMemberArray();
-}
--- dso-l1/src/main/java/com/tc/object/servermap/localcache/ServerMapLocalCacheRemoveCallback.java
+++
@@ -1,9 +0,0 @@
-/*
- * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
- */
-package com.tc.object.servermap.localcache;
-
-public interface ServerMapLocalCacheRemoveCallback {
-
-  public void removedElement(Object key, AbstractLocalCacheStoreValue value);
-}
