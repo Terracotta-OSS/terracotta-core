6887d2ac00da943f61a3457ee6be4a1c4a2cd6c7
cruise <cruise@terracottatech.com>
2011-08-31 18:45:21 +0000

    cleaned to do another sync
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/branches/newtrunk2@18583 7fc7bbf3-cf45-46d4-be06-341739edd864

--- common/src/test/java/com/tc/common/proxy/subpkg/TestInterface.java
+++
@@ -1,9 +0,0 @@
-/*
- * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
- */
-
-package com.tc.common.proxy.subpkg;
-
-public interface TestInterface {
-  public void method();
-}
