3ce90455f80c0f948bf6c73f2a546daf4efc5f83
cruise <cruise@terracottatech.com>
2011-09-06 16:44:32 +0000

    clean to resync
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/branches/newtrunk2@18620 7fc7bbf3-cf45-46d4-be06-341739edd864

--- dso-common/src/main/java/com/tc/object/dna/impl/ProxyInstance.java
+++
@@ -1,8 +0,0 @@
-/*
- * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
- */
-package com.tc.object.dna.impl;
-
-public interface ProxyInstance {
-  // This is a marker interface to indicate an instance is a dso shared proxy instance.
-}
--- dso-system-tests/src/test/java/com/tctest/ExportedClass.java
+++
@@ -1,8 +0,0 @@
-/*
- * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
- */
-package com.tctest;
-
-public class ExportedClass {
-  //
-}
