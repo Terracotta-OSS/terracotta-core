16e3b42933530d5ccfa55138e0ff8378815cac3a
Hung Huynh <hhuynh@gmail.com>
2011-09-13 19:04:12 +0000

    DEV-6207: stop parsing jdk patch level
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@18701 7fc7bbf3-cf45-46d4-be06-341739edd864

--- common-api/src/main/java/com/tc/util/runtime/VmVersion.java
+++ common-api/src/main/java/com/tc/util/runtime/VmVersion.java
@@ -16,16 +16,13 @@ import java.util.regex.Pattern;
  */
 public final class VmVersion {
 
-  private static final Pattern JVM_VERSION_PATTERN         = Pattern
-                                                               .compile("^(\\p{Digit})\\.(\\p{Digit})\\.(\\p{Digit})(?:[-_](.+))?$");
-  static final Pattern         IBM_SERVICE_RELEASE_PATTERN = Pattern
-                                                               .compile("^[^-]+-\\p{Digit}{8}[^\\p{Space}]*\\p{Space}*.*$");
+  private static final Pattern JVM_VERSION_PATTERN = Pattern
+                                                       .compile("^(\\p{Digit})\\.(\\p{Digit})\\.(\\p{Digit})(?:(.+))?$");
 
   private final String         vmVersion;
   private final int            mega;
   private final int            major;
   private final int            minor;
-  private final String         patch;
   private final boolean        isIBM;
   private final boolean        isJRockit;
   private final boolean        isAzul;
@@ -67,33 +64,10 @@ public final class VmVersion {
       mega = Integer.parseInt(versionMatcher.group(1));
       major = Integer.parseInt(versionMatcher.group(2));
       minor = Integer.parseInt(versionMatcher.group(3));
-      String version_patch = versionMatcher.groupCount() == 4 ? versionMatcher.group(4) : null;
-      if (isIBM) {
-        final Matcher serviceReleaseMatcher = IBM_SERVICE_RELEASE_PATTERN.matcher(runtimeVersion);
-        if (serviceReleaseMatcher.matches()) {
-          String serviceRelease = serviceReleaseMatcher.groupCount() == 1 ? serviceReleaseMatcher.group(1)
-              .toLowerCase() : null;
-          if (null == version_patch && null == serviceRelease) {
-            patch = null;
-          } else if (null == version_patch) {
-            patch = serviceRelease;
-          } else if (null == serviceRelease) {
-            patch = version_patch;
-          } else {
-            patch = version_patch + serviceRelease;
-          }
-        } else {
-          patch = version_patch;
-          // throw new UnknownRuntimeVersionException(vendorVersion,
-          // runtimeVersion);
-        }
-      } else {
-        patch = version_patch;
-      }
     } else {
       throw new RuntimeException("Unknown version: " + vendorVersion);
     }
-    this.vmVersion = this.mega + "." + this.major + "." + this.minor + (null == patch ? "" : "_" + patch);
+    this.vmVersion = this.mega + "." + this.major + "." + this.minor;
   }
 
   /**
@@ -122,15 +96,6 @@ public final class VmVersion {
    */
   public int getMinorVersion() {
     return minor;
-  }
-
-  /**
-   * Get patch level (ie 12 in 1.2.3_12)
-   * 
-   * @return Patch level
-   */
-  public String getPatchLevel() {
-    return patch;
   }
 
   /**
