34344074168168137ad3e4dabbed63274575d9f7
Nihit Purwar <npurwar@terracottatech.com>
2011-11-02 09:23:35 +0000

    collect logs for MNK-3059
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@18983 7fc7bbf3-cf45-46d4-be06-341739edd864

--- dso-crash-tests/src/test/java/com/tctest/DistributedWaitCrashTest.java
+++ dso-crash-tests/src/test/java/com/tctest/DistributedWaitCrashTest.java
@@ -1,27 +1,40 @@
 /*
- * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
  */
 package com.tctest;
+
+import java.util.ArrayList;
 
 public class DistributedWaitCrashTest extends TransparentTestBase {
 
   private static final int NODE_COUNT = 2;
-  
-  public DistributedWaitCrashTest() {
-    //this.disableAllUntil("2007-05-01");
-  }
-  
+
+  @Override
   public void doSetUp(TransparentTestIface t) throws Exception {
     t.getTransparentAppConfig().setClientCount(NODE_COUNT);
     t.initializeTestRunner();
   }
 
+  @Override
   protected Class getApplicationClass() {
     return DistributedWaitCrashTestApp.class;
   }
-  
+
+  @Override
   protected boolean canRunCrash() {
     return true;
-  }  
+  }
+
+  @Override
+  protected boolean useExternalProcess() {
+    return true;
+  }
+
+  @Override
+  protected void setExtraJvmArgs(ArrayList jvmArgs) {
+    super.setExtraJvmArgs(jvmArgs);
+    jvmArgs.add("-Dcom.tc.logging.maxBackups=300");
+  }
 
 }
