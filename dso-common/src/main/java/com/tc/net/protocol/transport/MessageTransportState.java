01f799224660654e1409af998a85a9c56516c4ba
Nishant Bangarwa <nbangarw@terracottatech.com>
2011-12-01 07:33:22 +0000

    DEV-6570 converted MessageTransportState to enum
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@19157 7fc7bbf3-cf45-46d4-be06-341739edd864

--- dso-common/src/main/java/com/tc/net/protocol/transport/MessageTransportState.java
+++ dso-common/src/main/java/com/tc/net/protocol/transport/MessageTransportState.java
@@ -4,52 +4,52 @@
  */
 package com.tc.net.protocol.transport;
 
-public class MessageTransportState {
+public enum MessageTransportState {
   /**
    * XXX: move to client state machine Initial state for client transports.
    */
-  public static final MessageTransportState STATE_START         = new MessageTransportState("START");
+  STATE_START("START"),
 
-  public static final MessageTransportState STATE_RESTART       = new MessageTransportState("RESTART");
+  STATE_RESTART("RESTART"),
 
   /**
    * XXX: Move to client state machine SYN message sent, waiting for reply
    */
-  public static final MessageTransportState STATE_SYN_SENT      = new MessageTransportState("SYN_SENT");
+  STATE_SYN_SENT("SYN_SENT"),
   // /**
   // * XXX: Move to server state machine
   // */
-  // public static final MessageTransportState STATE_SYN_ACK_SENT = new
-  // MessageTransportState("SYN_ACK_SENT");
+  // TATE_SYN_ACK_SENT("SYN_ACK_SENT"),
   /**
    * The client sends a SYN with a connection id that we can't reconnect to (e.g. we can't find the corresponding
    * server-side stack).
    */
-  public static final MessageTransportState STATE_SYN_ACK_ERROR = new MessageTransportState("SYN_ACK_ERROR");
+  STATE_SYN_ACK_ERROR("SYN_ACK_ERROR"),
 
   /**
    * XXX: Move to client state machine SYN_ACK received-- we're ready to talk!
    */
-  public static final MessageTransportState STATE_ESTABLISHED   = new MessageTransportState("ESTABLISHED");
+  STATE_ESTABLISHED("ESTABLISHED"),
 
   // Transport Closed
-  public static final MessageTransportState STATE_CLOSED        = new MessageTransportState("CLOSED");
+  STATE_CLOSED("CLOSED"),
 
   // Transport got disconnected -- probably health checker didnt like the client
-  public static final MessageTransportState STATE_DISCONNECTED  = new MessageTransportState("DISCONNECTED");
+  STATE_DISCONNECTED("DISCONNECTED"),
 
   /**
    * End state-- if the client is disconnected and isn't going to reconnect or if there is a handshake error (server or
    * client)
    */
-  public static final MessageTransportState STATE_END           = new MessageTransportState("END");
+  STATE_END("END");
 
-  private final String                      name;
+  private final String name;
 
   private MessageTransportState(String name) {
     this.name = name;
   }
 
+  @Override
   public String toString() {
     return this.name;
   }
