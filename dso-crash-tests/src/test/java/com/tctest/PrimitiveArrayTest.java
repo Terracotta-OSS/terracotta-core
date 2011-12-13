02f44384766dc6f73e2e3f543b305ad910e5ab09
Tim Eck <timeck@gmail.com>
2011-12-13 00:21:47 +0000

    merge 12299
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@19300 7fc7bbf3-cf45-46d4-be06-341739edd864

--- dso-crash-tests/src/test/java/com/tctest/PrimitiveArrayTest.java
+++ dso-crash-tests/src/test/java/com/tctest/PrimitiveArrayTest.java
@@ -1,7 +1,11 @@
 /*
- * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
  */
 package com.tctest;
+
+import com.tc.test.restart.RestartTestHelper;
+import com.tc.util.runtime.Os;
 
 public class PrimitiveArrayTest extends TransparentTestBase implements TestConfigurator {
 
@@ -9,15 +13,27 @@ public class PrimitiveArrayTest extends TransparentTestBase implements TestConfi
 
   // types, and 8 wrappers)
 
+  @Override
   public void doSetUp(TransparentTestIface t) throws Exception {
     t.getTransparentAppConfig().setClientCount(NODE_COUNT).setIntensity(1);
     t.initializeTestRunner();
   }
 
+  @Override
+  protected long getRestartInterval(RestartTestHelper helper) {
+    if (Os.isSolaris()) {
+      return super.getRestartInterval(helper) * 3;
+    } else {
+      return super.getRestartInterval(helper);
+    }
+  }
+
+  @Override
   protected Class getApplicationClass() {
     return PrimitiveArrayTestApp.class;
   }
 
+  @Override
   protected boolean canRunCrash() {
     return true;
   }
