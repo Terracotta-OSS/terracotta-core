29ffe342a1882d3a760c7c40fe8d32a45b55fddc
Nishant Bangarwa <nbangarw@terracottatech.com>
2012-01-18 18:53:34 +0000

    Added logging
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@19542 7fc7bbf3-cf45-46d4-be06-341739edd864

--- test-framework/src/main/java/org/terracotta/tests/base/TestClientLauncher.java
+++ test-framework/src/main/java/org/terracotta/tests/base/TestClientLauncher.java
@@ -11,21 +11,25 @@ public class TestClientLauncher {
 	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
 		String clientClassName = args[0];
-		//  Client Class must implement Runnable and has a constructor with arguments String[]
+		// Client Class must implement Runnable and has a constructor with
+		// arguments String[]
 		try {
 			Class<? extends Runnable> clientClass = (Class<? extends Runnable>) TestClientLauncher.class
 					.getClassLoader().loadClass(clientClassName);
 
 			String[][] clientArgs = new String[1][];
 			clientArgs[0] = Arrays.copyOfRange(args, 1, args.length);
-			Constructor<Runnable> constructor = (Constructor<Runnable>) clientClass
-					.getConstructor(String[].class);
+			Constructor<Runnable> constructor;
+				constructor = (Constructor<Runnable>) clientClass
+						.getConstructor(String[].class);
 			Runnable newInstance = constructor.newInstance(clientArgs);
 			newInstance.run();
-		} catch (Exception e) {
+		} catch (NoSuchMethodException e) {
+			System.out.println("Class "+clientClassName+"should have one constructor having argument and array of String( String[]) . ");
+			e.printStackTrace();
+		}catch (Exception e) {
 			e.printStackTrace();
 		}
 
 	}
-
 }
