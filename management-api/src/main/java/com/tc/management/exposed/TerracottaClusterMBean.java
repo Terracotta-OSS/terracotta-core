bf4b6ee13ee05357826346375f7a6dcfeea7bf5a
Hung Huynh <hhuynh@gmail.com>
2012-01-22 00:56:01 +0000

    fixed checkstyle
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@19571 7fc7bbf3-cf45-46d4-be06-341739edd864

--- management-api/src/main/java/com/tc/management/exposed/TerracottaClusterMBean.java
+++ management-api/src/main/java/com/tc/management/exposed/TerracottaClusterMBean.java
@@ -1,5 +1,5 @@
 /*
- * All content copyright (c) 2003-2008 Terracotta, Inc., except as may otherwise be noted in a separate copyright notice.  All rights reserved.
+ * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
  */
 package com.tc.management.exposed;
 
@@ -7,9 +7,9 @@ import com.tc.management.TerracottaMBean;
 
 public interface TerracottaClusterMBean extends TerracottaMBean {
 
-	boolean isConnected();
+  boolean isConnected();
 
-	String getNodeId();
+  String getNodeId();
 
-	String[] getNodesInCluster();
+  String[] getNodesInCluster();
 }
