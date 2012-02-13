eb65545018ec945eaee2bddd703a377a749caba1
Nishant Bangarwa <nbangarw@terracottatech.com>
2012-02-13 07:39:06 +0000

    DEV-6851 Added timeout per test config
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@19791 7fc7bbf3-cf45-46d4-be06-341739edd864

--- test-framework/src/main/java/org/terracotta/tests/base/AbstractTestBase.java
+++ test-framework/src/main/java/org/terracotta/tests/base/AbstractTestBase.java
@@ -43,6 +43,7 @@ import javax.management.MBeanServerConnection;
 
 @RunWith(value = TcTestRunner.class)
 public abstract class AbstractTestBase extends TCTestCase {
+  private static final int            TEST_TIMEOUT_PER_CONFIG         = 900;
   private static final String         SINGLE_SERVER_CONFIG = "single-server-config";
   protected static final String       SEP                  = File.pathSeparator;
   private final TestConfig            testConfig;
@@ -414,6 +415,11 @@ public abstract class AbstractTestBase extends TCTestCase {
 
   protected void stopClient(final int index) {
     this.clientRunner.stopClient(index);
+  }
+
+  @Override
+  public int getTimeoutValueInSeconds() {
+    return TEST_TIMEOUT_PER_CONFIG;
   }
 
 }
