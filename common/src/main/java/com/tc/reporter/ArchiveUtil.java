365606f3a667170f6facba11d3ee5568690306e8
Hung Huynh <hhuynh@gmail.com>
2012-02-24 17:37:18 +0000

    Findbugs: should be a UTF-8 encoding to prevent lossy conversion
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@19852 7fc7bbf3-cf45-46d4-be06-341739edd864

--- common/src/main/java/com/tc/reporter/ArchiveUtil.java
+++ common/src/main/java/com/tc/reporter/ArchiveUtil.java
@@ -42,32 +42,32 @@ public final class ArchiveUtil {
   private static final String DASH_N            = "-n";
   private static final String DASH_C            = "-c";
   private static final String USAGE             = "** Terracotta Archive Tool **\n\n"
-                                                    + "A utility for archiving Terracotta environment information.\n\n"
-                                                    + "\tValid Arguments are:\n\n\t["
-                                                    + DASH_N
-                                                    + "] (No Data - excludes data files)\n\t["
-                                                    + DASH_C
-                                                    + "] (Client - include files from the dso client)"
-                                                    + "\n\t<path to terracotta config xml file (tc-config.xml)>"
-                                                    + " | <path to data and/or logs directory>"
-                                                    + "\n\t[<output filename in .zip format>]\n\nExamples:\n\n\t"
-                                                    + "# java "
-                                                    + ArchiveUtil.class.getName()
-                                                    + " tc-config.xml /home/someuser/tc-archive_server.zip"
-                                                    + "\n\tor\n\t# java "
-                                                    + ArchiveUtil.class.getName()
-                                                    + " /export1/terracotta/server-logs"
-                                                    + "\n\nUsage Summary:\n\n\tTypically you will use this tool to create a full "
-                                                    + "archive of the Terracotta server instance.\n\t"
-                                                    + "You may also want to create archives on the DSO client machines using"
-                                                    + " the -c option. There are two\n\tscenarios where you may "
-                                                    + "need to use the directory location instead of the config file path."
-                                                    + "\n\n\t\t1. The DSO client may not have a local copy of the tc-config.xml"
-                                                    + "\n\t\t2. The tc-config.xml logs and data elements may contain wildcards"
-                                                    + " which use timestamps or \n\t\t   environment variables which cannot be"
-                                                    + " resolved.\n\nNotes:\n\n\tThe execution command may vary:"
-                                                    + "\n\t\t# ./archive-util ...\n\n\tSpecifying a directory location as the"
-                                                    + " first command will recursively archive it's entire contents";
+                                                  + "A utility for archiving Terracotta environment information.\n\n"
+                                                  + "\tValid Arguments are:\n\n\t["
+                                                  + DASH_N
+                                                  + "] (No Data - excludes data files)\n\t["
+                                                  + DASH_C
+                                                  + "] (Client - include files from the dso client)"
+                                                  + "\n\t<path to terracotta config xml file (tc-config.xml)>"
+                                                  + " | <path to data and/or logs directory>"
+                                                  + "\n\t[<output filename in .zip format>]\n\nExamples:\n\n\t"
+                                                  + "# java "
+                                                  + ArchiveUtil.class.getName()
+                                                  + " tc-config.xml /home/someuser/tc-archive_server.zip"
+                                                  + "\n\tor\n\t# java "
+                                                  + ArchiveUtil.class.getName()
+                                                  + " /export1/terracotta/server-logs"
+                                                  + "\n\nUsage Summary:\n\n\tTypically you will use this tool to create a full "
+                                                  + "archive of the Terracotta server instance.\n\t"
+                                                  + "You may also want to create archives on the DSO client machines using"
+                                                  + " the -c option. There are two\n\tscenarios where you may "
+                                                  + "need to use the directory location instead of the config file path."
+                                                  + "\n\n\t\t1. The DSO client may not have a local copy of the tc-config.xml"
+                                                  + "\n\t\t2. The tc-config.xml logs and data elements may contain wildcards"
+                                                  + " which use timestamps or \n\t\t   environment variables which cannot be"
+                                                  + " resolved.\n\nNotes:\n\n\tThe execution command may vary:"
+                                                  + "\n\t\t# ./archive-util ...\n\n\tSpecifying a directory location as the"
+                                                  + " first command will recursively archive it's entire contents";
 
   private static final Set    validDashArgs     = new HashSet();
   static {
@@ -82,7 +82,8 @@ public final class ArchiveUtil {
     if (fileName == null) {
       File userDir = new File(System.getProperty("user.dir"));
       if (!userDir.exists()) throw new RuntimeException(
-          "Unexpected error - system property user.dir does not resolve to an actual directory: " + userDir);
+                                                        "Unexpected error - system property user.dir does not resolve to an actual directory: "
+                                                            + userDir);
       DateFormat df = new SimpleDateFormat("y-M-d");
       String name = ARCHIVE_FILE_NAME + "_" + df.format(new Date(System.currentTimeMillis())) + ".zip";
       this.archiveFile = new File(userDir + File.separator + name);
@@ -125,16 +126,16 @@ public final class ArchiveUtil {
     boolean dashC = dashSet.contains(DASH_C);
     boolean dashN = dashSet.contains(DASH_N);
 
-    if (locationCmd < 0) escape(
-        "Please specify the Terracotta config file location or logs/data directory location\n\n" + USAGE, null);
+    if (locationCmd < 0) escape("Please specify the Terracotta config file location or logs/data directory location\n\n"
+                                    + USAGE, null);
     File tcConfigFile = new File(args[locationCmd]);
     if (!tcConfigFile.exists()) escape("\tTerracotta Configuration file: " + tcConfigFile + "\n\tdoes not exist\n\n"
-        + USAGE, null);
+                                       + USAGE, null);
     File outputFile = null;
     if (fileArg > 0) {
       outputFile = new File(new File(args[fileArg]).getAbsolutePath());
-      if (!new File(outputFile.getParent()).exists()) escape(
-          "\tThe directory specified for the output file does not exist", null);
+      if (!new File(outputFile.getParent()).exists()) escape("\tThe directory specified for the output file does not exist",
+                                                             null);
     }
     try {
       new ArchiveUtil(!dashN, dashC, tcConfigFile, outputFile).createArchive();
@@ -159,7 +160,7 @@ public final class ArchiveUtil {
     String clientLogs = ParameterSubstituter.substitute(logs);
     File clientLogsDir = makeAbsolute(new File(clientLogs));
     if (!clientLogsDir.exists()) quit("\nError occured while parsing: " + tcConfig
-        + "\n\tUnable to locate client log files at: " + clientLogs);
+                                      + "\n\tUnable to locate client log files at: " + clientLogs);
     return clientLogsDir;
   }
 
@@ -185,7 +186,8 @@ public final class ArchiveUtil {
       logs[i] = ParameterSubstituter.substitute(logs[i]);
       File serverLogsDir = makeAbsolute(new File(logs[i]));
       if (!serverLogsDir.exists()) quit("\nError occured while parsing: " + tcConfig
-          + "\n\tUnable to resolve the server log location element to an actual file: " + logs[i]);
+                                        + "\n\tUnable to resolve the server log location element to an actual file: "
+                                        + logs[i]);
       logFiles[i] = serverLogsDir;
     }
     return logFiles;
@@ -202,7 +204,8 @@ public final class ArchiveUtil {
       serverData[i] = ParameterSubstituter.substitute(serverData[i]);
       File serverDataDir = makeAbsolute(new File(serverData[i]));
       if (!serverDataDir.exists()) quit("\nError occured while parsing: " + tcConfig
-          + "\n\tUnable to resolve the server data location element to an actual file: " + serverData[i]);
+                                        + "\n\tUnable to resolve the server data location element to an actual file: "
+                                        + serverData[i]);
       dataFiles[i] = serverDataDir;
     }
     return dataFiles;
@@ -212,7 +215,7 @@ public final class ArchiveUtil {
     try {
       System.out.println("Archiving:\n----------------------------------------");
       ArchiveBuilder zip = new ZipBuilder(archiveFile, true);
-      zip.putEntry("env-stats", EnvStats.report().getBytes());
+      zip.putEntry("env-stats", EnvStats.report().getBytes("UTF-8"));
       zip.putTraverseDirectory(tcConfig, tcConfig.getName());
       zip.finish();
     } catch (IOException e) {
@@ -245,12 +248,12 @@ public final class ArchiveUtil {
       if (isClient) {
         if (clientLogsDir != null) zip.putTraverseDirectory(clientLogsDir, clientLogsDir.getName());
       } else {
-        for (int i = 0; i < serverLogsDir.length; i++) {
-          if (serverLogsDir[i] != null) zip.putTraverseDirectory(serverLogsDir[i], serverLogsDir[i].getName());
+        for (File element : serverLogsDir) {
+          if (element != null) zip.putTraverseDirectory(element, element.getName());
         }
         if (serverDataDir != null) {
-          for (int i = 0; i < serverDataDir.length; i++) {
-            zip.putTraverseDirectory(serverDataDir[i], serverDataDir[i].getName());
+          for (File element : serverDataDir) {
+            zip.putTraverseDirectory(element, element.getName());
           }
         }
       }
