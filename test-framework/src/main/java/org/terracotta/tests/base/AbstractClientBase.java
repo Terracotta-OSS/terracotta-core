376235d5a6e81a19bcb12e05bdb3fd38e2c58aff
Abhishek Singh <asingh@terracottatech.com>
2012-06-27 10:36:37 +0000

    toolkit2: add util debug method
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/branches/toolkit2@20391 7fc7bbf3-cf45-46d4-be06-341739edd864

--- test-framework/src/main/java/org/terracotta/tests/base/AbstractClientBase.java
+++ test-framework/src/main/java/org/terracotta/tests/base/AbstractClientBase.java
@@ -10,6 +10,8 @@ import com.tc.test.jmx.TestHandler;
 import com.tc.test.jmx.TestHandlerMBean;
 import com.tc.test.setup.GroupsData;
 
+import java.util.Date;
+
 import javax.management.MBeanServerConnection;
 import javax.management.remote.JMXConnector;
 
@@ -36,6 +38,7 @@ public abstract class AbstractClientBase implements Runnable {
     }
   }
 
+  @Override
   public final void run() {
     try {
       doTest();
@@ -70,5 +73,9 @@ public abstract class AbstractClientBase implements Runnable {
 
   public String getTerracottaUrl() {
     return this.testControlMBean.getTerracottaUrl();
+  }
+
+  public static void debug(String msg) {
+    System.out.println(new Date() + " " + Thread.currentThread().getName() + ": " + msg);
   }
 }
