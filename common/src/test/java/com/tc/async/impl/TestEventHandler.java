af4ec7ca2ccfb59954a94546eb2205f81cadd063
Tim Wu <tim.wu@terracottatech.com>
2012-07-12 06:21:43 +0000

    MNK-3832: Fix thread safety and test race
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@20437 7fc7bbf3-cf45-46d4-be06-341739edd864

--- common/src/test/java/com/tc/async/impl/TestEventHandler.java
+++ common/src/test/java/com/tc/async/impl/TestEventHandler.java
@@ -3,28 +3,43 @@
  */
 package com.tc.async.impl;
 
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
+
 import com.tc.async.api.AbstractEventHandler;
 import com.tc.async.api.EventContext;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author steve
  */
 public class TestEventHandler extends AbstractEventHandler {
-  private LinkedList contexts = new LinkedList();
+  private final List<EventContext> contexts = new LinkedList<EventContext>();
 
   /*
    * (non-Javadoc)
    * 
    * @see com.tc.async.api.EventHandler#handleEvent(com.tc.async.api.EventContext)
    */
-  public void handleEvent(EventContext context) {
+  @Override
+  public synchronized void handleEvent(EventContext context) {
     contexts.add(context);
+    notifyAll();
   }
 
-  public List getContexts() {
-    return contexts;
+  public synchronized void waitForEventContextCount(int count, long timeout, TimeUnit unit) throws InterruptedException {
+    if (count < 0) { throw new AssertionError("Count can not be less than 0. count=" + count); }
+    long timeoutNanos = unit.toNanos(timeout);
+    long start = System.nanoTime();
+    while (contexts.size() < count && (System.nanoTime() - start < timeoutNanos)) {
+      wait(NANOSECONDS.toMillis(System.nanoTime() - start));
+    }
+  }
+
+  public synchronized List<EventContext> getContexts() {
+    return new ArrayList<EventContext>(contexts);
   }
 }
