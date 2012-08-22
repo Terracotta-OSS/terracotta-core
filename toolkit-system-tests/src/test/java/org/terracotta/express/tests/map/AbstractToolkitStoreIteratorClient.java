450ae7cf7dab65a58ae88d8e60b88bf2a9b44f8b
Nishant Bangarwa <nbangarw@terracottatech.com>
2012-08-22 14:24:13 +0000

    CDV-1568: Adding tests to make sure iterator,values and entryset does not return null values or throw NPE
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@20577 7fc7bbf3-cf45-46d4-be06-341739edd864

---
+++ toolkit-system-tests/src/test/java/org/terracotta/express/tests/map/AbstractToolkitStoreIteratorClient.java
@@ -0,0 +1,67 @@
+/*
+ * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
+ */
+package org.terracotta.express.tests.map;
+
+import org.junit.Assert;
+import org.terracotta.express.tests.base.ClientBase;
+import org.terracotta.toolkit.Toolkit;
+import org.terracotta.toolkit.store.ToolkitStore;
+import org.terracotta.toolkit.store.ToolkitStoreConfigFields.Consistency;
+
+import java.util.Map.Entry;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+public abstract class AbstractToolkitStoreIteratorClient extends ClientBase {
+  private static final Random random = new Random();
+
+  public AbstractToolkitStoreIteratorClient(String[] args) {
+    super(args);
+  }
+
+  public abstract Consistency getConsistency();
+
+  private static int getNextRandom() {
+    return random.nextInt(300);
+  }
+
+  @Override
+  protected void test(Toolkit toolkit) throws Throwable {
+    final ToolkitStore<String, String> store = toolkit.getStore("MySampleStore", String.class);
+
+    for (int i = 0; i < 10; i++) {
+      Thread randomPutRemover = new Thread(new Runnable() {
+
+        @Override
+        public void run() {
+          while (true) {
+            int number = getNextRandom();
+            System.out.println("Put : " + number);
+            store.put(String.valueOf(number), String.valueOf(number));
+            number = getNextRandom();
+            System.out.println("Remove : " + number);
+            store.remove(String.valueOf(number));
+          }
+        }
+      });
+      randomPutRemover.setDaemon(true);
+      randomPutRemover.start();
+    }
+    long startTime = System.currentTimeMillis();
+    long test_duration = TimeUnit.MINUTES.toMillis(8);
+    while (System.currentTimeMillis() - startTime < test_duration) {
+      for (String s : store.keySet()) {
+        Assert.assertNotNull(s);
+      }
+      for (String s : store.values()) {
+        Assert.assertNotNull(s);
+      }
+      for (Entry<String, String> entry : store.entrySet()) {
+        Assert.assertNotNull(entry.getKey());
+        Assert.assertNotNull(entry.getValue());
+      }
+    }
+  }
+
+}
