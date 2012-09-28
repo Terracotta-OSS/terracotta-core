7a92c3d6ffb0777f2baf00d0a0663b0c4bd8814d
Nihit Purwar <npurwar@terracottatech.com>
2012-09-28 15:41:10 +0000

    revert to using earlier factory
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/branches/resource-management@20802 7fc7bbf3-cf45-46d4-be06-341739edd864

--- toolkit-impl/src/main/java/com/terracotta/toolkit/TerracottaToolkit.java
+++ toolkit-impl/src/main/java/com/terracotta/toolkit/TerracottaToolkit.java
@@ -39,7 +39,7 @@ import com.terracotta.toolkit.collections.ToolkitBlockingQueueImpl;
 import com.terracotta.toolkit.collections.ToolkitSetImpl;
 import com.terracotta.toolkit.collections.ToolkitSortedSetImpl;
 import com.terracotta.toolkit.collections.servermap.api.ServerMapLocalStoreFactory;
-import com.terracotta.toolkit.collections.servermap.api.gbimpl.GBServerMapLocalStoreFactory;
+import com.terracotta.toolkit.collections.servermap.api.ehcacheimpl.EhcacheSMLocalStoreFactory;
 import com.terracotta.toolkit.concurrent.locks.ToolkitLockImpl;
 import com.terracotta.toolkit.concurrent.locks.ToolkitReadWriteLockImpl;
 import com.terracotta.toolkit.config.UnclusteredConfiguration;
@@ -109,8 +109,7 @@ public class TerracottaToolkit implements ToolkitInternal {
     }
 
     defaultToolkitCacheManager = createDefaultToolkitCacheManager();
-    serverMapLocalStoreFactory = new GBServerMapLocalStoreFactory();// new
-                                                                    // EhcacheSMLocalStoreFactory(defaultToolkitCacheManager);
+    serverMapLocalStoreFactory = new EhcacheSMLocalStoreFactory(defaultToolkitCacheManager);
 
     ToolkitTypeRootsStaticFactory toolkitTypeRootsFactory = new ToolkitTypeRootsStaticFactory(weakValueMapManager);
 
