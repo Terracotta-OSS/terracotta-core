b2f6746dd0ad5be185d6f23117314caaf9278a4a
Gary Keim <gkeim@terracottatech.com>
2012-10-01 23:50:21 +0000

    forgot to check this in
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@20834 7fc7bbf3-cf45-46d4-be06-341739edd864

--- management/src/main/java/com/tc/management/remote/protocol/terracotta/RemoteJMXAttributeProcessor.java
+++ management/src/main/java/com/tc/management/remote/protocol/terracotta/RemoteJMXAttributeProcessor.java
@@ -10,34 +10,50 @@ import com.tc.management.JMXAttributeContext;
 import com.tc.net.protocol.tcm.TCMessageType;
 import com.tc.properties.TCPropertiesConsts;
 import com.tc.properties.TCPropertiesImpl;
+import com.tc.util.concurrent.ThreadUtil;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 public class RemoteJMXAttributeProcessor {
-  private final static int      MAXTHREADS = TCPropertiesImpl.getProperties()
-                                               .getInt(TCPropertiesConsts.L2_REMOTEJMX_MAXTHREADS);
-  private final static int      IDLETIME   = TCPropertiesImpl.getProperties()
-                                               .getInt(TCPropertiesConsts.L2_REMOTEJMX_IDLETIME);
+  private final static TCLogger logger = TCLogging.getLogger(RemoteJMXAttributeProcessor.class);
 
-  private final static Executor executor   = new ThreadPoolExecutor(1, MAXTHREADS, IDLETIME, TimeUnit.SECONDS,
-                                                                    new LinkedBlockingQueue<Runnable>());
-  private final static TCLogger logger     = TCLogging.getLogger(RemoteJMXAttributeProcessor.class);
+  private final Executor        executor;
+
+  public RemoteJMXAttributeProcessor() {
+    int maxThreads = TCPropertiesImpl.getProperties().getInt(TCPropertiesConsts.L2_REMOTEJMX_MAXTHREADS);
+    int idleTime = TCPropertiesImpl.getProperties().getInt(TCPropertiesConsts.L2_REMOTEJMX_IDLETIME);
+
+    executor = new ThreadPoolExecutor(1, maxThreads, idleTime, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
+  }
 
   public void add(final EventContext context) {
     try {
-      executor.execute(new Runnable() {
-        public void run() {
-          JMXAttributeContext attributeContext = (JMXAttributeContext) context;
+      int retries = 0;
+      while (true) {
+        try {
+          executor.execute(new Runnable() {
+            public void run() {
+              JMXAttributeContext attributeContext = (JMXAttributeContext) context;
 
-          JmxRemoteTunnelMessage messageEnvelope = (JmxRemoteTunnelMessage) attributeContext.getChannel()
-              .createMessage(TCMessageType.JMXREMOTE_MESSAGE_CONNECTION_MESSAGE);
-          messageEnvelope.setTunneledMessage(attributeContext.getOutboundMessage());
-          messageEnvelope.send();
+              JmxRemoteTunnelMessage messageEnvelope = (JmxRemoteTunnelMessage) attributeContext.getChannel()
+                  .createMessage(TCMessageType.JMXREMOTE_MESSAGE_CONNECTION_MESSAGE);
+              messageEnvelope.setTunneledMessage(attributeContext.getOutboundMessage());
+              messageEnvelope.send();
+            }
+          });
+          break;
+        } catch (RejectedExecutionException e) {
+          ThreadUtil.reallySleep(10);
+          retries++;
         }
-      });
+        if (retries % 100 == 0) {
+          logger.warn("JMX Attribute Processor is saturated. Retried processing a request " + retries + " times.");
+        }
+      }
     } catch (Throwable t) {
       logger.warn("Got an exception while trying to execute in thread pool: " + t.getMessage());
     }
