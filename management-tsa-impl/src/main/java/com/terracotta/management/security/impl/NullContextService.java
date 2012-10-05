d56ec393c005bfab906d4648067fb0b236dc6e04
Ludovic Orban <lorban@bitronix.be>
2012-10-05 16:15:28 +0000

    added missing license headers
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@20878 7fc7bbf3-cf45-46d4-be06-341739edd864

--- management-tsa-impl/src/main/java/com/terracotta/management/security/impl/NullContextService.java
+++ management-tsa-impl/src/main/java/com/terracotta/management/security/impl/NullContextService.java
@@ -1,3 +1,6 @@
+/*
+ * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
+ */
 package com.terracotta.management.security.impl;
 
 import com.terracotta.management.security.ContextService;
