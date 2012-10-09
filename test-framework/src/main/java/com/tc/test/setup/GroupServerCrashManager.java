0e32c55e77a17fe6c0c454f8feb91700f0e68437
Abhishek Singh <asingh@terracottatech.com>
2012-10-09 18:26:03 +0000

    support initial delay in server crasher
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@20902 7fc7bbf3-cf45-46d4-be06-341739edd864

--- test-framework/src/main/java/com/tc/test/setup/GroupServerCrashManager.java
+++ test-framework/src/main/java/com/tc/test/setup/GroupServerCrashManager.java
@@ -4,77 +4,80 @@
  */
 package com.tc.test.setup;
 
-import java.security.SecureRandom;
-import java.util.ArrayList;
-import java.util.List;
-
 import com.tc.test.config.model.CrashConfig;
 import com.tc.test.config.model.ServerCrashMode;
 import com.tc.test.config.model.TestConfig;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class GroupServerCrashManager implements Runnable {
-  private static boolean           DEBUG      = true;
   private final GroupServerManager serverManager;
-  private int                      crashCount = 0;
+  private int                      crashCount    = 0;
   private volatile boolean         done;
   private final List<Throwable>    errors;
   private final TestConfig         testConfig;
-  private final long               seed;
+  private final SimpleDateFormat   dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss.SSS");
 
   public GroupServerCrashManager(TestConfig testConfig, GroupServerManager groupServerManager) throws Exception {
     this.serverManager = groupServerManager;
     this.testConfig = testConfig;
     this.errors = new ArrayList<Throwable>();
-    SecureRandom srandom = SecureRandom.getInstance("SHA1PRNG");
-    seed = srandom.nextLong();
-    System.out.println("***** Random number generator seed=[" + seed + "]");
-
   }
 
+  @Override
   public void run() {
     if (getCrashConfig().getCrashMode().equals(ServerCrashMode.NO_CRASH)) {
       // Nothing to be done break
       return;
     }
+
+    long delayInSeconds = getCrashConfig().getInitialDelayInSeconds();
+    if (delayInSeconds > 0) {
+      debug("Sleeping for initial delay seconds before starting to crash servers - " + delayInSeconds);
+      sleep(delayInSeconds * 1000);
+    }
+
     while (!done) {
-      try {
-        Thread.sleep(getCrashConfig().getServerCrashWaitTimeInSec() * 1000);
-      } catch (InterruptedException e) {
-        errors.add(e);
-      }
+      sleep(getCrashConfig().getServerCrashWaitTimeInSec() * 1000);
 
       if ((getCrashConfig().getMaxCrashCount() > crashCount) && !done) {
         try {
 
           if (getCrashConfig().getCrashMode().equals(ServerCrashMode.RANDOM_ACTIVE_CRASH)) {
-            debugPrintln("***** ServerCrasher:  about to crash active servers in all groups  threadID=["
-                         + Thread.currentThread().getName() + "]");
+            debug("about to crash active servers in all groups");
             serverManager.crashActiveAndWaitForPassiveToTakeOver();
-            debugPrintln("***** ServerCrasher:  about to restart last crashed servers in all groups threadID=["
-                         + Thread.currentThread().getName() + "]");
+            debug("about to restart last crashed servers in all groups");
             serverManager.restartLastCrashedServer();
           } else if (getCrashConfig().getCrashMode().equals(ServerCrashMode.RANDOM_SERVER_CRASH)) {
-            debugPrintln("***** ServerCrasher:  about to crash server  threadID=[" + Thread.currentThread().getName()
-                         + "]");
+            debug("about to crash server");
             serverManager.crashRandomServer();
-            debugPrintln("***** ServerCrasher:  about to restart crashed server threadID=["
-                         + Thread.currentThread().getName() + "]");
+            debug("about to restart crashed server");
             serverManager.restartLastCrashedServer();
           }
 
           crashCount++;
         } catch (Exception e) {
-          debugPrintln("***** ServerCrasher:  error occured while crashing/restarting server  threadID=["
-                       + Thread.currentThread().getName() + "]");
+          debug("Error occured while crashing/restarting server");
 
           e.printStackTrace();
 
           break;
         }
       } else {
-        debugPrintln("***** ServerCrasher is done: errors[" + errors.size() + "] crashCount[" + crashCount + "]");
+        debug("ServerCrasher is done: errors[" + errors.size() + "] crashCount[" + crashCount + "]");
         break;
       }
+    }
+  }
+
+  private void sleep(long timeMillis) {
+    try {
+      Thread.sleep(timeMillis);
+    } catch (InterruptedException e) {
+      errors.add(e);
     }
   }
 
@@ -82,14 +85,13 @@ public class GroupServerCrashManager implements Runnable {
     return testConfig.getCrashConfig();
   }
 
-  public void debugPrintln(String s) {
-    if (DEBUG) {
-      System.err.println(s);
-    }
-  }
-
   public synchronized void stop() {
     this.done = true;
+    debug("Stopping crasher");
   }
 
+  public void debug(String msg) {
+    System.out.println("[****** ServerCrasher : " + dateFormatter.format(new Date()) + " '"
+                       + Thread.currentThread().getName() + "'] " + msg);
+  }
 }
