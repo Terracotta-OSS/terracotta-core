786c6909bc582530a47996bec10e5d5069c6ee85
Hung Huynh <hhuynh@gmail.com>
2012-10-10 02:49:07 +0000

    not needed
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@20909 7fc7bbf3-cf45-46d4-be06-341739edd864

--- toolkit-runtime/src/scripts/Util.groovy
+++
@@ -1,9 +0,0 @@
-public class Util {
-  static def preparePom(project) {
-    def ant = new AntBuilder()
-    def filteredPom = new File(project.build.directory, "dependency-reduced-pom.xml");
-    ant.replace(file: filteredPom,
-                token: '${toolkit-api-version}',
-                value: project.properties['toolkit-api-version'])
-  }
-}
