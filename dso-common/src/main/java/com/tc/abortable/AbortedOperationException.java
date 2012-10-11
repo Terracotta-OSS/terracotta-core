f67c9d25ff336f21e03d16cfaaac69499c24ca9f
Nihit Purwar <npurwar@terracottatech.com>
2012-10-11 06:53:24 +0000

    add abortable exception in platform
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/branches/nonstop@20917 7fc7bbf3-cf45-46d4-be06-341739edd864

---
+++ dso-common/src/main/java/com/tc/abortable/AbortedOperationException.java
@@ -0,0 +1,8 @@
+/*
+ * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
+ */
+package com.tc.abortable;
+
+public class AbortedOperationException extends Exception {
+  //
+}
