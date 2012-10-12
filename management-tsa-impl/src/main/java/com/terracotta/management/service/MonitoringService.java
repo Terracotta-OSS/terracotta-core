c8af5d09badf4a25619b74fcc497076dbfe791db
Ludovic Orban <lorban@bitronix.be>
2012-10-12 17:19:39 +0000

    added license header
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@20940 7fc7bbf3-cf45-46d4-be06-341739edd864

--- management-tsa-impl/src/main/java/com/terracotta/management/service/MonitoringService.java
+++ management-tsa-impl/src/main/java/com/terracotta/management/service/MonitoringService.java
@@ -1,3 +1,6 @@
+/*
+ * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
+ */
 package com.terracotta.management.service;
 
 import org.terracotta.management.ServiceExecutionException;
