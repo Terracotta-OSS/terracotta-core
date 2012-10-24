1ded88e58009275c31bbb06ec6f2b5f5f4ebb7f3
Tim Wu <tim.wu@terracottatech.com>
2012-10-24 02:18:46 +0000

    Fixed object removal
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/branches/resource-management@21076 7fc7bbf3-cf45-46d4-be06-341739edd864

--- dso-l2/src/main/java/com/tc/objectserver/persistence/ObjectIDSetMaintainer.java
+++ dso-l2/src/main/java/com/tc/objectserver/persistence/ObjectIDSetMaintainer.java
@@ -4,11 +4,7 @@ import org.terracotta.corestorage.KeyValueStorageMutationListener;
 import org.terracotta.corestorage.Retriever;
 
 import com.tc.object.ObjectID;
-import com.tc.objectserver.core.api.ManagedObjectState;
 import com.tc.util.ObjectIDSet;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
 
 /**
  * @author tim
@@ -49,42 +45,9 @@ public class ObjectIDSetMaintainer implements KeyValueStorageMutationListener<Lo
 
   @Override
   public synchronized void removed(Retriever<? extends Long> key, Retriever<? extends byte[]> value) {
-        CollectObjectID oid = new CollectObjectID() {
-
-          @Override
-          public void setObjectID(byte type, ObjectID oid) {
-                if (PersistentCollectionsUtil.isEvictableMapType(type)) {
-                  evictableObjectIDSet.remove(oid);
-                }
-
-                if (PersistentCollectionsUtil.isPersistableCollectionType(type)) {
-                  mapObjectIDSet.remove(oid);
-                }
-                extantObjectIDSet.add(oid);
-          }
-      };
-        
-        extractType(value, oid);
-  }
-  
-  interface CollectObjectID {
-      void setObjectID(byte type, ObjectID oid);
-  }
-  
-  private byte extractType(Retriever<? extends byte[]> value,CollectObjectID collector) {
-    try {
-        ObjectInputStream array = new ObjectInputStream(new ByteArrayInputStream(value.retrieve()));
-        long version = array.readLong();
-        ObjectID id = new ObjectID(array.readLong());
-        
-        byte type = array.readByte();
-        if (type <= 0 && type >= ManagedObjectState.MAX_TYPE) {
-            throw new AssertionError("unknown object type in stream: " + Byte.toString(type));
-        }
-        
-        return type;
-    } catch ( IOException ioe ) {
-        throw new RuntimeException("unknown type");
-    }
+    ObjectID oid = new ObjectID(key.retrieve());
+    evictableObjectIDSet.remove(oid);
+    mapObjectIDSet.remove(oid);
+    extantObjectIDSet.remove(oid);
   }
 }
