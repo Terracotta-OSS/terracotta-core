c37ac5374c758d202f905a550fe14cb0e7979f6a
Nihit Purwar <npurwar@terracottatech.com>
2012-11-02 09:51:41 +0000

    non-stop toolkit registry impl
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/branches/nonstop@21204 7fc7bbf3-cf45-46d4-be06-341739edd864

--- toolkit-impl/src/main/java/com/terracotta/toolkit/nonstop/NonStopConfigRegistryImpl.java
+++ toolkit-impl/src/main/java/com/terracotta/toolkit/nonstop/NonStopConfigRegistryImpl.java
@@ -7,71 +7,148 @@ import org.terracotta.toolkit.ToolkitObjectType;
 import org.terracotta.toolkit.nonstop.NonStopConfig;
 import org.terracotta.toolkit.nonstop.NonStopConfigRegistry;
 
-import com.tc.exception.ImplementMe;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 public class NonStopConfigRegistryImpl implements NonStopConfigRegistry {
+  private final ConcurrentMap<NonStopConfigKey, NonStopConfig> allConfigs = new ConcurrentHashMap<NonStopConfigKey, NonStopConfig>();
+
   @Override
   public void registerForType(NonStopConfig config, ToolkitObjectType... types) {
-    throw new ImplementMe();
-
+    for (ToolkitObjectType type : types) {
+      allConfigs.put(new NonStopConfigKey(null, type, null), config);
+    }
   }
 
   @Override
-  public void registerForInstance(NonStopConfig config, String toolkitTypeName, ToolkitObjectType... type) {
-    throw new ImplementMe();
-
+  public void registerForInstance(NonStopConfig config, String toolkitTypeName, ToolkitObjectType... types) {
+    for (ToolkitObjectType type : types) {
+      allConfigs.put(new NonStopConfigKey(null, type, toolkitTypeName), config);
+    }
   }
 
   @Override
-  public void registerForTypeMethod(NonStopConfig config, String methodName, ToolkitObjectType... type) {
-    throw new ImplementMe();
-
+  public void registerForTypeMethod(NonStopConfig config, String methodName, ToolkitObjectType... types) {
+    for (ToolkitObjectType type : types) {
+      allConfigs.put(new NonStopConfigKey(methodName, type, null), config);
+    }
   }
 
   @Override
   public void registerForInstanceMethod(NonStopConfig config, String methodName, String toolkitTypeName,
-                                        ToolkitObjectType... type) {
-    throw new ImplementMe();
-
+                                        ToolkitObjectType... types) {
+    for (ToolkitObjectType type : types) {
+      allConfigs.put(new NonStopConfigKey(methodName, type, toolkitTypeName), config);
+    }
   }
 
   @Override
   public NonStopConfig getConfigForType(ToolkitObjectType type) {
-    throw new ImplementMe();
+    NonStopConfigKey nonStopConfigKey = new NonStopConfigKey(null, type, null);
+    // TODO: instead of returning null should we return a default config
+    return allConfigs.get(nonStopConfigKey);
   }
 
   @Override
   public NonStopConfig getConfigForInstance(String toolkitTypeName, ToolkitObjectType type) {
-    throw new ImplementMe();
+    NonStopConfigKey nonStopConfigKey = new NonStopConfigKey(null, type, toolkitTypeName);
+    NonStopConfig nonStopConfig = allConfigs.get(nonStopConfigKey);
+    if (nonStopConfig == null) {
+      nonStopConfig = getConfigForType(type);
+    }
+    return nonStopConfig;
   }
 
   @Override
   public NonStopConfig getConfigForTypeMethod(String methodName, ToolkitObjectType type) {
-    throw new ImplementMe();
+    NonStopConfigKey nonStopConfigKey = new NonStopConfigKey(methodName, type, null);
+    NonStopConfig nonStopConfig = allConfigs.get(nonStopConfigKey);
+    if (nonStopConfig == null) {
+      nonStopConfig = getConfigForType(type);
+    }
+    return nonStopConfig;
   }
 
   @Override
   public NonStopConfig getConfigForInstanceMethod(String methodName, String toolkitTypeName, ToolkitObjectType type) {
-    throw new ImplementMe();
+    NonStopConfigKey nonStopConfigKey = new NonStopConfigKey(methodName, type, toolkitTypeName);
+    NonStopConfig nonStopConfig = allConfigs.get(nonStopConfigKey);
+
+    // try method & type
+    if (nonStopConfig == null) {
+      nonStopConfigKey = new NonStopConfigKey(methodName, type, null);
+      nonStopConfig = allConfigs.get(nonStopConfigKey);
+    }
+
+    // try for instance
+    if (nonStopConfig == null) {
+      nonStopConfig = getConfigForInstance(toolkitTypeName, type);
+    }
+
+    return nonStopConfig;
   }
 
   @Override
   public NonStopConfig deregisterForType(ToolkitObjectType type) {
-    throw new ImplementMe();
+    NonStopConfigKey nonStopConfigKey = new NonStopConfigKey(null, type, null);
+    return allConfigs.remove(nonStopConfigKey);
   }
 
   @Override
   public NonStopConfig deregisterForInstance(String toolkitTypeName, ToolkitObjectType type) {
-    throw new ImplementMe();
+    NonStopConfigKey nonStopConfigKey = new NonStopConfigKey(null, type, toolkitTypeName);
+    return allConfigs.remove(nonStopConfigKey);
   }
 
   @Override
   public NonStopConfig deregisterForTypeMethod(String methodName, ToolkitObjectType type) {
-    throw new ImplementMe();
+    NonStopConfigKey nonStopConfigKey = new NonStopConfigKey(methodName, type, null);
+    return allConfigs.remove(nonStopConfigKey);
   }
 
   @Override
   public NonStopConfig deregisterForInstanceMethod(String methodName, String toolkitTypeName, ToolkitObjectType type) {
-    throw new ImplementMe();
+    NonStopConfigKey nonStopConfigKey = new NonStopConfigKey(methodName, type, toolkitTypeName);
+    return allConfigs.remove(nonStopConfigKey);
   }
+
+  private static class NonStopConfigKey {
+    private final String            method;
+    private final String            name;
+    private final ToolkitObjectType objectType;
+
+    public NonStopConfigKey(String method, ToolkitObjectType objectType, String name) {
+      this.method = method;
+      this.name = name;
+      this.objectType = objectType;
+    }
+
+    @Override
+    public int hashCode() {
+      final int prime = 31;
+      int result = 1;
+      result = prime * result + ((method == null) ? 0 : method.hashCode());
+      result = prime * result + ((name == null) ? 0 : name.hashCode());
+      result = prime * result + ((objectType == null) ? 0 : objectType.hashCode());
+      return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if (this == obj) return true;
+      if (obj == null) return false;
+      if (getClass() != obj.getClass()) return false;
+      NonStopConfigKey other = (NonStopConfigKey) obj;
+      if (method == null) {
+        if (other.method != null) return false;
+      } else if (!method.equals(other.method)) return false;
+      if (name == null) {
+        if (other.name != null) return false;
+      } else if (!name.equals(other.name)) return false;
+      if (objectType != other.objectType) return false;
+      return true;
+    }
+
+  }
+
 }
