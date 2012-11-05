fde3d82fca16b2974af5f4a76cf3993a4b4617e2
Nishant Bangarwa <nbangarw@terracottatech.com>
2012-11-05 14:00:55 +0000

    pvt nonstop compilation fix + timeout behaviors
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/branches/nonstop@21223 7fc7bbf3-cf45-46d4-be06-341739edd864

--- toolkit-impl/src/main/java/com/terracotta/toolkit/nonstop/NonstopTimeoutBehaviorResolver.java
+++ toolkit-impl/src/main/java/com/terracotta/toolkit/nonstop/NonstopTimeoutBehaviorResolver.java
@@ -3,7 +3,100 @@
  */
 package com.terracotta.toolkit.nonstop;
 
-public interface NonstopTimeoutBehaviorResolver<T> {
+import org.terracotta.toolkit.ToolkitObjectType;
+import org.terracotta.toolkit.internal.cache.ToolkitCacheInternal;
+import org.terracotta.toolkit.nonstop.NonStopConfigFields.NonStopTimeoutBehavior;
 
-  T resolveTimeoutBehavior();
+import com.terracotta.toolkit.collections.map.LocalReadsToolkitCacheImpl;
+
+import java.lang.reflect.Proxy;
+
+public class NonstopTimeoutBehaviorResolver {
+
+  private final NoOpInvocationHandler               noOpInvocationHandler               = new NoOpInvocationHandler();
+  private final ExceptionOnTimeoutInvocationHandler exceptionOnTimeoutInvocationHandler = new ExceptionOnTimeoutInvocationHandler();
+
+  public <E> E create(ToolkitObjectType objectType, NonStopTimeoutBehavior nonStopBehavior, E e) {
+    switch (nonStopBehavior) {
+      case EXCEPTION_ON_TIMEOUT:
+        return createExceptionOnTimeOutBehaviour(objectType, e);
+      case NO_OP:
+        return createNoOpBehaviour(objectType, e);
+      case EXCEPTION_ON_MUTATE_AND_LOCAL_READS:
+        return createLocalReadsBehaviour(objectType, e, createExceptionOnTimeOutBehaviour(objectType, e));
+      case LOCAL_READS:
+        return createLocalReadsBehaviour(objectType, e, createNoOpBehaviour(objectType, e));
+    }
+    return null;
+  }
+
+  private <E> E createNoOpBehaviour(ToolkitObjectType objectType, E delegate) {
+    switch (objectType) {
+      case CACHE:
+      case STORE:
+        return (E) Proxy.newProxyInstance(this.getClass().getClassLoader(), new Class[] { delegate.getClass() },
+                                          noOpInvocationHandler);
+      case ATOMIC_LONG:
+      case BARRIER:
+      case BLOCKING_QUEUE:
+      case LIST:
+      case LOCK:
+      case MAP:
+      case NOTIFIER:
+      case READ_WRITE_LOCK:
+      case SET:
+      case SORTED_MAP:
+      case SORTED_SET:
+        throw new UnsupportedOperationException("NonStop Not Supported for ToolkitType " + objectType);
+    }
+    return null;
+
+  }
+
+  private <E> E createExceptionOnTimeOutBehaviour(ToolkitObjectType objectType, E delegate) {
+
+    switch (objectType) {
+      case CACHE:
+      case STORE:
+        return (E) Proxy.newProxyInstance(this.getClass().getClassLoader(), new Class[] { delegate.getClass() },
+                                          exceptionOnTimeoutInvocationHandler);
+      case ATOMIC_LONG:
+      case BARRIER:
+      case BLOCKING_QUEUE:
+      case LIST:
+      case LOCK:
+      case MAP:
+      case NOTIFIER:
+      case READ_WRITE_LOCK:
+      case SET:
+      case SORTED_MAP:
+      case SORTED_SET:
+        throw new UnsupportedOperationException("NonStop Not Supported for ToolkitType " + objectType);
+    }
+    return null;
+
+  }
+
+  private <E> E createLocalReadsBehaviour(ToolkitObjectType objectType, E delegate, E mutationBehaviour) {
+
+    switch (objectType) {
+      case CACHE:
+      case STORE:
+        return (E) new LocalReadsToolkitCacheImpl((ToolkitCacheInternal) delegate,
+                                                  (ToolkitCacheInternal) mutationBehaviour);
+      case ATOMIC_LONG:
+      case BARRIER:
+      case BLOCKING_QUEUE:
+      case LIST:
+      case LOCK:
+      case MAP:
+      case NOTIFIER:
+      case READ_WRITE_LOCK:
+      case SET:
+      case SORTED_MAP:
+      case SORTED_SET:
+        throw new UnsupportedOperationException("NonStop Not Supported for ToolkitType " + objectType);
+    }
+    return null;
+  }
 }
