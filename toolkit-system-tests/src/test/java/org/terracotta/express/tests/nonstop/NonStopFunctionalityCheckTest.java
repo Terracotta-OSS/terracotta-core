67ae47b489e9a27234b733f5772d0271df1a3bba
Nihit Purwar <npurwar@terracottatech.com>
2012-11-08 09:54:47 +0000

    modify test a bit
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/branches/nonstop@21249 7fc7bbf3-cf45-46d4-be06-341739edd864

--- toolkit-system-tests/src/test/java/org/terracotta/express/tests/nonstop/NonStopFunctionalityCheckTest.java
+++ toolkit-system-tests/src/test/java/org/terracotta/express/tests/nonstop/NonStopFunctionalityCheckTest.java
@@ -26,9 +26,10 @@ public class NonStopFunctionalityCheckTest extends AbstractToolkitTestBase {
   }
 
   public static class NonStopFunctionalityCheckTestClient extends NonStopClientBase {
-    private static final int CLIENT_COUNT           = 2;
-    private static final int NUMBER_OF_ELEMENTS     = 10;
-    private static final int MAX_ENTRIES_LOCAL_HEAP = 500;
+    private static final int  CLIENT_COUNT            = 2;
+    private static final int  NUMBER_OF_ELEMENTS      = 10;
+    private static final int  MAX_ENTRIES_LOCAL_HEAP  = 500;
+    private static final long NON_STOP_TIMEOUT_MILLIS = 10000;
 
     public NonStopFunctionalityCheckTestClient(String[] args) {
       super(args);
@@ -56,18 +57,29 @@ public class NonStopFunctionalityCheckTest extends AbstractToolkitTestBase {
 
       if (index == 1) {
         for (int i = 0; i < NUMBER_OF_ELEMENTS / 2; i++) {
-          Assert.assertNotNull(cache.get(i));
+          long time = System.currentTimeMillis();
+          try {
+            Assert.assertNotNull(cache.get(i));
+          } catch (NonStopException e) {
+            System.err.println("Time elapsed " + (System.currentTimeMillis() - time) + " , i=" + i);
+            throw e;
+          }
         }
 
         makeServerDie();
 
         for (int i = NUMBER_OF_ELEMENTS / 2; i < NUMBER_OF_ELEMENTS; i++) {
           boolean exceptionOccurred = false;
+          long time = System.currentTimeMillis();
           try {
             cache.get(i);
           } catch (NonStopException e) {
+            time = System.currentTimeMillis() - time;
+
+            Assert.assertTrue((time > (NON_STOP_TIMEOUT_MILLIS - 500)) && (time < (NON_STOP_TIMEOUT_MILLIS + 2000)));
+            System.err.println("Time consumed " + time);
+
             exceptionOccurred = true;
-            e.printStackTrace();
           }
 
           Assert.assertTrue(exceptionOccurred);
@@ -88,7 +100,7 @@ public class NonStopFunctionalityCheckTest extends AbstractToolkitTestBase {
     private ToolkitCache createCache(Toolkit toolkit) {
       String cacheName = "test-cache";
 
-      new NonStopConfigBuilder().timeoutMillis(5000).apply(toolkit);
+      new NonStopConfigBuilder().timeoutMillis(NON_STOP_TIMEOUT_MILLIS).apply(toolkit);
 
       ToolkitCacheConfigBuilder builder = new ToolkitCacheConfigBuilder();
       builder.maxCountLocalHeap(MAX_ENTRIES_LOCAL_HEAP);
