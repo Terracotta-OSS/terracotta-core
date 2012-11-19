accb55e132bed1cd07f019e31fb2f2bcacba621e
Tim Wu <tim.wu@terracottatech.com>
2012-11-19 23:42:51 +0000

    Update to using close() instead of shutdown().
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@21360 7fc7bbf3-cf45-46d4-be06-341739edd864

--- dso-l2/src/main/java/com/tc/objectserver/persistence/Persistor.java
+++ dso-l2/src/main/java/com/tc/objectserver/persistence/Persistor.java
@@ -75,7 +75,7 @@ public class Persistor {
 
   public void close() {
     checkStarted();
-    storageManager.shutdown();
+    storageManager.close();
   }
   
   public MonitoredResource getMonitoredResource() {
