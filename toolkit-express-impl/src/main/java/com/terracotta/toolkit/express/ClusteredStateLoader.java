b6e1951c8f4bab69face234bb8128d3fe26aa144
Hung Huynh <hhuynh@gmail.com>
2012-11-21 19:13:31 +0000

    devmode
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@21407 7fc7bbf3-cf45-46d4-be06-341739edd864

--- toolkit-express-impl/src/main/java/com/terracotta/toolkit/express/ClusteredStateLoader.java
+++ toolkit-express-impl/src/main/java/com/terracotta/toolkit/express/ClusteredStateLoader.java
@@ -5,10 +5,15 @@ package com.terracotta.toolkit.express;
 
 import com.terracotta.toolkit.express.loader.Util;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
+import java.net.URLClassLoader;
 import java.security.SecureClassLoader;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -29,11 +34,13 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 class ClusteredStateLoader extends SecureClassLoader {
   private static final boolean      USE_APP_JTA_CLASSES;
-  private static final String       PRIVATE_CLASS_SUFFIX = ".class_terracotta";
+  private static final String       PRIVATE_CLASS_SUFFIX     = ".class_terracotta";
+  private static final boolean      DEV_MODE                 = Boolean.getBoolean("com.tc.test.toolkit.devmode");
 
   private final ClassLoader         appLoader;
-  private final Map<String, byte[]> extraClasses         = new ConcurrentHashMap<String, byte[]>();
+  private final Map<String, byte[]> extraClasses             = new ConcurrentHashMap<String, byte[]>();
   private final List<String>        embeddedResourcePrefixes;
+  private final ClassLoader         devModeUrlClassLoader;
 
   static {
     String prop = System.getProperty(ClusteredStateLoader.class.getName() + ".USE_APP_JTA_CLASSES", "true");
@@ -45,15 +52,27 @@ class ClusteredStateLoader extends SecureClassLoader {
     super(null);
     this.appLoader = appLoader;
     this.embeddedResourcePrefixes = prefixes;
+    if (DEV_MODE) {
+      System.out.println("XXX TOOLKIT DEVMODE CLASS LOADING IS APPLIED.");
+      devModeUrlClassLoader = URLClassLoader.newInstance(generateDevModeUrls(), null);
+    } else {
+      devModeUrlClassLoader = null;
+    }
   }
 
   void addExtraClass(String name, byte[] classBytes) {
     extraClasses.put(name, classBytes);
   }
 
+  private URL loadDevModeClassAsResource(String name) {
+    if (name.startsWith("java") || name.startsWith("sun")) { return null; }
+    String resource = name.replace('.', '/') + ".class";
+    return devModeUrlClassLoader.getResource(resource);
+  }
+
   @Override
   public InputStream getResourceAsStream(String name) {
-    URL resource = findResourceWithPrefix(name);
+    URL resource = DEV_MODE ? devModeUrlClassLoader.getResource(name) : findResourceWithPrefix(name);
     if (resource != null) {
       try {
         return resource.openStream();
@@ -68,7 +87,7 @@ class ClusteredStateLoader extends SecureClassLoader {
 
   @Override
   public URL getResource(String name) {
-    URL resource = findResourceWithPrefix(name);
+    URL resource = DEV_MODE ? devModeUrlClassLoader.getResource(name) : findResourceWithPrefix(name);
     if (resource != null) { return resource; }
     resource = super.getResource(name);
     if (resource != null) { return resource; }
@@ -106,9 +125,8 @@ class ClusteredStateLoader extends SecureClassLoader {
       }
     }
 
-    // try loading class with prefixes, it could come from embedded jars
-    URL url = findClassWithPrefix(name);
-    if (url != null) { return loadClassFromPrefixResource(name, url); }
+    URL url = DEV_MODE ? loadDevModeClassAsResource(name) : findClassWithPrefix(name);
+    if (url != null) { return loadClassFromUrl(name, url); }
 
     // last path is to delegate to the app loader and finally to the thread context loader
     // A case where final fallback to the thread context is relevant is when the
@@ -123,7 +141,6 @@ class ClusteredStateLoader extends SecureClassLoader {
         //
         return contextClassLoader.loadClass(name);
       }
-
       throw cnfe;
     }
   }
@@ -147,7 +164,7 @@ class ClusteredStateLoader extends SecureClassLoader {
     return null;
   }
 
-  private Class<?> loadClassFromPrefixResource(String name, URL url) {
+  private Class<?> loadClassFromUrl(String name, URL url) {
     String packageName = name.substring(0, name.lastIndexOf('.'));
     if (getPackage(packageName) == null) {
       definePackage(packageName, null, null, null, null, null, null, null);
@@ -167,4 +184,30 @@ class ClusteredStateLoader extends SecureClassLoader {
     return defineClass(name, bytes, 0, bytes.length);
   }
 
+  private static URL[] generateDevModeUrls() {
+    List<URL> urlList = new ArrayList<URL>();
+    BufferedReader reader = null;
+    try {
+      reader = new BufferedReader(new InputStreamReader(
+                                                        ClusteredStateLoader.class
+                                                            .getResourceAsStream("/devmode-embedded-classpath.txt")));
+      String line = null;
+      while ((line = reader.readLine()) != null) {
+        line = line.trim();
+        if (line.length() == 0) continue;
+        for (String part : line.split(File.pathSeparator)) {
+          String path = part.trim();
+          if (path.length() == 0) continue;
+          File file = new File(path);
+          urlList.add(file.toURI().toURL());
+        }
+      }
+      System.out.println("XXX devmode classpath: " + urlList);
+      return urlList.toArray(new URL[0]);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    } finally {
+      Util.closeQuietly(reader);
+    }
+  }
 }
