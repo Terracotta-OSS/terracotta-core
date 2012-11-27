3f9a26213029bf7cca771936df0ade8c2a2fae22
Vitaliy Funshteyn <vfunshte@terracottatech.com>
2012-11-27 04:19:40 +0000

    Hmm... I thought setting election window time was supposed to work.
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@21465 7fc7bbf3-cf45-46d4-be06-341739edd864

--- test-framework/src/main/java/com/tc/config/test/schema/ConfigHelper.java
+++ test-framework/src/main/java/com/tc/config/test/schema/ConfigHelper.java
@@ -2,6 +2,7 @@ package com.tc.config.test.schema;
 
 import org.apache.commons.io.IOUtils;
 
+import com.tc.test.config.model.GroupConfig;
 import com.tc.test.config.model.L2Config;
 import com.tc.test.config.model.TestConfig;
 import com.tc.test.setup.GroupsData;
@@ -24,7 +25,7 @@ public class ConfigHelper {
   private final int           numOfGroups;
   private final int           numOfServersPerGroup;
   private final File          tcConfigFile;
-  private final File          temmDir;
+  private final File          tempDir;
   private final GroupsData[]  groupData;
 
   public ConfigHelper(final PortChooser portChooser, final TestConfig testConfig, final File tcConfigFile, File tempDir) {
@@ -34,7 +35,7 @@ public class ConfigHelper {
     numOfServersPerGroup = testConfig.getGroupConfig().getMemberCount();
     groupData = new GroupsData[numOfGroups];
     this.tcConfigFile = tcConfigFile;
-    this.temmDir = tempDir;
+    this.tempDir = tempDir;
     setServerPorts();
   }
 
@@ -48,7 +49,7 @@ public class ConfigHelper {
     numOfGroups = testConfig.getNumOfGroups();
     numOfServersPerGroup = testConfig.getGroupConfig().getMemberCount();
     this.tcConfigFile = tcConfigFile;
-    this.temmDir = tempDir;
+    this.tempDir = tempDir;
     portChooser = new PortChooser();
   }
 
@@ -117,6 +118,12 @@ public class ConfigHelper {
     l2sConfig.setL2s(l2s);
     l2sConfig.setGroups(groups);
 
+    HaConfigBuilder ha = new HaConfigBuilder(3);
+    GroupConfig gc = testConfig.getGroupConfig();
+    ha.setMode(HaConfigBuilder.HA_MODE_NETWORKED_ACTIVE_PASSIVE);
+    ha.setElectionTime(String.valueOf(gc.getElectionTime()));
+    l2sConfig.setHa(ha);
+
     return l2sConfig;
   }
 
@@ -177,8 +184,8 @@ public class ConfigHelper {
         l2GroupPorts[serverIndex] = portNum + 2;
         String serverName = SERVER_NAME + (groupIndex * numOfServersPerGroup + serverIndex);
         serverNames[serverIndex] = serverName;
-        dataDirectoryPath[serverIndex] = new File(temmDir, serverName + File.separator + "data").getAbsolutePath();
-        logDirectoryPath[serverIndex] = new File(temmDir, serverName).getAbsolutePath();
+        dataDirectoryPath[serverIndex] = new File(tempDir, serverName + File.separator + "data").getAbsolutePath();
+        logDirectoryPath[serverIndex] = new File(tempDir, serverName).getAbsolutePath();
       }
       if (isProxyDsoPort()) {
         proxyDsoPorts = new int[numOfServersPerGroup];
@@ -259,7 +266,7 @@ public class ConfigHelper {
   }
 
   private String getStatisticsDirectoryPath(int groupIndex, int serverIndex) {
-    return new File(temmDir, getServerName(groupIndex, serverIndex) + File.separator + "statistics").getAbsolutePath();
+    return new File(tempDir, getServerName(groupIndex, serverIndex) + File.separator + "statistics").getAbsolutePath();
   }
 
 }
