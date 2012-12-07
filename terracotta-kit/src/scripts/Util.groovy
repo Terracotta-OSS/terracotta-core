97447ce6a78c122c95253720e63321f2337c7d96
Gary Keim <gkeim@terracottatech.com>
2012-12-07 02:00:45 +0000

    BDB is gone so don't process the GT kit to remove it. Also cleaned up Util.groovy to remove processor and old Eclipse plugin cruft.
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@21684 7fc7bbf3-cf45-46d4-be06-341739edd864

--- terracotta-kit/src/scripts/Util.groovy
+++ terracotta-kit/src/scripts/Util.groovy
@@ -39,37 +39,6 @@ class Util {
       }
     }                  
     
-    static void createFeatureJar(project) {
-      def rootDir = project.properties['rootDir']
-      def featureJar = "org.terracotta.dso_" + project.properties['eclipse.plugin.version']
-      def destFile = rootDir + "/update/features/" + featureJar + ".jar" 
-      def baseDir =  rootDir + "/update/features"
-      ant.jar(destfile: destFile, basedir: baseDir)
-      ant.delete(file: rootDir + "/update/features/feature.xml")
-    }
-    
-    static void createPluginJar(project) {
-      def rootDir = project.properties['rootDir']
-      def pluginJar = "org.terracotta.dso_" + project.properties['eclipse.plugin.version']
-      def destFile = rootDir + "/update/plugins/" + pluginJar + ".jar" 
-      def baseDir =  rootDir + "/update/plugins"
-      def manifestFile = baseDir + "/META-INF/MANIFEST.MF"
-      // generate classpath from lib folder
-      def bundleClasspath = new StringBuilder()
-      def libs = new File(baseDir, "lib").listFiles()
-      for (def file : libs) {
-        bundleClasspath.append("lib/").append(file.getName()).append(",")
-      }
-      if (bundleClasspath.length() > 0) {
-        bundleClasspath.deleteCharAt(bundleClasspath.length() - 1)
-      }
-      ant.replace(file: manifestFile, token: "@BUNDLE-CLASSPATH@", value: bundleClasspath.toString())
-      ant.jar(destfile: destFile, basedir: baseDir, manifest: manifestFile)
-      ant.delete(includeEmptyDirs: "true") {
-        ant.fileset(dir: baseDir, excludes: pluginJar + ".jar")
-      }
-    }
-    
     static void processTerracottaJar(project, tcJar, targetDir) {
       def rootDir = project.properties['rootDir']
       def destFile = rootDir + "/" + targetDir + "/tc.jar"
@@ -87,41 +56,6 @@ class Util {
                 includes: "**/*.sh,**/*.bat,**/*.exe,**/bin/**,**/lib/**")
     }
     
-    static void setEclipsePluginVersion(project, revision, timestamp) {
-      // Eclipse plugin versioning: major.minor.service.qualifier
-      def m = project.version =~ /(\d+)\.(\d+)\.(\d+)(.*)/
-      if (m.matches()) {
-        def major = m[0][1]
-        def minor = m[0][2]
-        def service = m[0][3]
-        def tag = m[0][4] != null ? m[0][4] : ""
-        tag = tag.replace("-", ".")
-        def qualifier = "r" + revision + "_v" + timestamp + tag
-        def pluginVersion = major + "." + minor + "." + service + "." + qualifier
-        project.properties.setProperty('eclipse.plugin.version', pluginVersion)
-      } else {
-        throw new RuntimeException("version doesn't match Eclipse plugin pattern")
-      }
-    }  
-
-    static void processEnterpriseGtKit(project) {
-      def rootDir = project.properties['rootDir']
-      def binDir = new File(rootDir, "bin")
-      def libDir = new File(rootDir, "lib")
-      ant.delete(verbose: "true") {
-        fileset(dir: libDir, includes: "je-*.jar")
-      }
-      ant.replace(file: new File(binDir, "start-tc-server.sh"),
-                  token: "USE_DERBYDB=false",
-                  value: "USE_DERBYDB=true",
-                  failOnNoReplacements: "true")
-      ant.replace(file: new File(binDir, "start-tc-server.bat"),
-                  token: "set USE_DERBYDB=\"false\"",
-                  value: "set USE_DERBYDB=\"true\"",
-                  failOnNoReplacements: "true")
-                  
-    }    
-
     static final String BUILD_DATA_FILE_NAME = "build-data.txt"
     static final String PATCH_DATA_FILE_NAME = "patch-data.txt"
     static final String RESOURCES_DIR = "lib/resources"
