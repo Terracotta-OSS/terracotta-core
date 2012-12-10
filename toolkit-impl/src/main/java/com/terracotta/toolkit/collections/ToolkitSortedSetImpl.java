c70fd3e7a5b00b55472e371b015c9c573934b589
Sachin Sabbarwal <ssabbarw@terracottatech.com>
2012-12-10 14:00:58 +0000

    Rejoin: making sure that toolkit sorted set's subtypes won't work after rejoin
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@21718 7fc7bbf3-cf45-46d4-be06-341739edd864

--- toolkit-impl/src/main/java/com/terracotta/toolkit/collections/ToolkitSortedSetImpl.java
+++ toolkit-impl/src/main/java/com/terracotta/toolkit/collections/ToolkitSortedSetImpl.java
@@ -4,6 +4,7 @@
 package com.terracotta.toolkit.collections;
 
 import org.terracotta.toolkit.collections.ToolkitSortedSet;
+import org.terracotta.toolkit.rejoin.RejoinException;
 
 import java.util.Collection;
 import java.util.Comparator;
@@ -15,6 +16,7 @@ import java.util.concurrent.locks.ReadWriteLock;
 public class ToolkitSortedSetImpl<E extends Comparable<? super E>> extends ToolkitSetImpl<E> implements
     ToolkitSortedSet<E> {
   private final DestroyableToolkitSortedMap<E, Integer> sortedMap;
+  private int                                           currentRejoinCount;
 
   public ToolkitSortedSetImpl(DestroyableToolkitSortedMap<E, Integer> sortedMap) {
     super(sortedMap);
@@ -54,68 +56,87 @@ public class ToolkitSortedSetImpl<E extends Comparable<? super E>> extends Toolk
 
   private class SubTerracottaSortedSet implements SortedSet<E> {
     private final SortedMap<E, Integer> internalSortedMap;
+    private final int                   rejoinCount;
 
     public SubTerracottaSortedSet(SortedMap<E, Integer> internalSortedMap) {
       this.internalSortedMap = internalSortedMap;
+      this.rejoinCount = ToolkitSortedSetImpl.this.currentRejoinCount;
     }
 
     @Override
     public E first() {
+      exceptionIfRejoinOccured();
       return internalSortedMap.firstKey();
+    }
+
+    private void exceptionIfRejoinOccured() {
+      if (this.rejoinCount != ToolkitSortedSetImpl.this.currentRejoinCount) { throw new RejoinException(
+                                                                                                        "SubSet is not usable once rejoin has occured"); }
     }
 
     @Override
     public E last() {
+      exceptionIfRejoinOccured();
       return internalSortedMap.lastKey();
     }
 
     @Override
     public int size() {
+      exceptionIfRejoinOccured();
       return internalSortedMap.size();
     }
 
     @Override
     public boolean isEmpty() {
+      exceptionIfRejoinOccured();
       return internalSortedMap.isEmpty();
     }
 
     @Override
     public boolean contains(Object o) {
+      exceptionIfRejoinOccured();
       return internalSortedMap.keySet().contains(o);
     }
 
     @Override
     public Iterator<E> iterator() {
+      exceptionIfRejoinOccured();
       return internalSortedMap.keySet().iterator();
     }
 
     @Override
     public Object[] toArray() {
+      exceptionIfRejoinOccured();
       return internalSortedMap.keySet().toArray();
     }
 
     @Override
     public <T> T[] toArray(T[] a) {
+      exceptionIfRejoinOccured();
       return internalSortedMap.keySet().toArray(a);
     }
 
     @Override
     public boolean containsAll(Collection<?> c) {
+      exceptionIfRejoinOccured();
       return internalSortedMap.keySet().containsAll(c);
     }
 
     @Override
     public boolean add(E e) {
+      exceptionIfRejoinOccured();
       return internalSortedMap.put(e, DUMMY_VALUE) == null;
     }
 
     @Override
     public boolean remove(Object o) {
+      exceptionIfRejoinOccured();
       return internalSortedMap.keySet().remove(o);
     }
 
     @Override
     public boolean addAll(Collection<? extends E> c) {
+      exceptionIfRejoinOccured();
       ReadWriteLock lock = getReadWriteLock();
       lock.writeLock().lock();
 
@@ -133,36 +154,43 @@ public class ToolkitSortedSetImpl<E extends Comparable<? super E>> extends Toolk
 
     @Override
     public boolean retainAll(Collection<?> c) {
+      exceptionIfRejoinOccured();
       return internalSortedMap.keySet().retainAll(c);
     }
 
     @Override
     public boolean removeAll(Collection<?> c) {
+      exceptionIfRejoinOccured();
       return internalSortedMap.keySet().removeAll(c);
     }
 
     @Override
     public void clear() {
+      exceptionIfRejoinOccured();
       internalSortedMap.clear();
     }
 
     @Override
     public Comparator<? super E> comparator() {
+      exceptionIfRejoinOccured();
       return ToolkitSortedSetImpl.this.comparator();
     }
 
     @Override
     public SortedSet<E> subSet(E from, E to) {
+      exceptionIfRejoinOccured();
       return new SubTerracottaSortedSet(internalSortedMap.subMap(from, to));
     }
 
     @Override
     public SortedSet<E> headSet(E to) {
+      exceptionIfRejoinOccured();
       return new SubTerracottaSortedSet(internalSortedMap.headMap(to));
     }
 
     @Override
     public SortedSet<E> tailSet(E from) {
+      exceptionIfRejoinOccured();
       return new SubTerracottaSortedSet(internalSortedMap.tailMap(from));
     }
   }
