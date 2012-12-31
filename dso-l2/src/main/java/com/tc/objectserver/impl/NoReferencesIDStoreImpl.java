0ad520faf3a166c074808ccba45a027b8738e0f6
Myron Scott <mscott@terracottatech.com>
2012-12-31 22:28:56 +0000

    reference change does not work with restartable.  backout for now
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@21882 7fc7bbf3-cf45-46d4-be06-341739edd864

--- dso-l2/src/main/java/com/tc/objectserver/impl/NoReferencesIDStoreImpl.java
+++ dso-l2/src/main/java/com/tc/objectserver/impl/NoReferencesIDStoreImpl.java
@@ -26,44 +26,33 @@ public class NoReferencesIDStoreImpl implements NoReferencesIDStore {
     }
   }
 
-    @Override
   public void addToNoReferences(final ManagedObject mo) {
     this.delegate.addToNoReferences(mo);
   }
 
-    @Override
   public void clearFromNoReferencesStore(final ObjectID id) {
     this.delegate.clearFromNoReferencesStore(id);
   }
 
-    @Override
   public boolean hasNoReferences(final ObjectID id) {
     return this.delegate.hasNoReferences(id);
   }
 
   private static class OidSetStore implements NoReferencesIDStore {
+    private final StripedObjectIDSet store = new StripedObjectIDSet();
 
-    private final StripedObjectIDSet store = new StripedObjectIDSet(1);
-
-      @Override
     public void addToNoReferences(final ManagedObject mo) {
-/**
- * only track objects that have references.  Objects that disappear have no references 
- * so there is no need to know that an object exists and has no references.
- */
-        if (!mo.getManagedObjectState().hasNoReferences()) {
+      if (mo.getManagedObjectState().hasNoReferences()) {
         this.store.add(mo.getID());
       }
     }
 
-      @Override
     public void clearFromNoReferencesStore(final ObjectID id) {
       this.store.remove(id);
     }
 
-      @Override
     public boolean hasNoReferences(final ObjectID id) {
-      return !this.store.contains(id);
+      return this.store.contains(id);
     }
 
   }
