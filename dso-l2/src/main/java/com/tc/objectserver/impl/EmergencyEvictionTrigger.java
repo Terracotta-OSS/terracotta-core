5fd0a44367367e92439b9fca1f2f957c6e35f4d3
Myron Scott <mscott@terracottatech.com>
2013-01-04 22:40:59 +0000

    prefer evicting ojbects with lower oids
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@21899 7fc7bbf3-cf45-46d4-be06-341739edd864

--- dso-l2/src/main/java/com/tc/objectserver/impl/EmergencyEvictionTrigger.java
+++ dso-l2/src/main/java/com/tc/objectserver/impl/EmergencyEvictionTrigger.java
@@ -4,11 +4,16 @@
 package com.tc.objectserver.impl;
 
 import com.tc.object.ObjectID;
+import com.tc.objectserver.api.EvictableEntry;
 import com.tc.objectserver.api.EvictableMap;
 import com.tc.objectserver.api.ObjectManager;
 import com.tc.objectserver.context.ServerMapEvictionContext;
+import com.tc.objectserver.core.api.ManagedObject;
+import com.tc.objectserver.core.api.ManagedObjectState;
 import com.tc.objectserver.l1.impl.ClientObjectReferenceSet;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * This trigger is fired by the resource monitor if the monitored resource goes
@@ -27,22 +32,46 @@ public class EmergencyEvictionTrigger extends AbstractEvictionTrigger {
     private final int blowout;
     private int sampleCount;
     private int sizeCount;
+    private final ObjectManager mgr;
 
     public EmergencyEvictionTrigger(ObjectManager mgr, ObjectID oid, int blowout) {
         super(oid);
         this.blowout = blowout;
+        this.mgr = mgr;
     } 
 
     @Override
     public ServerMapEvictionContext collectEvictonCandidates(int max, String className, EvictableMap map, ClientObjectReferenceSet clients) {
         sizeCount = map.getSize();
-        int get = boundsCheckSampleSize(( blowout > 6 ) ? sizeCount : (int)Math.round(sizeCount * Math.pow(10,blowout-6)));
-        if ( get < 2 ) {
-            get = 2;
+        int get = boundsCheckSampleSize(( blowout > 6 ) ? sizeCount : (int)Math.round(sizeCount * Math.pow(10,blowout-6)) * 2);
+        if ( get < 10 * (blowout)) {
+            get = 10 * (blowout);
         }
-        Map sampled = map.getRandomSamples(get,clients);
+        Map<Object, ObjectID>  sampled = map.getRandomSamples(get,clients);
+        return createEvictionContext(className, filter(sampled));
+    }
+    /**
+     * only take the early half, make this pluggable?
+    */
+    private Map<Object, ObjectID> filter(Map<Object, ObjectID> sample) {
+        return filterByOid(sample);
+    }
 
-        return createEvictionContext(className, sampled);
+    private Map<Object, ObjectID> filterByOid(Map<Object, ObjectID> sample) {
+        TreeMap<ObjectID,Object> rev = new TreeMap<ObjectID, Object>();
+        for ( Map.Entry<Object,ObjectID> entry : sample.entrySet() ) {
+            rev.put(entry.getValue(),entry.getKey());
+        }
+        int nz = sample.size()/2;
+        int count = 0;
+        sample.clear();
+        for ( Iterator<Map.Entry<ObjectID,Object>> it = rev.entrySet().iterator();it.hasNext();/*nothing*/ ) {
+            Map.Entry<ObjectID,Object> ag = it.next();
+            if (count++>nz) {
+                sample.put(ag.getValue(),ag.getKey());
+            }
+        }
+        return sample;
     }
     
     public int getSampleCount() {
