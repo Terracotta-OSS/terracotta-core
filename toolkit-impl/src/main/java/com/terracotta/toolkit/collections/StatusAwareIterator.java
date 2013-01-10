38be3013754288db11dd9fdcd771f5a6c27f43e3
Sachin Sabbarwal <ssabbarw@terracottatech.com>
2013-01-10 11:10:36 +0000

    Dev-8819: replacing DestroyableIterator with StatusAwareIterator
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@21932 7fc7bbf3-cf45-46d4-be06-341739edd864

---
+++ toolkit-impl/src/main/java/com/terracotta/toolkit/collections/StatusAwareIterator.java
@@ -0,0 +1,49 @@
+/*
+ * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
+ */
+package com.terracotta.toolkit.collections;
+
+import org.terracotta.toolkit.rejoin.RejoinException;
+
+import com.terracotta.toolkit.util.ToolkitSubtypeStatus;
+
+import java.util.Iterator;
+
+public class StatusAwareIterator<E> implements Iterator<E> {
+
+  private final Iterator<E>          iterator;
+  private final ToolkitSubtypeStatus status;
+  private final int                  currentRejoinCount;
+
+  public StatusAwareIterator(Iterator<E> iterator, ToolkitSubtypeStatus status) {
+    this.iterator = iterator;
+    this.status = status;
+    this.currentRejoinCount = this.status.getCurrentRejoinCount();
+  }
+
+  private void assertStatus() {
+    if (status.isDestroyed()) { throw new IllegalStateException(
+                                                                "Can not perform operation because object has been destroyed"); }
+    if (this.currentRejoinCount != status.getCurrentRejoinCount()) { throw new RejoinException(
+                                                                                               "Can not performe operation because rejoin happened."); }
+  }
+
+  @Override
+  public boolean hasNext() {
+    assertStatus();
+    return iterator.hasNext();
+  }
+
+  @Override
+  public E next() {
+    assertStatus();
+    return iterator.next();
+  }
+
+  @Override
+  public void remove() {
+    assertStatus();
+    iterator.remove();
+  }
+
+}
