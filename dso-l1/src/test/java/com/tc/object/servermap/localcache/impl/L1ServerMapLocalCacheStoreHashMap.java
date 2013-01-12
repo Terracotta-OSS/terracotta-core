5bb6b6b798b15039253d3f638ef8cb36cbc21984
Alex Snaps <alex.snaps@gmail.com>
2013-01-12 11:54:49 +0000

    EHC-980 Stupid intellij user... can't you remove unused import before commiting
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@21964 7fc7bbf3-cf45-46d4-be06-341739edd864

--- dso-l1/src/test/java/com/tc/object/servermap/localcache/impl/L1ServerMapLocalCacheStoreHashMap.java
+++ dso-l1/src/test/java/com/tc/object/servermap/localcache/impl/L1ServerMapLocalCacheStoreHashMap.java
@@ -8,7 +8,6 @@ import com.tc.object.servermap.localcache.L1ServerMapLocalCacheStoreListener;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
