bd6eff70eb9f934e3ed433e4a2eda08993e0bc02
Ludovic Orban <lorban@bitronix.be>
2013-01-14 15:11:00 +0000

    DEV-8840: replaced secret byte[] and char[] conversions with SecurityUtils version
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@21970 7fc7bbf3-cf45-46d4-be06-341739edd864

--- management-agent/management-tsa-impl/src/main/java/com/terracotta/management/web/utils/L2KeyChainAccessor.java
+++ management-agent/management-tsa-impl/src/main/java/com/terracotta/management/web/utils/L2KeyChainAccessor.java
@@ -4,6 +4,7 @@ import com.tc.security.PwProviderUtil;
 import com.terracotta.management.keychain.KeyName;
 import com.terracotta.management.keychain.URIKeyName;
 import com.terracotta.management.security.KeyChainAccessor;
+import com.terracotta.management.security.SecretUtils;
 
 /**
  * @author Ludovic Orban
@@ -13,15 +14,7 @@ public class L2KeyChainAccessor implements KeyChainAccessor {
   @Override
   public byte[] retrieveSecret(KeyName alias) {
     URIKeyName uriKeyName = (URIKeyName)alias;
-    return toBytes(PwProviderUtil.getPasswordTo(uriKeyName.getURI()));
-  }
-
-  private byte[] toBytes(char[] chars) {
-    byte[] result = new byte[chars.length];
-    for (int i = 0; i < chars.length; i++) {
-      result[i] = (byte)chars[i];
-    }
-    return result;
+    return SecretUtils.toBytesAndWipe(PwProviderUtil.getPasswordTo(uriKeyName.getURI()));
   }
 
 }
