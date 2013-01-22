c17b017c8b23cc693a302f510fd7601b42fd316d
Ludovic Orban <lorban@bitronix.be>
2013-01-22 18:08:37 +0000

    DEV-8887: KeyChainAccessor implementations must return null instead of throwing NPE when the requested URI isn't found in the keychain
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@22071 7fc7bbf3-cf45-46d4-be06-341739edd864

--- management-agent/management-tsa-impl/src/main/java/com/terracotta/management/web/utils/L2KeyChainAccessor.java
+++ management-agent/management-tsa-impl/src/main/java/com/terracotta/management/web/utils/L2KeyChainAccessor.java
@@ -6,6 +6,8 @@ import com.terracotta.management.keychain.URIKeyName;
 import com.terracotta.management.security.KeyChainAccessor;
 import com.terracotta.management.security.SecretUtils;
 
+import java.net.URI;
+
 /**
  * @author Ludovic Orban
  */
@@ -14,7 +16,14 @@ public class L2KeyChainAccessor implements KeyChainAccessor {
   @Override
   public byte[] retrieveSecret(KeyName alias) {
     URIKeyName uriKeyName = (URIKeyName)alias;
-    return SecretUtils.toBytesAndWipe(PwProviderUtil.getPasswordTo(uriKeyName.getURI()));
+    URI uri = uriKeyName.getURI();
+    try {
+      char[] passwordTo = PwProviderUtil.getPasswordTo(uri);
+      return SecretUtils.toBytesAndWipe(passwordTo);
+    } catch (NullPointerException npe) {
+      // PwProviderUtil.getPasswordTo throws NPE when there is no such secret
+      return null;
+    }
   }
 
 }
