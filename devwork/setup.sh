0d1e2454a51417e8d7986213b8e0f0d088bde378
Gary Keim <gkeim@terracottatech.com>
2013-03-04 21:35:08 +0000

    removed projects that will have release versions
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@22370 7fc7bbf3-cf45-46d4-be06-341739edd864

--- devwork/setup.sh
+++ devwork/setup.sh
@@ -1,17 +1,6 @@
 # trunk: https://svn.terracotta.org/repo/internal/enterprise/trunk
-svn co https://svn.terracotta.org/repo/internal/search/trunk search
-svn co https://svn.terracotta.org/repo/forge/enterprise/terracotta-license/trunk/ terracotta-license
-svn co https://svn.terracotta.org/repo/forge/enterprise/security-modules/trunk security-modules
 svn co https://svn.terracotta.org/repo/forge/enterprise/management-apps/trunk management-apps
-svn co https://svn.terracotta.org/repo/internal/fast-restartable-store/trunk fast-restartable-store
-svn co https://svn.terracotta.org/repo/internal/offheap-store/trunk offheap-store
 svn co https://svn.terracotta.org/repo/ehcache/trunk/ehcache ehcache
-svn co https://svn.terracotta.org/repo/forge/projects/statistics/trunk statistics
 svn co https://svn.terracotta.org/repo/internal/ehcache-enterprise/ehcache-core-ee/trunk ehcache-ee
-svn co https://svn.terracotta.org/repo/forge/projects/management-core/trunk management-core
 svn co https://svn.terracotta.org/repo/quartz/trunk quartz
 svn co https://svn.terracotta.org/repo/forge/enterprise/quartz-ee/trunk quartz-ee
-svn co https://svn.terracotta.org/repo/forge/projects/terracotta-toolkit-api/trunk terracotta-toolkit-api
-svn co https://svn.terracotta.org/repo/forge/projects/core-storage-api/trunk core-storage-api
-svn co https://svn.terracotta.org/repo/internal/bigmemory-core-storage/trunk bigmemory-core-storage
-svn co https://svn.terracotta.org/repo/forge/projects/heap-core-storage/trunk heap-core-storage
