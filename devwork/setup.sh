9f831e265054b7e1fdd7bc71efc43285b0499f1c
Gary Keim <gkeim@terracottatech.com>
2013-03-06 22:14:34 +0000

    add search because it's apparently not in Nexus.
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@22375 7fc7bbf3-cf45-46d4-be06-341739edd864

--- devwork/setup.sh
+++ devwork/setup.sh
@@ -1,4 +1,5 @@
 # trunk: https://svn.terracotta.org/repo/internal/enterprise/trunk
+svn co https://svn.terracotta.org/repo/internal/search/trunk search
 svn co https://svn.terracotta.org/repo/forge/enterprise/management-apps/trunk management-apps
 svn co https://svn.terracotta.org/repo/ehcache/trunk/ehcache ehcache
 svn co https://svn.terracotta.org/repo/internal/ehcache-enterprise/ehcache-core-ee/trunk ehcache-ee
