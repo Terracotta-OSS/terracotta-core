baa23ccff12ece1a6d4064872b98969338212c6d
Hung Huynh <hhuynh@gmail.com>
2013-03-07 23:57:12 +0000

    test version that doesn't require surefire
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@22392 7fc7bbf3-cf45-46d4-be06-341739edd864

--- management-agent/management-tsa-impl/src/test/java/com/terracotta/management/ApplicationTsaTest.java
+++ management-agent/management-tsa-impl/src/test/java/com/terracotta/management/ApplicationTsaTest.java
@@ -1,61 +1,56 @@
 package com.terracotta.management;
 
+import static org.hamcrest.Matchers.equalTo;
+
+import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
 import org.junit.Test;
-import sun.misc.URLClassPath;
 
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.Field;
 import java.net.URL;
-import java.net.URLClassLoader;
-import java.net.URLDecoder;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Enumeration;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
-
-import static org.hamcrest.Matchers.equalTo;
+import java.util.jar.Manifest;
 
 /**
  * @author: Anthony Dahanne
  */
 public class ApplicationTsaTest {
-  private static final String[] PACKAGE_STARTS_WITH_FILTERS = new String[]{"java","javax","org.apache","com.sun","org.codehaus","org.hibernate"};
-  private static final String[] PATH_TO_JAR_CONTAINS_FILTERS = new String[]{"repository"};
+  private static final String[] PACKAGE_STARTS_WITH_FILTERS  = new String[] { "java", "javax", "org.apache", "com.sun",
+      "org.codehaus", "org.hibernate"                       };
+  private static final String[] PATH_TO_JAR_CONTAINS_FILTERS = new String[] { "repository" };
 
   @Test
   public void testGetClasses() throws Exception {
+    List<String> classpathElements = getClasspathElements();
 
     ApplicationTsa applicationTsa = new ApplicationTsa();
     Set<Class<?>> applicationClasses = applicationTsa.getClasses();
 
-    URLClassLoader classLoader = (URLClassLoader) ApplicationTsa.class.getClassLoader();
-//    Field f = ClassLoader.class.getDeclaredField("classes");
-//    f.setAccessible(true);
-    Field declaredFieldUcp = URLClassLoader.class.getDeclaredField("ucp");
-    declaredFieldUcp.setAccessible(true);
-    URLClassPath ucp = (URLClassPath) declaredFieldUcp.get(classLoader);
-    URL[] urLs = ucp.getURLs();
     Set<Class<?>> allClassesFound = new HashSet<Class<?>>();
-    for (URL urL : urLs) {
-      if(urL.toString().endsWith(".jar")) {
-        if(pathOfJarNotFiltered(urL.toString())) {
-          System.out.println("last scanned path : "+urL.toString());
-          allClassesFound.addAll(getAllClassesFromJar(urL));
+    for (String cpElement : classpathElements) {
+      if (cpElement.endsWith(".jar")) {
+        if (pathOfJarNotFiltered(cpElement)) {
+          System.out.println("last scanned path : " + cpElement);
+          allClassesFound.addAll(getAllClassesFromJar(cpElement));
         }
       } else {
-        System.out.println("last scanned path : "+urL.toString());
-        allClassesFound.addAll(getAllClassesFromDirectory(urL));
+        System.out.println("last scanned path : " + cpElement);
+        allClassesFound.addAll(getAllClassesFromDirectory(cpElement));
       }
     }
 
     Set<Class<?>> annotatedClasses = new HashSet<Class<?>>();
     for (Class<?> aClass : allClassesFound) {
       if (aClass.isAnnotationPresent(javax.ws.rs.ext.Provider.class)
-              || aClass.isAnnotationPresent(javax.ws.rs.Path.class)) {
+          || aClass.isAnnotationPresent(javax.ws.rs.Path.class)) {
         annotatedClasses.add(aClass);
       }
     }
@@ -64,9 +59,29 @@ public class ApplicationTsaTest {
 
   }
 
-  private Set<Class<?>> getAllClassesFromDirectory(URL urL) throws ClassNotFoundException {
-    Set<Class<?>> classes =  new HashSet<Class<?>>();
-    File rootPath = new File(urL.getFile());
+  private List<String> getClasspathElements() throws IOException {
+    List<String> elements = new ArrayList<String>();
+    String classpath = System.getProperty("java.class.path");
+    System.out.println("XXX Current classpath: " + classpath);
+    if (classpath.contains("surefirebooter")) {
+      JarFile surefireBooter = new JarFile(classpath);
+      Manifest manifest = surefireBooter.getManifest();
+      classpath = manifest.getMainAttributes().getValue("Class-path");
+      surefireBooter.close();
+      for (String urlElement : classpath.split(" ")) {
+        elements.add(FileUtils.toFile(new URL(urlElement)).getAbsolutePath());
+      }
+    } else {
+      for (String path : classpath.split(File.pathSeparator)) {
+        elements.add(path);
+      }
+    }
+    return elements;
+  }
+
+  private Set<Class<?>> getAllClassesFromDirectory(String cpElement) throws ClassNotFoundException {
+    Set<Class<?>> classes = new HashSet<Class<?>>();
+    File rootPath = new File(cpElement);
     File[] files = rootPath.listFiles();
     for (File file : files) {
       findAndAddClassesFromRootPath(file, classes, rootPath);
@@ -74,62 +89,57 @@ public class ApplicationTsaTest {
     return classes;
   }
 
-  private Collection<? extends Class<?>> getAllClassesFromJar(URL urL) throws IOException {
-    Set<Class<?>> classes =  new HashSet<Class<?>>();
-    String decode = URLDecoder.decode(urL.getFile(), "UTF-8");
-    JarFile jarFile = new JarFile(decode);
+  private Collection<? extends Class<?>> getAllClassesFromJar(String cpElement) throws IOException {
+    Set<Class<?>> classes = new HashSet<Class<?>>();
+    JarFile jarFile = new JarFile(cpElement);
     Enumeration<JarEntry> entries = jarFile.entries();
-    while(entries.hasMoreElements()) {
+    while (entries.hasMoreElements()) {
       JarEntry entry = entries.nextElement();
       String entryName = entry.getName();
-      if(entryName.endsWith(".class")) {
+      if (entryName.endsWith(".class")) {
         String className = entryName.replace('/', '.').replace('\\', '.').replace(".class", "");
         try {
-          if(packageOfClassNotFiltered(className)) {
+          if (packageOfClassNotFiltered(className)) {
             classes.add(Class.forName(className));
           }
-        }
-        catch (Throwable e) {
-          //not a problem
+        } catch (Throwable e) {
+          // not a problem
         }
       }
     }
+    jarFile.close();
     return classes;
   }
 
   private boolean packageOfClassNotFiltered(String className) {
-    for (String filter : PACKAGE_STARTS_WITH_FILTERS){
-      if(className.startsWith(filter)) {
-        return false;
-      }
+    for (String filter : PACKAGE_STARTS_WITH_FILTERS) {
+      if (className.startsWith(filter)) { return false; }
     }
     return true;
   }
 
   private boolean pathOfJarNotFiltered(String jarPath) {
-    for (String filter : PATH_TO_JAR_CONTAINS_FILTERS){
-      if(jarPath.contains(filter)) {
-        return true;
-      }
+    for (String filter : PATH_TO_JAR_CONTAINS_FILTERS) {
+      if (jarPath.contains(filter)) { return true; }
     }
     return false;
   }
 
-  private void findAndAddClassesFromRootPath(File file, Set<Class<?>> classes, File rootPath) throws ClassNotFoundException {
-    if(file.isFile() && file.getAbsolutePath().endsWith(".class")) {
+  private void findAndAddClassesFromRootPath(File file, Set<Class<?>> classes, File rootPath)
+      throws ClassNotFoundException {
+    if (file.isFile() && file.getAbsolutePath().endsWith(".class")) {
       String replace = file.getAbsolutePath().replace(rootPath.getAbsolutePath() + File.separator, "");
-      String className = replace.replace(File.separator, ".").substring(0,replace.length()-6);
+      String className = replace.replace(File.separator, ".").substring(0, replace.length() - 6);
       try {
         classes.add(Class.forName(className));
+      } catch (Throwable e) {
+        // not a problem
       }
-      catch (Throwable e) {
-        //not a problem
-      }
-    } else if (file.isDirectory()){
+    } else if (file.isDirectory()) {
       File[] listOfFiles = file.listFiles();
-      if(listOfFiles != null) {
-        for (int i = 0; i < listOfFiles.length; i++)
-          findAndAddClassesFromRootPath(listOfFiles[i], classes, rootPath);
+      if (listOfFiles != null) {
+        for (File listOfFile : listOfFiles)
+          findAndAddClassesFromRootPath(listOfFile, classes, rootPath);
       }
     }
   }
