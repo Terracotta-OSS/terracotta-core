e0300b79e2ae3281816c54cec1eea66ea562c9f5
Tim Wu <tim.wu@terracottatech.com>
2013-03-07 04:13:27 +0000

    Refactor test a bit and delete some unused classes.
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@22377 7fc7bbf3-cf45-46d4-be06-341739edd864

--- dso-l2/src/test/java/com/tc/objectserver/persistence/impl/GlobalTransactionIDBatchRequestHandlerTest.java
+++ dso-l2/src/test/java/com/tc/objectserver/persistence/impl/GlobalTransactionIDBatchRequestHandlerTest.java
@@ -4,97 +4,98 @@
  */
 package com.tc.objectserver.persistence.impl;
 
-import com.tc.l2.ha.L2HADisabledCooridinator;
-import com.tc.net.groups.SingleNodeGroupManager;
-import com.tc.object.persistence.api.PersistentMapStore;
-import com.tc.objectserver.api.TestSink;
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+
+import com.tc.async.api.EventContext;
+import com.tc.async.api.Sink;
+import com.tc.l2.api.L2Coordinator;
+import com.tc.l2.api.ReplicatedClusterStateManager;
 import com.tc.objectserver.core.impl.TestServerConfigurationContext;
 import com.tc.objectserver.handler.GlobalTransactionIDBatchRequestHandler;
 import com.tc.objectserver.handler.GlobalTransactionIDBatchRequestHandler.GlobalTransactionIDBatchRequestContext;
 import com.tc.test.TCTestCase;
-import com.tc.util.concurrent.NoExceptionLinkedQueue;
 import com.tc.util.sequence.BatchSequenceReceiver;
 
-import java.util.concurrent.ConcurrentHashMap;
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.mockito.Matchers.argThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class GlobalTransactionIDBatchRequestHandlerTest extends TCTestCase {
 
+  private ReplicatedClusterStateManager          clusterStateManager;
   private GlobalTransactionIDBatchRequestHandler provider;
-  private TestBatchSequenceReceiver              receiver;
   private TestMutableSequence                    persistentSequence;
-  private TestSink                               requestBatchSink;
+  private Sink                                   requestBatchSink;
 
   @Override
   public void setUp() throws Exception {
-    persistentSequence = new TestMutableSequence();
-    requestBatchSink = new TestSink();
+    persistentSequence = spy(new TestMutableSequence());
+    requestBatchSink = mock(Sink.class);
+    clusterStateManager = mock(ReplicatedClusterStateManager.class);
 
     provider = new GlobalTransactionIDBatchRequestHandler(persistentSequence);
     provider.setRequestBatchSink(requestBatchSink);
 
     TestServerConfigurationContext scc = new TestServerConfigurationContext();
-    scc.l2Coordinator = new L2HADisabledCooridinator(new SingleNodeGroupManager(), new TestPersistentMapStore());
+    scc.l2Coordinator = mock(L2Coordinator.class);
+    when(scc.l2Coordinator.getReplicatedClusterStateManager()).thenReturn(clusterStateManager);
     provider.initializeContext(scc);
-
-    receiver = new TestBatchSequenceReceiver();
   }
 
-  public void testx() throws Exception {
-    int batchSize = 5;
-    // make sure that the request context gets put in the sink properly.
-    provider.requestBatch(receiver, batchSize);
-    GlobalTransactionIDBatchRequestContext ctxt = (GlobalTransactionIDBatchRequestContext) requestBatchSink
-        .getInternalQueue().remove(0);
-    assertTrue(requestBatchSink.getInternalQueue().isEmpty());
-    assertSame(receiver, ctxt.getReceiver());
-    assertEquals(batchSize, ctxt.getBatchSize());
-
-    // now check the handler interface to make sure it handles the request properly
-    provider.handleEvent(ctxt);
-
-    // make sure it called the right thing on the sequence
-    Object[] args = (Object[]) persistentSequence.nextBatchQueue.poll(1);
-    assertEquals(Integer.valueOf(batchSize), args[0]);
-
-    // make sure it called back on the receiver
-    args = (Object[]) receiver.nextBatchQueue.poll(1);
-    assertEquals(Long.valueOf(persistentSequence.sequence - batchSize), args[0]);
-    assertEquals(Long.valueOf(persistentSequence.sequence), args[1]);
+  public void testRequestBatch() throws Exception {
+    BatchSequenceReceiver receiver = mock(BatchSequenceReceiver.class);
+    provider.requestBatch(receiver, 5);
+    verify(requestBatchSink).add(argThat(allOf(hasBatchSize(5), hasReceiver(receiver))));
   }
 
-  private static final class TestBatchSequenceReceiver implements BatchSequenceReceiver {
-
-    public final NoExceptionLinkedQueue nextBatchQueue = new NoExceptionLinkedQueue();
-
-    @Override
-    public void setNextBatch(long start, long end) {
-      nextBatchQueue.put(new Object[] { Long.valueOf(start), Long.valueOf(end) });
-    }
-
-    @Override
-    public boolean isBatchRequestPending() {
-      return true;
-    }
-
+  public void testHandleRequest() throws Exception {
+    BatchSequenceReceiver receiver = mock(BatchSequenceReceiver.class);
+    GlobalTransactionIDBatchRequestContext context = new GlobalTransactionIDBatchRequestContext(receiver, 5);
+    provider.handleEvent(context);
+    verify(clusterStateManager).publishNextAvailableGlobalTransactionID(5);
+    verify(persistentSequence).nextBatch(5);
+    verify(receiver).setNextBatch(0, 5);
   }
 
-  private static class TestPersistentMapStore implements PersistentMapStore {
-    private final ConcurrentHashMap<String, String> store = new ConcurrentHashMap<String, String>();
+  private static Matcher<EventContext> hasReceiver(final BatchSequenceReceiver receiver) {
+    return new BaseMatcher<EventContext>() {
+      @Override
+      public boolean matches(final Object o) {
+        if (o instanceof GlobalTransactionIDBatchRequestContext) {
+          return receiver.equals(((GlobalTransactionIDBatchRequestContext)o).getReceiver());
+        } else {
+          return false;
+        }
+      }
 
-    @Override
-    public String get(String key) {
-      return store.get(key);
-    }
-
-    @Override
-    public void put(String key, String value) {
-      store.put(key, value);
-    }
-
-    @Override
-    public boolean remove(String key) {
-      return store.remove(key) != null;
-    }
-
+      @Override
+      public void describeTo(final Description description) {
+        //
+      }
+    };
   }
+
+  private static Matcher<EventContext> hasBatchSize(final int batchSize) {
+    return new BaseMatcher<EventContext>() {
+      @Override
+      public boolean matches(final Object o) {
+        if (o instanceof GlobalTransactionIDBatchRequestContext) {
+          return ((GlobalTransactionIDBatchRequestContext)o).getBatchSize() == batchSize;
+        } else {
+          return false;
+        }
+      }
+
+      @Override
+      public void describeTo(final Description description) {
+        //
+      }
+    };
+  }
+
 }
