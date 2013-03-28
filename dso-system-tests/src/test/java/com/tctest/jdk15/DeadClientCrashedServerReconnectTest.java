2542c417b280c467bab630cba18accd2b9cdc34c
Kalai Kannaiyan <kkannaiy@terracottatech.com>
2013-03-28 21:37:59 +0000

    extend timebomb
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@22520 7fc7bbf3-cf45-46d4-be06-341739edd864

--- dso-system-tests/src/test/java/com/tctest/jdk15/DeadClientCrashedServerReconnectTest.java
+++ dso-system-tests/src/test/java/com/tctest/jdk15/DeadClientCrashedServerReconnectTest.java
@@ -47,7 +47,7 @@ public class DeadClientCrashedServerReconnectTest extends BaseDSOTestCase {
 
   public DeadClientCrashedServerReconnectTest() {
     // Move this test to toolkit-ee-system-tests, no real need for it to be here. And it's broken.
-    timebombTest("2013-03-28");
+    timebombTest("2013-04-28");
     tcProps = TCPropertiesImpl.getProperties();
     tcProps.setProperty(TCPropertiesConsts.L1_L2_CONFIG_VALIDATION_ENABLED, "false");
     System.out.println("L1 and L2 config match check disabled temporarily as we use proxy");
