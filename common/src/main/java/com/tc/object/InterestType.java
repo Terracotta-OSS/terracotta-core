5c34c064d55db8cb4ad5f2710a6556b781bdfc81
Tim Wu <tim.wu@terracottatech.com>
2013-04-29 21:24:34 +0000

    Revert r22763 for now to fix the build
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@22767 7fc7bbf3-cf45-46d4-be06-341739edd864

---
+++ common/src/main/java/com/tc/object/InterestType.java
@@ -0,0 +1,32 @@
+package com.tc.object;
+
+/**
+ * * @author Eugene Shelestovich
+ */
+public enum InterestType {
+  PUT(0), REMOVE(1), EVICT(2), EXPIRE(3);
+  private final int id;
+
+  InterestType(int id) {
+    this.id = id;
+  }
+
+  public static InterestType fromInt(int value) {
+    switch (value) {
+      case 0:
+        return PUT;
+      case 1:
+        return REMOVE;
+      case 2:
+        return EVICT;
+      case 3:
+        return EXPIRE;
+      default:
+        throw new IllegalArgumentException("Unknown interest type: " + value);
+    }
+  }
+
+  public int toInt() {
+    return id;
+  }
+}
