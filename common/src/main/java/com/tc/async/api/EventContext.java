1410a4e0937df7d0f217cb24ce24f0de8f74fc68
Abhishek Maheshwari <amaheshw@terracottatech.com>
2013-06-21 06:42:56 +0000

    minor change
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@23216 7fc7bbf3-cf45-46d4-be06-341739edd864

--- common/src/main/java/com/tc/async/api/EventContext.java
+++ common/src/main/java/com/tc/async/api/EventContext.java
@@ -6,7 +6,7 @@ package com.tc.async.api;
 /**
  * @author steve Interface for Event Context. It is a tagging interface with no implementation (I'm generally not a big
  *         tagging interface guy but I'm leaving this for now because seda has it and I'm giving it a chance For more
- *         information of this kind of stuff google SEDA -Staged Event Driven Architecure
+ *         information of this kind of stuff google SEDA -Staged Event Driven Architecture
  */
 public interface EventContext {
   //
