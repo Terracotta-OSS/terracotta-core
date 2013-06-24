30b015abcdab4a327538fc1b57a64ded513e6f2e
Myron Scott <mscott@terracottatech.com>
2013-06-24 19:11:15 +0000

    removed unsed imports
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@23232 7fc7bbf3-cf45-46d4-be06-341739edd864

--- dso-l2/src/main/java/com/tc/objectserver/impl/ProgressiveEvictionManager.java
+++ dso-l2/src/main/java/com/tc/objectserver/impl/ProgressiveEvictionManager.java
@@ -42,8 +42,6 @@ import com.tc.util.Conversion;
 import com.tc.util.ObjectIDSet;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
