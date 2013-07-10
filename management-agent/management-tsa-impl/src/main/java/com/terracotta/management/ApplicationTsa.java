267a7379fa1a492ef492724a1fabb692b842a811
Ludovic Orban <lorban@bitronix.be>
2013-07-10 15:48:30 +0000

    upgrade to latest security-modules and management-core
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@23302 7fc7bbf3-cf45-46d4-be06-341739edd864

--- management-agent/management-tsa-impl/src/main/java/com/terracotta/management/ApplicationTsa.java
+++ management-agent/management-tsa-impl/src/main/java/com/terracotta/management/ApplicationTsa.java
@@ -11,7 +11,7 @@ public class ApplicationTsa extends javax.ws.rs.core.Application {
 
 
   public Set<Class<?>> getClasses() {
-    Set<Class<?>> s = new HashSet<Class<?>>();
+    Set<Class<?>> s = new HashSet<Class<?>>(super.getClasses());
     s.add(ElementsResourceServiceImpl.class);
     s.add(BackupResourceServiceImpl.class);
     s.add(ConfigurationResourceServiceImpl.class);
@@ -29,9 +29,6 @@ public class ApplicationTsa extends javax.ws.rs.core.Application {
     s.add(net.sf.ehcache.management.resource.services.CacheManagerConfigsResourceServiceImpl.class);
     s.add(net.sf.ehcache.management.resource.services.CacheConfigsResourceServiceImpl.class);
     s.add(net.sf.ehcache.management.resource.services.AgentsResourceServiceImpl.class);
-    s.add(net.sf.ehcache.management.resource.exceptions.WebApplicationExceptionMapper.class);
-    s.add(net.sf.ehcache.management.resource.exceptions.DefaultExceptionMapper.class);
-    s.add(net.sf.ehcache.management.resource.exceptions.ResourceRuntimeExceptionMapper.class);
     return s;
   }
 
