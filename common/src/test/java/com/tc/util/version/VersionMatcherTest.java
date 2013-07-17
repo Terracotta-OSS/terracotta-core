7882bcf99dd002427d03f032b40bbdab0477cb3c
Hung Huynh <hhuynh@gmail.com>
2013-07-17 01:00:13 +0000

    INT-2448: check-short profile
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@23326 7fc7bbf3-cf45-46d4-be06-341739edd864

--- common/src/test/java/com/tc/util/version/VersionMatcherTest.java
+++ common/src/test/java/com/tc/util/version/VersionMatcherTest.java
@@ -3,23 +3,26 @@
  */
 package com.tc.util.version;
 
-import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.experimental.categories.Category;
+import org.terracotta.test.categories.SmokeTests;
 
-public class VersionMatcherTest extends TestCase {
+@Category(SmokeTests.class)
+public class VersionMatcherTest {
 
   public void testExactMatch() {
     VersionMatcher matcher = new VersionMatcher("3.0.0");
-    assertTrue(matcher.matches("3.0.0"));
+    Assert.assertTrue(matcher.matches("3.0.0"));
   }
 
   public void testExactMisMatch() {
     VersionMatcher matcher = new VersionMatcher("3.0.0");
-    assertFalse(matcher.matches("9.9.9"));
+    Assert.assertFalse(matcher.matches("9.9.9"));
   }
 
   public void testTcAny() {
     VersionMatcher matcher = new VersionMatcher("3.0.0");
-    assertTrue(matcher.matches("*"));
+    Assert.assertTrue(matcher.matches("*"));
   }
 
 }
---
+++ common/src/test/java/org/terracotta/test/categories/SmokeTests.java
@@ -0,0 +1,8 @@
+/*
+ * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
+ */
+package org.terracotta.test.categories;
+
+public interface SmokeTests {
+  // test marked with this will be run with check-short profile
+}
