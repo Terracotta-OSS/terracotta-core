11df5f893b0bf552cd7aa4120e2cec371aed2b11
Anthony Dahanne <anthony.dahanne@gmail.com>
2013-09-06 22:59:24 +0000

    ENG-231; CRQ-117 :  TC Server crashes when TMC connected to it The test was not compiling on JDK 1.6 because of Hamcrest; using simple enough matchers now to make JDK 1.6 happy
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@23667 7fc7bbf3-cf45-46d4-be06-341739edd864

--- management-agent/management-tsa-impl/src/test/java/com/terracotta/management/service/impl/util/TimeStringParserTest.java
+++ management-agent/management-tsa-impl/src/test/java/com/terracotta/management/service/impl/util/TimeStringParserTest.java
@@ -2,10 +2,8 @@ package com.terracotta.management.service.impl.util;
 
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.both;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import static org.hamcrest.Matchers.lessThan;
-import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
@@ -24,7 +22,9 @@ public class TimeStringParserTest {
     long tenDaysinMillis =  10 * 24 * 60 * 60 * 1000L;
     long millisBetweenNowAndTenDaysAgo = now - tenDaysinMillis;
 
-    assertThat(TimeStringParser.parseTime("10d"), is(both(greaterThanOrEqualTo(millisBetweenNowAndTenDaysAgo)).and(lessThan(millisBetweenNowAndTenDaysAgo + 500))));
+    long actual = TimeStringParser.parseTime("10d");
+    assertThat(actual, greaterThanOrEqualTo(millisBetweenNowAndTenDaysAgo));
+    assertThat(actual, lessThan(millisBetweenNowAndTenDaysAgo + 500));
   }
 
   @Test
@@ -33,7 +33,9 @@ public class TimeStringParserTest {
     long tenHoursinMillis =  10 * 60 * 60 * 1000L;
     long millisBetweenNowAndTenHoursAgo = now - tenHoursinMillis;
 
-    assertThat(TimeStringParser.parseTime("10h"), is(both(greaterThanOrEqualTo(millisBetweenNowAndTenHoursAgo)).and(lessThan(millisBetweenNowAndTenHoursAgo + 500))));
+    long actual = TimeStringParser.parseTime("10h");
+    assertThat(actual, greaterThanOrEqualTo(millisBetweenNowAndTenHoursAgo));
+    assertThat(actual, lessThan(millisBetweenNowAndTenHoursAgo + 500));
   }
 
   @Test
@@ -42,7 +44,9 @@ public class TimeStringParserTest {
     long tenMinutesinMillis =  10 * 60 * 1000L;
     long millisBetweenNowAndTenMinutessAgo = now - tenMinutesinMillis;
 
-    assertThat(TimeStringParser.parseTime("10m"), is(both(greaterThanOrEqualTo(millisBetweenNowAndTenMinutessAgo)).and(lessThan(millisBetweenNowAndTenMinutessAgo + 500))));
+    long actual = TimeStringParser.parseTime("10m");
+    assertThat(actual, greaterThanOrEqualTo(millisBetweenNowAndTenMinutessAgo));
+    assertThat(actual, lessThan(millisBetweenNowAndTenMinutessAgo + 500));
   }
 
   @Test
@@ -51,14 +55,18 @@ public class TimeStringParserTest {
     long tenSecondsinMillis =  10 * 1000L;
     long millisBetweenNowAndTenSecondsAgo = now - tenSecondsinMillis;
 
-    assertThat(TimeStringParser.parseTime("10s"), is(both(greaterThanOrEqualTo(millisBetweenNowAndTenSecondsAgo)).and(lessThan(millisBetweenNowAndTenSecondsAgo + 500))));
+    long actual = TimeStringParser.parseTime("10s");
+    assertThat(actual, greaterThanOrEqualTo(millisBetweenNowAndTenSecondsAgo));
+    assertThat(actual, lessThan(millisBetweenNowAndTenSecondsAgo + 500));
   }
 
   @Test
   public void testParseTime() throws Exception {
     long now = System.currentTimeMillis();
 
-    assertThat(TimeStringParser.parseTime(Long.toString(now)), is(both(greaterThanOrEqualTo(now)).and(lessThan(now + 500))));
+    long actual = TimeStringParser.parseTime(Long.toString(now));
+    assertThat(actual, greaterThanOrEqualTo(now));
+    assertThat(actual, lessThan(now + 500));
   }
 
 
