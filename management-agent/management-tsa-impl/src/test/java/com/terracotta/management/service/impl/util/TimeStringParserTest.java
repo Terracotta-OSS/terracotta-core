d01c7a283ff47425377ef4c101f5226945e96651
Anthony Dahanne <anthony.dahanne@gmail.com>
2013-09-06 21:38:55 +0000

    ENG-231; CRQ-117 :  TC Server crashes when TMC connected to it JodaTime uses of ThreadGroup.uncaughtException() use in the rest agent could make the TC server crash when some resource was not found. JodaTime is a convenient library for dates comparison and manipulation; but in the OperatorEvent and Log services cases, it was only used to substract some time to the current time. I got rid of this library using the JRE System.currentTimeInMillis; the tests confirm the behavior is unchanged
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@23658 7fc7bbf3-cf45-46d4-be06-341739edd864

---
+++ management-agent/management-tsa-impl/src/test/java/com/terracotta/management/service/impl/util/TimeStringParserTest.java
@@ -0,0 +1,68 @@
+package com.terracotta.management.service.impl.util;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.both;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author Anthony Dahanne
+ *
+ * Those tests are not mocking time; so they add a 500 millis margin to the result
+ * Those 500 millis could represent the difference between
+ *   the test's System.currentTimeMillis() and the class under test current time in millis
+ * This is precise enough for management
+ *
+ */
+public class TimeStringParserTest {
+  @Test
+  public void testParseTime__day() throws Exception {
+    long now = System.currentTimeMillis();
+    long tenDaysinMillis =  10 * 24 * 60 * 60 * 1000L;
+    long millisBetweenNowAndTenDaysAgo = now - tenDaysinMillis;
+
+    assertThat(TimeStringParser.parseTime("10d"), is(both(greaterThanOrEqualTo(millisBetweenNowAndTenDaysAgo)).and(lessThan(millisBetweenNowAndTenDaysAgo + 500))));
+  }
+
+  @Test
+  public void testParseTime__hour() throws Exception {
+    long now = System.currentTimeMillis();
+    long tenHoursinMillis =  10 * 60 * 60 * 1000L;
+    long millisBetweenNowAndTenHoursAgo = now - tenHoursinMillis;
+
+    assertThat(TimeStringParser.parseTime("10h"), is(both(greaterThanOrEqualTo(millisBetweenNowAndTenHoursAgo)).and(lessThan(millisBetweenNowAndTenHoursAgo + 500))));
+  }
+
+  @Test
+  public void testParseTime__minute() throws Exception {
+    long now = System.currentTimeMillis();
+    long tenMinutesinMillis =  10 * 60 * 1000L;
+    long millisBetweenNowAndTenMinutessAgo = now - tenMinutesinMillis;
+
+    assertThat(TimeStringParser.parseTime("10m"), is(both(greaterThanOrEqualTo(millisBetweenNowAndTenMinutessAgo)).and(lessThan(millisBetweenNowAndTenMinutessAgo + 500))));
+  }
+
+  @Test
+  public void testParseTime_second() throws Exception {
+    long now = System.currentTimeMillis();
+    long tenSecondsinMillis =  10 * 1000L;
+    long millisBetweenNowAndTenSecondsAgo = now - tenSecondsinMillis;
+
+    assertThat(TimeStringParser.parseTime("10s"), is(both(greaterThanOrEqualTo(millisBetweenNowAndTenSecondsAgo)).and(lessThan(millisBetweenNowAndTenSecondsAgo + 500))));
+  }
+
+  @Test
+  public void testParseTime() throws Exception {
+    long now = System.currentTimeMillis();
+
+    assertThat(TimeStringParser.parseTime(Long.toString(now)), is(both(greaterThanOrEqualTo(now)).and(lessThan(now + 500))));
+  }
+
+
+}
