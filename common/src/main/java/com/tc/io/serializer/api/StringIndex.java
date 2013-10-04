0baf7365db26518935273a3b2c9aa9ffcee2d501
Tim Eck <timeck@gmail.com>
2013-10-04 19:06:47 +0000

    delete
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@23878 7fc7bbf3-cf45-46d4-be06-341739edd864

--- common/src/main/java/com/tc/io/serializer/api/StringIndex.java
+++
@@ -1,9 +0,0 @@
-/*
- * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
- */
-package com.tc.io.serializer.api;
-
-public interface StringIndex {
-  public long getOrCreateIndexFor(String string);
-  public String getStringFor(long index);
-}
