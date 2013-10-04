52abe6b4a970a42a68b651c95ea456699ba77632
Tim Eck <timeck@gmail.com>
2013-10-04 22:32:06 +0000

    unused
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@23893 7fc7bbf3-cf45-46d4-be06-341739edd864

--- dso-l1-api/src/main/java/com/tc/object/config/ClassReplacementTest.java
+++
@@ -1,8 +0,0 @@
-/*
- * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
- */
-package com.tc.object.config;
-
-public interface ClassReplacementTest {
-  boolean accepts(String origClassName, ClassLoader loader);
-}
