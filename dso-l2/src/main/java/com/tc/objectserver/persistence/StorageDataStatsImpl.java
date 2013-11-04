95b15947bcea4ba9c54c7652e69d1fd97cd955e6
Hung Huynh <hhuynh@gmail.com>
2013-11-04 21:17:37 +0000

    ENG-517: ported offheap stats from existing code
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@24039 7fc7bbf3-cf45-46d4-be06-341739edd864

--- dso-l2/src/main/java/com/tc/objectserver/persistence/StorageDataStatsImpl.java
+++ dso-l2/src/main/java/com/tc/objectserver/persistence/StorageDataStatsImpl.java
@@ -3,19 +3,35 @@
  */
 package com.tc.objectserver.persistence;
 
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
+
 import org.terracotta.corestorage.monitoring.MonitoredResource;
 
 import com.tc.objectserver.storage.api.StorageData;
 import com.tc.objectserver.storage.api.StorageDataStats;
+import com.tc.text.PrettyPrintable;
+import com.tc.text.PrettyPrinter;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
 
-public class StorageDataStatsImpl implements StorageDataStats {
+public class StorageDataStatsImpl implements StorageDataStats, PrettyPrintable {
   public static final long               serialVersionUID = 1L;
 
   private final MonitoredResource        monitoredResource;
   private final Map<String, StorageData> storageStats     = new HashMap<String, StorageData>();
+
+  private static final long              REFRESH_INTERVAL = 10000;
+
+  private final ExecutorService          executorService  = Executors.newCachedThreadPool();
+  private final AtomicBoolean            refreshing       = new AtomicBoolean();
+
+  private long                           lastRefreshTime  = System.nanoTime();
+
+  private volatile long                  usedSize         = 0;
 
   public StorageDataStatsImpl(MonitoredResource monitoredResource) {
     this.monitoredResource = monitoredResource;
@@ -23,6 +39,7 @@ public class StorageDataStatsImpl implements StorageDataStats {
 
   @Override
   public Map<String, StorageData> getStorageStats() {
+    collectStats();
     return storageStats;
   }
 
@@ -31,4 +48,46 @@ public class StorageDataStatsImpl implements StorageDataStats {
     return monitoredResource;
   }
 
+  private void collectStats() {
+    // offheap
+    StorageData offheapStorageData = new StorageData();
+    if (isOffheapResource()) {
+      offheapStorageData.setMaxSize(monitoredResource.getTotal());
+      offheapStorageData.setReservedSize(monitoredResource.getReserved());
+      refreshUsedSizeIfNecessary();
+      offheapStorageData.setUsedSize(usedSize);
+    }
+
+    storageStats.put("offheap", offheapStorageData);
+  }
+
+  private void refreshUsedSizeIfNecessary() {
+    if (!refreshing.get() && NANOSECONDS.toMillis(System.nanoTime() - lastRefreshTime) > REFRESH_INTERVAL) {
+      if (refreshing.compareAndSet(false, true)) {
+        executorService.submit(new Runnable() {
+          @Override
+          public void run() {
+            try {
+              usedSize = monitoredResource.getUsed();
+              lastRefreshTime = System.nanoTime();
+            } finally {
+              refreshing.set(false);
+            }
+          }
+        });
+      }
+    }
+  }
+
+  private boolean isOffheapResource() {
+    return monitoredResource.getType() == MonitoredResource.Type.OFFHEAP;
+  }
+
+  @Override
+  public PrettyPrinter prettyPrint(PrettyPrinter out) {
+    out.flush();
+    out.println("Storage data Stats:");
+    // TODO: fill out these stats in readable format
+    return out;
+  }
 }
