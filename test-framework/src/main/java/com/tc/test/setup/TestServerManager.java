c507bd22776024154fe0af98b50dced0afed9a85
Vitaliy Funshteyn <vfunshte@terracottatech.com>
2013-12-13 09:02:12 +0000

    MNK-5893: catch InterruptedException on stop
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@24235 7fc7bbf3-cf45-46d4-be06-341739edd864

--- test-framework/src/main/java/com/tc/test/setup/TestServerManager.java
+++ test-framework/src/main/java/com/tc/test/setup/TestServerManager.java
@@ -73,7 +73,11 @@ public class TestServerManager {
     }
 
     for (int i = 0; i < grpCount; i++) {
-      threads[i].join();
+      try {
+        threads[i].join();
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
     }
 
   }
