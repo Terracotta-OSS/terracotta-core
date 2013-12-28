fe46d651abd868b6c39d251aa02450bf33a7ce49
Tim Wu <tim.wu@terracottatech.com>
2013-12-28 01:18:22 +0000

    Exempt this cas thing.
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@24296 7fc7bbf3-cf45-46d4-be06-341739edd864

--- common/src/test/java/com/tc/util/TCPropertiesConstsTest.java
+++ common/src/test/java/com/tc/util/TCPropertiesConstsTest.java
@@ -93,6 +93,8 @@ public class TCPropertiesConstsTest extends TCTestCase {
     exemptedProperties.add(TCPropertiesConsts.L2_SERVER_EVENT_BATCHER_INTERVAL_MS);
     exemptedProperties.add(TCPropertiesConsts.L2_SERVER_EVENT_BATCHER_QUEUE_SIZE);
     exemptedProperties.add(TCPropertiesConsts.L1_SERVER_EVENT_DELIVERY_THREADS);
+
+    exemptedProperties.add(TCPropertiesConsts.CAS_LOGGING_ENABLED);
   }
 
   private void loadDefaults(String propFile) {
