1bdd431fbd3eb3869908375d0fd49a100a1de754
Anthony Dahanne <anthony.dahanne@gmail.com>
2014-05-14 19:42:51 +0000

    TAB-4562 : adding management-tsa-common
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@24921 7fc7bbf3-cf45-46d4-be06-341739edd864

---
+++ management-agent/management-tsa-common/src/main/java/com/terracotta/management/ApplicationTsa.java
@@ -0,0 +1,25 @@
+/*
+ * All content copyright (c) 2003-2012 Terracotta, Inc., except as may otherwise be noted in a separate copyright
+ * notice. All rights reserved.
+ */
+package com.terracotta.management;
+
+import java.util.HashSet;
+import java.util.ServiceLoader;
+import java.util.Set;
+
+import javax.ws.rs.core.Application;
+
+public class ApplicationTsa extends Application {
+
+  @Override
+  public Set<Class<?>> getClasses() {
+    Set<Class<?>> s = new HashSet<Class<?>>();
+    ServiceLoader<ApplicationTsaService> loader = ServiceLoader.load(ApplicationTsaService.class);
+    for (ApplicationTsaService applicationTsaService : loader) {
+      s.addAll(applicationTsaService.getClasses());
+    }
+    return s;
+  }
+
+}
---
+++ management-agent/management-tsa-common/src/main/java/com/terracotta/management/ApplicationTsaService.java
@@ -0,0 +1,7 @@
+package com.terracotta.management;
+
+import java.util.Set;
+
+public interface ApplicationTsaService {
+  public Set<Class<?>> getClasses();
+}
