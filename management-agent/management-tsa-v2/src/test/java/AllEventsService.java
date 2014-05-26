f358e06e673b31287839a86af783b22ed6104569
Anthony Dahanne <anthony.dahanne@gmail.com>
2014-05-26 18:40:01 +0000

    TAB-4562 : testing the first SSE service
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@24957 7fc7bbf3-cf45-46d4-be06-341739edd864

---
+++ management-agent/management-tsa-v2/src/test/java/AllEventsService.java
@@ -0,0 +1,33 @@
+import org.glassfish.jersey.media.sse.EventInput;
+import org.glassfish.jersey.media.sse.InboundEvent;
+import org.glassfish.jersey.media.sse.SseFeature;
+import org.junit.Test;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
+/*
+ * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
+ */
+
+public class AllEventsService {
+
+  @Test
+  public void allEventsTest() {
+    Client client = ClientBuilder.newBuilder().register(SseFeature.class).build();
+    WebTarget target = client.target("http://localhost:9530/tc-management-api/v2/agents/sse/allEvents");
+
+    EventInput eventInput = target.request().get(EventInput.class);
+    while (!eventInput.isClosed()) {
+      final InboundEvent inboundEvent = eventInput.read();
+      if (inboundEvent == null) {
+        // connection has been closed
+        break;
+      }
+      System.out.println(inboundEvent.getName() + "; " + inboundEvent.readData(String.class));
+    }
+
+  }
+
+}
