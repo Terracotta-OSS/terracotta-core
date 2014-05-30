7680d6292a3bcbf0841009476ddd1485fad57f65
Anthony Dahanne <anthony.dahanne@gmail.com>
2014-05-30 20:01:43 +0000

    TAB-4562 TABQA-7400 : this test was not meant to be committed, it was just here to validater SSE locally
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@25005 7fc7bbf3-cf45-46d4-be06-341739edd864

--- management-agent/management-tsa-v2/src/test/java/com/terracotta/management/resource/services/AllEventsServiceTest.java
+++ management-agent/management-tsa-v2/src/test/java/com/terracotta/management/resource/services/AllEventsServiceTest.java
@@ -1,7 +1,9 @@
 package com.terracotta.management.resource.services;
+
 import org.glassfish.jersey.media.sse.EventInput;
 import org.glassfish.jersey.media.sse.InboundEvent;
 import org.glassfish.jersey.media.sse.SseFeature;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import javax.ws.rs.client.Client;
@@ -15,9 +17,10 @@ import javax.ws.rs.client.WebTarget;
 public class AllEventsServiceTest {
 
   @Test
+  @Ignore
   public void allEventsTest() {
     Client client = ClientBuilder.newBuilder().register(SseFeature.class).build();
-    WebTarget target = client.target("http://localhost:9530/tc-management-api/v2/agents/sse/allEvents");
+    WebTarget target = client.target("http://localhost:9530/tc-management-api/v2/agents/events/all");
 
     EventInput eventInput = target.request().get(EventInput.class);
     while (!eventInput.isClosed()) {
@@ -26,7 +29,25 @@ public class AllEventsServiceTest {
         // connection has been closed
         break;
       }
-      System.out.println(inboundEvent.getName() + "; " + inboundEvent.readData(String.class));
+      // CacheManagerEntityEventV2 cacheManagerEntityEventV2 = inboundEvent.readData(CacheManagerEntityEventV2.class);
+      //
+      //
+      // System.out.println(inboundEvent.getName() + " message was received from the L2 SSE Resource Service");
+      // System.out.println("Here are the details :");
+      // System.out.println("Type of the event : " + cacheManagerEntityEventV2.getType());
+      //
+      // System.out.println("CacheManager name : " + cacheManagerEntityEventV2.getCacheManagerName());
+      // if(cacheManagerEntityEventV2.getCacheEntities() != null) {
+      // System.out.println("Cache Attributes :");
+      // for (Map<String, Object> cacheEntities : cacheManagerEntityEventV2.getCacheEntities()) {
+      // for (Entry<String, Object> iterable_element : cacheEntities.entrySet()) {
+      // System.out.println(iterable_element.getKey() + "  " + iterable_element.getValue());
+      // }
+      //
+      // }
+      // }
+      // System.out.println("end of the event");
+
     }
 
   }
