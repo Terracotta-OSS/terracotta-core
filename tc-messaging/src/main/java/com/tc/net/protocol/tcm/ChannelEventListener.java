a044adf4cb4891928792a273e7c0dc0fa6adbc0c
Chris Dennis <chris.w.dennis@gmail.com>
2014-06-10 14:08:20 +0000

    TAB-4711 : delete the internal tc-messaging copy, and depend on the external one
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@25050 7fc7bbf3-cf45-46d4-be06-341739edd864

--- tc-messaging/src/main/java/com/tc/net/protocol/tcm/ChannelEventListener.java
+++
@@ -1,9 +0,0 @@
-/*
- * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
- */
-package com.tc.net.protocol.tcm;
-
-
-public interface ChannelEventListener {
-  public void notifyChannelEvent(ChannelEvent event);
-}
--- tc-messaging/src/main/java/com/tc/net/protocol/transport/SynMessage.java
+++
@@ -1,8 +0,0 @@
-/*
- * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
- */
-package com.tc.net.protocol.transport;
-
-public interface SynMessage extends TransportHandshakeMessage {
-  int getCallbackPort();
-}
