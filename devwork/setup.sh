1eb5135b208116964cab181204cd191a94bda9c9
Gary Keim <gkeim@terracottatech.com>
2014-06-17 18:41:25 +0000

    add management-common
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@25092 7fc7bbf3-cf45-46d4-be06-341739edd864

--- devwork/setup.sh
+++ devwork/setup.sh
@@ -5,3 +5,5 @@ svn co https://svn.terracotta.org/repo/ehcache/trunk/ehcache ehcache
 svn co https://svn.terracotta.org/repo/internal/ehcache-enterprise/ehcache-core-ee/trunk ehcache-ee
 svn co https://svn.terracotta.org/repo/quartz/trunk quartz
 svn co https://svn.terracotta.org/repo/forge/enterprise/quartz-ee/trunk quartz-ee
+svn co https://svn.terracotta.org/repo/forge/projects/management-core/trunk management-core
+svn co https://svn.terracotta.org/repo/forge/projects/management-common/trunk management-common
