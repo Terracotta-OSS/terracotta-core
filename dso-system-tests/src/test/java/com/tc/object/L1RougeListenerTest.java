0dc0cb33cb883d7cd8fd0f0e095a8f0c9f7892d5
Sreekanth Ramakrishnan <Sreekanth.Ramakrishnan@softwareag.com>
2014-11-04 07:10:49 +0000

    Fixing flaky DistributedObjectClientTest & L1RougeListenerTest
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@25816 7fc7bbf3-cf45-46d4-be06-341739edd864

--- dso-system-tests/src/test/java/com/tc/object/L1RougeListenerTest.java
+++ dso-system-tests/src/test/java/com/tc/object/L1RougeListenerTest.java
@@ -7,7 +7,6 @@ import com.tc.cluster.DsoClusterListener;
 import com.tc.config.schema.setup.L1ConfigurationSetupManager;
 import com.tc.config.schema.setup.L2ConfigurationSetupManager;
 import com.tc.config.schema.setup.TestConfigurationSetupManagerFactory;
-import com.tc.exception.TCRuntimeException;
 import com.tc.lang.StartupHelper;
 import com.tc.lang.TCThreadGroup;
 import com.tc.lang.ThrowableHandlerImpl;
@@ -15,6 +14,8 @@ import com.tc.logging.CallbackOnExitHandler;
 import com.tc.logging.CallbackOnExitState;
 import com.tc.logging.TCLogger;
 import com.tc.logging.TCLogging;
+import com.tc.net.GroupID;
+import com.tc.net.NodeID;
 import com.tc.net.protocol.tcm.TCMessageType;
 import com.tc.net.protocol.tcm.msgs.PingMessage;
 import com.tc.object.bytecode.MockClassProvider;
@@ -25,21 +26,28 @@ import com.tc.objectserver.managedobject.ManagedObjectStateFactory;
 import com.tc.platform.rejoin.RejoinManagerImpl;
 import com.tc.platform.rejoin.RejoinManagerInternal;
 import com.tc.properties.TCPropertiesConsts;
-import com.tc.server.TCServer;
-import com.tc.server.TCServerImpl;
+import com.tc.server.*;
 import com.tc.util.PortChooser;
 import com.tcclient.cluster.DsoClusterInternal;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.terracotta.test.util.WaitUtil;
 
 import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.concurrent.Callable;
+import java.util.concurrent.atomic.AtomicBoolean;
 
+import static com.tc.server.ServerEventType.EVICT;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
-public class DistributedObjectClientTest extends BaseDSOTestCase {
+public class L1RougeListenerTest extends BaseDSOTestCase {
 
+  private boolean originalReconnect;
   private PreparedComponentsFromL2Connection preparedComponentsFromL2Connection;
-  private boolean                            originalReconnect;
 
   @Override
   protected void setUp() throws Exception {
@@ -47,17 +55,19 @@ public class DistributedObjectClientTest extends BaseDSOTestCase {
 
     originalReconnect = tcProps.getBoolean(TCPropertiesConsts.L2_L1RECONNECT_ENABLED);
     tcProps.setProperty(TCPropertiesConsts.L2_L1RECONNECT_ENABLED, "true");
+    tcProps.setProperty(TCPropertiesConsts.L1_SERVER_EVENT_DELIVERY_TIMEOUT_INTERVAL, "1");
     System.setProperty("com.tc." + TCPropertiesConsts.L2_L1RECONNECT_ENABLED, "true");
   }
 
   protected final TCThreadGroup group = new TCThreadGroup(
-          new MockThrowableHandlerImpl(TCLogging
-                  .getLogger(DistributedObjectServer.class)));
+      new MockThrowableHandlerImpl(TCLogging
+          .getLogger(DistributedObjectServer.class)));
+
 
   protected class StartAction implements StartupHelper.StartupAction {
     private final int tsaPort;
     private final int jmxPort;
-    private TCServer  server = null;
+    private TCServer server = null;
 
     private StartAction(final int tsaPort, final int jmxPort) {
       this.tsaPort = tsaPort;
@@ -103,6 +113,7 @@ public class DistributedObjectClientTest extends BaseDSOTestCase {
   private static class MockThrowableHandlerImpl extends ThrowableHandlerImpl {
 
     private ArrayList<CallbackOnExitHandler> handlers = new ArrayList<CallbackOnExitHandler>();
+
     /**
      * Construct a new ThrowableHandler with a logger
      *
@@ -115,7 +126,7 @@ public class DistributedObjectClientTest extends BaseDSOTestCase {
     @Override
     public void handleThrowable(final Thread thread, final Throwable t) {
       //do nothing
-      for(CallbackOnExitHandler h : handlers) {
+      for (CallbackOnExitHandler h : handlers) {
         h.callbackOnExit(new CallbackOnExitState(t));
       }
     }
@@ -126,7 +137,7 @@ public class DistributedObjectClientTest extends BaseDSOTestCase {
     }
   }
 
-  public void testNodeErrorEventWhenUncaughtExceptionTriggered() throws Exception {
+  public void testRougeListener() throws Exception {
     final PortChooser pc = new PortChooser();
     final int tsaPort = pc.chooseRandomPort();
     final int jmxPort = pc.chooseRandomPort();
@@ -141,26 +152,53 @@ public class DistributedObjectClientTest extends BaseDSOTestCase {
     RejoinManagerInternal rejoinManagerInternal = new RejoinManagerImpl(false);
     TCThreadGroup threadGroup = new TCThreadGroup(new MockThrowableHandlerImpl(TCLogging.getLogger(DistributedObjectClient.class)));
     DsoClusterInternal dsoCluster = new DsoClusterImpl(rejoinManagerInternal);
-    DistributedObjectClient client = new DistributedObjectClient(new StandardDSOClientConfigHelperImpl(manager),
-            threadGroup,
-            new MockClassProvider(),
-            preparedComponentsFromL2Connection,
-            dsoCluster,
-            new NullAbortableOperationManager(),
-            rejoinManagerInternal);
+    final DistributedObjectClient client = new DistributedObjectClient(new StandardDSOClientConfigHelperImpl(manager),
+        threadGroup,
+        new MockClassProvider(),
+        preparedComponentsFromL2Connection,
+        dsoCluster,
+        new NullAbortableOperationManager(),
+        rejoinManagerInternal);
+
+    ServerEventDestination dest = mock(ServerEventDestination.class);
+    final AtomicBoolean mockCompleted = new AtomicBoolean(false);
+
+
+    Mockito.doAnswer(new Answer<Void>() {
+      @Override
+      public Void answer(InvocationOnMock invocationOnMock) throws Throwable {
+        Thread.sleep(1 * 10 * 1000);
+        mockCompleted.set(true);
+        return null;
+      }
+    }).when(dest).handleServerEvent(any(ServerEvent.class));
+    Mockito.doAnswer(new Answer<String>() {
+      @Override
+      public String answer(InvocationOnMock invocationOnMock) throws Throwable {
+        return "cache1";
+      }
+    }).when(dest).getDestinationName();
     client.start();
+    client.getServerEventListenerManager().registerListener(dest, EnumSet.of(ServerEventType.EVICT));
     dsoCluster.init(client.getClusterMetaDataManager(), client.getObjectManager(), client.getClusterEventsStage());
     DsoClusterListener mockDsoClusterListener = mock(DsoClusterListener.class);
     dsoCluster.addClusterListener(mockDsoClusterListener);
+    final NodeID remoteNode = new GroupID(1);
+    final ServerEvent event1 = new BasicServerEvent(EVICT, "key-1", "cache1");
 
     Thread workerThread = new Thread(threadGroup, new Runnable() {
       @Override
       public void run() {
-        throw new TCRuntimeException("uncaught exception");
+        client.getServerEventListenerManager().dispatch(event1, remoteNode);
       }
     });
     workerThread.start();
-    Thread.sleep(1000);
+    WaitUtil.waitUntilCallableReturnsTrue(new Callable<Boolean>() {
+      @Override
+      public Boolean call() throws Exception {
+        return mockCompleted.get();
+      }
+    });
     verify(mockDsoClusterListener).nodeError(any(DsoClusterEvent.class));
   }
 
@@ -171,3 +209,4 @@ public class DistributedObjectClientTest extends BaseDSOTestCase {
     System.setProperty("com.tc." + TCPropertiesConsts.L2_L1RECONNECT_ENABLED, "" + originalReconnect);
   }
 }
+
