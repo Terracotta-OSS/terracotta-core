4bcc7fd6dba105d1fe04999f09e2220fc0b93e39
Gary Keim <gkeim@terracottatech.com>
2014-12-02 00:17:26 +0000

    Added WAN
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@25864 7fc7bbf3-cf45-46d4-be06-341739edd864

--- devwork/setup.sh
+++ devwork/setup.sh
@@ -3,7 +3,9 @@ svn co https://svn.terracotta.org/repo/internal/search/trunk search
 svn co https://svn.terracotta.org/repo/forge/enterprise/management-apps/trunk management-apps
 svn co https://svn.terracotta.org/repo/ehcache/trunk/ehcache ehcache
 svn co https://svn.terracotta.org/repo/internal/ehcache-enterprise/ehcache-core-ee/trunk ehcache-ee
-svn co https://svn.terracotta.org/repo/quartz/trunk quartz
-svn co https://svn.terracotta.org/repo/forge/enterprise/quartz-ee/trunk quartz-ee
+# svn co https://svn.terracotta.org/repo/quartz/trunk quartz
+# svn co https://svn.terracotta.org/repo/forge/enterprise/quartz-ee/trunk quartz-ee
 svn co https://svn.terracotta.org/repo/forge/projects/management-core/trunk management-core
 svn co https://svn.terracotta.org/repo/forge/projects/management-common/trunk management-common
+svn co https://svn.terracotta.org/repo/forge/enterprise/terracotta-session/trunk terracotta-sessions
+svn co https://svn.terracotta.org/repo/internal/wan40/trunk wan40
