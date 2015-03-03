fb0632357a6a1d87dae3db34c52bbdd562e45217
Tim Wu <tim.wu.0@gmail.com>
2015-03-03 15:28:57 -0800

    Add methods to the invocation build for requesting acks from the server

--- entity-test-lib/src/main/java/org/terracotta/entity/PassthroughEndpoint.java
+++ entity-test-lib/src/main/java/org/terracotta/entity/PassthroughEndpoint.java
@@ -1,8 +1,11 @@
 package org.terracotta.entity;
 
 import com.google.common.util.concurrent.Futures;
+import com.tc.entity.Request;
 
 import java.io.Serializable;
+import java.util.EnumSet;
+import java.util.Set;
 import java.util.concurrent.Future;
 
 /**
@@ -38,6 +41,31 @@ public class PassthroughEndpoint implements EntityClientEndpoint {
   private class InvocationBuilderImpl implements InvocationBuilder {
     private boolean returnsValue = false;
     private byte[] payload = null;
+    private final Set<Request.Acks> acks = EnumSet.noneOf(Request.Acks.class);
+
+    @Override
+    public InvocationBuilder ackReceipt() {
+      acks.add(Request.Acks.RECEIPT);
+      return this;
+    }
+
+    @Override
+    public InvocationBuilder ackReplicated() {
+      acks.add(Request.Acks.REPLICATED);
+      return this;
+    }
+
+    @Override
+    public InvocationBuilder ackLogged() {
+      acks.add(Request.Acks.PERSIST_IN_SEQUENCER);
+      return this;
+    }
+
+    @Override
+    public InvocationBuilder ackCompleted() {
+      acks.add(Request.Acks.APPLIED);
+      return this;
+    }
 
     @Override
     public InvocationBuilder returnsValue(boolean returnsValue) {
