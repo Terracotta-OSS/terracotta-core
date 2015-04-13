b7627e7ba90a5c0ac1d0139f9f83f695cf61efe1
Tim Wu <tim.wu@terracottatech.com>
2015-04-13 23:03:07 +0000

    Unnecessary! Already handled by TAB-4927's fix
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@26240 7fc7bbf3-cf45-46d4-be06-341739edd864

--- dso-l2/src/main/java/com/tc/objectserver/event/ClientChannelMonitorImpl.java
+++ dso-l2/src/main/java/com/tc/objectserver/event/ClientChannelMonitorImpl.java
@@ -9,7 +9,6 @@ import com.google.common.collect.Multimaps;
 import com.google.common.collect.SetMultimap;
 import com.tc.async.api.ConfigurationContext;
 import com.tc.async.api.PostInit;
-import com.tc.async.api.Sink;
 import com.tc.l2.objectserver.ServerTransactionFactory;
 import com.tc.net.ClientID;
 import com.tc.net.NodeID;
@@ -20,29 +19,20 @@ import com.tc.object.dna.impl.ObjectStringSerializer;
 import com.tc.object.dna.impl.ObjectStringSerializerImpl;
 import com.tc.object.net.DSOChannelManager;
 import com.tc.object.net.DSOChannelManagerEventListener;
-import com.tc.object.tx.ServerTransactionID;
-import com.tc.objectserver.context.ServerTransactionCompleteContext;
 import com.tc.objectserver.core.api.ServerConfigurationContext;
 import com.tc.objectserver.impl.ServerTransactionBatchContext;
-import com.tc.objectserver.tx.AbstractServerTransactionListener;
 import com.tc.objectserver.tx.ServerTransaction;
 import com.tc.objectserver.tx.TransactionBatchContext;
 import com.tc.objectserver.tx.TransactionBatchManager;
 
 import java.util.Collection;
-import java.util.Collections;
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
 
-public class ClientChannelMonitorImpl extends AbstractServerTransactionListener implements ClientChannelMonitor, DSOChannelManagerEventListener, PostInit {
+public class ClientChannelMonitorImpl implements ClientChannelMonitor, DSOChannelManagerEventListener, PostInit {
   private final Multimap<ClientID, ObjectID> subscriberMap;
   private final DSOChannelManager            channelManager;
   private final ServerTransactionFactory     serverTransactionFactory;
   private GroupManager                       groupManager;
   private TransactionBatchManager            transactionBatchManager;
-  
-  private final Set<ServerTransactionID> channelRemovalTransactions = Collections.newSetFromMap(new ConcurrentHashMap<ServerTransactionID, Boolean>());
-  private Sink lwmSink;
 
   public ClientChannelMonitorImpl(final DSOChannelManager channelManager,
                                   final ServerTransactionFactory serverTransactionFactory) {
@@ -58,8 +48,6 @@ public class ClientChannelMonitorImpl extends AbstractServerTransactionListener 
     final ServerConfigurationContext scc = (ServerConfigurationContext) context;
     this.groupManager = scc.getL2Coordinator().getGroupManager();
     this.transactionBatchManager = scc.getTransactionBatchManager();
-    scc.getTransactionManager().addTransactionListener(this);
-    lwmSink = scc.getStage(ServerConfigurationContext.TRANSACTION_LOWWATERMARK_STAGE).getSink();
   }
 
   @Override
@@ -82,13 +70,6 @@ public class ClientChannelMonitorImpl extends AbstractServerTransactionListener 
     }
   }
 
-  @Override
-  public void transactionCompleted(ServerTransactionID stxID) {
-    if (channelRemovalTransactions.remove(stxID)) {
-      lwmSink.add(new ServerTransactionCompleteContext(stxID));
-    }
-  }
-
   private void informClientDisconnectionToSubscribers(final ClientID clientID, final Collection<ObjectID> subscribedOIDs) {
     NodeID localNodeID = groupManager.getLocalNodeID();
     ObjectStringSerializer serializer = new ObjectStringSerializerImpl();
@@ -97,8 +78,6 @@ public class ClientChannelMonitorImpl extends AbstractServerTransactionListener 
       ServerTransaction serverTransaction = serverTransactionFactory
           .createRemoveEventListeningClientTransaction(objectID, clientID, serializer);
 
-      channelRemovalTransactions.add(serverTransaction.getServerTransactionID());
-      
       TransactionBatchContext batchContext = new ServerTransactionBatchContext(localNodeID, serverTransaction, serializer);
       transactionBatchManager.processTransactions(batchContext);
     }
