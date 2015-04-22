a8f1cda7e14a9bfed8d0f9eea24ff5024abd397e
Boyu Sun <Boyu.Sun@softwareag.com>
2015-04-22 18:38:20 +0000

    Added offheap size 512m to L1RougeListenerTest.java
    git-svn-id: https://svn.terracotta.org/repo/tc/dso/trunk@26278 7fc7bbf3-cf45-46d4-be06-341739edd864

--- dso-system-tests/src/test/java/com/tc/object/L1RougeListenerTest.java
+++ dso-system-tests/src/test/java/com/tc/object/L1RougeListenerTest.java
@@ -111,6 +111,9 @@ public class L1RougeListenerTest extends BaseDSOTestCase {
       manager.dsoL2Config().tsaPort().setIntValue(tsaPort);
       manager.dsoL2Config().tsaPort().setBind("127.0.0.1");
 
+      manager.dsoL2Config().getDataStorage().setSize("512m");
+      manager.dsoL2Config().getOffheap().setSize("512m");
+
       manager.commonl2Config().jmxPort().setIntValue(jmxPort);
       manager.commonl2Config().jmxPort().setBind("127.0.0.1");
       server = new TCServerImpl(manager);
