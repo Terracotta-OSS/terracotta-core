c2b3c00457e89d1bad23fbaaa49dfb92cdc42bfd
Myron Scott <mys@softwareag.com>
2015-06-29 23:13:42 -0700

    changes to simplify service interface

--- entity-test-lib/src/main/java/org/terracotta/entity/PassthroughStripe.java
+++ entity-test-lib/src/main/java/org/terracotta/entity/PassthroughStripe.java
@@ -80,7 +80,7 @@ public class PassthroughStripe implements Service<ClientCommunicator>, ClientCom
   }
 
   @Override
-  public void initialize(ServiceConfiguration<?> configuration) {
+  public void initialize(ServiceConfiguration<? extends ClientCommunicator> configuration) {
     //do nothing
   }
 
@@ -97,8 +97,24 @@ public class PassthroughStripe implements Service<ClientCommunicator>, ClientCom
 
   private class FakeServiceRegistry implements ServiceRegistry {
     @Override
-    public <K, S extends Service<K>, SC extends ServiceConfiguration<S>> Optional<K> getService(SC configuration) {
-      return (Optional<K>) Optional.of(PassthroughStripe.this);
+    public Optional getService(ServiceConfiguration configuration) {
+      return Optional.of(new Service() {
+
+        @Override
+        public Object get() {
+          return PassthroughStripe.this;
+        }
+
+        @Override
+        public void initialize(ServiceConfiguration configuration) {
+
+        }
+
+        @Override
+        public void destroy() {
+
+        }  
+      });
     }
 
     @Override
