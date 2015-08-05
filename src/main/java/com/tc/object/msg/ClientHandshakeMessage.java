254847e519a39eac4ca625160a88804a3e882d55
Jeff Disher <jeff.disher@softwareag.com>
2015-08-05 23:21:28 +0000

    TAB-6183 :  Design transaction resend approach
    -removed some unused code surrounding ClientHandshake.  Much of this had to do with 4.x notions of communicating object IDs and some other ways that transactions manifested in that line.  These were unused and do not apply to 5.x.
    -DistributedObjectClient no longer extends SEDA, now just creating an instance of SEDA to get its StageManager (which seems to be the only reason it was being extended, in the current code)
    -some DistributedObjectClient uses of Stages, directly, has been replaced by the uses of the corresponding Sinks, as this is what was actually being used
    -Invalidations have been removed as they aren't used any more
    
    git-svn-id: https://svn.terracotta.org/repo/tc/tc-messaging/branches/private/voltron@26434 7fc7bbf3-cf45-46d4-be06-341739edd864

--- src/main/java/com/tc/object/msg/ClientHandshakeMessage.java
+++ src/main/java/com/tc/object/msg/ClientHandshakeMessage.java
@@ -5,25 +5,9 @@ package com.tc.object.msg;
 
 import com.tc.net.protocol.tcm.TCMessage;
 import com.tc.object.locks.ClientServerExchangeLockContext;
-import com.tc.object.tx.TransactionID;
-import com.tc.util.ObjectIDSet;
-import com.tc.util.SequenceID;
-
 import java.util.Collection;
-import java.util.List;
 
 public interface ClientHandshakeMessage extends TCMessage {
-
-  List<SequenceID> getTransactionSequenceIDs();
-
-  ObjectIDSet getObjectIDs();
-
-  void setObjectIDs(ObjectIDSet objectIDs);
-
-  ObjectIDSet getObjectIDsToValidate();
-
-  void setObjectIDsToValidate(ObjectIDSet objectIDsToValidate);
-
   void addLockContext(ClientServerExchangeLockContext ctxt);
 
   Collection<ClientServerExchangeLockContext> getLockContexts();
@@ -31,20 +15,6 @@ public interface ClientHandshakeMessage extends TCMessage {
   void setClientVersion(String v);
 
   String getClientVersion();
-
-  void addTransactionSequenceIDs(List<SequenceID> transactionSequenceIDs);
-
-  void addResentTransactionIDs(List<TransactionID> resentTransactionIDs);
-
-  List<TransactionID> getResentTransactionIDs();
-
-  void setIsObjectIDsRequested(boolean request);
-
-  boolean isObjectIDsRequested();
-
-  void setServerHighWaterMark(long serverHighWaterMark);
-
-  long getServerHighWaterMark();
 
   void setEnterpriseClient(boolean isEnterpirseClient);
 
--- src/main/java/com/tc/object/msg/ClientHandshakeMessageImpl.java
+++ src/main/java/com/tc/object/msg/ClientHandshakeMessageImpl.java
@@ -11,40 +11,21 @@ import com.tc.net.protocol.tcm.TCMessageHeader;
 import com.tc.net.protocol.tcm.TCMessageType;
 import com.tc.object.locks.ClientServerExchangeLockContext;
 import com.tc.object.session.SessionID;
-import com.tc.object.tx.TransactionID;
-import com.tc.util.BasicObjectIDSet;
-import com.tc.util.ObjectIDSet;
-import com.tc.util.SequenceID;
-
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
-public class ClientHandshakeMessageImpl extends DSOMessageBase implements ClientHandshakeMessage {
 
-  private static final byte   MANAGED_OBJECT_IDS       = 1;
+public class ClientHandshakeMessageImpl extends DSOMessageBase implements ClientHandshakeMessage {
   private static final byte   LOCK_CONTEXT             = 2;
-  private static final byte   TRANSACTION_SEQUENCE_IDS = 3;
-  private static final byte   RESENT_TRANSACTION_IDS   = 4;
-  private static final byte   REQUEST_OBJECT_IDS       = 5;
   private static final byte   CLIENT_VERSION           = 6;
-  private static final byte   SERVER_HIGH_WATER_MARK   = 7;
   private static final byte   ENTERPRISE_CLIENT        = 8;
-  private static final byte   OBJECTS_TO_VALIDATE      = 9;
   private static final byte   LOCAL_TIME_MILLS         = 10;
 
   private final Set<ClientServerExchangeLockContext> lockContexts             = new HashSet<ClientServerExchangeLockContext>();
-  private final List<SequenceID>    sequenceIDs        = new ArrayList<SequenceID>();
-  private final List<TransactionID> txnIDs             = new ArrayList<TransactionID>();
-  private ObjectIDSet         objectsToValidate        = new BasicObjectIDSet();
-  private ObjectIDSet         objectIDs                = new BasicObjectIDSet();
   private long                currentLocalTimeMills    = System.currentTimeMillis();
-  private boolean             requestObjectIDs;
   private boolean             enterpriseClient         = false;
-  private long                serverHighWaterMark      = 0;
   private String              clientVersion            = "UNKNOW";
 
   public ClientHandshakeMessageImpl(SessionID sessionID, MessageMonitor monitor, TCByteBufferOutputStream out,
@@ -63,58 +44,8 @@ public class ClientHandshakeMessageImpl extends DSOMessageBase implements Client
   }
 
   @Override
-  public ObjectIDSet getObjectIDs() {
-    return objectIDs;
-  }
-
-  @Override
-  public void setObjectIDs(ObjectIDSet objectIDs) {
-    this.objectIDs = objectIDs;
-  }
-
-  @Override
-  public ObjectIDSet getObjectIDsToValidate() {
-    return this.objectsToValidate;
-  }
-
-  @Override
-  public void setObjectIDsToValidate(ObjectIDSet objectIDsToValidate) {
-    this.objectsToValidate = objectIDsToValidate;
-  }
-
-  @Override
-  public List<SequenceID> getTransactionSequenceIDs() {
-    return this.sequenceIDs;
-  }
-
-  @Override
-  public List<TransactionID> getResentTransactionIDs() {
-    return this.txnIDs;
-  }
-
-  @Override
-  public boolean isObjectIDsRequested() {
-    return this.requestObjectIDs;
-  }
-
-  @Override
   public String getClientVersion() {
     return this.clientVersion;
-  }
-
-  @Override
-  public void addTransactionSequenceIDs(List<SequenceID> seqIDs) {
-    this.sequenceIDs.addAll(seqIDs);
-  }
-
-  @Override
-  public void addResentTransactionIDs(List<TransactionID> resentTransactionIDs) {
-    this.txnIDs.addAll(resentTransactionIDs);
-  }
-
-  @Override
-  public void setIsObjectIDsRequested(boolean request) {
-    this.requestObjectIDs = request;
   }
 
   @Override
@@ -125,16 +56,6 @@ public class ClientHandshakeMessageImpl extends DSOMessageBase implements Client
   @Override
   public void addLockContext(ClientServerExchangeLockContext ctxt) {
     this.lockContexts.add(ctxt);
-  }
-
-  @Override
-  public long getServerHighWaterMark() {
-    return this.serverHighWaterMark;
-  }
-
-  @Override
-  public void setServerHighWaterMark(long serverHWM) {
-    this.serverHighWaterMark = serverHWM;
   }
 
   @Override
@@ -154,53 +75,25 @@ public class ClientHandshakeMessageImpl extends DSOMessageBase implements Client
 
   @Override
   protected void dehydrateValues() {
-    putNVPair(MANAGED_OBJECT_IDS, objectIDs);
-    putNVPair(OBJECTS_TO_VALIDATE, objectsToValidate);
     for (final ClientServerExchangeLockContext lockContext : this.lockContexts) {
       putNVPair(LOCK_CONTEXT, lockContext);
     }
-    for (final SequenceID sequenceID : this.sequenceIDs) {
-      putNVPair(TRANSACTION_SEQUENCE_IDS, sequenceID);
-    }
-    for (final TransactionID txnID : this.txnIDs) {
-      putNVPair(RESENT_TRANSACTION_IDS, txnID);
-    }
-    putNVPair(REQUEST_OBJECT_IDS, this.requestObjectIDs);
     putNVPair(ENTERPRISE_CLIENT, this.enterpriseClient);
     putNVPair(CLIENT_VERSION, this.clientVersion);
-    putNVPair(SERVER_HIGH_WATER_MARK, this.serverHighWaterMark);
     putNVPair(LOCAL_TIME_MILLS, this.currentLocalTimeMills);
   }
 
   @Override
   protected boolean hydrateValue(byte name) throws IOException {
     switch (name) {
-      case MANAGED_OBJECT_IDS:
-        objectIDs = getObject(new BasicObjectIDSet());
-        return true;
-      case OBJECTS_TO_VALIDATE:
-        objectsToValidate = getObject(new BasicObjectIDSet());
-        return true;
       case LOCK_CONTEXT:
         this.lockContexts.add(getObject(new ClientServerExchangeLockContext()));
-        return true;
-      case TRANSACTION_SEQUENCE_IDS:
-        this.sequenceIDs.add(new SequenceID(getLongValue()));
-        return true;
-      case RESENT_TRANSACTION_IDS:
-        this.txnIDs.add(new TransactionID(getLongValue()));
-        return true;
-      case REQUEST_OBJECT_IDS:
-        this.requestObjectIDs = getBooleanValue();
         return true;
       case ENTERPRISE_CLIENT:
         this.enterpriseClient = getBooleanValue();
         return true;
       case CLIENT_VERSION:
         this.clientVersion = getStringValue();
-        return true;
-      case SERVER_HIGH_WATER_MARK:
-        this.serverHighWaterMark = getLongValue();
         return true;
       case LOCAL_TIME_MILLS:
         this.currentLocalTimeMills = getLongValue();
@@ -209,5 +102,4 @@ public class ClientHandshakeMessageImpl extends DSOMessageBase implements Client
         return false;
     }
   }
-
 }
