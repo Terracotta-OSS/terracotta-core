16930f0cba2a05e31e2bf5e6cd3e46a0017ec715
Jeff Disher <jeff.disher@softwareag.com>
2016-04-04 11:57:58 -0400

    Inlined the only method here to remove extraneous indirection from the call stack. -this was a remnant of a pre-migration refactoring

--- galvan-support/src/main/java/org/terracotta/testing/master/BasicHarnessEntry.java
+++ galvan-support/src/main/java/org/terracotta/testing/master/BasicHarnessEntry.java
@@ -29,16 +29,11 @@ public class BasicHarnessEntry extends AbstractHarnessEntry<BasicTestClusterConf
   // Run the one configuration.
   protected boolean runOneConfiguration(VerboseLogger logger, ContextualLogger fileHelperLogger, String kitOriginPath, String configTestDirectory, String clientClassPath, DebugOptions debugOptions, int clientsToCreate, String testClassName, boolean isRestartable, List<String> extraJarPaths, String namespaceFragment, String serviceFragment, BasicTestClusterConfiguration runConfiguration) throws IOException, FileNotFoundException, InterruptedException {
     int serversToCreate = runConfiguration.serversInStripe;
-    return runOneBasicConfiguration(logger, fileHelperLogger, kitOriginPath, configTestDirectory, clientClassPath, debugOptions, serversToCreate, clientsToCreate, testClassName, isRestartable, extraJarPaths, namespaceFragment, serviceFragment);
-  }
-
-  // A single-active stripe.
-  private boolean runOneBasicConfiguration(VerboseLogger logger, ContextualLogger fileHelperLogger, String serverInstallDirectory, String testParentDirectory, String clientClassPath, DebugOptions debugOptions, int serversPerStripe, int clientsToCreate, String testClassName, boolean isRestartable, List<String> extraJarPaths, String namespaceFragment, String serviceFragment) throws IOException, FileNotFoundException, InterruptedException {
-    Assert.assertTrue(serversPerStripe > 0);
+    Assert.assertTrue(serversToCreate > 0);
     
     // This is the simple case of a single-stripe so we don't need to wrap or decode anything.
     String stripeName = "stripe" + 0;
-    ReadyStripe oneStripe = CommonIdioms.setupConfigureAndStartStripe(logger, fileHelperLogger, serverInstallDirectory, testParentDirectory, serversPerStripe, SERVER_START_PORT, 0, isRestartable, extraJarPaths, namespaceFragment, serviceFragment, stripeName);
+    ReadyStripe oneStripe = CommonIdioms.setupConfigureAndStartStripe(logger, fileHelperLogger, kitOriginPath, configTestDirectory, serversToCreate, SERVER_START_PORT, 0, isRestartable, extraJarPaths, namespaceFragment, serviceFragment, stripeName);
     // We just want to unwrap this, directly.
     IMultiProcessControl processControl = oneStripe.stripeControl;
     String connectUri = oneStripe.stripeUri;
@@ -46,7 +41,7 @@ public class BasicHarnessEntry extends AbstractHarnessEntry<BasicTestClusterConf
     Assert.assertTrue(null != connectUri);
     
     // The cluster is now running so install and run the clients.
-    boolean wasCompleteSuccess = CommonIdioms.installAndRunClients(logger, testParentDirectory, clientClassPath, debugOptions, clientsToCreate, testClassName, processControl, connectUri);
+    boolean wasCompleteSuccess = CommonIdioms.installAndRunClients(logger, configTestDirectory, clientClassPath, debugOptions, clientsToCreate, testClassName, processControl, connectUri);
     // Stop all the servers.
     processControl.shutDown();
     return wasCompleteSuccess;
