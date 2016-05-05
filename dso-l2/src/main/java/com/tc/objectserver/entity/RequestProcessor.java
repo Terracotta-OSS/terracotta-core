34ff8165d388ef637a4a18532bf7e49fc79b9432
Jeff Disher <jeff.disher@softwareag.com>
2016-05-05 17:19:18 -0400

    Fix #88: Parallel invokes between active and passive
    -the active will block on the passive RECEIVED ack, before it starts, and then will block on the passive COMPLETED ack, before returning

--- dso-l2/src/main/java/com/tc/objectserver/entity/RequestProcessor.java
+++ dso-l2/src/main/java/com/tc/objectserver/entity/RequestProcessor.java
@@ -213,12 +213,13 @@ public class RequestProcessor implements StateChangeListener {
     
     void invoke()  {
       try {
-//  if this lockstep of waiting for passives before executing on actives is changed, make sure exclusive mode
-//  in ManagedEntityImpl is accounted for.  For exclusive mode, ManagedEntityImpl
-//  in passive mode relies on this control flow in the active so that no new messages are 
-//  received on the passive entity before exclusive mode is completed
-        this.replicationWaiter.waitForCompleted();
+        // NOTE:  We want to wait to hear that the passive has received the replicated invoke.
+        this.replicationWaiter.waitForReceived();
+        // We can now run the invoke.
         invoke.run();
+        // Now that we are done, wait for the passive to finish.
+        this.replicationWaiter.waitForCompleted();
+        // We are now completely finished.
         finish();
       } catch (InterruptedException interrupted) {
 //  shutdown logic?  uniterruptable?
