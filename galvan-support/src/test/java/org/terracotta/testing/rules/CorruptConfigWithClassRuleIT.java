833ce4c92faaa49fae1b64334e8734e18f82cc32
Jeff Disher <jeff.disher@softwareag.com>
2016-06-10 12:53:32 -0400

    Fix #32: Create a shepherding thread to act as the harness logic thread
    -this is required to avoid hangs in the entire harness when the client (where the testing thread is actually running) is blocked
    -added some tests to verify this behavior around servers which fail to start due to invaid configs

---
+++ galvan-support/src/test/java/org/terracotta/testing/rules/CorruptConfigWithClassRuleIT.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.terracotta.testing.rules;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Properties;
+
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.terracotta.connection.Connection;
+import org.terracotta.connection.ConnectionException;
+import org.terracotta.connection.ConnectionFactory;
+
+
+/**
+ * This test is effectly the same as BasicExternalClusterClassRuleIT except that it deliberately corrupts the server config
+ * so that the server will fail to start.  It then runs 2 tests on the same class rule to ensure that re-using this broken
+ * cluster instance will return failure in both cases.
+ */
+public class CorruptConfigWithClassRuleIT {
+  @ClassRule
+  public static final Cluster CLUSTER = new BasicExternalCluster(new File("target/cluster"), 1, Collections.<File>emptyList(), "", "", "BOGUS<ENTITY<FRAGMENT");
+
+  @Test(expected=ConnectionException.class)
+  public void testDirectConnection() throws IOException, ConnectionException {
+    Connection connection = CLUSTER.newConnection();
+    try {
+      //do nothing
+    } finally {
+      connection.close();
+    }
+  }
+
+  @Test(expected=ConnectionException.class)
+  public void testConnectionViaURI() throws IOException, ConnectionException {
+    Connection connection = ConnectionFactory.connect(CLUSTER.getConnectionURI(), new Properties());
+    try {
+      //do nothing
+    } finally {
+      connection.close();
+    }
+  }
+}
