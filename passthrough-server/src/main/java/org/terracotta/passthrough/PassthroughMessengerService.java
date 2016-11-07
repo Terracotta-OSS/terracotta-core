a5123e2c72edf243bd5299052fee475869f57792
Jeff Disher <jeff.disher@softwareag.com>
2016-11-07 12:34:19 -0500

    Support new IEntityMessenger timer interface
    -this uses the new PassthroughTimerThread
    -also update to terracotta-apis 1.0.11.beta

--- passthrough-server/src/main/java/org/terracotta/passthrough/PassthroughMessengerService.java
+++ passthrough-server/src/main/java/org/terracotta/passthrough/PassthroughMessengerService.java
@@ -47,19 +47,8 @@ public class PassthroughMessengerService implements IEntityMessenger {
   @Override
   public void messageSelf(EntityMessage message) throws MessageCodecException {
     // Serialize the message.
-    @SuppressWarnings("unchecked")
-    MessageCodec<EntityMessage, ?> codec = (MessageCodec<EntityMessage, ?>) this.entityContainer.codec;
-    byte[] serializedMessage = codec.encodeMessage(message);
-    // We use the invalid instance 0 since this is not a connected client.
-    long clientInstanceID = 0;
-    boolean shouldReplicateToPassives = true;
-    PassthroughMessage passthroughMessage = PassthroughMessageCodec.createInvokeMessage(this.entityClassName, this.entityName, clientInstanceID, serializedMessage, shouldReplicateToPassives);
-    boolean shouldWaitForSent = false;
-    boolean shouldWaitForReceived = false;
-    boolean shouldWaitForCompleted = false;
-    boolean shouldWaitForRetired = false;
-    boolean shouldBlockGetUntilRetire = false;
-    this.pseudoConnection.invokeActionAndWaitForAcks(passthroughMessage, shouldWaitForSent, shouldWaitForReceived, shouldWaitForCompleted, shouldWaitForRetired, shouldBlockGetUntilRetire);
+    PassthroughMessage passthroughMessage = makePassthroughMessage(message);
+    commonSendMessage(passthroughMessage);
   }
 
   @Override
@@ -73,5 +62,95 @@ public class PassthroughMessengerService implements IEntityMessenger {
     boolean shouldReplicateToPassives = true;
     PassthroughMessage passthroughMessage = PassthroughMessageCodec.createInvokeMessage(this.entityClassName, this.entityName, clientInstanceID, serializedMessage, shouldReplicateToPassives);
     this.passthroughServerProcess.sendMessageToActiveFromInsideActive(newMessageToSchedule, passthroughMessage);
+  }
+
+  @Override
+  public ScheduledToken messageSelfAfterDelay(EntityMessage message, long millisBeforeSend) throws MessageCodecException {
+    final PassthroughMessage passthroughMessage = makePassthroughMessage(message);
+    // We need to have the entity at this point.
+    Assert.assertTrue(null != this.entityContainer.entity);
+    long token = this.timerThread.scheduleAfterDelay(new Runnable(){
+      @Override
+      public void run() {
+        // If the entity disappeared, we are implicitly cancelled.
+        if (null != PassthroughMessengerService.this.entityContainer.entity) {
+          commonSendMessage(passthroughMessage);
+        } else {
+          System.err.println("WARNING:  Cancelled SINGLE delayed message to destroyed entity");
+        }
+      }}, millisBeforeSend);
+    return new TokenWrapper(token);
+  }
+
+  @Override
+  public ScheduledToken messageSelfPeriodically(EntityMessage message, long millisBetweenSends) throws MessageCodecException {
+    final PassthroughMessage passthroughMessage = makePassthroughMessage(message);
+    // We need to have the entity at this point.
+    Assert.assertTrue(null != this.entityContainer.entity);
+    SelfCancellingRunnable runnable = new SelfCancellingRunnable(passthroughMessage);
+    long token = this.timerThread.schedulePeriodically(runnable, millisBetweenSends);
+    runnable.setToken(token);
+    return new TokenWrapper(token);
+  }
+
+  @Override
+  public void cancelTimedMessage(ScheduledToken token) {
+    // If this is the wrong type, the ClassCastException is a reasonable error since it means we got something invalid.
+    this.timerThread.cancelMessage(((TokenWrapper)token).token);
+  }
+
+
+  private PassthroughMessage makePassthroughMessage(EntityMessage message) throws MessageCodecException {
+    @SuppressWarnings("unchecked")
+    MessageCodec<EntityMessage, ?> codec = (MessageCodec<EntityMessage, ?>) this.entityContainer.codec;
+    byte[] serializedMessage = codec.encodeMessage(message);
+    // We use the invalid instance 0 since this is not a connected client.
+    long clientInstanceID = 0;
+    boolean shouldReplicateToPassives = true;
+    PassthroughMessage passthroughMessage = PassthroughMessageCodec.createInvokeMessage(this.entityClassName, this.entityName, clientInstanceID, serializedMessage, shouldReplicateToPassives);
+    return passthroughMessage;
+  }
+
+  private void commonSendMessage(PassthroughMessage passthroughMessage) {
+    boolean shouldWaitForSent = false;
+    boolean shouldWaitForReceived = false;
+    boolean shouldWaitForCompleted = false;
+    boolean shouldWaitForRetired = false;
+    boolean shouldBlockGetUntilRetire = false;
+    this.pseudoConnection.invokeActionAndWaitForAcks(passthroughMessage, shouldWaitForSent, shouldWaitForReceived, shouldWaitForCompleted, shouldWaitForRetired, shouldBlockGetUntilRetire);
+  }
+
+
+  private class SelfCancellingRunnable implements Runnable {
+    private final PassthroughMessage message;
+    private Long token;
+    
+    public SelfCancellingRunnable(PassthroughMessage message) {
+      this.message = message;
+    }
+    
+    public void setToken(Long token) {
+      this.token = token;
+    }
+    
+    @Override
+    public void run() {
+      Assert.assertTrue(this.token > 0);
+      // If the entity disappeared, we are implicitly cancelled.
+      if (null != PassthroughMessengerService.this.entityContainer.entity) {
+        commonSendMessage(this.message);
+      } else {
+        PassthroughMessengerService.this.timerThread.cancelMessage(this.token);
+        System.err.println("WARNING:  Cancelled PERIODIC delayed message to destroyed entity");
+      }
+    }
+  }
+
+
+  private static class TokenWrapper implements ScheduledToken {
+    public final long token;
+    public TokenWrapper(long token) {
+      this.token = token;
+    }
   }
 }
