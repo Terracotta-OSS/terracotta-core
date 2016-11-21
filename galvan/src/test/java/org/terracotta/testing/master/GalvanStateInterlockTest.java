0421278f22f4803027e90c2eb8423198469aecd9
Jeff Disher <jeff.disher@softwareag.com>
2016-11-21 18:52:26 -0500

    Fix #106: Handle zap of passives or actives
    -while this is less common than the zap of a server in an unknown state, this can happen when a network partition is healed

--- galvan/src/test/java/org/terracotta/testing/master/GalvanStateInterlockTest.java
+++ galvan/src/test/java/org/terracotta/testing/master/GalvanStateInterlockTest.java
@@ -53,8 +53,109 @@ public class GalvanStateInterlockTest {
       }
     };
     testThread.start();
-    this.interlock.waitForAllServerReady();
+    this.interlock.waitForAllServerRunning();
     this.interlock.waitForActiveServer();
+    testThread.join();
+  }
+
+  @Test
+  public void testZapUnknown() throws Exception {
+    Thread testThread = new Thread() {
+      @Override
+      public void run() {
+        ServerProcess active = mock(ServerProcess.class);
+        GalvanStateInterlockTest.this.interlock.registerNewServer(active);
+        ServerProcess passive = mock(ServerProcess.class);
+        GalvanStateInterlockTest.this.interlock.registerNewServer(passive);
+        // Have them both come online.
+        GalvanStateInterlockTest.this.interlock.serverDidStartup(active);
+        GalvanStateInterlockTest.this.interlock.serverDidStartup(passive);
+        // Bring the active online.
+        GalvanStateInterlockTest.this.interlock.serverBecameActive(active);
+        // Zap the unknown server.
+        GalvanStateInterlockTest.this.interlock.serverWasZapped(passive);
+        // Start up the zapped server and have it come up as a passive.
+        GalvanStateInterlockTest.this.interlock.serverDidStartup(passive);
+        GalvanStateInterlockTest.this.interlock.serverBecamePassive(passive);
+      }
+    };
+    testThread.start();
+    this.interlock.waitForAllServerRunning();
+    this.interlock.waitForAllServerReady();
+    testThread.join();
+  }
+
+  @Test
+  public void testZapPassive() throws Exception {
+    Thread testThread = new Thread() {
+      @Override
+      public void run() {
+        ServerProcess active = mock(ServerProcess.class);
+        GalvanStateInterlockTest.this.interlock.registerNewServer(active);
+        ServerProcess passive = mock(ServerProcess.class);
+        GalvanStateInterlockTest.this.interlock.registerNewServer(passive);
+        // (we also want to create a slow passive to extend the test until this is done).
+        ServerProcess slow = mock(ServerProcess.class);
+        GalvanStateInterlockTest.this.interlock.registerNewServer(slow);
+        
+        // Have them all come online.
+        GalvanStateInterlockTest.this.interlock.serverDidStartup(active);
+        GalvanStateInterlockTest.this.interlock.serverDidStartup(passive);
+        GalvanStateInterlockTest.this.interlock.serverDidStartup(slow);
+        // Bring the active online.
+        GalvanStateInterlockTest.this.interlock.serverBecameActive(active);
+        // Bring the passive online.
+        GalvanStateInterlockTest.this.interlock.serverBecamePassive(passive);
+        // Zap the passive.
+        GalvanStateInterlockTest.this.interlock.serverWasZapped(passive);
+        // Start up the zapped server and have it come up as a passive.
+        GalvanStateInterlockTest.this.interlock.serverDidStartup(passive);
+        GalvanStateInterlockTest.this.interlock.serverBecamePassive(passive);
+        // Now, have the slow server become passive.
+        GalvanStateInterlockTest.this.interlock.serverBecamePassive(slow);
+      }
+    };
+    testThread.start();
+    this.interlock.waitForAllServerRunning();
+    this.interlock.waitForAllServerReady();
+    testThread.join();
+  }
+
+  @Test
+  public void testZapActive() throws Exception {
+    Thread testThread = new Thread() {
+      @Override
+      public void run() {
+        ServerProcess active = mock(ServerProcess.class);
+        GalvanStateInterlockTest.this.interlock.registerNewServer(active);
+        ServerProcess passive = mock(ServerProcess.class);
+        GalvanStateInterlockTest.this.interlock.registerNewServer(passive);
+        // (we also want to create a slow passive to extend the test until this is done).
+        ServerProcess slow = mock(ServerProcess.class);
+        GalvanStateInterlockTest.this.interlock.registerNewServer(slow);
+        
+        // Have them all come online.
+        GalvanStateInterlockTest.this.interlock.serverDidStartup(active);
+        GalvanStateInterlockTest.this.interlock.serverDidStartup(passive);
+        GalvanStateInterlockTest.this.interlock.serverDidStartup(slow);
+        // Bring the active online.
+        GalvanStateInterlockTest.this.interlock.serverBecameActive(active);
+        // Bring the passive online.
+        GalvanStateInterlockTest.this.interlock.serverBecamePassive(passive);
+        // Zap the active.
+        GalvanStateInterlockTest.this.interlock.serverWasZapped(active);
+        // Promote the passive.
+        GalvanStateInterlockTest.this.interlock.serverBecameActive(passive);
+        // Start up the zapped server and have it come up as a passive.
+        GalvanStateInterlockTest.this.interlock.serverDidStartup(active);
+        GalvanStateInterlockTest.this.interlock.serverBecamePassive(active);
+        // Now, have the slow server become passive.
+        GalvanStateInterlockTest.this.interlock.serverBecamePassive(slow);
+      }
+    };
+    testThread.start();
+    this.interlock.waitForAllServerRunning();
+    this.interlock.waitForAllServerReady();
     testThread.join();
   }
 
