6cc577f1ab923711724480211db50ae0e40529f8
Jeff Disher <jeff.disher@softwareag.com>
2017-02-13 17:21:37 -0500

    Wrap RuntimeEntityExceptions in VoltronWrapperException
    -this allows us to plumb these through the server, and over the wire, without creating a parallel path through the code for the different type of exception
    -the client-side decoding just needs to be aware of this
    -updated to terracotta-apis 1.2.0-pre15
    -updated to terracotta-configuration 10.2.0-pre15

--- dso-l1/src/main/java/com/tc/object/ExceptionUtils.java
+++ dso-l1/src/main/java/com/tc/object/ExceptionUtils.java
@@ -1,7 +1,9 @@
 package com.tc.object;
 
 import com.tc.exception.EntityBusyException;
-import com.tc.exception.EntityReferencedException;
+import com.tc.exception.VoltronWrapperException;
+import com.tc.util.Assert;
+
 import org.terracotta.exception.EntityAlreadyExistsException;
 import org.terracotta.exception.EntityConfigurationException;
 import org.terracotta.exception.EntityException;
@@ -10,20 +12,22 @@ import org.terracotta.exception.EntityNotProvidedException;
 import org.terracotta.exception.EntityUserException;
 import org.terracotta.exception.EntityVersionMismatchException;
 import org.terracotta.exception.PermanentEntityException;
+import org.terracotta.exception.RuntimeEntityException;
 
 import java.util.Arrays;
-/**
- * @author vmad
- */
+
+
 public class ExceptionUtils {
-  public static EntityException addLocalStackTraceToEntityException(EntityException e) {
+  public static EntityException addLocalStackTraceToEntityException(EntityException e) throws RuntimeEntityException {
     EntityException wrappedException;
     if(e instanceof EntityUserException) {
       wrappedException = new EntityUserException(e.getClassName(), e.getEntityName(), e);
     } else if(e instanceof EntityNotFoundException) {
       wrappedException = new EntityNotFoundException(e.getClassName(), e.getEntityName(), e);
-    } else if(e instanceof PermanentEntityException) {
-      wrappedException = new PermanentEntityException(e.getClassName(), e.getEntityName(), e);
+    } else if(e instanceof VoltronWrapperException) {
+      throwRuntimeExceptionWithLocalStack(((VoltronWrapperException)e).getWrappedException());
+      // We won't reach this point.
+      wrappedException = null;
     } else if(e instanceof EntityNotProvidedException) {
       wrappedException = new EntityNotProvidedException(e.getClassName(), e.getEntityName(), e);
     } else if(e instanceof EntityVersionMismatchException) {
@@ -44,5 +48,14 @@ public class ExceptionUtils {
     //strip last two recent elements - getStackTrace() and this method
     wrappedException.setStackTrace(Arrays.copyOfRange(stackTrace, 2, stackTrace.length));
     return wrappedException;
+  }
+
+  private static void throwRuntimeExceptionWithLocalStack(RuntimeEntityException wrappedException) throws RuntimeEntityException {
+    if (wrappedException instanceof PermanentEntityException) {
+      throw new PermanentEntityException(wrappedException.getClassName(), wrappedException.getEntityName(), wrappedException);
+    } else {
+      // Unknown exception - this must be populated.
+      Assert.fail("Unhandled runtime exception type");
+    }
   }
 }
