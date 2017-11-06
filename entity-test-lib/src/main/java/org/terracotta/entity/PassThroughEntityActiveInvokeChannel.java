c30cdf395135ed6d591eb527ec19ccb2a83dd0aa
Myron Scott <myron.scott@softwareag.com>
2017-11-06 12:22:24 -0800

    only send on open channels

--- entity-test-lib/src/main/java/org/terracotta/entity/PassThroughEntityActiveInvokeChannel.java
+++ entity-test-lib/src/main/java/org/terracotta/entity/PassThroughEntityActiveInvokeChannel.java
@@ -6,18 +6,21 @@ import org.terracotta.exception.EntityServerException;
 public class PassThroughEntityActiveInvokeChannel<R extends EntityResponse> implements ActiveInvokeChannel<R> {
   
   private final InvokeMonitor<R> monitor;
+  private volatile boolean closed = false;
 
   public PassThroughEntityActiveInvokeChannel(InvokeMonitor<R> monitor) {
     this.monitor = monitor;
   }
 
   @Override
-  public void sendResponse(R r) {
+  public synchronized void sendResponse(R r) {
+    checkClosed();
     monitor.accept(r);
   }
 
   @Override
-  public void sendException(Exception ee) {
+  public synchronized void sendException(Exception ee) {
+    checkClosed();
     if (ee instanceof EntityException) {
       monitor.exception((EntityException)ee);
     } else {
@@ -26,7 +29,23 @@ public class PassThroughEntityActiveInvokeChannel<R extends EntityResponse> impl
   }
 
   @Override
-  public void close() {
-    monitor.close();
+  public synchronized void close() {
+    if (attemptClose()) {
+      monitor.close();
+    }
+  }
+  // serialized gating should be enough due to thread interaction at upper layers
+  private synchronized boolean attemptClose() {
+    try {
+      return !closed;
+    } finally {
+      closed = true;
+    }
+  }
+  // serialized gating should be enough due to thread interaction at upper layers
+  private synchronized void checkClosed() {
+    if (closed) {
+      throw new IllegalStateException("channel closed");
+    }
   }
 }
