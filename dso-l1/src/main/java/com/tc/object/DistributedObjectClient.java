ef8fd7bfd4e67e8480dd6975f0f0c64ef8ab659c
Myron Scott <myron.scott@softwareag.com>
2019-02-07 12:52:26 -0800

    make sure all bytes of a message are pushed to the network

--- dso-l1/src/main/java/com/tc/object/DistributedObjectClient.java
+++ dso-l1/src/main/java/com/tc/object/DistributedObjectClient.java
@@ -666,13 +666,17 @@ public class DistributedObjectClient implements TCClient {
 
     @Override
     public void run() {
+      int tryCount = 0;
       while (threadGroup.activeCount() > 0) {
         for (Thread liveThread : getLiveThreads(threadGroup)) {
-          liveThread.dumpStack();
+          Exception e = new Exception("thread is stuck " + liveThread.getName());
+          e.setStackTrace(liveThread.getStackTrace());
+          DSO_LOGGER.warn("stray connection threads not stopping", e);
           liveThread.interrupt();
         }
         try {
           Thread.sleep(1000);
+          if (tryCount++ > 30) break;
         } catch (final InterruptedException e) {
           // ignore
         }
