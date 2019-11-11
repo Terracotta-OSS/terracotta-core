0cc5d959911eb2b556929de2f94b3ede90a05b0a
Myron Scott <myron.scott@softwareag.com>
2019-11-10 19:19:53 -0800

    add new appender

---
+++ appenders/src/main/java/org/terracotta/tripwire/JFRAppender.java
@@ -0,0 +1,120 @@
+/*
+ *
+ *  The contents of this file are subject to the Terracotta Public License Version
+ *  2.0 (the "License"); You may not use this file except in compliance with the
+ *  License. You may obtain a copy of the License at
+ *
+ *  http://terracotta.org/legal/terracotta-public-license.
+ *
+ *  Software distributed under the License is distributed on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ *  the specific language governing rights and limitations under the License.
+ *
+ *  The Covered Software is Terracotta Core.
+ *
+ *  The Initial Developer of the Covered Software is
+ *  Terracotta, Inc., a Software AG company
+ *
+ */
+package org.terracotta.tripwire;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.AppenderBase;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.ParseException;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.regex.Pattern;
+import jdk.jfr.Configuration;
+import jdk.jfr.Recording;
+
+public class JFRAppender extends AppenderBase<ILoggingEvent> {
+  
+  private final Recording continuous;
+  private String path;
+  private int maxAgeMinutes = 5;
+  private Pattern dumpRegex;
+
+  public JFRAppender() {
+    try {
+      continuous = new Recording(Configuration.getConfiguration("default"));
+      continuous.setToDisk(true);
+    } catch (IOException | ParseException boot) {
+      throw new RuntimeException(boot);
+    }
+  }
+
+  @Override
+  public void stop() {
+    continuous.stop();
+    super.stop();
+  }
+
+  @Override
+  public void start() {
+    if (maxAgeMinutes > 0) {
+      continuous.setMaxAge(Duration.ofMinutes(maxAgeMinutes));
+    } else {
+      continuous.setMaxAge(null);
+    }
+    continuous.start();
+    super.start();
+  }
+
+  public String getPath() {
+    return path;
+  }
+
+  public void setPath(String path) {
+    this.path = path;
+  }
+
+  public int getMaxAgeMinutes() {
+    return maxAgeMinutes;
+  }
+
+  public void setMaxAgeMinutes(int maxAgeMinutes) {
+    if (maxAgeMinutes > 0) {
+      this.maxAgeMinutes = maxAgeMinutes;
+    } else {
+      this.maxAgeMinutes = -1;
+    }
+  }
+  
+  public void setDumpRegex(String regex) {
+    if (regex != null) {
+      dumpRegex = Pattern.compile(regex);
+    } else {
+      dumpRegex = null;
+    }
+  }
+  
+  public String getDumpRegex() {
+    return dumpRegex != null ? dumpRegex.pattern() : null;
+  }
+
+  @Override
+  protected void append(ILoggingEvent eventObject) {
+    if (dumpRegex != null) {
+      if (!dumpRegex.matcher(eventObject.getFormattedMessage()).matches()) {
+        return;
+      }
+    }
+    String timestamp = DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(LocalDateTime.now());
+    if (path == null) {
+      path = System.getProperty("user.dir");
+    }
+    Path dest = Paths.get(path);
+    if (!dest.toFile().exists()) {
+      dest.toFile().mkdirs();
+    }
+    try {
+      continuous.dump(dest.resolve(timestamp + ".jfr"));
+    } catch (IOException ioe) {
+      throw new RuntimeException(ioe);
+    }
+  }
+}
