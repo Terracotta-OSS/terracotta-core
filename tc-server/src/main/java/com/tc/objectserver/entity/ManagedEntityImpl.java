6ac28dd9b3e9c78fcc4fc59f1b9a1d865d8f3295
Myron Scott <myron.scott@softwareag.com>
2021-09-23 12:29:41 -0700

    cleanup temporary entity flag

--- tc-server/src/main/java/com/tc/objectserver/entity/ManagedEntityImpl.java
+++ tc-server/src/main/java/com/tc/objectserver/entity/ManagedEntityImpl.java
@@ -117,7 +117,7 @@ public class ManagedEntityImpl implements ManagedEntity {
   //  for destroy, passives need to reference count to understand if entity is deletable
   private int clientReferenceCount = 0;
   private final boolean canDelete;
-  private boolean isTemp;
+  private final boolean isTemp;
   private volatile boolean isDestroyed;
   // We only track a single CreateListener since it is intended to be the EntityMessengerService attached to this.
   private final List<LifecycleListener> createListener = new CopyOnWriteArrayList<>();
@@ -158,6 +158,7 @@ public class ManagedEntityImpl implements ManagedEntity {
     this.clientEntityStateManager = clientEntityStateManager;
     this.eventCollector = eventCollector;
     this.factory = factory;
+    this.isTemp = this.factory.getClass().isAnnotationPresent(TemporaryEntity.class);
     this.executor = process;
     // Create the RetirementManager here, since it is currently scoped per-entity.
     this.retirementManager = new RetirementManager();
@@ -805,9 +806,6 @@ public class ManagedEntityImpl implements ManagedEntity {
 
     notifyEntityCreated();
     this.isDestroyed = false;
-    if (this.factory.getClass().isAnnotationPresent(TemporaryEntity.class)) {
-      this.isTemp = true;
-    }
     eventCollector.entityWasCreated(id, this.consumerID, isInActiveState);
     response.complete();
   }
