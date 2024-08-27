2644038a46c38d02290921a36c21372f925623dd
Myron Scott <myron.scott@softwareag.com>
2024-08-27 12:32:30 -0700

    address comments

--- tc-server/src/test/java/com/tc/classloader/ServiceLocatorTest.java
+++ tc-server/src/test/java/com/tc/classloader/ServiceLocatorTest.java
@@ -15,10 +15,7 @@
  *  limitations under the License.
  *
  */
-/*
- * All content copyright (c) 2014 Terracotta, Inc., except as may otherwise
- * be noted in a separate copyright notice. All rights reserved.
- */
+
 package com.tc.classloader;
 
 import static com.tc.server.Directories.TC_INSTALL_ROOT_PROPERTY_NAME;
--- tc-server/src/test/java/com/tc/net/protocol/transport/ServerStackProviderTest.java
+++ tc-server/src/test/java/com/tc/net/protocol/transport/ServerStackProviderTest.java
@@ -15,9 +15,6 @@
  *  limitations under the License.
  *
  */
-/*
- * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
- */
 package com.tc.net.protocol.transport;
 
 import com.tc.net.StripeID;
