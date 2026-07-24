c0dd6364dd71c2cb740c9c4a6176bfcb0d9feb20
Myron Scott <myron.scott@ibm.com>
2026-07-24 13:14:12 -0700

    add testVM compliance from @chrisd

--- build-logic/src/main/java/org/terracotta/conventions/JavaVersionConvention.java
+++ build-logic/src/main/java/org/terracotta/conventions/JavaVersionConvention.java
@@ -4,6 +4,10 @@ import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.plugins.JavaPlugin;
 import org.gradle.api.plugins.JavaPluginExtension;
+import org.gradle.api.plugins.JvmTestSuitePlugin;
+import org.gradle.api.plugins.jvm.JvmTestSuite;
+import org.gradle.testing.base.TestingExtension;
+import org.gradle.jvm.toolchain.JavaToolchainService;
 import org.terracotta.build.plugins.JavaVersionPlugin;
 
 public class JavaVersionConvention implements Plugin<Project> {
@@ -18,5 +22,20 @@ public class JavaVersionConvention implements Plugin<Project> {
         project.getExtensions().configure(JavaPluginExtension.class, java ->
             java.toolchain(toolchain ->
                 toolchain.getLanguageVersion().convention(javaVersions.getCompileVersion()))));
+
+    project.getPlugins().withType(JvmTestSuitePlugin.class).configureEach(plugin -> {
+        project.getExtensions().configure(TestingExtension.class, testing -> {
+            testing.getSuites().withType(JvmTestSuite.class).configureEach(testSuite -> {
+                testSuite.getTargets().configureEach(target -> {
+                    target.getTestTask().configure(test -> {
+                        test.getJavaLauncher().set(project.getExtensions().getByType(JavaToolchainService.class)
+                                .launcherFor(spec ->
+                                        spec.getLanguageVersion().convention(javaVersions.getTestVersion())));
+                        test.environment("JAVA_HOME", test.getJavaLauncher().get().getMetadata().getInstallationPath());
+                    });
+                });
+            });
+        });
+    });
   }
 }
